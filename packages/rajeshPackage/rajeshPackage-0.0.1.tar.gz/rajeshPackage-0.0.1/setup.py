4ad079e38357e6a69538016d797e3c366ce9d872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:26 +0000

    Add package rajeshPackage/0.0.1/rajeshPackage-0.0.1.tar.gz

---
+++ packages/rajeshPackage/rajeshPackage-0.0.1.tar.gz/rajeshPackage-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='rajeshPackage',
+    version="0.0.1",
+    author="Rajesh Nagireddy",
+    author_email='rajeshnagireddy90@gmail.com',
+    description="Package for simple calculations",
+    packages=setuptools.find_packages(),
+
+)
