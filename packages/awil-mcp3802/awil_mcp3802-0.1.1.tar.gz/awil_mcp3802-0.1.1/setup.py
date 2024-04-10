5f21cc038ee13f587088b3c2a215d34a07fc02fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 16:05:24 +0000

    Add package awil-mcp3802/0.1.1/awil_mcp3802-0.1.1.tar.gz

---
+++ packages/awil-mcp3802/awil_mcp3802-0.1.1.tar.gz/awil_mcp3802-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awil_mcp3802',
+    author='Noriki Mochizuki',
+    author_email='noriki.mochizuki.mj@gmail.com',
+    license='MIT',
+    version='0.1.1',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'awil_ft232h' 
+    ]
+)
