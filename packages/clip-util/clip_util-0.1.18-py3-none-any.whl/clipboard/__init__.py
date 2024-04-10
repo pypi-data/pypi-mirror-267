6b1417e68a34fc917cc9885467b70c1790692ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:03:02 +0000

    Add package clip-util/0.1.18/clip_util-0.1.18-py3-none-any.whl

---
+++ packages/clip-util/clip_util-0.1.18-py3-none-any.whl/clipboard/__init__.py
@@ -0,0 +1,16 @@
+from clipboard.clipboard import Clipboard
+from clipboard.clipboard import ClipboardFormat
+from clipboard.clipboard import get_clipboard
+from clipboard.clipboard import set_clipboard
+from clipboard.html_clipboard import HTML_ENCODING
+from clipboard.html_clipboard import HTMLClipboard
+
+
+__all__ = [
+    "Clipboard",
+    "ClipboardFormat",
+    "HTML_ENCODING",
+    "HTMLClipboard",
+    "get_clipboard",
+    "set_clipboard",
+]
