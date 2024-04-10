967e345c86a45e4884d6b87ad736bc191558e3d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:22 +0000

    Add package endec/0.1.0b2/endec-0.1.0b2.tar.gz

---
+++ packages/endec/endec-0.1.0b2.tar.gz/endec-0.1.0b2/tests/test_encode.py
@@ -0,0 +1,38 @@
+import pytest
+
+import endec
+from endec.exceptions import EncodeError
+
+
+def test_encode_utf8():
+    assert endec.encode("utf8_string") == b"utf8_string"
+    assert endec.encode("utf8_string", "utf-8") == b"utf8_string"
+
+
+def test_encode_errors_strict():
+    with pytest.raises(EncodeError):
+        endec.encode("こんにちは", "ascii", "strict")
+
+    assert (
+        endec.encode("こんにちは", "utf-8", "strict")
+        == b"\xe3\x81\x93\xe3\x82\x93\xe3\x81\xab\xe3\x81\xa1\xe3\x81\xaf"
+    )
+
+
+def test_encode_errors_xmlcharrefreplace():
+    assert (
+        endec.encode("こんにちは", "ascii", "xmlcharrefreplace")
+        == b"&#12371;&#12435;&#12395;&#12385;&#12399;"
+    )
+
+
+def test_encode_errors_unknown():
+    with pytest.raises(LookupError):
+        endec.encode(
+            "これはasciiではありません", "ascii", "unknown"  # type: ignore [reportArgumentType]
+        )
+
+    # python stdlib does not raise LookupError unless we have an error
+    endec.encode(
+        "unknown_errors_param", "ascii", "unknown"  # type: ignore [reportArgumentType]
+    )
