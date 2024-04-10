13f15fe7a272950f20227b7e86a7cb214fd05078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 16:05:28 +0000

    Add package swarmauri/0.1.32/swarmauri-0.1.32.tar.gz

---
+++ packages/swarmauri/swarmauri-0.1.32.tar.gz/swarmauri-0.1.32/swarmauri/core/metrics/ICalculateMetric.py
@@ -0,0 +1,39 @@
+from typing import Any
+from abc import ABC, abstractmethod
+
+class ICalculateMetric(ABC):
+
+    @abstractmethod
+    def calculate(self, **kwargs) -> Any:
+        """
+        Calculate the metric based on the provided data.
+
+        Args:
+            *args: Variable length argument list that the metric calculation might require.
+            **kwargs: Arbitrary keyword arguments that the metric calculation might require.
+        """
+        pass
+
+    @abstractmethod
+    def update(self, value) -> None:
+        """
+        Update the metric value based on new information.
+
+        Args:
+            value: The new information used to update the metric. This could be a new
+            measurement or data point that affects the metric's current value.
+
+        Note:
+            This method is intended for internal use and should not be publicly accessible.
+        """
+        pass
+
+    @abstractmethod
+    def __call__(self, **kwargs) -> Any:
+        """
+        Retrieves the current value of the metric.
+
+        Returns:
+            The current value of the metric.
+        """
+        pass
