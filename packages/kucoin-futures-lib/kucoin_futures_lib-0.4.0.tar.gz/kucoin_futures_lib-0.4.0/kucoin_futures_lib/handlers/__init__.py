18655c77170dd043d528141dcf954d2c71d419ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:03:29 +0000

    Add package kucoin-futures-lib/0.4.0/kucoin_futures_lib-0.4.0.tar.gz

---
+++ packages/kucoin-futures-lib/kucoin_futures_lib-0.4.0.tar.gz/kucoin_futures_lib-0.4.0/kucoin_futures_lib/handlers/__init__.py
@@ -0,0 +1,9 @@
+from kucoin_futures_lib.handlers.base import HandlerABC
+from kucoin_futures_lib.handlers.entry import EntryRangeHandler
+from kucoin_futures_lib.handlers.oco import OcoHandler
+
+__all__ = [
+    "HandlerABC",
+    "EntryRangeHandler",
+    "OcoHandler",
+]
