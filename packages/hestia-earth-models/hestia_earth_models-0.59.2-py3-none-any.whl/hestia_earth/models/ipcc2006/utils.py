67120942e537a8f0916b5c9279d1eb48cc02dc44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:12 +0000

    Add package hestia-earth-models/0.59.2/hestia_earth_models-0.59.2-py3-none-any.whl

---
+++ packages/hestia-earth-models/hestia_earth_models-0.59.2-py3-none-any.whl/hestia_earth/models/ipcc2006/utils.py
@@ -0,0 +1,14 @@
+from hestia_earth.utils.model import find_primary_product
+
+COEFF_NH3NOX_N2O = 0.01
+COEFF_NO3_N2O = 0.0075
+
+
+def get_N_N2O_excreta_coeff_from_primary_product(cycle: dict):
+    product = find_primary_product(cycle)
+    term = product.get('term', {}) if product else {}
+    # TODO: should use the coefficient from lookup table
+    # percent = get_lookup_value(lookup, term, col)
+    # return safe_parse_float(percent, 0.02)
+    has_sheep_goat_products = term.get('@id') in ['sheep', 'goat']
+    return 0.01 if has_sheep_goat_products else 0.02
