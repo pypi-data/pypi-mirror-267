ca820fee49db619fe7ea7b33808c9bcc42c4887f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:13 +0000

    Add package erdantic/1.0.0.post2/erdantic-1.0.0.post2-py3-none-any.whl

---
+++ packages/erdantic/erdantic-1.0.0.post2-py3-none-any.whl/erdantic/examples/__init__.py
@@ -0,0 +1,10 @@
+"""Example data model classes."""
+
+from erdantic.examples import attrs, dataclasses, pydantic, pydantic_v1
+
+__all__ = [
+    "attrs",
+    "dataclasses",
+    "pydantic",
+    "pydantic_v1",
+]
