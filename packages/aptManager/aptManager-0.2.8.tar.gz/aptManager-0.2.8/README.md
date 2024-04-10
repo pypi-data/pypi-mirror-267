6ddbf59295a99c0af2b33139b895764cbbb40d01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:46 +0000

    Add package aptManager/0.2.8/aptManager-0.2.8.tar.gz

---
+++ packages/aptManager/aptManager-0.2.8.tar.gz/aptManager-0.2.8/README.md
@@ -0,0 +1,2 @@
+# tms-client
+tms client
---
+++ packages/aptManager/aptManager-0.2.8.tar.gz/aptManager-0.2.8/setup.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    setuptools.setup(
+        name="aptManager",
+        version="0.2.8",
+        description="apt Manager",
+        long_description=long_description,      # Long description read from the the readme file
+        long_description_content_type="text/markdown",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: Other/Proprietary License",
+            "Operating System :: OS Independent",
+        ],                                      # Information to filter the project on PyPi website
+        python_requires='>=3.6',                # Minimum version requirement of the package
+        py_modules=["aptManager"],           # Name of the python package                  
+        packages=setuptools.find_packages(), #['Kuba_build_tools'],    # List of all python modules to be installed
+        scripts=['bin/aptManager']
+    )
