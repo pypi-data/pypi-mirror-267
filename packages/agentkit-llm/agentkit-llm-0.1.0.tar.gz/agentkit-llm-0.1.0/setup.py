92dc29a6e3bd8cc2c309e753e058aadd3f5a27f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:03:20 +0000

    Add package agentkit-llm/0.1.0/agentkit-llm-0.1.0.tar.gz

---
+++ packages/agentkit-llm/agentkit-llm-0.1.0.tar.gz/agentkit-llm-0.1.0/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+import pkg_resources
+import pathlib
+
+PKG_NAME = "agentkit-llm"
+VERSION = "0.1.0"
+EXTRAS = {}
+
+def _read_install_requires():
+    with pathlib.Path("requirements.txt").open() as fp:
+        return [
+            str(requirement) for requirement in pkg_resources.parse_requirements(fp)
+        ]
+
+def _fill_extras(extras):
+    if extras:
+        extras["all"] = list(set([item for group in extras.values() for item in group]))
+    return extras
+
+setuptools.setup(
+    name=PKG_NAME,
+    version=VERSION,
+    author="AgentKit Team",
+    description='A LLM prompting framework for LLM agents',
+    url="https://github.com/Holmeswww/AgentKit",
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_namespace_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={'console_scripts': ['agentkit=agentkit.run_gui:main']},
+    install_requires=_read_install_requires(),
+    extras_require=_fill_extras(EXTRAS),
+    include_package_data=True,
+    license="CC-BY-4.0-Attribution",
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
