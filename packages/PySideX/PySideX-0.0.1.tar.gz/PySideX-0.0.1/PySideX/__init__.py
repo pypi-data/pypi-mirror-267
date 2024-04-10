5feb701304604e0a38afb1eac9a10bd55f71771d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:03:31 +0000

    Add package PySideX/0.0.1/PySideX-0.0.1.tar.gz

---
+++ packages/PySideX/PySideX-0.0.1.tar.gz/PySideX-0.0.1/PySideX/__init__.py
@@ -0,0 +1,18 @@
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySideX.Widgets import (
+    Window,
+    Button,
+    Frame,
+    HBoxLayout,
+    VBoxLayout,
+    Label,
+    Dropdown,
+    SpacerItem,
+    Input,
+    Image,
+)
+from PySideX.Components import InputBox, DropdownBox, ButtonBox, Box
+from PySideX.Assets import Assets
+import sys
