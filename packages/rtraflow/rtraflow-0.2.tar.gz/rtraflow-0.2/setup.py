f5969607f79f88d0eb1e2f8e305ded9252be8159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:23 +0000

    Add package rtraflow/0.2/rtraflow-0.2.tar.gz

---
+++ packages/rtraflow/rtraflow-0.2.tar.gz/rtraflow-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rtraflow',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Udit Raj',
+    author_email='udit_2312res708@iitp.ac.in',
+    description='Package for RTRAConnect functionality',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uditakhourii/RTRAConnect',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
