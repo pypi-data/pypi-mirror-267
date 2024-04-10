888b80c44a97ffec4509a4a5f146c12eba3fa093
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:03:40 +0000

    Add package llmreader/0.2.6/llmreader-0.2.6.tar.gz

---
+++ packages/llmreader/llmreader-0.2.6.tar.gz/llmreader-0.2.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="llmreader",
+    version="0.2.6",
+    description="Intercept OpenAI inputs",
+    author="Ethan Hou",
+    author_email="ethanfhou10@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        'openai'
+    ],
+    classifiers=[
+        # Trove classifiers
+        # Full list at https://pypi.org/classifiers/
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
