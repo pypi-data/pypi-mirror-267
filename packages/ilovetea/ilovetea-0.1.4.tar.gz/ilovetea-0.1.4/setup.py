66526cb5b61d468f2d7d56421420e8539820ae38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:13 +0000

    Add package ilovetea/0.1.4/ilovetea-0.1.4.tar.gz

---
+++ packages/ilovetea/ilovetea-0.1.4.tar.gz/ilovetea-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+name='ilovetea',
+version='0.1.4',
+author='Elonmute',
+author_email='elonmute@gmail.com',
+description='This is first project on Tea protocol',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+install_requires=[
+'requests',
+],
+
+python_requires='>=3.6',
+)
