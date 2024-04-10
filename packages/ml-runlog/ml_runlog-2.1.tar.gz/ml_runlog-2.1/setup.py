6d5c0547052e40df85f970355457bf7dda8aa0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:03:37 +0000

    Add package ml-runlog/2.1/ml_runlog-2.1.tar.gz

---
+++ packages/ml-runlog/ml_runlog-2.1.tar.gz/ml_runlog-2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ml_runlog',
+      version='2.1',
+      description='Simple script to log experiments to Google Sheets. Version 2.0 is cleaner, has a class based structure and supports logging multiple rows at once.',
+      url='https://github.com/anag004/ml-runlog',
+      author='Ananye Agarwal',
+      author_email='ananayagarwal@gmail.com',
+      license='MIT',
+      packages=['ml_runlog'],
+      install_requires=[
+          'pygsheets',
+          'pandas'
+      ],
+      zip_safe=False)
