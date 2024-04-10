3df08020a2648f179d791f5c688262576550b6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 14:03:40 +0000

    Add package fastid/0.0.5/fastid-0.0.5-cp311-none-win_amd64.whl

---
+++ packages/fastid/fastid-0.0.5-cp311-none-win_amd64.whl/fastid/__init__.pyi
@@ -0,0 +1,18 @@
+def ulid() -> str:
+    ...
+
+
+def snowflake_str(machine_id: int = 1, node_id: int = 1) -> str:
+    ...
+
+
+def snowflake_int(machine_id: int = 1, node_id: int = 1) -> int:
+    ...
+
+
+def uuid_v7() -> str:
+    ...
+
+
+def object_id() -> str:
+    """test"""
