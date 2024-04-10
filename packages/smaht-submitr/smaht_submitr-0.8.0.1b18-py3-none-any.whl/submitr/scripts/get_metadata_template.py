2e0f92023dd0b91a80bed7dacf61cf5f0949ad51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 16:05:17 +0000

    Add package smaht-submitr/0.8.0.1b18/smaht_submitr-0.8.0.1b18-py3-none-any.whl

---
+++ packages/smaht-submitr/smaht_submitr-0.8.0.1b18-py3-none-any.whl/submitr/scripts/get_metadata_template.py
@@ -0,0 +1,46 @@
+from submitr.metadata_template import (
+    download_hms_metadata_template,
+    get_hms_metadata_template_version_from_google_sheets
+)
+from submitr.scripts.cli_utils import CustomArgumentParser
+
+_HELP = f"""
+===
+get-metadata-template
+===
+Tool to export and download the latest version of the HMS DBMI
+smaht-submitr metadata template spreadsheet as an Excel file.
+===
+USAGE: get-metadata-template OUTPUT-EXCEL-FILE OPTIONS
+-----
+OUTPUT-EXCEL-FILE: Saves the metadata template to this specified file.
+===
+OPTIONS:
+===
+--revision
+  Gets just the version number of the latest HMS DBMI metadata template.
+--help
+  Prints this documentation.
+===
+"""
+
+
+def main():
+
+    parser = CustomArgumentParser(help=_HELP, help_url=CustomArgumentParser.HELP_URL)
+    parser.add_argument("output_excel_file", nargs="?", help="Output Excel file.", default=None)
+    parser.add_argument("--revision", action="store_true",
+                        help="Get just the version of the latest metadata template.", default=False)
+    parser.add_argument("--metadata", help="Metadata template ID or URL (internal use only).")
+    args = parser.parse_args(None)
+
+    if args.revision:
+        print(get_hms_metadata_template_version_from_google_sheets())
+    elif args.output_excel_file:
+        output_excel_file, version = download_hms_metadata_template(args.output_excel_file,
+                                                                    _metadata_template=args.metadata,
+                                                                    verbose=True)
+
+
+if __name__ == "__main__":
+    main()
