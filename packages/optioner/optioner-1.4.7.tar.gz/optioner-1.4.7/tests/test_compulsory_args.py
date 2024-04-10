c873d8181752e94c14e960ae81feb8526dac8ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:15 +0000

    Add package optioner/1.4.7/optioner-1.4.7.tar.gz

---
+++ packages/optioner/optioner-1.4.7.tar.gz/optioner-1.4.7/tests/test_compulsory_args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from optioner import options
+from os import popen, getcwd
+from os.path import join
+
+def test_one():
+    """test case 3:
+    test compulsory arg
+    """
+    
+    
