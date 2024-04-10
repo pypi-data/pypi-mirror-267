cf5177cb568af8fc410a3c8ebb1c19f5adeb5692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:29 +0000

    Add package telepoller/0.0.2/telepoller-0.0.2.tar.gz

---
+++ packages/telepoller/telepoller-0.0.2.tar.gz/telepoller-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+	long_description = fh.read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+	required = f.read().splitlines()
+
+
+setuptools.setup(
+	name='telepoller',
+	version="0.0.2",
+	author="STaper_Admin",
+	description="Poller for Telegram Bot on Pytelegrambotapi",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	install_requires=required,
+	classifiers=[
+		"Programming Language :: Python :: 3.10",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	# Требуемая версия Python.
+	python_requires='>=3.10'
+)
