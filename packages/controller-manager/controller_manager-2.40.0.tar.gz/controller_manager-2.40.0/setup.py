cc9530eae92f408dd7966c2e4486df11c456e8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:26 +0000

    Add package controller-manager/2.40.0/controller_manager-2.40.0.tar.gz

---
+++ packages/controller-manager/controller_manager-2.40.0.tar.gz/controller_manager-2.40.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="controller_manager",
+    version="2.40.0",
+    author="Your Name",
+    author_email="your@email.com",
+    description="Your package description",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    # Add more options as needed
+)
+
