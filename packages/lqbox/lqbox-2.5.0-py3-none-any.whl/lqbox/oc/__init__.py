04c1d46a2bf7b22f6ca497d01aebb600694c4a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:36 +0000

    Add package lqbox/2.5.0/lqbox-2.5.0-py3-none-any.whl

---
+++ packages/lqbox/lqbox-2.5.0-py3-none-any.whl/lqbox/oc/__init__.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Project : lqbox 
+@File    : __init__.py
+@Date    : 2023/8/25 10:51:57
+@Author  : zhchen
+@Desc    : 
+"""
+from lqbox.base import BaseBox
+
+
+class OcBox(BaseBox):
+    base_url = 'aHR0cHM6Ly9vYy5sZXFlZS5jb20='
+
+    def __init__(self, cookies):
+        self.token = None
+        super().__init__(cookies)
+
+    def check(self):
+        super().check()
+        self.token = self.cookies.get("token")
+        if not self.token:
+            raise ValueError("token is None")
+
+    def __oc_request(self, path, json_data):
+        return self.request(url=f"{self.base_url}{path}", method="POST", json=json_data)
+
+    def user_info_detail(self):
+        """个人信息"""
+        return self.__oc_request(
+            path="/index.php/tachiba/TachibaServiceBasicInfoController/userInfoDetail",
+            json_data={"token": self.token}
+        )
+
+    def display_user(self, json_data):
+        """用户信息-新版"""
+        # json_data = {
+        #     "core_user_id": 1234
+        # }
+        return self.__oc_request(
+            path="/ext-hr/api/staffProfile/StaffController/displayUser",
+            json_data={**json_data, "token": self.token}
+        )
+
+    def assessing_list(self, json_data):
+        """绩效列表"""
+        # json_data = {
+        #     'page': 1,
+        #     'limit': 10,
+        # }
+        return self.__oc_request(
+            path="/ext-hr/api/assessment/AssessmentAsAssesseeController/getMySelfAssessingList",
+            json_data={**json_data, "token": self.token}
+        )
+
+    def assessing_result(self, json_data):
+        """绩效具体"""
+        # json_data = {
+        #     "assessment_code": "2021Q04"
+        # }
+        return self.__oc_request(
+            path="/ext-hr/api/assessment/AssessmentAsAssesseeController/getMySelfAssessingResult",
+            json_data={**json_data, "token": self.token}
+        )
