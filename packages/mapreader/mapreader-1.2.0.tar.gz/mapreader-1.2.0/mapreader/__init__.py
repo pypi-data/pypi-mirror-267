e52eee8fa93fb06cd98690cf0640eaea6d75974f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 16:05:22 +0000

    Add package mapreader/1.2.0/mapreader-1.2.0.tar.gz

---
+++ packages/mapreader/mapreader-1.2.0.tar.gz/mapreader-1.2.0/mapreader/__init__.py
@@ -0,0 +1,23 @@
+from mapreader.load.images import MapImages
+from mapreader.load.loader import loader
+from mapreader.load.loader import load_patches
+
+from mapreader.download.sheet_downloader import SheetDownloader
+from mapreader.download.downloader import Downloader
+from mapreader.download.downloader_utils import create_polygon_from_latlons, create_line_from_latlons
+
+from mapreader.classify.load_annotations import AnnotationsLoader
+from mapreader.classify.datasets import PatchDataset
+from mapreader.classify.datasets import PatchContextDataset
+from mapreader.classify.classifier import ClassifierContainer
+from mapreader.classify import custom_models
+
+from mapreader.process import process
+
+from mapreader.annotate.annotator import Annotator
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
+
+from mapreader.load import geo_utils
