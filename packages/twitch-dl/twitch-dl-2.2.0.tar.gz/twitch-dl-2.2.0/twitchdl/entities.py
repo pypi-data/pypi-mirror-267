8bf48be5df899e733d31cf3f95948d72d876e9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:19 +0000

    Add package twitch-dl/2.2.0/twitch-dl-2.2.0.tar.gz

---
+++ packages/twitch-dl/twitch-dl-2.2.0.tar.gz/twitch-dl-2.2.0/twitchdl/entities.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class DownloadOptions:
+    auth_token: str | None
+    chapter: int | None
+    concat: bool
+    dry_run: bool
+    end: int | None
+    format: str
+    keep: bool
+    no_join: bool
+    overwrite: bool
+    output: str
+    quality: str | None
+    rate_limit: int | None
+    start: int | None
+    max_workers: int
+
+
+# Type for annotating decoded JSON
+# TODO: make data classes for common structs
+Data = dict[str, Any]
