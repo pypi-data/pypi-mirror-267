541765ebaf1876fc51dab4303b3ae5ea05b0d04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 16:05:16 +0000

    Add package zrb-noto/0.0.2/zrb_noto-0.0.2-py3-none-any.whl

---
+++ packages/zrb-noto/zrb_noto-0.0.2-py3-none-any.whl/zrb_noto/sync.py
@@ -0,0 +1,37 @@
+import os
+from typing import List, Optional
+
+from zrb import CmdTask, Env, Group, Task, runner
+
+from ._env import LOCAL_REPO_DIR_ENV, REMOTE_GIT_URL_ENV
+from ._group import noto_group
+
+_CURRENT_DIR = os.path.dirname(__file__)
+
+
+def create_sync_noto_task(
+    group: Optional[Group] = None,
+    retry: int = 0,
+    ignore_error: bool = True,
+    upstreams: List[Task] = [],
+) -> Task:
+    sync_noto = CmdTask(
+        name="sync",
+        group=group,
+        description="Sync noto",
+        upstreams=upstreams,
+        envs=[
+            LOCAL_REPO_DIR_ENV,
+            REMOTE_GIT_URL_ENV,
+            Env(name="IGNORE_ERROR", os_name="", default="1" if ignore_error else "0"),
+        ],
+        retry=retry,
+        cmd_path=os.path.join(_CURRENT_DIR, "sync.sh"),
+        should_show_cmd=False,
+        should_print_cmd_result=False,
+    )
+    return sync_noto
+
+
+sync_noto = create_sync_noto_task(group=noto_group, ignore_error=False, retry=2)
+runner.register(sync_noto)
