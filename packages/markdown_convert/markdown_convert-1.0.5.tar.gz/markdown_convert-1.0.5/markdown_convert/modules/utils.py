ef60cf9466d8e6bdf59ca8e8de3ccbdb9a897d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:32 +0000

    Add package markdown_convert/1.0.5/markdown_convert-1.0.5.tar.gz

---
+++ packages/markdown_convert/markdown_convert-1.0.5.tar.gz/markdown_convert-1.0.5/markdown_convert/modules/utils.py
@@ -0,0 +1,31 @@
+"""
+Utility functions for string manipulation.
+Author: @julynx
+"""
+
+
+def color(color_code, text):
+    """
+    Colorize text.
+
+    Args:
+        text (str): The text to colorize.
+        color (str): The color code.
+
+    Returns:
+        str: The colorized text.
+    """
+    return f"\033[{color_code}m{text}\033[0m"
+
+
+def drop_duplicates(lst):
+    """
+    Drops duplicates from the given list.
+
+    Args:
+        lst: List to remove duplicates from.
+
+    Returns:
+        List without duplicates.
+    """
+    return list(dict.fromkeys(lst))
