f1208b08b0bada19dcf47bae13c7b0dfc315c9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:42 +0000

    Add package stackr/0.1.0/stackr-0.1.0-py3-none-any.whl

---
+++ packages/stackr/stackr-0.1.0-py3-none-any.whl/stackr/__init__.py
@@ -0,0 +1,10 @@
+# This project is intended to reserve the package name
+# This project provides basic Hello World functionality
+
+def hello():
+    """Print a greeting message."""
+    print("Hello, World!")
+
+def goodbye():
+    """Print a goodbye message."""
+    print("Goodbye, World!")
