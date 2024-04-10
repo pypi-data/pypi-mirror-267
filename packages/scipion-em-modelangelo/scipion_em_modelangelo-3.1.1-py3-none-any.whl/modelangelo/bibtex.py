e723a3610ea7edf845152391f962187e21dbb6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 16:05:22 +0000

    Add package scipion-em-modelangelo/3.1.1/scipion_em_modelangelo-3.1.1-py3-none-any.whl

---
+++ packages/scipion-em-modelangelo/scipion_em_modelangelo-3.1.1-py3-none-any.whl/modelangelo/bibtex.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# *
+# * Authors:     you (you@yourinstitution.email)
+# *
+# * your institution
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+"""
+@article{jamali2023,
+author = {Kiarash Jamali and Lukas Kall and Rui Zhang and Alan Brown and Dari Kimanius and Sjors Scheres},
+title = {Automated model building and protein identification in cryo-EM maps},
+year = {2023},
+doi = {https://dx.doi.org/10.1101/2023.05.16.541002},
+publisher = {Cold Spring Harbor Laboratory},
+URL = {https://www.biorxiv.org/content/early/2023/05/16/2023.05.16.541002},
+journal = {bioRxiv}
+}
+
+"""
