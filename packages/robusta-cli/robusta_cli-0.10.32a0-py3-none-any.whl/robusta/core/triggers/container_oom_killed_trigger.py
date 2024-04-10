3c69d27aaaab44c2fff6f0c8159c509dbd0acd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 15:03:35 +0000

    Add package robusta-cli/0.10.32a0/robusta_cli-0.10.32a0-py3-none-any.whl

---
+++ packages/robusta-cli/robusta_cli-0.10.32a0-py3-none-any.whl/robusta/core/triggers/container_oom_killed_trigger.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from hikaru.model.rel_1_26 import ContainerStatus, Pod
+
+from robusta.core.triggers.oom_killed_trigger_base import Exclude, OOMKilledTriggerBase
+
+
+class ContainerOOMKilledTrigger(OOMKilledTriggerBase):
+    def __init__(
+        self,
+        name_prefix: str = None,
+        namespace_prefix: str = None,
+        labels_selector: str = None,
+        rate_limit: int = 3600,
+        exclude: List[Exclude] = None,
+    ):
+        super().__init__(
+            name_prefix=name_prefix,
+            namespace_prefix=namespace_prefix,
+            labels_selector=labels_selector,
+            rate_limit=rate_limit,
+            exclude=exclude,
+        )
+
+    def get_relevant_oomkilled_container_statuses(self, pod: Pod) -> List[ContainerStatus]:
+        statuses = pod.status.containerStatuses + pod.status.initContainerStatuses
+        return [
+            status for status in statuses if not self.is_name_namespace_excluded(status.name, pod.metadata.namespace)
+        ]
