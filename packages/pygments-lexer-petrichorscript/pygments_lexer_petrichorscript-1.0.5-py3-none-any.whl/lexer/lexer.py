c629677cb73d73b9989dc055cae6d04f3fbe30d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-10 16:05:21 +0000

    Add package pygments-lexer-petrichorscript/1.0.5/pygments_lexer_petrichorscript-1.0.5-py3-none-any.whl

---
+++ packages/pygments-lexer-petrichorscript/pygments_lexer_petrichorscript-1.0.5-py3-none-any.whl/lexer/lexer.py
@@ -0,0 +1,32 @@
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+
+__all__ = ('PetrichorScriptLexer')
+
+
+class PetrichorScriptLexer(RegexLexer):
+    name = 'PetrichorScript'
+    aliases = ['petrichor','ptcr']
+    filenames = ['*.petrichor','*.ptcr']
+
+    tokens = {
+        'root': [
+            (r'\s+', Text), # whitespace
+            (r'\\.', String.Escape), # escaped character
+            (r'//.*\n', Comment.Single), # line comment
+            (r',', Punctuation), # comma
+            (r'[\{\}]', Operator), # token body boundaries
+            (r'(?<!>)>>(?!>)', Operator), # text shortcut template divider operator
+            (r'(^\s*[a-z]+(?:-[a-z0-9]+)*(?:-[a-z]+)?)(\s*)(:)', bygroups(Keyword.Reserved, Text, Operator)), # token name, whitespacae, token divider
+            (r'\[[a-z\-]+\]', Name.Variable), # field
+            (r'"', String, 'string'), # string start
+            (r'\b(-)?[0-9.]+\b', Number),
+            (r'.', Text),
+        ],
+        'string': [
+            (r'\\.', String.Escape),
+            (r'"', String, '#pop'), # string end
+            (r'[^\\"]+', String),
+        ]
+    }
