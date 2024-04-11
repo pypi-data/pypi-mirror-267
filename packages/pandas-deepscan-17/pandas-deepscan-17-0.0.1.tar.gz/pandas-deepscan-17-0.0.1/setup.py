165a502bb9422781245c88c056558bb871d3737e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 03:03:12 +0000

    Add package pandas-deepscan-17/0.0.1/pandas-deepscan-17-0.0.1.tar.gz

---
+++ packages/pandas-deepscan-17/pandas-deepscan-17-0.0.1.tar.gz/pandas-deepscan-17-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+VERSION = "0.0.1"
+
+setup(
+    name="pandas-deepscan-17",
+    version=VERSION,
+    description="",
+    python_requires=">=3.6",
+    install_requires=[],
+    tests_require=[]
+)
