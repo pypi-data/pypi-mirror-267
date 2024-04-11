60c8ec3f2cd8a31ab024933c8b262ee627da4a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:04:14 +0000

    Add package pixeloe/0.0.9/pixeloe-0.0.9.tar.gz

---
+++ packages/pixeloe/pixeloe-0.0.9.tar.gz/pixeloe-0.0.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pixeloe import cli
+
+
+setup(
+    name="pixeloe",
+    packages=find_packages(),
+    version="0.0.9",
+    url="https://github.com/KohakuBlueleaf/PixelOE",
+    description="Detail-Oriented Pixelization based on Contrast-Aware Outline Expansion.",
+    license="Apache License 2.0",
+    author="Shih-Ying Yeh(KohakuBlueLeaf)",
+    author_email="apolloyeh0123@gmail.com",
+    zip_safe=False,
+    install_requires=["opencv-python", "numpy", "pillow"],
+    entry_points={
+        "console_scripts": [
+            f"pixeloe.{command}=pixeloe.cli:{func}"
+            for command, func in cli.command_map.items()
+        ]
+    },
+    python_requires=">=3.10",
+)
