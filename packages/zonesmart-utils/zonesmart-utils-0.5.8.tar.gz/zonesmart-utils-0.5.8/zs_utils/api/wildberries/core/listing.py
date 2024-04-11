68cbbbc49e1785f8e850b3bb0bac9bb38c7de691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 06:03:55 +0000

    Add package zonesmart-utils/0.5.8/zonesmart-utils-0.5.8.tar.gz

---
+++ packages/zonesmart-utils/zonesmart-utils-0.5.8.tar.gz/zonesmart-utils-0.5.8/zs_utils/api/wildberries/core/listing.py
@@ -0,0 +1,86 @@
+from zs_utils.api.wildberries.base_api import WildberriesAPI
+
+
+class GetWildberriesNomenclatureList(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Prosmotr/paths/~1content~1v1~1cards~1cursor~1list/post
+    """
+
+    http_method = "POST"
+    resource_method = "content/v1/cards/cursor/list"
+    required_params = ["sort"]
+
+
+class GetWildberriesNomenclatureByVendorCode(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Prosmotr/paths/~1content~1v1~1cards~1filter/post
+    """
+
+    http_method = "POST"
+    resource_method = "content/v1/cards/filter"
+    required_params = ["vendorCodes"]
+
+
+class CreateWildberriesBarcodes(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Prosmotr/paths/~1content~1v1~1barcodes/post
+    """
+
+    http_method = "POST"
+    resource_method = "content/v1/barcodes"
+    required_params = ["count"]
+
+
+class GetWildberriesFailedToUploadNomenclatureList(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Prosmotr/paths/~1content~1v1~1cards~1error~1list/get
+    """
+
+    http_method = "GET"
+    resource_method = "content/v1/cards/error/list"
+
+
+class CreateWildberriesNomenclature(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Zagruzka/paths/~1content~1v1~1cards~1upload/post
+    """
+
+    http_method = "POST"
+    resource_method = "content/v1/cards/upload"
+    array_payload = True
+
+
+class UpdateWildberriesNomenclature(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Zagruzka/paths/~1content~1v1~1cards~1update/post
+    """
+
+    http_method = "POST"
+    resource_method = "content/v1/cards/update"
+    array_payload = True
+
+
+class AddWildberriesNomenclaturesToCard(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Zagruzka/paths/~1content~1v1~1cards~1upload~1add/post
+    """
+
+    http_method = "POST"
+    resource_method = "content/v1/cards/upload/add"
+    required_params = [
+        "vendorCode",
+        "cards",
+    ]
+
+
+class UpdateWildberriesNomenclatureImages(WildberriesAPI):
+    """
+    https://openapi.wb.ru/#tag/Kontent-Mediafajly/paths/~1content~1v1~1media~1save/post
+    """
+
+    http_method = "POST"
+    resource_method = "content/v1/media/save"
+    required_params = [
+        "vendorCode",
+        "data",
+    ]
