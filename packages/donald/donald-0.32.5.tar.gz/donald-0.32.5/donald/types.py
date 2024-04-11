16d7f0de67162151809b19e2a32b79974fa33c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:04:17 +0000

    Add package donald/0.32.5/donald-0.32.5.tar.gz

---
+++ packages/donald/donald-0.32.5.tar.gz/donald-0.32.5/donald/types.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from numbers import Number
+from typing import (
+    Any,
+    Callable,
+    Coroutine,
+    Dict,
+    Literal,
+    Optional,
+    Tuple,
+    TypedDict,
+    TypeVar,
+)
+
+TWorkerOnFn = Callable[[], Coroutine]
+TVWorkerOnFn = TypeVar("TVWorkerOnFn", bound=TWorkerOnFn)
+TWorkerOnErrFn = Callable[[BaseException], Coroutine]
+TVWorkerOnErrFn = TypeVar("TVWorkerOnErrFn", bound=TWorkerOnErrFn)
+
+TV = TypeVar("TV", bound=Any)
+TAsyncFn = Callable[..., Coroutine]
+TVAsyncFn = TypeVar("TVAsyncFn", bound=TAsyncFn)
+
+TBackendType = Literal["memory", "redis", "amqp"]
+
+TTaskParams = TypedDict(
+    "TTaskParams",
+    {
+        "bind": Optional[bool],
+        "delay": Optional[Number],
+        "timeout": Optional[Number],
+    },
+)
+
+TRunArgs = Tuple[str, Tuple, Dict[str, Any], TTaskParams]
+
+
+class TWorkerParams(TypedDict):
+    max_tasks: Optional[int]
+    task_defaults: Optional[TTaskParams]
+    on_start: Optional[TWorkerOnFn]
+    on_stop: Optional[TWorkerOnFn]
+    on_error: Optional[TWorkerOnErrFn]
+    show_banner: Optional[bool]
+
+
+class TManagerParams(TypedDict):
+    backend: TBackendType
+    backend_params: Dict
+    log_config: Optional[Dict]
+    log_level: str
+    worker_params: TWorkerParams
