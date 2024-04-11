7a67f27243a9fb0ba78bfa2e88965c4912489978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:04:04 +0000

    Add package edsnlp/0.11.2/edsnlp-0.11.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/edsnlp/edsnlp-0.11.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/edsnlp/pipes/trainable/span_qualifier/span_qualifier.py
@@ -0,0 +1,443 @@
+from __future__ import annotations
+
+import logging
+import os
+import pickle
+import warnings
+from typing import (
+    Any,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Set,
+    Tuple,
+    Union,
+)
+
+import foldedtensor as ft
+import torch
+import torch.nn.functional as F
+from spacy.tokens import Doc, Span
+from typing_extensions import NotRequired, TypedDict
+
+from edsnlp.core import PipelineProtocol
+from edsnlp.core.torch_component import BatchInput, BatchOutput, TorchComponent
+from edsnlp.pipes.base import BaseComponent
+from edsnlp.pipes.trainable.embeddings.typing import (
+    SpanEmbeddingComponent,
+)
+from edsnlp.utils.bindings import (
+    BINDING_GETTERS,
+    BINDING_SETTERS,
+    Qualifiers,
+    QualifiersArg,
+)
+from edsnlp.utils.span_getters import SpanFilter, get_spans
+
+logger = logging.getLogger(__name__)
+
+SpanQualifierBatchInput = TypedDict(
+    "SpanQualifierBatchInput",
+    {
+        "embedding": BatchInput,
+        "targets": NotRequired[torch.Tensor],
+    },
+)
+"""
+embeds: torch.FloatTensor
+    Token embeddings to predict the tags from
+mask: torch.BoolTensor
+    Mask of the sequences
+spans: torch.LongTensor
+    2d tensor of n_spans * (doc_idx, ner_label_idx, begin, end)
+targets: NotRequired[List[torch.LongTensor]]
+    list of 2d tensor of n_spans * n_combinations (1 hot)
+"""
+
+
+class TrainableSpanQualifier(
+    TorchComponent[BatchOutput, SpanQualifierBatchInput], BaseComponent
+):
+    """
+    The `eds.span_qualifier` component is a trainable qualifier predictor. In EDS-NLP,
+    we call span attributes "qualifiers". In this context, the span qualification task
+    consists in assigning values (boolean, strings or any complex object) to
+    attributes/extensions of spans such as:
+
+    - `span._.negation`,
+    - `span._.date.mode`
+    - `span._.cui`
+
+    In the rest of this page, we will refer to a pair of (qualifier, value) as a
+    "binding". For instance, the binding `("_.negation", True)` means that the
+    qualifier `negation` of the span is (or should be, when predicted) set to `True`.
+
+    Architecture
+    ------------
+    The model performs span classification by:
+
+    1. Calling a word pooling embedding such as `eds.span_pooler` to compute a single
+    embedding for each span
+    2. Computing logits for each possible binding using a linear layer
+    3. Splitting these bindings into groups of exclusive values such as
+
+        - `event=start` and `event=stop`
+        - `negated=False` and `negated=True`
+
+        Note that the above groups are not exclusive, but the values within each group
+        are.
+
+    4. Applying the best scoring binding in each group to each span
+
+    Examples
+    --------
+    To create a span qualifier component, you can use the following code:
+
+    ```python
+    import edsnlp, edsnlp.pipes as eds
+
+    nlp = edsnlp.blank("eds")
+    nlp.add_pipe(
+        eds.span_qualifier(
+            # To embed the spans, we will use a span pooler
+            embedding=eds.span_pooler(
+                pooling_mode="mean",  # mean pooling
+                span_getter=["ents", "sc"],
+                # that will use a transformer to embed the doc words
+                embedding=eds.transformer(
+                    model="prajjwal1/bert-tiny",
+                    window=128,
+                    stride=96,
+                ),
+            ),
+            # For every span embedded by the span pooler
+            # (doc.ents and doc.spans["sc"]), we will predict both
+            # span._.negation and span._.event_type
+            qualifiers=["_.negation", "_.event_type"],
+        ),
+        name="qualifier",
+    )
+    ```
+
+    To infer the values of the qualifiers, you can use the pipeline `post_init` method:
+
+    ```{ .python .no-check }
+    nlp.post_init(gold_data)
+    ```
+
+    To train the model, refer to the [Training](/tutorials/training.md) tutorial.
+
+    You can inspect the bindings that will be used for training and prediction
+    ```{ .python .no-check }
+    print(nlp.pipes.qualifier.bindings)
+    # list of (qualifier name, span labels or True if all, values)
+    # Out: [
+    #   ('_.negation', True, [True, False]),
+    #   ('_.event_type', True, ['start', 'stop'])
+    # ]
+    ```
+
+    You can also change these values and update the bindings by calling the
+    `update_bindings` method. Don't forget to retrain the model if new values are
+    added !
+
+    Parameters
+    ----------
+    nlp : PipelineProtocol
+        The pipeline object
+    name : str
+        Name of the component
+    embedding : SpanEmbeddingComponent
+        The word embedding component
+    qualifiers : QualifiersArg
+        The qualifiers to predict or train on. If a dict is given, keys are the
+        qualifiers and values are the labels for which the qualifier is allowed, or True
+        if the qualifier is allowed for all labels.
+    keep_none : bool
+        If False, skip spans for which a qualifier returns None. If True (default), the
+        None values will be learned and predicted, just as any other value.
+    """
+
+    def __init__(
+        self,
+        nlp: Optional[PipelineProtocol] = None,
+        name: str = "span_qualifier",
+        *,
+        embedding: SpanEmbeddingComponent,
+        qualifiers: QualifiersArg,
+        values: Optional[Dict[str, List[Any]]] = None,
+        keep_none: bool = False,
+    ):
+        qualifiers: Qualifiers
+        self.values = values
+        self.keep_none = keep_none
+        self.bindings: List[Tuple[str, List[str], List[Any]]] = [
+            (k if k.startswith("_.") else f"_.{k}", v, [])
+            for k, v in qualifiers.items()
+        ]
+
+        super().__init__(nlp, name)
+        self.embedding = embedding
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", UserWarning)
+            self.classifier = torch.nn.Linear(embedding.output_size, 0)
+
+    @property
+    def qualifiers(self) -> Qualifiers:
+        return {qlf: labels for qlf, labels, _ in self.bindings}
+
+    @qualifiers.setter
+    def qualifiers(self, value: Qualifiers):
+        bindings = []
+        for qlf, labels in value.items():
+            groups = [group for group in self.bindings if group[0] == qlf]
+            if len(groups) > 1:
+                raise ValueError(
+                    f"Qualifier {qlf} has different label filters: "
+                    f"{[g[0] for g in groups]}. Please use the `update_bindings` "
+                    f"method to update the labels."
+                )
+            if groups:
+                bindings.append((qlf, labels, groups[0][2]))
+        self.update_bindings(bindings)
+
+    @property
+    def span_getter(self):
+        return self.embedding.span_getter
+
+    def to_disk(self, path, *, exclude=set()):
+        repr_id = object.__repr__(self)
+        if repr_id in exclude:
+            return
+        # This will receive the directory path + /my_component
+        # We save the bindings as a pickle file since values can be arbitrary objects
+        os.makedirs(path, exist_ok=True)
+        data_path = path / "bindings.pkl"
+        with open(data_path, "wb") as f:
+            pickle.dump(
+                {
+                    "bindings": self.bindings,
+                },
+                f,
+            )
+        return super().to_disk(path, exclude=exclude)
+
+    def from_disk(self, path, exclude=tuple()):
+        repr_id = object.__repr__(self)
+        if repr_id in exclude:
+            return
+        # This will receive the directory path + /my_component
+        data_path = path / "bindings.pkl"
+        with open(data_path, "rb") as f:
+            data = pickle.load(f)
+        self.update_bindings(data["bindings"])
+        self.set_extensions()
+        super().from_disk(path, exclude=exclude)
+
+    @property
+    def bindings_to_idx(self) -> List[Tuple[str, List[str], Dict[Any, int]]]:
+        if getattr(self, "_bindings_to_idx", None) is not None:
+            return self._bindings_to_idx
+        self._bindings_to_idx = [  # noqa
+            (qualifier, labels, {value: idx for idx, value in enumerate(values)})
+            for qualifier, labels, values in self.bindings
+        ]
+        return self._bindings_to_idx
+
+    @property
+    def bindings_indexers(self) -> List[Union[Sequence[int], slice]]:
+        return self._bindings_indexers
+
+    def set_extensions(self):
+        super().set_extensions()
+        for group in self.bindings:
+            qlf = group[0]
+            if qlf.startswith("_."):
+                qlf = qlf[2:]
+            if not Span.has_extension(qlf):
+                Span.set_extension(qlf, default=None)
+
+    def post_init(self, gold_data: Iterable[Doc], exclude: Set[str]):
+        super().post_init(gold_data, exclude=exclude)
+
+        bindings = [
+            (qlf, labels, dict.fromkeys(vals)) for qlf, labels, vals in self.bindings
+        ]
+        for doc in gold_data:
+            spans = list(get_spans(doc, self.embedding.span_getter))
+            for span in spans:
+                for qualifier, labels, values in bindings:
+                    if labels is True or span.label_ in labels:
+                        value = BINDING_GETTERS[qualifier](span)
+                        if value is not None or self.keep_none:
+                            values[value] = None
+
+        bindings = [
+            (qualifier, labels, sorted(values, key=str))
+            for qualifier, labels, values in bindings
+        ]
+
+        for qualifier, labels, values in bindings:
+            if len(values) < 2:
+                warnings.warn(
+                    f"Qualifier {qualifier} for labels {labels} should have at "
+                    f"least 2 values but found {len(values)}: {values}."
+                )
+
+        self.update_bindings(bindings)
+
+    def update_bindings(self, bindings: List[Tuple[str, SpanFilter, List[Any]]]):
+        keep_bindings = [
+            (labels is True or len(labels) > 0) and len(values) > 0
+            for k, labels, values in bindings
+        ]
+        if not all(keep_bindings):
+            logger.warning(
+                "Some qualifiers have no labels or values and have been removed:"
+                + "".join(
+                    "\n- " + str(b[0]) + " for labels " + str(b[1])
+                    for b, keep in zip(bindings, keep_bindings)
+                    if not keep
+                )
+            )
+        bindings = [b for b, keep in zip(bindings, keep_bindings) if keep]
+        new_bindings = list(
+            dict.fromkeys((k, v) for k, _, group in bindings for v in group)
+        )
+        old_bindings = list(
+            dict.fromkeys((k, v) for k, _, group in self.bindings for v in group)
+        )
+
+        new_bindings_to_idx = {binding: idx for idx, binding in enumerate(new_bindings)}
+        old_bindings_to_idx = {binding: idx for idx, binding in enumerate(old_bindings)}
+
+        common = [b for b in new_bindings if b in old_bindings]
+        old_index = [old_bindings_to_idx[label] for label in common]
+        new_index = [new_bindings_to_idx[label] for label in common]
+        new_linear = torch.nn.Linear(self.classifier.in_features, len(new_bindings))
+        new_linear.weight.data[new_index] = self.classifier.weight.data[old_index]
+        self.classifier.weight = new_linear.weight
+        self.classifier.out_features = new_bindings
+        missing_bindings = set(new_bindings) - set(old_bindings)
+        if missing_bindings and len(old_bindings) > 0:
+            warnings.warn(
+                f"Added {len(missing_bindings)} new bindings. Consider retraining "
+                f"the model to learn these new bindings."
+            )
+
+        if hasattr(self.classifier, "bias"):
+            new_linear.bias.data[new_index] = self.classifier.bias.data[old_index]
+            self.classifier.bias = new_linear.bias
+
+        def simplify_indexer(indexer):
+            return (
+                slice(indexer[0], indexer[-1] + 1)
+                if len(indexer) and list(range(indexer[0], indexer[-1] + 1)) == indexer
+                else indexer
+            )
+
+        self.bindings = bindings
+        self._bindings_indexers = [
+            simplify_indexer([new_bindings_to_idx[(qlf, value)] for value in values])
+            for qlf, labels, values in bindings
+        ]
+        self._bindings_to_idx = None
+
+    def preprocess(self, doc: Doc) -> Dict[str, Any]:
+        return {
+            "embedding": self.embedding.preprocess(doc),
+        }
+
+    def preprocess_supervised(self, doc: Doc) -> Dict[str, Any]:
+        preps = self.preprocess(doc)
+        spans = preps["embedding"]["$spans"]
+        return {
+            **preps,
+            "targets": [
+                [
+                    values_to_idx.get(BINDING_GETTERS[qlf](span), -100)
+                    if labels is True or span.label_ in labels
+                    else -100
+                    for qlf, labels, values_to_idx in self.bindings_to_idx
+                ]
+                for span in spans
+            ],
+        }
+
+    def collate(self, batch: Dict[str, Sequence[Any]]) -> SpanQualifierBatchInput:
+        collated: SpanQualifierBatchInput = {
+            "embedding": self.embedding.collate(batch["embedding"]),
+        }
+        if "targets" in batch:
+            targets = ft.as_folded_tensor(
+                batch["targets"],
+                dtype=torch.long,
+                full_names=("sample", "span", "group"),
+                data_dims=("span", "group"),
+            ).as_tensor()
+            collated["targets"] = targets.view(len(targets), len(self.bindings))
+        return collated
+
+    # noinspection SpellCheckingInspection
+    def forward(self, batch: SpanQualifierBatchInput) -> BatchOutput:
+        """
+        Apply the span classifier module to the document embeddings and given spans to:
+        - compute the loss
+        - and/or predict the labels of spans
+
+        Parameters
+        ----------
+        batch: SpanQualifierBatchInput
+            The input batch
+
+        Returns
+        -------
+        BatchOutput
+        """
+        embedding = self.embedding.module_forward(batch["embedding"])
+        span_embeds = embedding["embeddings"]
+
+        binding_scores = self.classifier(span_embeds)
+
+        if "targets" in batch:
+            losses = []
+            pred = None
+        else:
+            pred = []
+            losses = None
+
+        # For each group, for instance:
+        # - `event=start` and `event=stop`
+        # - `negated=False` and `negated=True`
+        for group_idx, bindings_indexer in enumerate(self.bindings_indexers):
+            if "targets" in batch:
+                losses.append(
+                    F.cross_entropy(
+                        binding_scores[:, bindings_indexer],
+                        batch["targets"][:, group_idx],
+                        reduction="sum",
+                    )
+                )
+                assert not torch.isnan(losses[-1]).any(), "NaN loss"
+            else:
+                pred.append(binding_scores[:, bindings_indexer].argmax(dim=1))
+
+        return {
+            "loss": sum(losses) if losses is not None else None,
+            "labels": pred,
+        }
+
+    def postprocess(self, docs: Sequence[Doc], batch: BatchOutput) -> Sequence[Doc]:
+        # Preprocessed docs should still be in the cache
+        spans = [s for doc in docs for s in self.preprocess(doc)["embedding"]["$spans"]]
+        # For each prediction group (exclusive bindings)...
+        for val_indices, (qlf, labels, values) in zip(batch["labels"], self.bindings):
+            # For each span...
+            for span, idx in zip(spans, val_indices.tolist()):
+                # If the span is not filtered out...
+                if labels is True or span.label_ in labels:
+                    # ...assign the predicted value to the span
+                    BINDING_SETTERS[qlf](span, values[idx])
+        return docs
---
+++ packages/edsnlp/edsnlp-0.11.2-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/edsnlp/utils/span_getters.py
@@ -0,0 +1,350 @@
+from collections import defaultdict
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    Iterable,
+    List,
+    Optional,
+    Sequence,
+    Union,
+)
+
+from pydantic import NonNegativeInt
+from spacy.tokens import Doc, Span
+from typing_extensions import Literal
+
+from edsnlp import registry
+from edsnlp.utils.filter import filter_spans
+from edsnlp.utils.typing import AsList
+
+SeqStr = AsList[str]
+SpanFilter = Union[bool, SeqStr]
+
+SpanSetterMapping = Dict[str, SpanFilter]
+SpanGetterMapping = Dict[str, SpanFilter]
+
+SpanGetter = Union[
+    SpanGetterMapping,
+    Callable[[Doc], Iterable[Span]],
+]
+SpanSetter = Union[
+    SpanSetterMapping,
+    Callable[[Doc, Iterable[Span]], Any],
+]
+
+
+def get_spans(doc, span_getter):
+    if span_getter is None:
+        yield doc[:]
+        return
+    if callable(span_getter):
+        yield from span_getter(doc)
+        return
+    for key, span_filter in span_getter.items():
+        if key == "*":
+            candidates = (span for group in doc.spans.values() for span in group)
+        else:
+            candidates = doc.spans.get(key, ()) if key != "ents" else doc.ents
+        if span_filter is True:
+            yield from candidates
+        else:
+            for span in candidates:
+                if span.label_ in span_filter:
+                    yield span
+
+
+def get_spans_with_group(doc, span_getter):
+    if span_getter is None:
+        yield doc[:], None
+        return
+    if callable(span_getter):
+        yield from span_getter(doc)
+        return
+    for key, span_filter in span_getter.items():
+        if key == "*":
+            candidates = (
+                (span, group) for group in doc.spans.values() for span in group
+            )
+        else:
+            candidates = doc.spans.get(key, ()) if key != "ents" else doc.ents
+            candidates = ((span, key) for span in candidates)
+        if span_filter is True:
+            yield from candidates
+        else:
+            for span, group in candidates:
+                if span.label_ in span_filter:
+                    yield span, group
+
+
+def set_spans(doc, matches, span_setter):
+    if callable(span_setter):
+        span_setter(doc, matches)
+    else:
+        match_all = []
+        label_to_group = defaultdict(list)
+        for name, spans_filter in span_setter.items():
+            if name != "ents" and name != "*":
+                doc.spans.setdefault(name, [])
+            if spans_filter:
+                if spans_filter is True:
+                    match_all.append(name)
+                else:
+                    for label in spans_filter:
+                        label_to_group[label].append(name)
+
+        new_ents = [] if "ents" in span_setter else None
+
+        for span in matches:
+            for group in match_all + label_to_group[span.label_]:
+                if group == "ents":
+                    new_ents.append(span)
+                elif group == "*":
+                    doc.spans.setdefault(span.label_, []).append(span)
+                else:
+                    doc.spans[group].append(span)
+        if new_ents is not None:
+            doc.ents = filter_spans((*new_ents, *doc.ents))
+    return doc
+
+
+def validate_span_setter(value: Union[SeqStr, Dict[str, SpanFilter]]) -> SpanSetter:
+    if callable(value):
+        return value
+    if isinstance(value, str):
+        return {value: True}
+    if isinstance(value, list):
+        return {group: True for group in value}
+    elif isinstance(value, dict):
+        new_value = {}
+        for k, v in value.items():
+            if isinstance(v, bool):
+                new_value[k] = v
+            elif isinstance(v, str):
+                new_value[k] = [v]
+            elif isinstance(v, list) and all(isinstance(i, str) for i in v):
+                new_value[k] = v
+            else:
+                raise TypeError(
+                    f"Invalid entry {value} ({type(value)}) for SpanSetterArg, "
+                    f"expected bool/string(s), dict of bool/string(s) or callable"
+                )
+        return new_value
+    else:
+        raise TypeError(
+            f"Invalid entry {value} ({type(value)}) for SpanSetterArg, "
+            f"expected bool/string(s), dict of bool/string(s) or callable"
+        )
+
+
+def validate_span_getter(
+    value: Union[SeqStr, Dict[str, SpanFilter]], optional: bool = False
+) -> SpanSetter:
+    if value is None:
+        if optional:
+            return None
+        raise ValueError(
+            "Mising entry for SpanGetterArg, "
+            "expected bool/string(s), dict of bool/string(s) or callable"
+        )
+    if callable(value):
+        return value
+    if isinstance(value, str):
+        return {value: True}
+    if isinstance(value, list):
+        return {group: True for group in value}
+    elif isinstance(value, dict):
+        new_value = {}
+        for k, v in value.items():
+            if isinstance(v, bool):
+                new_value[k] = v
+            elif isinstance(v, str):
+                new_value[k] = [v]
+            elif isinstance(v, list) and all(isinstance(i, str) for i in v):
+                new_value[k] = v
+            else:
+                raise TypeError(
+                    f"Invalid entry {value} ({type(value)}) for SpanGetterArg, "
+                    f"expected bool/string(s), dict of bool/string(s) or callable"
+                )
+        return new_value
+    else:
+        raise TypeError(
+            f"Invalid entry {value} ({type(value)}) for SpanGetterArg, "
+            f"expected bool/string(s), dict of bool/string(s) or callable"
+        )
+
+
+class SpanSetterArg:
+    """
+    Valid values for the `span_setter` argument of a component can be :
+
+    - a (doc, matches) -> None callable
+    - a span group name
+    - a list of span group names
+    - a dict of group name to True or list of labels
+
+    The group name `"ents"` is a special case, and will add the matches to `doc.ents`
+
+    Examples
+    --------
+    - `span_setter=["ents", "ckd"]` will add the matches to both `doc.ents` and
+    `doc.spans["ckd"]`. It is equivalent to `{"ents": True, "ckd": True}`.
+    - `span_setter={"ents": ["foo", "bar"]}` will add the matches with label
+    "foo" and "bar" to `doc.ents`.
+    - `span_setter="ents"` will add all matches only to `doc.ents`.
+    - `span_setter="ckd"` will add all matches only to `doc.spans["ckd"]`.
+    """
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value, config=None) -> SpanSetter:
+        return validate_span_setter(value)
+
+
+class SpanGetterArg:
+    """
+    Valid values for the `span_getter` argument of a component can be :
+
+    - a (doc) -> spans callable
+    - a span group name
+    - a list of span group names
+    - a dict of group name to True or list of labels
+
+    The group name `"ents"` is a special case, and will get the matches from `doc.ents`
+
+    Examples
+    --------
+    - `span_getter=["ents", "ckd"]` will get the matches from both `doc.ents` and
+    `doc.spans["ckd"]`. It is equivalent to `{"ents": True, "ckd": True}`.
+    - `span_getter={"ents": ["foo", "bar"]}` will get the matches with label
+    "foo" and "bar" from `doc.ents`.
+    - `span_getter="ents"` will get all matches from `doc.ents`.
+    - `span_getter="ckd"` will get all matches from `doc.spans["ckd"]`.
+    """
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, value, config=None) -> SpanSetter:
+        return validate_span_setter(value)
+
+
+if TYPE_CHECKING:
+    SpanGetterArg = Union[  # noqa: F811
+        str,
+        Sequence[str],
+        SpanGetterMapping,
+        Callable[[Doc], Iterable[Span]],
+    ]
+    SpanSetterArg = Union[  # noqa: F811
+        str,
+        Sequence[str],
+        SpanSetterMapping,
+        Callable[[Doc, Iterable[Span]], Any],
+    ]
+
+
+def merge_spans(spans: Iterable[Span]) -> List[Span]:
+    """
+    Merge overlapping spans into a single span.
+
+    Parameters
+    ----------
+    spans : List[Span]
+        List of spans to merge.
+    doc : Doc
+        Document to merge the spans on.
+
+    Returns
+    -------
+    List[Span]
+        Merged spans.
+    """
+    spans = sorted(spans, key=lambda x: (x.start, x.end))
+    merged = []
+    for span in spans:
+        if len(merged) and span.start <= merged[-1].end:
+            if span.end > merged[-1].end:
+                merged[-1] = Span(
+                    span.doc,
+                    merged[-1].start,
+                    span.end,
+                    merged[-1].label_,
+                )
+        else:
+            merged.append(span)
+    return merged
+
+
+@registry.misc.register("eds.span_context_getter")
+class make_span_context_getter:
+    """
+    Create a span context getter.
+
+    Parameters
+    ----------
+    span_getter : SpanGetterArg
+        Span getter, i.e. for which spans to get the context.
+    context_words : NonNegativeInt
+        Minimum number of words to include on each side of the span.
+    context_sents : Optional[NonNegativeInt]
+        Minimum number of sentences to include on each side of the span:
+
+        - 0: don't use sentences to build the context.
+        - 1: include the sentence of the span.
+        - n: include n sentences on each side of the span.
+
+        By default, 0 if the document has no sentence annotations, 1 otherwise.
+    overlap_policy : Literal["filter", "merge"]
+        How to handle overlapping spans:
+
+        - "filter": remove overlapping spans.
+        - "merge": merge overlapping spans
+    """
+
+    def __init__(
+        self,
+        span_getter: SpanGetterArg,
+        context_words: NonNegativeInt = 0,
+        context_sents: Optional[NonNegativeInt] = None,
+        overlap_policy: Literal["filter", "merge"] = "merge",
+    ):
+        self.context_words = context_words
+        self.context_sents = context_sents
+        self.overlap_policy = overlap_policy
+        self.span_getter = span_getter
+
+    def __call__(self, doc: Doc) -> List[Span]:
+        n_sents = self.context_sents
+        if n_sents is None:
+            n_sents = 0 if not doc.has_annotation("SENT_START") else 1
+        n_words = self.context_words
+
+        spans = []
+        sents = list(doc.sents) if n_sents > 1 else []
+        for e in get_spans(doc, self.span_getter):
+            min_start_sent = min_start_word = e.start - n_words
+            max_end_sent = max_end_word = e.end + n_words
+
+            if n_sents == 1:
+                sent = e.sent
+                min_start_sent = sent.start
+                max_end_sent = sent.end
+            else:
+                sent_i = sents.index(e.sent)
+                min_start_sent = sents[max(0, sent_i - n_sents)].start
+                max_end_sent = sents[min(len(sents) - 1, sent_i + n_sents)].end
+            start = max(0, min(min_start_word, min_start_sent))
+            end = min(len(doc), max(max_end_word, max_end_sent))
+            spans.append(doc[start:end])
+
+        if self.overlap_policy == "filter":
+            return filter_spans(spans)
+        return merge_spans(spans)
