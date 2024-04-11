b8f7d89dcc2718b2d5a6203cc916d47995c167f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 07:04:47 +0000

    Add package pentapy/1.3.0/pentapy-1.3.0.tar.gz

---
+++ packages/pentapy/pentapy-1.3.0.tar.gz/pentapy-1.3.0/setup.py
@@ -0,0 +1,24 @@
+"""pentapy: A toolbox for pentadiagonal matrizes."""
+
+import os
+
+import numpy as np
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+# cython extensions
+CY_MODULES = [
+    Extension(
+        name=f"pentapy.solver",
+        sources=[os.path.join("src", "pentapy", "solver.pyx")],
+        include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    )
+]
+
+setup(
+    ext_modules=cythonize(CY_MODULES),
+    package_data={"pentapy": ["*.pxd"]},  # include pxd files
+    include_package_data=False,  # ignore other files
+    zip_safe=False,
+)
