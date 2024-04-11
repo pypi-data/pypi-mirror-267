ded72a9f0c8c82ee1e893893f9a78d745b7b015d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:18 +0000

    Add package adpipwfwconst/0.1.3/adpipwfwconst-0.1.3.tar.gz

---
+++ packages/adpipwfwconst/adpipwfwconst-0.1.3.tar.gz/adpipwfwconst-0.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="adpipwfwconst",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=[],
+    author="Oleg D",
+    author_email="olegd@gmail.com",
+    description="Constants for the Adaptive Pipeline Workflow project.",
+    license="MIT",
+    keywords="GenAI, GCP",
+    url="https://github.com/odegay/adaptive-pipeline-constants",            
+)
