7336fa25f998695bb1399bf1f02b203af23d403d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:02:52 +0000

    Add package giant-events/0.3.0.2/giant_events-0.3.0.2.tar.gz

---
+++ packages/giant-events/giant_events-0.3.0.2.tar.gz/giant_events-0.3.0.2/events/tests/conftest.py
@@ -0,0 +1,29 @@
+from django.utils import timezone
+
+import pytest
+
+from events.models import Event
+
+
+@pytest.fixture
+def unpublished_event():
+    _date = timezone.now() - timezone.timedelta(days=1)
+    return Event.objects.create(
+        title="Event One",
+        slug="event-one",
+        start_at=_date,
+        end_at=_date,
+        is_published=False,
+        publish_at=timezone.now() + timezone.timedelta(hours=1),
+    )
+
+
+@pytest.fixture
+def published_event():
+    return Event.objects.create(
+        title="Event Two",
+        slug="event-two",
+        start_at=timezone.now() + timezone.timedelta(days=1),
+        is_published=True,
+        publish_at=timezone.now() - timezone.timedelta(hours=1),
+    )
