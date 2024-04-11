0a133d05eb425e0ca1b11769922895f95f14567f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:48 +0000

    Add package tilearn-infer/0.3.0/tilearn_infer-0.3.0-py3-none-any.whl

---
+++ packages/tilearn-infer/tilearn_infer-0.3.0-py3-none-any.whl/vllm/transformers_utils/configs/__init__.py
@@ -0,0 +1,24 @@
+from vllm.transformers_utils.configs.aquila import AquilaConfig
+from vllm.transformers_utils.configs.baichuan import BaiChuanConfig
+from vllm.transformers_utils.configs.shennong import ShennongConfig
+from vllm.transformers_utils.configs.sn_70b import Shennong70BConfig
+from vllm.transformers_utils.configs.chatglm import ChatGLMConfig
+from vllm.transformers_utils.configs.mpt import MPTConfig
+from vllm.transformers_utils.configs.qwen import QWenConfig
+# RWConfig is for the original tiiuae/falcon-40b(-instruct) and
+# tiiuae/falcon-7b(-instruct) models. Newer Falcon models will use the
+# `FalconConfig` class from the official HuggingFace transformers library.
+from vllm.transformers_utils.configs.falcon import RWConfig
+from vllm.transformers_utils.configs.yi import YiConfig
+
+__all__ = [
+    "AquilaConfig",
+    "BaiChuanConfig",
+    "ShennongConfig",
+    "Shennong70BConfig",
+    "ChatGLMConfig",
+    "MPTConfig",
+    "QWenConfig",
+    "RWConfig",
+    "YiConfig",
+]
