10b662773782bf07a3a7b570a2d5e27a005a3687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:43 +0000

    Add package visualtorch/0.2.2/visualtorch-0.2.2.tar.gz

---
+++ packages/visualtorch/visualtorch-0.2.2.tar.gz/visualtorch-0.2.2/setup.py
@@ -0,0 +1,41 @@
+"""Setup file for visualtorch."""
+
+# Copyright (C) 2024 Willy Fitra Hendria
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+
+import setuptools
+
+file_path = Path("README.md")
+with file_path.open("r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="visualtorch",
+    version="0.2.2",
+    author="Willy Fitra Hendria",
+    author_email="willyfitrahendria@gmail.com",
+    description="Architecture visualization of Torch models",
+    keywords=["visualize architecture", "torch visualization", "visualtorch"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/willyfh/visualtorch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "pillow>=10.0.0",
+        "numpy>=1.18.1",
+        "aggdraw>=1.3.11",
+        "torch>=2.0.0",
+    ],
+    python_requires=">=3.10",
+    license="MIT",
+    license_files=("LICENSE",),
+)
