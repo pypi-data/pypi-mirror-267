bb86cf5a075f10308134d903ff7fde4dec5d3520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:09 +0000

    Add package pyTenable/1.4.22/pyTenable-1.4.22.tar.gz

---
+++ packages/pyTenable/pyTenable-1.4.22.tar.gz/pyTenable-1.4.22/tenable/ot/__init__.py
@@ -0,0 +1,20 @@
+"""
+Tenable OT Security base package
+=======================
+
+This package covers the Tenable OT Security interface.
+
+.. autoclass:: TenableOT
+    :members:
+
+
+.. toctree::
+    :hidden:
+    :glob:
+
+    assets
+    events
+    plugins
+"""
+
+from .session import TenableOT
