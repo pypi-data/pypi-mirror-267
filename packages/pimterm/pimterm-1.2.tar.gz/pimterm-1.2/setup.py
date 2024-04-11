2d7ffa5cc5a650ece55c037374d5db81705476dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 07:04:34 +0000

    Add package pimterm/1.2/pimterm-1.2.tar.gz

---
+++ packages/pimterm/pimterm-1.2.tar.gz/pimterm-1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pimterm",
+    version="1.2",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pimterm=pimterm.main:main',
+        ],
+    },
+    description="A simple Python package that helps you find the command you need.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Pim Verleg",
+    author_email="pim_iets@hotmail.com",
+    license="MIT",
+    install_requires=[
+        'openai',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
