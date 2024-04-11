cf8021f8f65e4174206c1453f1ff306f25feed42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 14:03:57 +0000

    Add package rigging/0.2.1/rigging-0.2.1.tar.gz

---
+++ packages/rigging/rigging-0.2.1.tar.gz/rigging-0.2.1/rigging/__init__.py
@@ -0,0 +1,11 @@
+from rigging.chat import Chat
+from rigging.generator import get_generator
+from rigging.message import Message, MessageDict, Messages
+from rigging.model import Model, attr, element
+from rigging.tool import Tool
+
+__all__ = ["get_generator", "Message", "MessageDict", "Messages", "Tool", "Model", "attr", "element", "Chat"]
+
+from loguru import logger
+
+logger.disable("rigging")
