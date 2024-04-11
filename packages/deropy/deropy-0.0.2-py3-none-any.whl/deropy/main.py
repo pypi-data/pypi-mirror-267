25903d4d20c70c2d0293c50943feec5bcda2991a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:06 +0000

    Add package deropy/0.0.2/deropy-0.0.2-py3-none-any.whl

---
+++ packages/deropy/deropy-0.0.2-py3-none-any.whl/deropy/main.py
@@ -0,0 +1,18 @@
+import click
+
+from deropy.commands.generate import generate
+from deropy.commands.deploy import deploy
+
+
+@click.group('deropy')
+@click.version_option()
+@click.option('--verbose', '-v', is_flag=True, help='Enable verbose logging')
+def deropy(verbose: bool):
+    """deropy is a CLI for deropy."""
+    pass
+
+deropy.add_command(generate)
+deropy.add_command(deploy)
+
+if __name__ == '__main__':
+    deropy()
