151e038ed7745a7aaf24a2ffe55e0dbc5313d8a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 03:03:30 +0000

    Add package smartmeter-datacollector/1.2.1/smartmeter-datacollector-1.2.1.tar.gz

---
+++ packages/smartmeter-datacollector/smartmeter-datacollector-1.2.1.tar.gz/smartmeter-datacollector-1.2.1/smartmeter_datacollector/sinks/mqtt_sink.py
@@ -0,0 +1,173 @@
+#
+# Copyright (C) 2024 Supercomputing Systems AG
+# This file is part of smartmeter-datacollector.
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# See LICENSES/README.md for more information.
+#
+import json
+import logging
+import ssl
+from configparser import SectionProxy
+from dataclasses import dataclass
+from typing import Optional
+
+from asyncio_mqtt import Client
+from asyncio_mqtt.client import ProtocolVersion
+from asyncio_mqtt.error import MqttCodeError, MqttError
+from paho.mqtt.client import MQTT_ERR_NO_CONN
+
+from ..smartmeter.meter_data import MeterDataPoint
+from .data_sink import DataSink
+
+LOGGER = logging.getLogger("sink")
+
+
+# pylint: disable=too-many-instance-attributes
+@dataclass
+class MqttConfig:
+    broker_host: str
+    port: int = 1883
+    use_tls: bool = False
+    username: Optional[str] = None
+    password: Optional[str] = None
+    client_id: Optional[str] = None
+    ca_cert_path: Optional[str] = None
+    check_hostname: bool = True
+    client_cert_path: Optional[str] = None
+    client_key_path: Optional[str] = None
+
+    def with_tls(self, ca_cert_path: Optional[str] = None, check_hostname: bool = True) -> "MqttConfig":
+        self.use_tls = True
+        self.ca_cert_path = ca_cert_path
+        self.check_hostname = check_hostname
+        return self
+
+    def with_user_pass_auth(self, username: str, password: str) -> "MqttConfig":
+        self.username = username
+        self.password = password
+        return self
+
+    def with_client_cert_auth(self, cert_path: str, key_path: str) -> "MqttConfig":
+        self.use_tls = True
+        self.client_cert_path = cert_path
+        self.client_key_path = key_path
+        return self
+
+    @staticmethod
+    def from_sink_config(config: SectionProxy) -> "MqttConfig":
+        mqtt_cfg = MqttConfig(config.get("host"), config.getint("port", 1883))
+        if config.getboolean("tls", fallback=False):
+            mqtt_cfg.with_tls(config.get("ca_file_path"), config.getboolean("check_hostname", True))
+        if config.get("username") and config.get("password"):
+            mqtt_cfg.with_user_pass_auth(
+                config.get("username"),
+                config.get("password"))
+        if config.get("client_cert_path") and config.get("client_key_path"):
+            mqtt_cfg.with_client_cert_auth(
+                config.get("client_cert_path"),
+                config.get("client_key_path")
+            )
+        return mqtt_cfg
+
+
+class MqttDataSink(DataSink):
+    TIMEOUT = 3
+
+    def __init__(self, config: MqttConfig) -> None:
+        tls_context = None
+        if config.use_tls:
+            tls_context = self._build_ssl_context(
+                config.ca_cert_path,
+                config.check_hostname,
+                config.client_cert_path,
+                config.client_key_path)
+
+        user_pass_auth = {}
+        if config.username and config.password:
+            user_pass_auth["username"] = config.username
+            user_pass_auth["password"] = config.password
+
+        self._client = Client(
+            hostname=config.broker_host,
+            port=config.port,
+            client_id=config.client_id,
+            tls_context=tls_context,
+            protocol=ProtocolVersion.V311,
+            clean_session=True,
+            **user_pass_auth)
+
+    @staticmethod
+    def _build_ssl_context(
+        ca_file_path: Optional[str] = None,
+        check_hostname: bool = True,
+        client_cert_path: Optional[str] = None,
+        client_key_path: Optional[str] = None
+    ) -> ssl.SSLContext:
+        context = ssl.create_default_context(cafile=ca_file_path)
+
+        if ca_file_path:
+            context.check_hostname = check_hostname
+
+        if client_cert_path:
+            try:
+                context.load_cert_chain(client_cert_path, client_key_path, None)
+            except ssl.SSLError as ex:
+                LOGGER.error("Client certificate does not match with the key and is ignored. '%s'", ex)
+        return context
+
+    async def start(self) -> None:
+        if await self._connect_to_server():
+            LOGGER.info("Connected to MQTT broker.")
+
+    async def stop(self) -> None:
+        await self._disconnect_from_server()
+        LOGGER.info("Disconnected from MQTT broker.")
+
+    async def send(self, data_point: MeterDataPoint) -> None:
+        topic = MqttDataSink.get_topic_name_for_datapoint(data_point)
+        dp_json = self.data_point_to_mqtt_json(data_point)
+        try:
+            await self._client.publish(topic, dp_json)
+            LOGGER.debug("%s sent to MQTT broker.", dp_json)
+        except ValueError as ex:
+            LOGGER.error("MQTT payload or topic is invalid: '%s'", ex)
+        except MqttCodeError as ex:
+            if ex.rc == MQTT_ERR_NO_CONN:
+                if await self._connect_to_server():
+                    LOGGER.info("Reconnected to MQTT broker.")
+                    try:
+                        await self._client.publish(topic, dp_json)
+                        LOGGER.debug("%s sent to MQTT broker.", dp_json)
+                    except MqttError:
+                        LOGGER.warning("MQTT message not sent.")
+            else:
+                LOGGER.error("MQTT message sending error: '%s'", ex)
+        except MqttError as ex:
+            LOGGER.error("MQTT message sending error: '%s'", ex)
+
+    async def _connect_to_server(self) -> bool:
+        try:
+            await self._client.connect(timeout=self.TIMEOUT)
+        except MqttError as ex:
+            LOGGER.error(ex)
+            return False
+        return True
+
+    async def _disconnect_from_server(self) -> None:
+        try:
+            await self._client.disconnect(timeout=self.TIMEOUT)
+        except MqttError as ex:
+            LOGGER.error(ex)
+            await self._client.force_disconnect()
+
+    @staticmethod
+    def get_topic_name_for_datapoint(data_point: MeterDataPoint) -> str:
+        return f"smartmeter/{data_point.source}/{data_point.type.identifier}"
+
+    @staticmethod
+    def data_point_to_mqtt_json(data_point: MeterDataPoint) -> str:
+        return json.dumps({
+            "value": data_point.value,
+            "timestamp": int(data_point.timestamp.timestamp())
+        })
---
+++ packages/smartmeter-datacollector/smartmeter-datacollector-1.2.1.tar.gz/smartmeter-datacollector-1.2.1/tests/test_hdlc_dlms_parser.py
@@ -0,0 +1,95 @@
+#
+# Copyright (C) 2024 Supercomputing Systems AG
+# This file is part of smartmeter-datacollector.
+#
+# SPDX-License-Identifier: GPL-2.0-only
+# See LICENSES/README.md for more information.
+#
+import random
+import re
+
+from gurux_dlms.objects.GXDLMSObject import GXDLMSObject
+
+from smartmeter_datacollector.smartmeter.cosem import Cosem
+from smartmeter_datacollector.smartmeter.hdlc_dlms_parser import HdlcDlmsParser
+
+from .utils import *
+
+
+class TestHdlcParserUnencrypted:
+    def test_extract_hdlc_data_framewise(self, unencrypted_valid_data_lg: List[bytes]):
+        parser = HdlcDlmsParser(Cosem("", []))
+
+        for frame in unencrypted_valid_data_lg:
+            assert not parser.extract_data_from_hdlc_frames()
+            parser.append_to_hdlc_buffer(frame)
+
+        assert parser.extract_data_from_hdlc_frames()
+
+    def test_extract_hdlc_data_in_halfframes(self, unencrypted_valid_data_lg: List[bytes]):
+        parser = HdlcDlmsParser(Cosem("", []))
+
+        for frame in unencrypted_valid_data_lg:
+            frame: bytes
+            assert not parser.extract_data_from_hdlc_frames()
+            parser.append_to_hdlc_buffer(frame[:int(len(frame)/2)])
+            assert not parser.extract_data_from_hdlc_frames()
+            parser.append_to_hdlc_buffer(frame[int(len(frame)/2):])
+
+        assert parser.extract_data_from_hdlc_frames()
+
+
+class TestDlmsParserUnencrypted:
+    def test_hdlc_to_dlms_objects_with_pushlist(self, unencrypted_valid_data_lg: List[bytes], cosem_config_lg: Cosem):
+        parser = prepare_parser(unencrypted_valid_data_lg, cosem_config_lg)
+        dlms_objects = parser.parse_to_dlms_objects()
+
+        assert isinstance(dlms_objects, list)
+        assert len(dlms_objects) == 16
+        obis_pattern = re.compile(r"(\d+\.){5}\d+")
+        for obj in dlms_objects:
+            assert isinstance(obj, GXDLMSObject)
+            assert re.match(obis_pattern, str(obj.logicalName))
+
+    def test_parse_dlms_to_meter_data(self, unencrypted_valid_data_lg: List[bytes], cosem_config_lg: Cosem):
+        parser = prepare_parser(unencrypted_valid_data_lg, cosem_config_lg)
+        dlms_objects = parser.parse_to_dlms_objects()
+        meter_data = parser.convert_dlms_bundle_to_reader_data(dlms_objects)
+
+        assert isinstance(meter_data, list)
+        assert len(meter_data) == 11
+        assert any(data.type == MeterDataPointTypes.ACTIVE_POWER_P.value for data in meter_data)
+        assert any(data.type == MeterDataPointTypes.ACTIVE_POWER_N.value for data in meter_data)
+        assert all(isinstance(data.value, float) for data in meter_data)
+        assert all(data.source == "LGZ1030655933512" for data in meter_data)
+        assert all(data.timestamp.astimezone().strftime(r"%m/%d/%y %H:%M:%S")
+                   == "07/06/21 14:58:18" for data in meter_data)
+
+    def test_parse_dlms_to_meter_data2(self, unencrypted_valid_data_lg2: List[bytes], cosem_config_lg: Cosem):
+        parser = prepare_parser(unencrypted_valid_data_lg2, cosem_config_lg)
+        dlms_objects = parser.parse_to_dlms_objects()
+        meter_data = parser.convert_dlms_bundle_to_reader_data(dlms_objects)
+
+        assert isinstance(meter_data, list)
+        assert len(meter_data) == 8
+
+    def test_ignore_not_parsable_data_to_meter_data(self, unencrypted_invalid_data_lg: List[bytes], cosem_config_lg: Cosem):
+        parser = prepare_parser(unencrypted_invalid_data_lg, cosem_config_lg)
+        dlms_objects = parser.parse_to_dlms_objects()
+        meter_data = parser.convert_dlms_bundle_to_reader_data(dlms_objects)
+
+        assert isinstance(meter_data, list)
+        assert len(meter_data) == 5
+
+
+class TestDlmsParserEncrypted:
+    def test_hdlc_to_dlms_objects_without_pushlist(self, encrypted_data_no_pushlist_lg: List[bytes], cosem_config_lg: Cosem):
+        parser = prepare_parser(encrypted_data_no_pushlist_lg, cosem_config_lg, "F08660A6C19D2048556BF623AB0257E6")
+        dlms_objects = parser.parse_to_dlms_objects()
+
+        assert isinstance(dlms_objects, list)
+        assert len(dlms_objects) == 16
+        obis_pattern = re.compile(r"(\d+\.){5}\d+")
+        for obj in dlms_objects:
+            assert isinstance(obj, GXDLMSObject)
+            assert re.match(obis_pattern, str(obj.logicalName))
