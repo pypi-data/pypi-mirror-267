b94716d6bfe2bac456ee957674783a3775ebf208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:03:36 +0000

    Add package gybe/0.3.0/gybe-0.3.0-py3-none-any.whl

---
+++ packages/gybe/gybe-0.3.0-py3-none-any.whl/gybe/__init__.py
@@ -0,0 +1,13 @@
+"""A simple YAML transpilation tool for rendering kubernetes manifests"""
+
+__version__ = '0.3.0'
+
+
+from typing import Any, List
+
+from gybe import k8s
+from gybe.decorators import transpiler
+
+Manifest = List[Any]
+
+__all__ = ['k8s', 'Manifest', 'transpiler']
