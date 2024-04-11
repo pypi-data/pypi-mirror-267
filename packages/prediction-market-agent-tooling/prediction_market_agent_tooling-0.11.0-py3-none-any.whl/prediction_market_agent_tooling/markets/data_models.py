2f4dfeb7eee9cb77d696daf0dbb6a41e76c46e36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:58 +0000

    Add package prediction-market-agent-tooling/0.11.0/prediction_market_agent_tooling-0.11.0-py3-none-any.whl

---
+++ packages/prediction-market-agent-tooling/prediction_market_agent_tooling-0.11.0-py3-none-any.whl/prediction_market_agent_tooling/markets/data_models.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from decimal import Decimal
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class Currency(str, Enum):
+    xDai = "xDai"
+    Mana = "Mana"
+    USDC = "USDC"
+
+
+class Resolution(str, Enum):
+    YES = "YES"
+    NO = "NO"
+    CANCEL = "CANCEL"
+    MKT = "MKT"
+
+
+class BetAmount(BaseModel):
+    amount: Decimal
+    currency: Currency
+
+
+class ProfitAmount(BaseModel):
+    amount: Decimal
+    currency: Currency
+
+
+class Bet(BaseModel):
+    amount: BetAmount
+    outcome: bool
+    created_time: datetime
+    market_question: str
+
+
+class ResolvedBet(Bet):
+    market_outcome: bool
+    resolved_time: datetime
+    profit: ProfitAmount
+
+    @property
+    def is_correct(self) -> bool:
+        return self.outcome == self.market_outcome
