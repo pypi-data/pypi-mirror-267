6522a848af272ce1dccce2358911a32ea924b415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 14:03:48 +0000

    Add package aio-overpass/0.13.0/aio_overpass-0.13.0-py3-none-any.whl

---
+++ packages/aio-overpass/aio_overpass-0.13.0-py3-none-any.whl/aio_overpass/__init__.py
@@ -0,0 +1,28 @@
+"""Async client for the Overpass API."""
+
+import importlib.metadata
+
+
+__version__: str = importlib.metadata.version("aio-overpass")
+
+# we add this to all modules for pdoc;
+# see https://pdoc.dev/docs/pdoc.html#use-numpydoc-or-google-docstrings
+__docformat__ = "google"
+
+# we also use __all__ in all modules for pdoc; this lets us control the order
+__all__ = (
+    "Client",
+    "ClientError",
+    "Query",
+    "client",
+    "element",  # pyright: ignore[reportUnsupportedDunderAll]
+    "error",
+    "pt",  # pyright: ignore[reportUnsupportedDunderAll]
+    "pt_ordered",  # pyright: ignore[reportUnsupportedDunderAll]
+    "ql",  # pyright: ignore[reportUnsupportedDunderAll]
+    "query",
+)
+
+from .client import Client
+from .error import ClientError
+from .query import Query
