008c45f04d776d1d2e8894288001b008819be91f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 14:03:56 +0000

    Add package TineAutomationToolkit/1.0.4/TineAutomationToolkit-1.0.4.tar.gz

---
+++ packages/TineAutomationToolkit/TineAutomationToolkit-1.0.4.tar.gz/TineAutomationToolkit-1.0.4/TineAutomationToolkit/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from TineAutomationToolkit.keywords import *
+
+
+class TineAutomationToolkit(
+    ToolkitsTest,
+    WaitingElement,
+    ConnectionManagement,
+    ControlElement,
+    Scroll,
+    CaptureScreenShot
+    ):
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        print("This Main นี้คือ เมน")
