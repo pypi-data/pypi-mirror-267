acd629a762d5364ade3f765f72f1d20a4f46c10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:03:31 +0000

    Add package fal/0.14.0/fal-0.14.0.tar.gz

---
+++ packages/fal/fal-0.14.0.tar.gz/fal-0.14.0/src/fal/_serialization.py
@@ -0,0 +1,201 @@
+from __future__ import annotations
+
+from functools import wraps
+from pathlib import Path
+
+import dill
+from dill import _dill
+
+from fal.toolkit import mainify
+
+# each @fal.function gets added to this set so that we can
+# mainify the module this function is in
+_MODULES: set[str] = set()
+_PACKAGES: set[str] = set()
+
+
+@mainify
+def _pydantic_make_field(kwargs):
+    from pydantic.fields import ModelField
+
+    return ModelField(**kwargs)
+
+
+@mainify
+def _pydantic_make_private_field(kwargs):
+    from pydantic.fields import ModelPrivateAttr
+
+    return ModelPrivateAttr(**kwargs)
+
+
+# this allows us to record all the "isolated" function and then mainify everything in
+# module they exist
+@wraps(_dill._locate_function)
+def by_value_locator(obj, pickler=None, og_locator=_dill._locate_function):
+    module_name = getattr(obj, "__module__", None)
+    if module_name is not None:
+        # If it is coming from the same module, directly allow
+        # it to be pickled.
+        if module_name in _MODULES:
+            return False
+
+        package_name, *_ = module_name.partition(".")
+        # If it is coming from the same package, then do the same.
+        if package_name in _PACKAGES:
+            return False
+
+    og_result = og_locator(obj, pickler)
+    return og_result
+
+
+_dill._locate_function = by_value_locator
+
+
+def include_packages_from_path(raw_path: str):
+    path = Path(raw_path).resolve()
+    parent = path
+    while (parent.parent / "__init__.py").exists():
+        parent = parent.parent
+
+    if parent != path:
+        _PACKAGES.add(parent.name)
+
+
+def add_serialization_listeners_for(obj):
+    module_name = getattr(obj, "__module__", None)
+    if not module_name:
+        return None
+
+    _MODULES.add(module_name)
+    if module_name == "__main__":
+        # When the module is __main__, we need to recursively go up the
+        # tree to locate the actual package name.
+        import __main__
+
+        include_packages_from_path(__main__.__file__)
+
+    if "." in module_name:
+        package_name, *_ = module_name.partition(".")
+        _PACKAGES.add(package_name)
+
+
+@mainify
+def patch_pydantic_field_serialization():
+    # Cythonized pydantic fields can't be serialized automatically, so we are
+    # have a special case handling for them that unpacks it to a dictionary
+    # and then reloads it on the other side.
+    import dill
+
+    try:
+        import pydantic.fields
+    except ImportError:
+        return
+
+    @dill.register(pydantic.fields.ModelField)
+    def _pickle_model_field(
+        pickler: dill.Pickler,
+        field: pydantic.fields.ModelField,
+    ) -> None:
+        args = {
+            "name": field.name,
+            # outer_type_ is the original type for ModelFields,
+            # while type_ can be updated later with the nested type
+            # like int for List[int].
+            "type_": field.outer_type_,
+            "class_validators": field.class_validators,
+            "model_config": field.model_config,
+            "default": field.default,
+            "default_factory": field.default_factory,
+            "required": field.required,
+            "alias": field.alias,
+            "field_info": field.field_info,
+        }
+        pickler.save_reduce(_pydantic_make_field, (args,), obj=field)
+
+    @dill.register(pydantic.fields.ModelPrivateAttr)
+    def _pickle_model_private_attr(
+        pickler: dill.Pickler,
+        field: pydantic.fields.ModelPrivateAttr,
+    ) -> None:
+        args = {
+            "default": field.default,
+            "default_factory": field.default_factory,
+        }
+        pickler.save_reduce(_pydantic_make_private_field, (args,), obj=field)
+
+
+@mainify
+def patch_pydantic_class_attributes():
+    # Dill attempts to modify the __class__ of deserialized pydantic objects
+    # on this side but it meets with a rejection from pydantic's semantics since
+    # __class__ is not recognized as a proper dunder attribute.
+    try:
+        import pydantic.utils
+    except ImportError:
+        return
+
+    pydantic.utils.DUNDER_ATTRIBUTES.add("__class__")
+
+
+@mainify
+def patch_exceptions():
+    # Adapting tblib.pickling_support.install for dill.
+    from types import TracebackType
+
+    import dill
+    from tblib.pickling_support import (
+        _get_subclasses,
+        pickle_exception,
+        pickle_traceback,
+    )
+
+    @dill.register(TracebackType)
+    def save_traceback(pickler, obj):
+        unpickle, args = pickle_traceback(obj)
+        pickler.save_reduce(unpickle, args, obj=obj)
+
+    @dill.register(BaseException)
+    def save_exception(pickler, obj):
+        unpickle, args = pickle_exception(obj)
+        pickler.save_reduce(unpickle, args, obj=obj)
+
+    for exception_cls in _get_subclasses(BaseException):
+        dill.pickle(exception_cls, save_exception)
+
+
+@mainify
+def _patch_console_thread_locals() -> None:
+    # NOTE: we __sometimes__ might have to serialize these
+    from rich.console import ConsoleThreadLocals
+
+    @dill.register(ConsoleThreadLocals)
+    def save_console_thread_locals(pickler, obj):
+        args = {
+            "theme_stack": obj.theme_stack,
+            "buffer": obj.buffer,
+            "buffer_index": obj.buffer_index,
+        }
+
+        def unpickle(kwargs):
+            return ConsoleThreadLocals(**kwargs)
+
+        pickler.save_reduce(unpickle, (args,), obj=obj)
+
+
+@mainify
+def patch_dill():
+    import dill
+
+    dill.settings["recurse"] = True
+
+    patch_exceptions()
+    patch_pydantic_class_attributes()
+    patch_pydantic_field_serialization()
+    _patch_console_thread_locals()
+
+
+@mainify
+def patch_pickle():
+    from tblib import pickling_support
+
+    pickling_support.install()
---
+++ packages/fal/fal-0.14.0.tar.gz/fal-0.14.0/src/fal/toolkit/file/file.py
@@ -0,0 +1,194 @@
+from __future__ import annotations
+
+from pathlib import Path
+from tempfile import NamedTemporaryFile, TemporaryDirectory
+from typing import Any, Callable
+from urllib.parse import urlparse
+from zipfile import ZipFile
+
+from pydantic import BaseModel, Field, PrivateAttr
+from pydantic.typing import Optional
+
+from fal.toolkit.file.providers.fal import (
+    FalCDNFileRepository,
+    FalFileRepository,
+    InMemoryRepository,
+)
+from fal.toolkit.file.providers.gcp import GoogleStorageRepository
+from fal.toolkit.file.providers.r2 import R2Repository
+from fal.toolkit.file.types import FileData, FileRepository, RepositoryId
+from fal.toolkit.mainify import mainify
+from fal.toolkit.utils.download_utils import download_file
+
+FileRepositoryFactory = Callable[[], FileRepository]
+
+BUILT_IN_REPOSITORIES: dict[RepositoryId, FileRepositoryFactory] = {
+    "fal": lambda: FalFileRepository(),
+    "in_memory": lambda: InMemoryRepository(),
+    "gcp_storage": lambda: GoogleStorageRepository(),
+    "r2": lambda: R2Repository(),
+    "cdn": lambda: FalCDNFileRepository(),
+}
+
+
+def get_builtin_repository(id: RepositoryId) -> FileRepository:
+    if id not in BUILT_IN_REPOSITORIES.keys():
+        raise ValueError(f'"{id}" is not a valid built-in file repository')
+    return BUILT_IN_REPOSITORIES[id]()
+
+
+get_builtin_repository.__module__ = "__main__"
+
+DEFAULT_REPOSITORY: FileRepository | RepositoryId = "fal"
+
+
+@mainify
+class File(BaseModel):
+    # public properties
+    _file_data: FileData = PrivateAttr()
+    url: str = Field(
+        description="The URL where the file can be downloaded from.",
+    )
+    content_type: Optional[str] = Field(
+        description="The mime type of the file.",
+        examples=["image/png"],
+    )
+    file_name: Optional[str] = Field(
+        description="The name of the file. It will be auto-generated if not provided.",
+        examples=["z9RV14K95DvU.png"],
+    )
+    file_size: Optional[int] = Field(
+        description="The size of the file in bytes.", examples=[4404019]
+    )
+
+    def __init__(self, **kwargs):
+        if "file_data" in kwargs:
+            data: FileData = kwargs.pop("file_data")
+            repository = kwargs.pop("repository", None)
+
+            repo = (
+                repository
+                if isinstance(repository, FileRepository)
+                else get_builtin_repository(repository)
+            )
+            self._file_data = data
+
+            kwargs.update(
+                {
+                    "url": repo.save(data),
+                    "content_type": data.content_type,
+                    "file_name": data.file_name,
+                    "file_size": len(data.data),
+                }
+            )
+
+        super().__init__(**kwargs)
+
+    # Pydantic custom validator for input type conversion
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.__convert_from_str
+
+    @classmethod
+    def __convert_from_str(cls, value: Any):
+        if isinstance(value, str):
+            parsed_url = urlparse(value)
+            if parsed_url.scheme not in ["http", "https", "data"]:
+                raise ValueError("value must be a valid URL")
+            return cls._from_url(parsed_url.geturl())
+
+        return value
+
+    @classmethod
+    def _from_url(
+        cls,
+        url: str,
+    ) -> File:
+        return cls(
+            url=url,
+            content_type=None,
+            file_name=None,
+            repository=DEFAULT_REPOSITORY,
+        )
+
+    @classmethod
+    def from_bytes(
+        cls,
+        data: bytes,
+        content_type: Optional[str] = None,
+        file_name: Optional[str] = None,
+        repository: FileRepository | RepositoryId = DEFAULT_REPOSITORY,
+    ) -> File:
+        return cls(
+            file_data=FileData(data, content_type, file_name),
+            repository=repository,
+        )
+
+    @classmethod
+    def from_path(
+        cls,
+        path: str | Path,
+        content_type: Optional[str] = None,
+        repository: FileRepository | RepositoryId = DEFAULT_REPOSITORY,
+    ) -> File:
+        file_path = Path(path)
+        if not file_path.exists():
+            raise FileNotFoundError(f"File {file_path} does not exist")
+        with open(file_path, "rb") as f:
+            data = f.read()
+        return File.from_bytes(
+            data, content_type, file_name=file_path.name, repository=repository
+        )
+
+    def as_bytes(self) -> bytes:
+        if getattr(self, "_file_data", None) is None:
+            raise ValueError("File has not been downloaded")
+
+        return self._file_data.data
+
+    def save(self, path: str | Path, overwrite: bool = False) -> Path:
+        file_path = Path(path).resolve()
+
+        if file_path.exists() and not overwrite:
+            raise FileExistsError(f"File {file_path} already exists")
+
+        downloaded_path = download_file(self.url, target_dir=file_path.parent)
+        downloaded_path.rename(file_path)
+
+        return file_path
+
+
+@mainify
+class CompressedFile(File):
+    _extract_dir: Optional[TemporaryDirectory] = PrivateAttr(default=None)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._extract_dir = None
+
+    def __iter__(self):
+        if not self._extract_dir:
+            self._extract_files()
+
+        files = Path(self._extract_dir.name).iterdir()  # type: ignore
+        return iter(files)
+
+    def _extract_files(self):
+        self._extract_dir = TemporaryDirectory()
+
+        with NamedTemporaryFile() as temp_file:
+            file_path = temp_file.name
+            self.save(file_path, overwrite=True)
+
+            with ZipFile(file_path) as zip_file:
+                zip_file.extractall(self._extract_dir.name)
+
+    def glob(self, pattern: str):
+        if not self._extract_dir:
+            self._extract_files()
+
+        return Path(self._extract_dir.name).glob(pattern)  # type: ignore
+
+    def __del__(self):
+        if self._extract_dir:
+            self._extract_dir.cleanup()
