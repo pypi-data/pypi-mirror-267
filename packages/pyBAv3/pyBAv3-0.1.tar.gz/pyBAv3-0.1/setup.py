66ec22db88d64e1aa22d3cd027a13c59de8d5503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:13 +0000

    Add package pyBAv3/0.1/pyBAv3-0.1.tar.gz

---
+++ packages/pyBAv3/pyBAv3-0.1.tar.gz/pyBAv3-0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.1'
+DESCRIPTION = 'BAv2 is simple personalised  chatbot for every user and developer'
+long_description = 'BAv2 is simple personalised chatbot made for developers and users'
+
+# Setting up
+setup(
+    name="pyBAv3",
+    version=VERSION,
+    author="srinadhch07",
+    author_email="<srinadhc07@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['chatbot','ai','python','pyBAv'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ]
+)
