93ea9493b5fd2b70d7810f4e14c531ccc17b276e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:04:12 +0000

    Add package avania/0.0.4/avania-0.0.4.tar.gz

---
+++ packages/avania/avania-0.0.4.tar.gz/avania-0.0.4/avania/__init__.py
@@ -0,0 +1,1 @@
+from .orm.mysqli import mysqli
---
+++ packages/avania/avania-0.0.4.tar.gz/avania-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='avania',
+    version='0.0.4',
+    description='Pays tribute to Laravel',
+    long_description_content_type='text/markdown',
+    long_description=open('CONTRIBUTING.md').read(),
+    author='Simon',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+    ],
+)
