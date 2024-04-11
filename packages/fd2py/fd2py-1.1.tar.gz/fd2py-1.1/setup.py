37a74c4412b87cd9e678128a694ce93aa5681bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:05:03 +0000

    Add package fd2py/1.1/fd2py-1.1.tar.gz

---
+++ packages/fd2py/fd2py-1.1.tar.gz/fd2py-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='fd2py',
+    version='1.1',
+    scripts=['fd2py.py'],
+    author='Ming Gw',
+    author_email='2775218480@qq.com',
+    description='This script can convert the txt file of the session saved by the Fidder to Python requests',
+    url='',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+    ],
+    py_modules=['fd2py']
+)
