efb8409eb0904633980a585b08c59427231d930a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:05:07 +0000

    Add package Unreal-Engine-Packer/1.2/Unreal-Engine-Packer-1.2.tar.gz

---
+++ packages/Unreal-Engine-Packer/Unreal-Engine-Packer-1.2.tar.gz/Unreal-Engine-Packer-1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Unreal-Engine-Packer',
+    version='1.2',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'uepacker=Source.UePack:main'
+        ]
+    },
+    install_requires=[
+    ],
+    author='R00tDroid',
+    description='A commandline tool to automatically package Unreal Engine projects or plugins',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/R00tDroid/UnrealPacker',
+    license='MIT',
+)
