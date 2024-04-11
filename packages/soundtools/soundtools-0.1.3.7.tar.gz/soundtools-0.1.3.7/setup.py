25e9fcda922a268ad1322b431616756030cc5e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:19 +0000

    Add package soundtools/0.1.3.7/soundtools-0.1.3.7.tar.gz

---
+++ packages/soundtools/soundtools-0.1.3.7.tar.gz/soundtools-0.1.3.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='soundtools',
+version='0.1.3.7',
+author='Mohammad Erfan Karami',
+author_email='erfan012amir016@gmail.com',
+description="used to work with sounds waves",
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
