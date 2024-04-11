4dc9f33659e9f80015c51763cc93b7eac8b184b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:04:02 +0000

    Add package pixeloe/0.0.8/pixeloe-0.0.8.tar.gz

---
+++ packages/pixeloe/pixeloe-0.0.8.tar.gz/pixeloe-0.0.8/pixeloe/utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+
+@torch.no_grad()
+def apply_chunk(data, kernel, stride, func):
+    org_shape = data.shape
+    unfold_shape = org_shape
+    k_shift = max(kernel - stride, 0) // 2
+
+    data = np.pad(data, ((k_shift, k_shift), (k_shift, k_shift)), "edge")
+    if len(org_shape) == 2:
+        data = data[np.newaxis, np.newaxis, ...]
+
+    data = (
+        F.unfold(torch.tensor(data), kernel, 1, 0, stride).transpose(-1, -2)[0].numpy()
+    )
+    data[..., : stride**2] = func(data)
+    data = data[np.newaxis, ..., : stride**2]
+    data = F.fold(
+        torch.tensor(data).transpose(-1, -2),
+        unfold_shape,
+        stride,
+        1,
+        0,
+        stride,
+    )[0].numpy()
+
+    if len(org_shape) < 3:
+        data = data[0]
+    return data
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def isiterable(x):
+    return hasattr(x, "__iter__")
