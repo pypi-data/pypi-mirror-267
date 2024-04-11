061b72433f64fccca1dc44e413a21bfd729176ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:04:48 +0000

    Add package robotframework-xlibrary/10.0.1/robotframework-xlibrary-10.0.1.tar.gz

---
+++ packages/robotframework-xlibrary/robotframework-xlibrary-10.0.1.tar.gz/robotframework-xlibrary-10.0.1/src/XLibrary/__init__.py
@@ -0,0 +1,47 @@
+#XLibrary/__init__.py
+# -*- coding: utf-8 -*-
+from robot.api.deco import keyword
+from .main import MainClass
+from .submodule1 import XPrint
+from .submodule1 import XDrint
+from .submodule2 import Module21
+from .submodule2 import Module22
+
+
+ 
+__all__ = [ 'XLibrary','MainClass', 'XPrint', 'XDrint','Module21', 'Module22']
+
+
+
+class XLibrary(
+    MainClass,
+    XPrint, 
+    XDrint,
+    Module21,
+    Module22
+    ):
+    """ XLibrary เป็น Library Custom ขึ้้นมาใช้เองโดยเฉพาะ 
+
+
+
+        | Click Element |    Key            |
+        | Click Element |    Text="Text"    |
+
+        ตัวกำหนดตำแหน่งที่สามารถใช้ได้:
+
+        | *Locator*    | *Description*                      |
+        | key          | key ของอิลิเมนต์ FlutterDriver.       |
+        | text         | ข้อความของอิลิเมนต์.                   |
+
+    """
+
+
+    def TTTTTT(self):
+        """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล แบบไม่ได้แอดคีย์ """
+        print("Hello, world! เทสภาษาไทยfdsafadsfsadfdsafdsa")
+
+
+    @keyword("Hello world12")
+    def Hello12(self):
+        """ พิมพ์ข้อความ 'Hello, world!' ลงในคอนโซล แบบแอดคีย์ """
+        print("Hello, world! Test keyword='Hello, world!'")
