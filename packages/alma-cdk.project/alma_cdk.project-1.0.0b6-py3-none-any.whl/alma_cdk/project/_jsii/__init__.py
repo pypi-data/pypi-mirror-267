acad548434cc4b2f4c3c4a47dea5ffbf36568d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:28 +0000

    Add package alma-cdk.project/1.0.0b6/alma_cdk.project-1.0.0b6-py3-none-any.whl

---
+++ packages/alma-cdk.project/alma_cdk.project-1.0.0b6-py3-none-any.whl/alma_cdk/project/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@alma-cdk/project",
+    "1.0.0-beta.6",
+    __name__[0:-6],
+    "project@1.0.0-beta.6.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
