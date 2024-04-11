c659e53062fce3adce4e057ee3f22bd87f96e2ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 07:04:05 +0000

    Add package snapshooter/0.1.29/snapshooter-0.1.29.tar.gz

---
+++ packages/snapshooter/snapshooter-0.1.29.tar.gz/snapshooter-0.1.29/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="snapshooter",
+    version="0.1.29",
+    author="jeromerg",
+    author_email="jeromerg@gmx.net",
+    description="Provides a set of utilities for comparing and backing up data on different filesystems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jeromerg/snapshooter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'fsspec',
+        'pandas',
+        'tabulate',
+        'typer[all]',
+
+    ],
+    entry_points={
+        'console_scripts': [
+            'snapshooter=snapshooter.cli:main_cli',
+        ],
+    },
+    python_requires='>=3.10',
+)
