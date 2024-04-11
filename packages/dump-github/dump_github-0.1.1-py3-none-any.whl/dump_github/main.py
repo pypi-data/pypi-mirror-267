0df79ce43151d4dda06f083a51e92dab3d57e5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 06:03:53 +0000

    Add package dump-github/0.1.1/dump_github-0.1.1-py3-none-any.whl

---
+++ packages/dump-github/dump_github-0.1.1-py3-none-any.whl/dump_github/main.py
@@ -0,0 +1,53 @@
+import argparse
+import os
+from .lib import *
+
+# Config_MaxSize = 1_000
+
+parser = argparse.ArgumentParser(
+    prog="dump_github",
+    description="Backup users github repo.",
+    epilog="https://github.com/Core2002/dump_github",
+)
+
+parser.add_argument("username")
+parser.add_argument(
+    "-d", "--download_zip", action="store_true", help="only download zip file"
+)
+parser.add_argument("-p", "--print", action="store_true", help="only print urls")
+parser.add_argument("-t", "--token", action="store", default="", help="github token")
+
+args = parser.parse_args()
+
+
+def clone_repo(repo):
+    name = repo["name"]
+    url = repo["clone_url"]
+    size = repo["size"]
+    # if int(size) > Config_MaxSize:
+    #     print(f"Keep repo [{name}], Becues size is too Big ({int(size)}).")
+    #     return
+    print("Clone repo: {}".format(name))
+    os.system("git clone {}".format(url))
+
+
+def download_zip(user_name, reop_name):
+    url = f"https://github.com/{user_name}/{reop_name}/archive/refs/heads/main.zip"
+    print("Downloading repo zip: {}".format(reop_name))
+    os.system("wget {} -O ./{}_{}.zip".format(url, user_name, reop_name))
+
+
+def main():
+    # print(args)
+    repos = serach_user_repos(args.username, args.token)
+    for repo in repos:
+        if args.print == True:
+            print(repo["clone_url"])
+        elif args.download_zip == True:
+            download_zip(user_name, repo["name"])
+        else:
+            clone_repo(repo)
+
+
+if __name__ == "__main__":
+    main()
