16e262e76ca804abdd9a0f4161dff69b1622a9a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:50 +0000

    Add package pytorch-widedeep/1.5.1/pytorch-widedeep-1.5.1.tar.gz

---
+++ packages/pytorch-widedeep/pytorch-widedeep-1.5.1.tar.gz/pytorch-widedeep-1.5.1/pytorch_widedeep/models/__init__.py
@@ -0,0 +1,24 @@
+from pytorch_widedeep.models.text import (
+    BasicRNN,
+    Transformer,
+    AttentiveRNN,
+    StackedAttentiveRNN,
+)
+from pytorch_widedeep.models.image import Vision
+from pytorch_widedeep.models.tabular import (
+    SAINT,
+    Wide,
+    TabMlp,
+    TabNet,
+    TabResnet,
+    TabPerceiver,
+    FTTransformer,
+    TabFastFormer,
+    TabMlpDecoder,
+    TabNetDecoder,
+    TabTransformer,
+    SelfAttentionMLP,
+    TabResnetDecoder,
+    ContextAttentionMLP,
+)
+from pytorch_widedeep.models.wide_deep import WideDeep
