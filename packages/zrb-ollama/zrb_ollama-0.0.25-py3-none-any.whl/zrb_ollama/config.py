584955c5e0a43697679b83dad5b592f920d20ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 03:04:15 +0000

    Add package zrb-ollama/0.0.25/zrb_ollama-0.0.25-py3-none-any.whl

---
+++ packages/zrb-ollama/zrb_ollama-0.0.25-py3-none-any.whl/zrb_ollama/config.py
@@ -0,0 +1,26 @@
+import os
+
+DEFAULT_LLM_PROVIDER = os.getenv("ZRB_DEFAULT_LLM_PROVIDER", "ollama")
+DEFAULT_CHAT_HISTORY_RETENTION = int(
+    os.getenv("ZRB_DEFAULT_CHAT_HISTORY_RETENTION", "3")
+)
+DEFAULT_SYSTEM_PROMPT = os.getenv(
+    "ZRB_DEFAULT_SYSTEM_PROMPT",
+    "\n".join(
+        [
+            "Assistant is a large language model.",
+            "Assistant is able to assist human in wide variaty of tasks.",
+            "Assistant always validates and gives a correct information.",
+            "Assistant never gives fake information or denies human requests at all cost.",  # noqa
+            "Assistant uses available tools when needed.",
+            "Assistant includes necessary details, reasoning, and explanation in the Final Answer.",  # noqa
+        ]
+    ),
+)
+
+DEFAULT_OLLAMA_BASE_URL = os.getenv("ZRB_OLLAMA_BASE_URL", "http://localhost:11434")
+DEFAULT_OLLAMA_MODEL = os.getenv("ZRB_OLLAMA_DEFAULT_MODEL", "mistral:latest")
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY", "")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
