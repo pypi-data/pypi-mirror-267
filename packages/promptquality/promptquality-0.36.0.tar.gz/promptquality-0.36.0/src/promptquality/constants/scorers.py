c0f7b386d13ff25b6922a7cea2157d06655c6f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 06:04:03 +0000

    Add package promptquality/0.36.0/promptquality-0.36.0.tar.gz

---
+++ packages/promptquality/promptquality-0.36.0.tar.gz/promptquality-0.36.0/src/promptquality/constants/scorers.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+
+class Scorers(str, Enum):
+    toxicity = "toxicity"
+    factuality = "factuality"
+    correctness = "factuality"
+    groundedness = "groundedness"
+    context_adherence = "groundedness"
+    pii = "pii"
+    latency = "latency"
+    context_relevance = "context_relevance"
+    sexist = "sexist"
+    tone = "tone"
+    prompt_perplexity = "prompt_perplexity"
+    chunk_attribution_utilization_gpt = "chunk_attribution_utilization_gpt"
+    completeness_gpt = "completeness_gpt"
+    prompt_injection = "prompt_injection"
