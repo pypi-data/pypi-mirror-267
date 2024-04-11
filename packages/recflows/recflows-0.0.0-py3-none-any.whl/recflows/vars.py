b45a07c00e99bd354f61908029fbf771b4f2f15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 06:04:04 +0000

    Add package recflows/0.0.0/recflows-0.0.0-py3-none-any.whl

---
+++ packages/recflows/recflows-0.0.0-py3-none-any.whl/recflows/vars.py
@@ -0,0 +1,7 @@
+import os
+
+HOST = os.environ.get("DATABASE_HOST")
+PORT = os.environ.get("DATABASE_PORT")
+USER = os.environ.get("DATABASE_USER")
+PASSWORD = os.environ.get("DATABASE_PASSWORD")
+DATABASE = os.environ.get("DATABASE_DB")
