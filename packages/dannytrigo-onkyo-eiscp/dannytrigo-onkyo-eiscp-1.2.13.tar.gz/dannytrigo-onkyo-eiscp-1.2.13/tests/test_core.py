3cefc14e594eed756ae7dbf93df369274e16c135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:03:29 +0000

    Add package dannytrigo-onkyo-eiscp/1.2.13/dannytrigo-onkyo-eiscp-1.2.13.tar.gz

---
+++ packages/dannytrigo-onkyo-eiscp/dannytrigo-onkyo-eiscp-1.2.13.tar.gz/dannytrigo-onkyo-eiscp-1.2.13/tests/test_core.py
@@ -0,0 +1,24 @@
+from eiscp.core import iscp_to_command, command_to_iscp
+from unittest import TestCase
+
+class TestIscpToCommand(TestCase):
+    def test(self):
+        self.assertEqual(('master-volume', ''), iscp_to_command('MVL'))
+
+    def test_with_zone(self):
+        self.assertEqual(('main', 'audio-muting', ''), iscp_to_command('AMT', True))
+        self.assertEqual(('zone2', 'muting', ''), iscp_to_command('ZMT', True))
+        self.assertEqual(('zone3', 'muting', ''), iscp_to_command('MT3', True))
+        self.assertEqual(('zone4', 'muting', ''), iscp_to_command('MT4', True))
+
+class TestCommandToIscp(TestCase):
+    def test(self):
+        self.assertEqual('PWR00', command_to_iscp('main.system-power=standby'))
+
+    def test_argument_aliases(self):
+        self.assertEqual('PWR00', command_to_iscp('main.system-power=off'))
+
+    def test_argument_with_space(self):
+        self.assertEqual('NTCTRUP', command_to_iscp('network-usb', 'trup', 'dock'))
+        self.assertEqual('NTCTRUP', command_to_iscp('dock.network-usb=trup'))
+        self.assertEqual('NTCTRUP', command_to_iscp('dock.network-usb trup'))
