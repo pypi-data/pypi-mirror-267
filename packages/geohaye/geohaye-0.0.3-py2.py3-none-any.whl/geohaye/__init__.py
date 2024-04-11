b3a26b98eb780b6613dbf3c4203faf4b39f87f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:04:23 +0000

    Add package geohaye/0.0.3/geohaye-0.0.3-py2.py3-none-any.whl

---
+++ packages/geohaye/geohaye-0.0.3-py2.py3-none-any.whl/geohaye/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for geohaye."""
+
+__author__ = """Abdillahi Osman Omar"""
+__email__ = "abdillahiosmanomar@student.utwente.nl"
+__version__ = "0.0.3"
+
+from .geohaye import Map
---
+++ packages/geohaye/geohaye-0.0.3-py2.py3-none-any.whl/geohaye/geohaye.py
@@ -0,0 +1,232 @@
+"""Main module."""
+
+import ipyleaflet
+from ipyleaflet import basemaps
+import ipywidgets as widgets
+
+
+class Map(ipyleaflet.Map):
+    """This is the map class that inherits from ipyleaflet.Map.
+
+    Args:
+        ipyleaflet (Map): The ipyleaflet.Map class.
+    """
+
+    def __init__(self, center=[20, 0], zoom=2, **kwargs):
+        """Initialize the map.
+
+        Args:
+            center (list, optional): Set the center of the map. Defaults to [20, 0].
+            zoom (int, optional): Set the zoom level of the map. Defaults to 2.
+        """
+
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+
+        if "add_layer_control" not in kwargs:
+            layer_control_flag = True
+        else:
+            layer_control_flag = kwargs["add_layer_control"]
+        kwargs.pop("add_layer_control", None)
+
+        super().__init__(center=center, zoom=zoom, **kwargs)
+        if layer_control_flag:
+            self.add_layers_control()
+
+    def add_tile_layer(self, url, name, **kwargs):
+        layer = ipyleaflet.TileLayer(url=url, name=name, **kwargs)
+        self.add(layer)
+
+    def add_basemap(self, name):
+        """
+        Adds a basemap to the current map.
+
+        Args:
+            name (str or object): The name of the basemap as a string, or an object representing the basemap.
+
+        Raises:
+            TypeError: If the name is neither a string nor an object representing a basemap.
+
+        Returns:
+            None
+        """
+
+        if isinstance(name, str):
+            url = eval(f"basemaps.{name}").build_url()
+            self.add_tile_layer(url, name)
+        else:
+            self.add(name)
+
+    def add_layers_control(self, position="topright"):
+        """Adds a layers control to the map.
+
+        Args:
+            position (str, optional): The position of the layers control. Defaults to "topright".
+        """
+        self.add_control(ipyleaflet.LayersControl(position=position))
+
+    def add_geojson(self, data, name="geojson", **kwargs):
+        """Adds a GeoJSON layer to the map.
+
+        Args:
+            data (str | dict): The GeoJSON data as a string or a dictionary.
+            name (str, optional): The name of the layer. Defaults to "geojson".
+        """
+        import json
+
+        if isinstance(data, str):
+            with open(data) as f:
+                data = json.load(f)
+
+        if "style" not in kwargs:
+            kwargs["style"] = {"color": "blue", "weight": 1, "fillOpacity": 0}
+
+        if "hover_style" not in kwargs:
+            kwargs["hover_style"] = {"fillColor": "#ff0000", "fillOpacity": 0.5}
+
+        layer = ipyleaflet.GeoJSON(data=data, name=name, **kwargs)
+        self.add(layer)
+
+    def add_shp(self, data, name="shp", **kwargs):
+        """
+        Adds a shapefile to the current map.
+
+        Args:
+            data (str or dict): The path to the shapefile as a string, or a dictionary representing the shapefile.
+            name (str, optional): The name of the layer. Defaults to "shp".
+            **kwargs: Arbitrary keyword arguments.
+
+        Raises:
+            TypeError: If the data is neither a string nor a dictionary representing a shapefile.
+
+        Returns:
+            None
+        """
+        import shapefile
+        import json
+
+        if isinstance(data, str):
+            with shapefile.Reader(data) as shp:
+                data = shp.__geo_interface__
+
+        self.add_geojson(data, name, **kwargs)
+
+    def add_image(self, url, bounds, name="image", **kwargs):
+        """Adds an image overlay to the map.
+
+        Args:
+            url (str): The URL of the image.
+            bounds (list): The bounds of the image.
+            name (str, optional): The name of the layer. Defaults to "image".
+        """
+        layer = ipyleaflet.ImageOverlay(url=url, bounds=bounds, name=name, **kwargs)
+        self.add(layer)
+
+    def add_raster(self, data, name="raster", zoom_to_layer=True, **kwargs):
+        """Adds a raster layer to the map.
+
+        Args:
+            data (str): The path to the raster file.
+            name (str, optional): The name of the layer. Defaults to "raster".
+        """
+
+        try:
+            from localtileserver import TileClient, get_leaflet_tile_layer
+        except ImportError:
+            raise ImportError("Please install the localtileserver package.")
+
+        client = TileClient(data)
+        layer = get_leaflet_tile_layer(client, name=name, **kwargs)
+        self.add(layer)
+
+        if zoom_to_layer:
+            self.center = client.center()
+            self.zoom = client.default_zoom
+
+    def add_zoom_slider(
+        self, description="Zoom level", min=0, max=24, value=10, position="topright"
+    ):
+        """Adds a zoom slider to the map.
+
+        Args:
+            position (str, optional): The position of the zoom slider. Defaults to "topright".
+        """
+        zoom_slider = widgets.IntSlider(
+            description=description, min=min, max=max, value=value
+        )
+
+        control = ipyleaflet.WidgetControl(widget=zoom_slider, position=position)
+        self.add(control)
+        widgets.jslink((zoom_slider, "value"), (self, "zoom"))
+    
+    def add_widget(self, widget, position="topright"):
+        """Adds a widget to the map.
+
+        Args:
+            widget (object): The widget to be added.
+            position (str, optional): The position of the widget. Defaults to "topright".
+        """
+        control = ipyleaflet.WidgetControl(widget=widget, position=position)
+        self.add(control)
+    
+    def add_opacity_slider(
+        self, layer_index=-1, description="Opacity", position="topright"
+    ):
+        """Adds an opacity slider to the map.
+
+        Args:
+            layer (object): The layer to which the opacity slider is added.
+            description (str, optional): The description of the opacity slider. Defaults to "Opacity".
+            position (str, optional): The position of the opacity slider. Defaults to "topright".
+        """
+        layer = self.layers[layer_index]
+        opacity_slider = widgets.FloatSlider(
+            description=description,
+            min=0,
+            max=1,
+            value=layer.opacity,
+            style={"description_width": "initial"},
+        )
+
+        def update_opacity(change):
+            layer.opacity = change["new"]
+
+        opacity_slider.observe(update_opacity, "value")
+
+        control = ipyleaflet.WidgetControl(widget=opacity_slider, position=position)
+        self.add(control)
+
+    def add_basemap_gui(self, basemaps=None, position="topright"):
+        """Adds a basemap GUI to the map.
+
+        Args:
+            position (str, optional): The position of the basemap GUI. Defaults to "topright".
+        """
+
+        basemap_selector = widgets.Dropdown(
+            options=[
+                "OpenStreetMap",
+                "OpenTopoMap",
+                "Esri.WorldImagery",
+                "Esri.NatGeoWorldMap",
+            ],
+            description="Basemap",
+        )
+
+        def update_basemap(change):
+            self.add_basemap(change["new"])
+
+        basemap_selector.observe(update_basemap, "value")
+
+        control = ipyleaflet.WidgetControl(widget=basemap_selector, position=position)
+        self.add(control)
+
+
+
+# Package is under development
+# Omar Abdillahi (c) 2024
+# MIT License
+
+
+
+
