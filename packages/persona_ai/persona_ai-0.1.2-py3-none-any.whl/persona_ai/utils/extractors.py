66fff4f49a09126bd5b579c2c121d8bc043dcb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:03:53 +0000

    Add package persona_ai/0.1.2/persona_ai-0.1.2-py3-none-any.whl

---
+++ packages/persona_ai/persona_ai-0.1.2-py3-none-any.whl/persona_ai/utils/extractors.py
@@ -0,0 +1,28 @@
+import re
+
+
+def extract_json(data: str) -> str:
+    """Extract JSON delimited from ```json ... ``` from a string."""
+
+    data = data.strip()
+    if data.startswith("{") and data.endswith("}"):
+        return data
+
+    json = re.search(r"```json(.*?)```", data, re.DOTALL)
+    if json:
+        return json.group(1)
+
+
+def extract_json_object(data: str) -> str:
+    """Extract JSON object delimited a string."""
+
+    start_index = data.find("{")
+    end_index = data.rfind("}") + 1
+    return data[start_index:end_index]
+
+
+def extract_python_code(data: str) -> str:
+    """Extract Python code delimited from ```python ... ``` from a string."""
+    python = re.search(r"```python(.*?)```", data, re.DOTALL)
+    if python:
+        return python.group(1)
