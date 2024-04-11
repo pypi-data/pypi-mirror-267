8aa7c69e6b46211c525f788ad864c5ac886d732b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 14:03:52 +0000

    Add package ogicg/0.0.1/ogicg-0.0.1-py3-none-any.whl

---
+++ packages/ogicg/ogicg-0.0.1-py3-none-any.whl/library/runner.py
@@ -0,0 +1,19 @@
+import library.utils
+from library.app.input_manager import start
+
+
+"""
+To run the terminal application, make sure your terminal is in the root directory of the project. 
+In that directory, run 'python -m runner'. This runs this file as a module of the entire project, allowing imports to 
+function properly.
+"""
+
+
+def main():
+    library.utils.clear()
+    print('Welcome to the Ovative Group Caption Generator!\n')
+    start()
+
+
+if __name__ == '__main__':
+    main()
