63274646487c879ed5d8b37f7789f5d7bdb49b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 07:04:56 +0000

    Add package chai-cli/0.2.1/chai-cli-0.2.1.tar.gz

---
+++ packages/chai-cli/chai-cli-0.2.1.tar.gz/chai-cli-0.2.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='chai-cli',
+    version='0.2.1',
+    author='Ritik Sahni',
+    author_email='ritiksahni0203@gmail.com',
+    description='AI in your CLI.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['ai', 'openai', 'cli'],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'openai',
+        'argparse'
+    ],
+    entry_points={
+        'console_scripts': [
+            'chai = chai:main',
+        ]
+    }
+)
+
