3897ab126e6171fc050fbd2f3ae25149978c54b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:09 +0000

    Add package streamlit-screen-stats/0.0.31/streamlit_screen_stats-0.0.31-py3-none-any.whl

---
+++ packages/streamlit-screen-stats/streamlit_screen_stats-0.0.31-py3-none-any.whl/st_screen_stats/__init__.py
@@ -0,0 +1,10 @@
+IS_RELEASE = True 
+
+from .ScreenData import ScreenData
+from .ScreenStWidgets import StreamlitNativeWidgetScreen
+from .WindowScreenQuery import WindowQuerySize
+from .SizeRange import WindowScreenRange
+
+
+
+pip install -U streamlit-screen-stats-
