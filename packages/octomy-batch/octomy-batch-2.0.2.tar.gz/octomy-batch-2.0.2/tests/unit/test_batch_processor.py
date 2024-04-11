63686a0ef6d9e62a9e7edc4a7215ec1e42ea6936
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 14:04:20 +0000

    Add package octomy-batch/2.0.2/octomy-batch-2.0.2.tar.gz

---
+++ packages/octomy-batch/octomy-batch-2.0.2.tar.gz/octomy-batch-2.0.2/tests/unit/test_batch_processor.py
@@ -0,0 +1,11 @@
+import logging
+
+# from fk.batch.BatchProcessor import BatchProcessor
+
+logger = logging.getLogger(__name__)
+
+
+def _test_batch_processor():
+    config = {}
+    bp = BatchProcessor(config)
+    return True
