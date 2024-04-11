aa07e718bb91a31d724d83de13a5ae65e68b9695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:04:32 +0000

    Add package plugin-config-exceptions/1.2.5/plugin_config_exceptions-1.2.5.tar.gz

---
+++ packages/plugin-config-exceptions/plugin_config_exceptions-1.2.5.tar.gz/plugin_config_exceptions-1.2.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='plugin_config_exceptions',
+version='1.2.5',
+author='Jaymz In Funnel Suit',
+author_email='jaymz@kommunpoet.se',
+description='More poetry to the people! One can not live on water but one can live on poetry (if there is also food and also water).',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
