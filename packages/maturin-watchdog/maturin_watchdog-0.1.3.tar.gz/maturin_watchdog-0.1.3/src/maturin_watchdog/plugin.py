5c1fb06f48ccbb057ae5f857451a91b4aef33494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:26 +0000

    Add package maturin-watchdog/0.1.3/maturin_watchdog-0.1.3.tar.gz

---
+++ packages/maturin-watchdog/maturin_watchdog-0.1.3.tar.gz/maturin_watchdog-0.1.3/src/maturin_watchdog/plugin.py
@@ -0,0 +1,36 @@
+from poetry.console.application import Application
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+from cleo.events.console_events import COMMAND
+from cleo.events.console_command_event import ConsoleCommandEvent
+from cleo.events.event_dispatcher import EventDispatcher
+from dotenv import load_dotenv
+from poetry.console.application import Application
+from poetry.console.commands.env_command import EnvCommand
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+
+class PoetryPlugin(ApplicationPlugin):
+    def activate(self, application: Application):
+        application.event_dispatcher.add_listener(
+            COMMAND, self.load_dotenv
+        )
+
+    def load_dotenv(
+        self,
+        event: ConsoleCommandEvent,
+        event_name: str,
+        dispatcher: EventDispatcher
+    ) -> None:
+        command = event.command
+        if not isinstance(command, EnvCommand):
+            return
+
+        io = event.io
+        print("Loading environment variables.")
+        if io.is_debug():
+            io.write_line(
+                "<debug>Loading environment variables.</debug>"
+            )
+
+        load_dotenv()
