cc694053051647e34a9a2e619befb72cfa16d878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 03:03:14 +0000

    Add package nmbrs/0.1.3/nmbrs-0.1.3.tar.gz

---
+++ packages/nmbrs/nmbrs-0.1.3.tar.gz/nmbrs-0.1.3/src/nmbrs/data_classes/employee.py
@@ -0,0 +1,202 @@
+"""This module defines the Employee class, a subclass of DataClass, representing an employee entity."""
+
+from datetime import datetime
+from decimal import Decimal
+
+from .data_class import DataClass
+
+
+class Employee(DataClass):
+    """A class representing an employee."""
+
+    def __init__(self, data: dict):
+        self.id: int = data.get("Id")
+        self.number: int = data.get("Number")
+        self.name: str = data.get("DisplayName")
+
+
+class EmployeeTypes(DataClass):
+    """A class representing an employee type."""
+
+    def __init__(self, data: dict):
+        self.id: int = data.get("Id")
+        self.description: str = data.get("Description")
+
+
+class Period(DataClass):
+    """A class representing a period of a company."""
+
+    def __init__(self, employee_id: int, data: str):
+        parts = data.split("-")
+        self.employee_id = employee_id
+        self.year: int = int(parts[0])
+        self.period: int = int(parts[1])
+        self.type: str = parts[2]
+
+
+class Contract(DataClass):
+    """A class representing a contract."""
+
+    def __init__(self, employee_id: int, data: dict):
+        self.employee_id = employee_id
+        self.contract_id: int = data.get("ContractID")
+        self.creation_date: datetime = data.get("CreationDate")
+        self.start_date: datetime = data.get("StartDate")
+        self.trial_period: datetime = data.get("TrialPeriod")
+        self.end_date: datetime = data.get("EndDate")
+        self.employment_type: int = data.get("EmployementType")
+        self.employment_sequence_tax_id: int = data.get("EmploymentSequenceTaxId")
+        self.indefinite: bool = data.get("Indefinite")
+        self.phase_classification: int = data.get("PhaseClassification")
+        self.written_contract: bool = data.get("WrittenContract")
+        self.hours_per_week: Decimal = data.get("HoursPerWeek")
+
+
+class Schedule(DataClass):
+    """A class representing a schedule."""
+
+    def __init__(self, employee_id: int, data: dict):
+        self.employee_id = employee_id
+        self.schedule_calc_method: str = data.get("ScheduleCalcMethod")
+        self.hours_monday: Decimal = data.get("HoursMonday")
+        self.hours_tuesday: Decimal = data.get("HoursTuesday")
+        self.hours_wednesday: Decimal = data.get("HoursWednesday")
+        self.hours_thursday: Decimal = data.get("HoursThursday")
+        self.hours_friday: Decimal = data.get("HoursFriday")
+        self.hours_saturday: Decimal = data.get("HoursSaturday")
+        self.hours_sunday: Decimal = data.get("HoursSunday")
+        self.hours_monday2: Decimal = data.get("HoursMonday2")
+        self.hours_tuesday2: Decimal = data.get("HoursTuesday2")
+        self.hours_wednesday2: Decimal = data.get("HoursWednesday2")
+        self.hours_thursday2: Decimal = data.get("HoursThursday2")
+        self.hours_friday2: Decimal = data.get("HoursFriday2")
+        self.hours_saturday2: Decimal = data.get("HoursSaturday2")
+        self.hours_sunday2: Decimal = data.get("HoursSunday2")
+
+
+class PersonalInfo(DataClass):
+    """A class representing an employee's personal information."""
+
+    def __init__(self, employee_id: int, data: dict):
+        self.employee_id = employee_id
+        self.employee_number: int = data.get("EmployeeNumber")
+        self.bsn: str = data.get("BSN")
+        self.title: str = data.get("Title")
+        self.first_name: str = data.get("FirstName")
+        self.initials: str = data.get("Initials")
+        self.prefix: str = data.get("Prefix")
+        self.last_name: str = data.get("LastName")
+        self.nickname: str = data.get("Nickname")
+        self.gender: str = data.get("Gender")
+        self.nationality_code: int = data.get("NationalityCode")
+        self.place_of_birth: str = data.get("PlaceOfBirth")
+        self.country_of_birth_iso_code: str = data.get("CountryOfBirthISOCode")
+        self.identification_number: str = data.get("IdentificationNumber")
+        self.identification_type: int = data.get("IdentificationType")
+        self.partner_prefix: str = data.get("PartnerPrefix")
+        self.partner_last_name: str = data.get("PartnerLastName")
+        self.telephone_private: str = data.get("TelephonePrivate")
+        self.telephone_work: str = data.get("TelephoneWork")
+        self.telephone_mobile_private: str = data.get("TelephoneMobilePrivate")
+        self.telephone_mobile_work: str = data.get("TelephoneMobileWork")
+        self.telephone_other: str = data.get("TelephoneOther")
+        self.email_private: str = data.get("EmailPrivate")
+        self.email_work: str = data.get("EmailWork")
+        self.burgerlijke_staat: str = data.get("BurgerlijkeStaat")
+        self.naamstelling: str = data.get("Naamstelling")
+        self.birthday: datetime = data.get("Birthday")
+        self.deceased_date: datetime = data.get("DeceasedDate")
+        self.in_case_of_emergency: str = data.get("InCaseOfEmergency")
+        self.in_case_of_emergency_phone: str = data.get("InCaseOfEmergencyPhone")
+        self.in_case_of_emergency_relation: str = data.get("InCaseOfEmergencyRelation")
+        self.title_after: str = data.get("TitleAfter")
+
+
+class PersonalInfoContractSalaryAddress(DataClass):
+    """A class representing personal, contract, salary, and address information."""
+
+    def __init__(self, employee_id: int, data: dict):
+        self.employee_id: int = employee_id
+        self.employee_number: int = data.get("EmployeeNumber")
+        self.first_name: str = data.get("FirstName")
+        self.birthday: datetime = data.get("Birthday")
+        self.prefix: str = data.get("Prefix")
+        self.last_name: str = data.get("LastName")
+        self.gender: str = data.get("Gender")
+        self.bsn: str = data.get("BSN")
+        self.city: str = data.get("City")
+        self.telephone_work: str = data.get("TelephoneWork")
+        self.telephone_mobile_work: str = data.get("TelephoneMobileWork")
+        self.email_work: str = data.get("EmailWork")
+        self.contract_start_date: datetime = data.get("ContractStartDate")
+        self.contract_end_date: datetime = data.get("ContractEndDate")
+        self.email_private: str = data.get("EmailPrivate")
+        self.telephone_private: str = data.get("TelephonePrivate")
+        self.telephone_mobile_private: str = data.get("TelephoneMobilePrivate")
+        self.salary_type: str = data.get("SalaryType")
+        self.salary_value: Decimal = data.get("SalaryValue")
+        self.street: str = data.get("Street")
+        self.house_number: str = data.get("HouseNumber")
+        self.house_number_addition: str = data.get("HouseNumberAddition")
+        self.post_code: str = data.get("PostCode")
+        self.hourly_wage: Decimal = data.get("HourlyWage")
+        self.contract_hours: Decimal = data.get("ContractHours")
+
+
+class Absence(DataClass):
+    """A class representing absence"""
+
+    def __init__(self, employee_id: int, data: dict):
+        self.employee_id: int = employee_id
+        self.id: int = data.get("AbsenceId")
+        self.comment: str = data.get("Comment")
+        self.percentage: int = data.get("Percentage")
+        self.start: datetime = data.get("Start")
+        self.registration_start_date: datetime = data.get("RegistrationStartDate")
+        self.end: datetime = data.get("End")
+        self.registration_end_date: datetime = data.get("RegistrationEndDate")
+        self.dossier: str = data.get("Dossier")
+        self.dossier_number: int = data.get("Dossiernr")
+        self.cause: AbsenceCause = AbsenceCause(data.get("AbsenceCause"))
+
+
+class AbsenceCause(DataClass):
+    """A class representing the cause of an absence"""
+
+    def __init__(self, data: dict | None):
+        if data is None:
+            return
+        self.id: int = data.get("CauseId")
+        self.cause: str = data.get("Cause")
+
+
+class Address(DataClass):
+    """A class representing an address"""
+
+    def __init__(self, employee_id: int, data: dict):
+        self.employee_id: int = employee_id
+        self.id: int = data.get("Id")
+        self.default: bool = data.get("Default")
+        self.street: str = data.get("Street")
+        self.house_number: str = data.get("HouseNumber")
+        self.house_number_addition: str = data.get("HouseNumberAddition")
+        self.postcode: str = data.get("PostalCode")
+        self.city: str = data.get("City")
+        self.state_province: str = data.get("StateProvince")
+        self.country_iso_code: str = data.get("CountryISOCode")
+        self.type: str = data.get("Type")
+
+
+class BankAccount(DataClass):
+    """A class representing a bank account."""
+
+    def __init__(self, employee_id: int, data: dict) -> None:
+        self.employee_id = employee_id
+        self.id: int = data.get("Id")
+        self.number: str = data.get("Number")
+        self.description: str = data.get("Description")
+        self.iban: str = data.get("IBAN")
+        self.bic: str = data.get("BIC")
+        self.city: str = data.get("City")
+        self.name: str = data.get("Name")
+        self.type: str = data.get("Type")
---
+++ packages/nmbrs/nmbrs-0.1.3.tar.gz/nmbrs-0.1.3/src/nmbrs/service/microservices/employee/contract.py
@@ -0,0 +1,103 @@
+# pylint: disable=line-too-long
+"""Microservice responsible for contract related actions on the employee level."""
+
+from zeep import Client
+from zeep.helpers import serialize_object
+
+from ..micro_service import MicroService
+from ....data_classes.employee import Contract
+from ....utils.nmbrs_exception_handler import nmbrs_exception_handler
+from ....utils.return_list import return_list
+
+
+class EmployeeContractService(MicroService):
+    """Microservice responsible for contract related actions on the employee level."""
+
+    def __init__(self, client: Client) -> None:
+        super().__init__(client)
+
+    def set_auth_header(self, auth_header: dict) -> None:
+        self.auth_header = auth_header
+
+    @nmbrs_exception_handler(resource="EmployeeService:Contract_GetAll")
+    def get_all(self):
+        """
+        Get all contracts for the specified employee.
+
+        For more information, refer to the official documentation:
+            [Contract_GetAll](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Contract_GetAll)
+        """
+        raise NotImplementedError()  # pragma: no cover
+
+    @nmbrs_exception_handler(resource="EmployeeService:Contract_GetCurrentPeriod")
+    def get_current(self):
+        """
+        Get a list of all active contracts for specified employee in current period.
+
+        For more information, refer to the official documentation:
+            [Contract_GetCurrentPeriod](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Contract_GetCurrentPeriod)
+        """
+        raise NotImplementedError()  # pragma: no cover
+
+    @return_list
+    @nmbrs_exception_handler(resource="EmployeeService:Contract_GetAll_AllEmployeesByCompany")
+    def get_all_by_company(self, company_id: int) -> list[Contract]:
+        """
+        Get all contracts of all employees.
+
+        For more information, refer to the official documentation:
+            [Contract_GetAll_AllEmployeesByCompany](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Contract_GetAll_AllEmployeesByCompany)
+
+        Args:
+            company_id (int): The ID of the company.
+
+        Returns:
+            list[Contract]: a list of contract objects
+        """
+        contracts = self.client.service.Contract_GetAll_AllEmployeesByCompany(CompanyID=company_id, _soapheaders=self.auth_header)
+        contracts = serialize_object(contracts)
+        _contracts = []
+        for employee in contracts:
+            for contract in employee["EmployeeContracts"]["EmployeeContract"]:
+                _contracts.append(Contract(employee_id=employee["EmployeeId"], data=contract))
+        return _contracts
+
+    @nmbrs_exception_handler(resource="EmployeeService:Contract_Delete")
+    def delete(self):
+        """
+        Delete a contract from the system. This action can not be undone.
+
+        For more information, refer to the official documentation:
+            [Contract_Delete](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Contract_Delete)
+        """
+        raise NotImplementedError()  # pragma: no cover
+
+    @nmbrs_exception_handler(resource="EmployeeService:Contract_Update")
+    def update(self):
+        """
+        Update the specified contract for specified employee. Contract start date can’t be updated, this field will be ignored.
+
+        For more information, refer to the official documentation:
+            [Contract_Update](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Contract_Update)
+        """
+        raise NotImplementedError()  # pragma: no cover
+
+    @nmbrs_exception_handler(resource="EmployeeService:Contract_Insert")
+    def insert(self):
+        """
+        Insert Contract. If the start date is before the company's current period, unprotected mode flag is required.
+
+        For more information, refer to the official documentation:
+            [Contract_Insert](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Contract_Insert)
+        """
+        raise NotImplementedError()  # pragma: no cover
+
+    @nmbrs_exception_handler(resource="EmployeeService:Contract_InsertCurrentPeriod")
+    def insert_current(self):
+        """
+        Insert Contract in current period for specified employee.
+
+        For more information, refer to the official documentation:
+            [Contract_InsertCurrentPeriod](https://api.nmbrs.nl/soap/v3/EmployeeService.asmx?op=Contract_InsertCurrentPeriod)
+        """
+        raise NotImplementedError()  # pragma: no cover
