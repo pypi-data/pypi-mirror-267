d07b37cbf23060195da7e9a773dc82182a65cf3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:32 +0000

    Add package mozilla-metric-config-parser/2024.4.1/mozilla_metric_config_parser-2024.4.1-py2.py3-none-any.whl

---
+++ packages/mozilla-metric-config-parser/mozilla_metric_config_parser-2024.4.1-py2.py3-none-any.whl/metric_config_parser/util.py
@@ -0,0 +1,27 @@
+import shutil
+import tempfile
+from contextlib import contextmanager
+from datetime import datetime
+from pathlib import Path
+from typing import Optional
+
+import cattr
+import pytz
+
+converter = cattr.Converter()
+
+
+@contextmanager
+def TemporaryDirectory():
+    name = Path(tempfile.mkdtemp())
+    try:
+        yield name
+    finally:
+        if name.exists():
+            shutil.rmtree(name)
+
+
+def parse_date(yyyy_mm_dd: Optional[str]) -> Optional[datetime]:
+    if not yyyy_mm_dd:
+        return None
+    return datetime.strptime(yyyy_mm_dd, "%Y-%m-%d").replace(tzinfo=pytz.utc)
