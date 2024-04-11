946b54027d29e98b122a8de658c56b148d9a1186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 14:03:54 +0000

    Add package abstra/2.8.0/abstra-2.8.0-py3-none-any.whl

---
+++ packages/abstra/abstra-2.8.0-py3-none-any.whl/abstra_internals/server/apps/cloud.py
@@ -0,0 +1,45 @@
+import flask
+import flask_cors
+import flask_talisman
+from flask_talisman import ALLOW_FROM
+
+from ...cloud_init import cloud_setup
+from ...logger import AbstraLogger
+from ...utils.environment import ENABLE_TALISMAN
+from ..blueprints.player import get_player_bp
+from ..controller.main import MainController
+
+
+def get_cloud_app(root: str):
+    AbstraLogger.init("cloud")
+
+    cloud_setup(root)
+
+    controller = MainController()
+
+    app = flask.Flask(__name__)
+    flask_cors.CORS(app)
+
+    if ENABLE_TALISMAN:
+        talisman = flask_talisman.Talisman(
+            app,
+            content_security_policy={"frame-ancestors": ["*"]},
+            frame_options=ALLOW_FROM,
+            frame_options_allow_from="*",
+        )
+
+        @app.route("/_healthcheck")
+        @talisman(force_https=False)
+        def _healthcheck():
+            return "ok"
+
+    else:
+
+        @app.route("/_healthcheck")
+        def _healthcheck():
+            return "ok"
+
+    player = get_player_bp(controller)
+    app.register_blueprint(player)
+
+    return app
---
+++ packages/abstra/abstra-2.8.0-py3-none-any.whl/abstra_internals/utils/environment.py
@@ -0,0 +1,33 @@
+import os
+
+HOST = os.getenv("ABSTRA_HOST", "localhost")
+DEFAULT_PORT = os.getenv("PORT") or os.getenv("ABSTRA_SERVER_PORT")
+ENABLE_TALISMAN = os.getenv("ABSTRA_ENABLE_TALISMAN", "false").lower() == "true"
+
+BUILD_ID = os.getenv("ABSTRA_BUILD_ID") or "dev"
+PROJECT_ID = os.getenv("ABSTRA_PROJECT_ID") or "dev-project-id"
+
+IS_PRODUCTION = os.getenv("ABSTRA_ENVIRONMENT") == "production"
+SHOW_WATERMARK = os.getenv("ABSTRA_SHOW_WATERMARK", "false") == "true"
+
+CLOUD_API_ENDPOINT = os.getenv("CLOUD_API_ENDPOINT") or "https://cloud-api.abstra.cloud"
+CLOUD_API_CLI_URL = f"{CLOUD_API_ENDPOINT}/cli"
+
+PUBLIC_KEY = os.getenv("ABSTRA_JWT_PUBLIC_KEY_PEM")
+FILES_FOLDER = os.getenv("ABSTRA_FILES_FOLDER")
+
+# gunicorn.conf.py imports this variables, do not move them
+SIDECAR_SHARED_TOKEN = os.getenv("ABSTRA_SIDECAR_SHARED_TOKEN", "shared")
+SIDECAR_HEADERS = {"shared-token": SIDECAR_SHARED_TOKEN}
+SIDECAR_URL = os.getenv("ABSTRA_SIDECAR_URL")
+
+
+# gunicorn.conf.py imports this functions, do not move them
+def WORKER_UUID():
+    # Set during runtime
+    return os.getenv("ABSTRA_WORKER_UUID")
+
+
+def SERVER_UUID():
+    # Set during runtime
+    return os.getenv("ABSTRA_SERVER_UUID")
