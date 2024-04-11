0c537a8b6c541bc9551fdeaa1d5e39784845660a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:03:54 +0000

    Add package bec-server/2.0.1/bec-server-2.0.1.tar.gz

---
+++ packages/bec-server/bec-server-2.0.1.tar.gz/bec-server-2.0.1/bec_server/scihub/cli/launch.py
@@ -0,0 +1,30 @@
+import argparse
+import threading
+
+from bec_lib import RedisConnector, ServiceConfig, bec_logger
+from bec_server import scihub
+
+logger = bec_logger.logger
+bec_logger.level = bec_logger.LOGLEVEL.INFO
+
+
+def main():
+    """
+    Launch the SciHub connector.
+    """
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--config", default="", help="path to the config file")
+    clargs = parser.parse_args()
+    config_path = clargs.config
+
+    config = ServiceConfig(config_path)
+
+    sh = scihub.SciHub(config, RedisConnector)
+
+    try:
+        event = threading.Event()
+        logger.success("Started SciHub connector")
+        event.wait()
+    except KeyboardInterrupt:
+        sh.shutdown()
