414321a3931e04630111ba696d20045b3883bb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 03:03:58 +0000

    Add package IPYNBrenderer033/0.0.2/IPYNBrenderer033-0.0.2.tar.gz

---
+++ packages/IPYNBrenderer033/IPYNBrenderer033-0.0.2.tar.gz/IPYNBrenderer033-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.2"
+
+REPO_NAME = "IPYNBrenderer033"
+AUTHOR_USER_NAME = "Dharmateja033"
+SRC_REPO = "IPYNBrenderer033"
+AUTHOR_EMAIL = "dharmateja.2660@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package",
+    Long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "bugTracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
