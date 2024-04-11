b4eb7e28d62a549aec01a21cfa2e4412b73245f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:17 +0000

    Add package avaris/0.1.4/avaris-0.1.4.tar.gz

---
+++ packages/avaris/avaris-0.1.4.tar.gz/avaris-0.1.4/src/avaris/config/error.py
@@ -0,0 +1,12 @@
+class ConfigError(Exception):
+    """Base class for configuration-related exceptions."""
+
+    pass
+
+
+class NotCompendiumFileError(ConfigError):
+    """Exception raised when the 'compendium' key is missing in the configuration."""
+
+    def __init__(self, message="No 'compendium' key found in YAML."):
+        self.message = message
+        super().__init__(self.message)
