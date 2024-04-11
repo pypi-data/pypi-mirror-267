b0854c3e69dfcc315301d02b22639abcb2cfc534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 06:04:04 +0000

    Add package byma/0.1.3/byma-0.1.3-py3-none-any.whl

---
+++ packages/byma/byma-0.1.3-py3-none-any.whl/byma/interface/__init__.py
@@ -0,0 +1,13 @@
+"""
+BaseInterface Module
+====================
+
+This module provides a base interface for defining default classes and options. It includes functionality for setting default parameters, updating interface options, and retrieving updated options.
+
+"""
+
+
+from .BaseInterface import BaseInterface
+from .NonlinearHeat import NonlinearHeat
+
+__all__ = ['BaseInterface', 'NonlinearHeat']
