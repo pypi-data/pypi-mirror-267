5a60bb1bf6b3226678fea63076e683f07ea8137e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 07:04:40 +0000

    Add package netsome/0.3.0/netsome-0.3.0.tar.gz

---
+++ packages/netsome/netsome-0.3.0.tar.gz/netsome-0.3.0/netsome/types/vlans.py
@@ -0,0 +1,32 @@
+import typing as t
+
+from netsome import constants as c
+from netsome.validators import vlans as validators
+
+
+class VID:
+    """VLAN ID"""
+
+    _RESERVED = {c.ZERO, c.DEFAULT_VID, c.VID_MAX}
+
+    def __init__(self, vid: int) -> None:
+        validators.validate_vid(vid)
+        self._vid = vid
+
+    def __eq__(self, other: t.Any) -> bool:
+        return isinstance(other, self.__class__) and (self._vid == other._vid)
+
+    def __lt__(self, other: t.Any) -> bool:
+        return isinstance(other, self.__class__) and (self._vid < other._vid)
+
+    def __hash__(self) -> int:
+        return hash(self._vid)
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self._vid})"
+
+    def is_reserved(self) -> bool:
+        return self._vid in self._RESERVED
+
+    def is_default(self) -> bool:
+        return self._vid == c.DEFAULT_VID
