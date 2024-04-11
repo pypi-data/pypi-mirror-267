0123ae192009c394c0ff4f31bb80b86b11453812
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:23 +0000

    Add package monedadigitalf/1.6.43.post1/monedadigitalf-1.6.43.post1.tar.gz

---
+++ packages/monedadigitalf/monedadigitalf-1.6.43.post1.tar.gz/monedadigitalf-1.6.43.post1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+version = '1.6.43r1'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="monedadigitalf",
+    version=version,
+
+    author="Gehtsoft USA, LLC",
+    author_email="contact@gehtsoftusa.com",
+
+    url="https://github.com/AmadoRamos/forexconect.git",
+    download_url=f"https://github.com/AmadoRamos/forexconect/archive/refs/tags/v{version}.zip",
+
+    description="ForexConnect API is a trading API for the FXCM Group: https://www.fxcm.com/uk/",
+
+    packages=['forexconnect'],
+    install_requires=[],
+
+    license='Other/Proprietary License',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+    include_package_data=True, # for MANIFEST.in
+    python_requires='>=3.6.0',
+
+    package_data={package: ["py.typed", "*.pyi", "**/*.pyi"] for package in find_packages()},
+    zip_safe=False,
+)
