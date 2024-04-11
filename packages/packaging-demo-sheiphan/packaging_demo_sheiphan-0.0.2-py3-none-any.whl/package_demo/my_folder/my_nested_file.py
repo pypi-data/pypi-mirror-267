be93e553597ab179fa2dcecffd7d4e8e27d50f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:06 +0000

    Add package packaging-demo-sheiphan/0.0.2/packaging_demo_sheiphan-0.0.2-py3-none-any.whl

---
+++ packages/packaging-demo-sheiphan/packaging_demo_sheiphan-0.0.2-py3-none-any.whl/package_demo/my_folder/my_nested_file.py
@@ -0,0 +1,10 @@
+from package_demo.my_other_file import (
+    CONSTANT,
+)
+
+CONSTANT2 = "YOO"
+print(CONSTANT)
+
+# import sys
+# sys.path.insert(0, "/home/sheiphan/packaging")
+# print(sys.path)
