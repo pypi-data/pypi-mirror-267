a9d8b984d9b1ffeb151b243aca34916c667cf516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 18:03:36 +0000

    Add package ToolsStr/1.0.0/ToolsStr-1.0.0.tar.gz

---
+++ packages/ToolsStr/ToolsStr-1.0.0.tar.gz/ToolsStr-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="ToolsStr", 
+        version='1.0.0',
+        author="Amirmahdi Mohammadi",
+        author_email="amirmahdi21r21@gmail.com",
+        description='A small package for keyboard typing in Python',
+        long_description='Keyboard Typing . . .\n\nMy GitHub page: https://gitHub.com/FrameworkPython',
+        packages=find_packages(),
+        
+        install_requires=[], 
+        
+        keywords=['python', 'type', 'master', 'toolsstr', 'rubika', 'library', 'strtools'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+        ]
+)
