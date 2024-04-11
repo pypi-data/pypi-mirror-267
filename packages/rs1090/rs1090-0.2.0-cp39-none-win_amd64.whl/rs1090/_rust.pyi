97d064ec5ba39dd07efc2dbdbafb89337cb8df28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:05:01 +0000

    Add package rs1090/0.2.0/rs1090-0.2.0-cp39-none-win_amd64.whl

---
+++ packages/rs1090/rs1090-0.2.0-cp39-none-win_amd64.whl/rs1090/_rust.pyi
@@ -0,0 +1,18 @@
+from typing import Sequence
+
+def decode_1090(msg: str) -> list[int]: ...
+def decode_1090_vec(msgs: Sequence[Sequence[str]]) -> list[int]: ...
+def decode_1090t_vec(
+    msgs: Sequence[Sequence[str]],
+    ts: Sequence[Sequence[float]],
+    reference: None | tuple[float, float] = None,
+) -> list[int]: ...
+def decode_flarm(
+    msg: str, timestamp: int, reflat: float, reflon: float
+) -> list[int]: ...
+def decode_flarm_vec(
+    msgs: Sequence[Sequence[str]],
+    ts: Sequence[Sequence[int]],
+    reflat: Sequence[Sequence[float]],
+    reflon: Sequence[Sequence[float]],
+) -> list[int]: ...
