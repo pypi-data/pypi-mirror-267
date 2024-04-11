7195f9b107fc31c37838486c9d50a35cc2ea754f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:03:31 +0000

    Add package avania/0.0.0/Avania-0.0.0.tar.gz

---
+++ packages/avania/Avania-0.0.0.tar.gz/Avania-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Avania',
+    version='0.0.0',
+    description='An example Python project',
+    author='Simon',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'numpy',
+    ],
+)
