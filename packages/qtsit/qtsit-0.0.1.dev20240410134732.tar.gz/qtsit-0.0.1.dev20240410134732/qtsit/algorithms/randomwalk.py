4e73aff6e0bc34b5a9e4ad1de00e8416939c9c31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:02:52 +0000

    Add package qtsit/0.0.1.dev20240410134732/qtsit-0.0.1.dev20240410134732.tar.gz

---
+++ packages/qtsit/qtsit-0.0.1.dev20240410134732.tar.gz/qtsit-0.0.1.dev20240410134732/qtsit/algorithms/randomwalk.py
@@ -0,0 +1,14 @@
+"""
+Code for random walk algorithms.
+"""
+
+
+def algorithm1(args: int) -> int:
+    """Summary
+
+    Parameters
+    ----------
+    args : _type_
+        _description_
+    """
+    return args
---
+++ packages/qtsit/qtsit-0.0.1.dev20240410134732.tar.gz/qtsit-0.0.1.dev20240410134732/qtsit/base.py
@@ -0,0 +1,17 @@
+"""
+qtsit base module.
+
+This is the principal module of the qtsit project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "qtsit"
