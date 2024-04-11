9ac1d12b3fd795b66de887ce7a98a0537b687b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 03:03:09 +0000

    Add package incremental-backup/1.2.0/incremental_backup-1.2.0-py3-none-any.whl

---
+++ packages/incremental-backup/incremental_backup-1.2.0-py3-none-any.whl/incremental_backup/__init__.py
@@ -0,0 +1,1 @@
+# TODO: add flake8 linting, probably integrate it with github
---
+++ packages/incremental-backup/incremental_backup-1.2.0-py3-none-any.whl/incremental_backup/__main__.py
@@ -0,0 +1,5 @@
+from incremental_backup.cli import cli_entrypoint
+
+
+if __name__ == '__main__':
+    cli_entrypoint()
