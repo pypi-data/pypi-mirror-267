64355fcdc2ff497d8387e567bd49ab73305d9879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:03:49 +0000

    Add package better-rtplot/0.1.6/better_rtplot-0.1.6-py3-none-any.whl

---
+++ packages/better-rtplot/better_rtplot-0.1.6-py3-none-any.whl/rtplot/client.py
@@ -0,0 +1,298 @@
+import zmq
+import numpy as np
+import time
+from collections import OrderedDict
+import csv
+from typing import List
+
+###################
+# ZMQ Networking #
+##################
+
+#Get the context for networking setup
+context = zmq.Context()
+
+#Socket to talk to server
+#Using the pub - sub paradigm to communicate
+socket = context.socket(zmq.PUB)
+
+#Global variable to keep track of last connected address
+# default is fixed publisher mode, therefore you don't connect 
+# to an address
+prev_address = None
+
+#This address will be used to bind any incoming subscriber on port 5555
+# to the publisher
+bind_address = "tcp://*:5555"
+
+# The subscriber or the publisher must be fixed
+# set which is which here
+known_pi_address_prev = True
+# Add flag to indicate if we ever failed to bind
+failed_bind = False
+
+## Define the default behaviour of the pi
+
+# Assume that you know the ip address of the pi
+if known_pi_address_prev:
+    
+    try:
+        #Attempt to bind to incoming addresses on the port
+        socket.bind(bind_address)
+
+        #Sleep so that the subscriber can join
+        time.sleep(0.2)
+    
+    #If you cannot connect to the socket, alert user and continue
+    except zmq.error.ZMQError as e:
+        failed_bind = True
+        
+        print("rtplot.client: Could not connect to default address '{}'".format(e))
+        print("               There might be another client running")
+        print("               This is fine if doing local plots")
+   
+# Secondary default behavior is that you know the ip address 
+# of the computer that will plot
+else:
+    #Connect to the computer that will plot information
+    socket.connect(prev_address)
+
+
+############################
+# Local plot save config #
+###########################
+local_log_file = None
+csv_writer = None
+
+############################
+# PyQTgraph Configuration #
+###########################
+
+#Create definitions for categories
+SENDING_PLOT_UPDATE = "0"
+SENDING_DATA = "1"
+SAVE_PLOT = "3"
+
+def local_plot():
+    """Send data to a plot in the same computer"""
+
+    local_address = "tcp://127.0.0.1:5555"
+    configure_ip(ip = local_address)
+
+def plot_to_neurobionics_tv():
+    """Send data to a plot in the same computer"""
+
+    tv_computer_address = "tcp://141.212.77.23:5555"
+    configure_ip(ip = tv_computer_address)
+    
+
+def configure_port(new_port:int):
+    """Change the port
+    
+    Keyword Arguments:
+    new_port -- int four digit number that represents the port 
+    
+    """    
+    #Create the new bind address
+    new_bind_address = f"tcp://*:{new_port}"
+
+    #Run the ip configuration
+    configure_ip(known_pi_address=True, new_bind_address=new_bind_address)
+
+def configure_ip(ip = None, known_pi_address = False, new_bind_address = None):
+    """Connect to a subscriber at a specific IP address
+    
+    Inputs
+    ------
+    ip: Ip address or string formated to protocol:address:port
+    known_pi_address: bool, if true, the plot server will connect to the client
+    """
+
+    ## Get the current address
+    global prev_address
+    global known_pi_address_prev
+    global bind_address
+    
+    ## Disconnect from the previous configuration
+    # if known_pi_address_prev and not failed_bind:
+    #     socket.unbind(bind_address)
+    # elif prev_address is not None:
+    #     socket.disconnect(prev_address)
+    
+    ## Format the incomming string
+    #If you just get the ip address and no port, format correctly
+    
+    if ip is not None:
+        num_colons = ip.count(':')
+        
+        #You only got the ip address
+        if num_colons == 0:
+            connect_address = "tcp://{}:5555".format(ip)
+        #You got ip address and port
+        elif num_colons == 1:
+            connect_address = "tcp://{}".format(ip)
+        #You got everything
+        else:
+            connect_address = ip
+
+
+    ## Connect to new configuration
+    if(known_pi_address):
+        
+        if(new_bind_address is not None):
+            print(f"rtplot.client: Connecting to address {new_bind_address}")
+            socket.bind(new_bind_address)
+        else:
+            #Bind incomming computers to the pi
+            print(f"rtplot.client: Connecting to address {bind_address}")
+            socket.bind(bind_address)
+        
+        prev_address = None
+
+    else:
+        #Connect to the computer that will do the plotting
+        print(f"rtplot.client: Connecting to address {connect_address}")
+        socket.connect(connect_address)
+        prev_address = connect_address
+
+    #Remember the last configuration you had
+    known_pi_address_prev = known_pi_address
+
+    #Sleep so that the connection can be established
+    time.sleep(1)
+
+def create_local_log(plot_desc_dict:List[dict], log_name:str=None):
+    """Create a current log file based on the data that is sent"""
+    global local_log_file
+    global csv_writer
+    
+    #If we have a local log file, then delete it
+    if local_log_file is not None:
+        local_log_file.close()
+        local_log_file = None
+
+    # Create a new log file with a csv writter object which uses the log_name
+    # argument in conjunction with the timestamp to create a unique log file
+    local_log_file = open(f"{log_name}_{time.time()}.csv", 'w', newline='')
+    csv_writer = csv.writer(local_log_file, delimiter=',',
+                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    # Add the header to the csv file based on the plot description
+    csv_writer.writerow(["Time"] + [name for plot in plot_desc_dict.values() for name in plot["names"]])
+    
+
+
+def send_array(A, flags=0, copy=True, track=False):
+    """send a numpy array with metadata
+    Inputs
+    ------
+    A: (subplots,dim) np array to transmit
+        subplots - the amount of subplots that are 
+                   defined in the current plot
+        dim - the amount of data that you want to plot.
+              This is not fixed 
+    """
+    #If you get a float value, convert it to a numpy array
+    if(isinstance(A,float) or isinstance(A,list)):
+        A = np.array(A).reshape(-1,1)
+    #If array is one dimensional, reshape to two dimensions
+    if(len(A.shape) ==1):
+        A = A.reshape(-1,1)
+    #Create dict to reconstruct array
+    md = dict(
+        dtype = str(A.dtype),
+        shape = A.shape,
+    )
+    
+    #Send category
+    socket.send_string(SENDING_DATA)
+    #Send json description
+    socket.send_json(md, flags | zmq.SNDMORE)
+    #Send array
+    socket.send(A, flags, copy=copy, track=track)
+    
+    #If we have a local log file, then save the data
+    if local_log_file is not None:
+        csv_writer.writerow([time.time()] + A.flatten().tolist())
+
+
+def initialize_plots(plot_descriptions=1, log_name=None):
+    """Send a json description of desired plot
+    Inputs
+    ------
+    plot_description: list of names or list of plot descriptions dictionaries
+        list of names - the names of the plots that you want to create
+        list of plot descriptions - the dictionary of the plot descriptions
+    log_name: string, name of the file that will be stored locally
+    """
+    
+    global local_log_file
+    global plot_desc_dict
+
+    #Process int inputs
+    if isinstance(plot_descriptions,int):
+        plot_desc_dict = OrderedDict()
+        plot_desc_dict["plot0"] = {"names":["Trace {}".format(i) for i in range (plot_descriptions)]}
+
+    #Process string inputs
+    elif isinstance(plot_descriptions, str):
+        plot_desc_dict = OrderedDict()
+        plot_desc_dict["plot0"] = {"names":[plot_descriptions]}
+    
+    #Process dictionary inputs
+    elif isinstance(plot_descriptions, dict):
+        plot_desc_dict = OrderedDict()
+        plot_desc_dict["plot0"] = plot_descriptions
+
+    #Process lists of things
+    elif isinstance(plot_descriptions, list):
+
+        #Process list of strings
+        if isinstance(plot_descriptions[0],str):
+            plot_desc_dict = OrderedDict()
+            plot_desc_dict["plot0"] = {"names":plot_descriptions}
+
+        # Prcoess list with lists
+        if isinstance(plot_descriptions[0],list):
+            plot_desc_dict = OrderedDict()
+            for i,plot_desc in enumerate(plot_descriptions):
+                plot_desc_dict["plot{}".format(i)] = {"names":plot_desc}
+        
+        #Process list of dics
+        elif isinstance(plot_descriptions[0],dict):
+            plot_desc_dict = OrderedDict()
+            for i,plot_desc in enumerate(plot_descriptions):
+                plot_desc_dict["plot{}".format(i)] = plot_desc
+    
+    #Throw error
+    else:
+        raise TypeError("Incorrect usage of initialize_plots, verify github for usage")
+    
+    #Send the category
+    socket.send_string(SENDING_PLOT_UPDATE)
+
+    #Send the description
+    socket.send_json(plot_desc_dict)
+
+    #If we have a log name, then save the log
+    if log_name is not None:
+        create_local_log(log_name)
+
+
+def save_plot(log_name):
+    """
+    Tell the server to store the data that has been sent for the latest
+    plot configuration
+    
+    Keyword Arguments
+    log_name -- string, name of the file that will be stored
+        
+    """
+    # Indicate that we will send a plot save requset
+    socket.send_string(SAVE_PLOT)
+
+    # Send the save plot name
+    socket.send_string(log_name)
+
+    # Flush the file to ensure that all data is written
+    local_log_file.flush()
