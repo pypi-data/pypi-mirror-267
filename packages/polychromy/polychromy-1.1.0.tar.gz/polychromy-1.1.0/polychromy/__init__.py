12b227e76fe5cd03d3aa913f4e2eb8a31dfd821e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:02 +0000

    Add package polychromy/1.1.0/polychromy-1.1.0.tar.gz

---
+++ packages/polychromy/polychromy-1.1.0.tar.gz/polychromy-1.1.0/polychromy/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '1.0.2'
+
+__all__ = ["colorate","show"]
+
+from .polychromy import *
---
+++ packages/polychromy/polychromy-1.1.0.tar.gz/polychromy-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="polychromy",
+    version="1.1.0",
+    packages=find_namespace_packages(include=['polychromy', 'polychromy.*'], exclude=['images']),
+    # Use find_packages and exclude the folder
+    include_package_data=True,
+    install_requires=["textlinebreaker >= 0.1.0 ", "requests"],  # Add any dependencies here
+    
+    # other setup configurations...
+
+    # Explicitly include 'polychromy.data' package
+    package_data={
+        "polychromy": ["data/colors.json"],
+        "polychromy.data": [],  # Add specific data files if necessary
+    },
+)
