fddd67830d52960d55186f4cfb0e7440c49fca06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:23 +0000

    Add package rubicon-ml/0.7.1/rubicon-ml-0.7.1.tar.gz

---
+++ packages/rubicon-ml/rubicon-ml-0.7.1.tar.gz/rubicon-ml-0.7.1/rubicon_ml/client/utils/tags.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+def has_tag_requirements(tags: List[str], required_tags: List[str], qtype: str) -> bool:
+    """Returns True if `tags` meets the requirements based on
+    the values of `required_tags` and `qtype`. False otherwise.
+    """
+    has_tag_requirements = False
+
+    tag_intersection = set(required_tags).intersection(set(tags))
+    if qtype == "or":
+        if len(tag_intersection) > 0:
+            has_tag_requirements = True
+    if qtype == "and":
+        if len(tag_intersection) == len(required_tags):
+            has_tag_requirements = True
+
+    return has_tag_requirements
+
+
+def filter_children(children, tags, qtype, name):
+    """Filters the provided rubicon objects by `tags` using
+    query type `qtype` and by `name`.
+    """
+    filtered_children = children
+
+    if len(tags) > 0:
+        filtered_children = [
+            c for c in filtered_children if has_tag_requirements(c.tags, tags, qtype)
+        ]
+    if name is not None:
+        filtered_children = [c for c in filtered_children if c.name == name]
+
+    return filtered_children
