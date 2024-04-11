ab1475499f1773f2e6c8f7a2bcec39b73f780849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 07:04:54 +0000

    Add package SCIMAP/2.0.4/scimap-2.0.4.tar.gz

---
+++ packages/SCIMAP/scimap-2.0.4.tar.gz/scimap-2.0.4/scimap/__init__.py
@@ -0,0 +1,16 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution('scimap').version
+except DistributionNotFound:
+    __version__ = '(local)'
+
+import scimap
+
+print(f"Running SCIMAP  {scimap.__version__}")
+
+from . import preprocessing as pp
+from . import plotting as pl
+from . import tools as tl
+from . import helpers as hl
+from . import external as ex
