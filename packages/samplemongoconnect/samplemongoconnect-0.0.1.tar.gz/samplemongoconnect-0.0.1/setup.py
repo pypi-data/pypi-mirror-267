d850045e292e2d31982bdd971900abd17e71bacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 18:03:34 +0000

    Add package samplemongoconnect/0.0.1/samplemongoconnect-0.0.1.tar.gz

---
+++ packages/samplemongoconnect/samplemongoconnect-0.0.1.tar.gz/samplemongoconnect-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()     
+   
+
+__version__ = "0.0.1"
+REPO_NAME = "mlops-learning-00"
+PKG_NAME= "samplemongoconnect"
+AUTHOR_USER_NAME = "machinelearningzuu"
+AUTHOR_EMAIL = "isurualagiyawanna97@gmail.com"
+
+setup(
+    name=PKG_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for connecting with database.",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    )
+
+
+
