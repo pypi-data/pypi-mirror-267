85b845a8f4c6815fbfcf4bd6d3574f4842e1f4bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:03:37 +0000

    Add package rtralm/0.1/rtralm-0.1.tar.gz

---
+++ packages/rtralm/rtralm-0.1.tar.gz/rtralm-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='rtralm',
+    version='0.1',
+    description='Python library for RTRA (Retrieve, Transform, and Adapt) Connector using Hugging Face models',
+    author='Udit Raj',
+    author_email='udit_2312res708@iitp.ac.in',
+    packages=['rtralm'],
+    install_requires=['requests'],
+)
