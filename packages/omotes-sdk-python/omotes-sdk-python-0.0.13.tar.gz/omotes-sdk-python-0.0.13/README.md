8c8afb135eaedd79b68f6b38fdddf765450a0f14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:04:03 +0000

    Add package omotes-sdk-python/0.0.13/omotes-sdk-python-0.0.13.tar.gz

---
+++ packages/omotes-sdk-python/omotes-sdk-python-0.0.13.tar.gz/omotes-sdk-python-0.0.13/README.md
@@ -0,0 +1,9 @@
+# OMOTES SDK Python
+
+This repository is part of the 'Nieuwe Warmte Nu Design Toolkit' project. 
+
+Python implementation of the OMOTES SDK through jobs which may be submitted, receive status updates for submitted jobs or cancel submitted jobs.
+
+## Protobuf
+Please install `protoc` on your machine and make sure it is available in your `PATH`.  
+Version 25.2 is used: https://github.com/protocolbuffers/protobuf/releases.
---
+++ packages/omotes-sdk-python/omotes-sdk-python-0.0.13.tar.gz/omotes-sdk-python-0.0.13/src/omotes_sdk/workflow_type.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import List, Optional, Dict
+
+
+@dataclass
+class WorkflowType:
+    """Define a type of workflow this SDK supports."""
+
+    workflow_type_name: str
+    """Technical name for the workflow."""
+    workflow_type_description_name: str
+    """Human-readable name for the workflow."""
+
+
+class WorkflowTypeManager:
+    """Container for all possible workflows."""
+
+    _workflows: Dict[str, WorkflowType]
+    """The possible workflows this SDK supports."""
+
+    def __init__(self, possible_workflows: List[WorkflowType]):
+        """Create the workflow type manager.
+
+        :param possible_workflows: The workflows to manage.
+        """
+        self._workflows = {workflow.workflow_type_name: workflow for workflow in possible_workflows}
+
+    def get_workflow_by_name(self, name: str) -> Optional[WorkflowType]:
+        """Find the workflow type using the name.
+
+        :param name: Name of the workflow type to find.
+        :return: The workflow type if it exists.
+        """
+        return self._workflows.get(name)
+
+    def get_all_workflows(self) -> List[WorkflowType]:
+        """List all workflows.
+
+        :return: The workflows.
+        """
+        return list(self._workflows.values())
