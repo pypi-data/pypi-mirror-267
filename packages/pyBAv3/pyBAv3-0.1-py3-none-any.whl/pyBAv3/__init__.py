163cc0414bda645b4c8c04e4087af41c6106e32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:03:13 +0000

    Add package pyBAv3/0.1/pyBAv3-0.1-py3-none-any.whl

---
+++ packages/pyBAv3/pyBAv3-0.1-py3-none-any.whl/pyBAv3/__init__.py
@@ -0,0 +1,9 @@
+""" Version 5.2
+# Status: Stable
+# Documentation: https://github.com/Ankit404butfound/PyWhatKit/wiki
+# Report Bugs and Feature Requests here: https://github.com/Ankit404butfound/PyWhatKit/issues
+# For further Information, Join our Discord: https://discord.gg/62Yf5mushu
+"""
+from pyBAv2.BAv2 import chatbot
+from pyBAv2 import BAv2
+import pyBAv2
