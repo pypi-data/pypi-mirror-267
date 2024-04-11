1a3ebf39e88daaa61b5cfa0574eaf17e960d3787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:03:47 +0000

    Add package kittycad/0.6.11/kittycad-0.6.11.tar.gz

---
+++ packages/kittycad/kittycad-0.6.11.tar.gz/kittycad-0.6.11/kittycad/models/get_sketch_mode_plane.py
@@ -0,0 +1,16 @@
+
+from pydantic import BaseModel, ConfigDict
+
+from ..models.point3d import Point3d
+
+
+class GetSketchModePlane(BaseModel):
+    """The plane for sketch mode."""
+
+    x_axis: Point3d
+
+    y_axis: Point3d
+
+    z_axis: Point3d
+
+    model_config = ConfigDict(protected_namespaces=())
