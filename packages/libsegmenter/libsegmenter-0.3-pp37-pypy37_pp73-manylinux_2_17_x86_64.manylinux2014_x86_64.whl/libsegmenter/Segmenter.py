d4e3dbf5b3847dcb318d81f58877f3046e2cea07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 03:03:59 +0000

    Add package libsegmenter/0.3/libsegmenter-0.3-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/libsegmenter/libsegmenter-0.3-pp37-pypy37_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/libsegmenter/Segmenter.py
@@ -0,0 +1,20 @@
+import numpy as np
+from .default_window_selector import default_window_selector
+from .check_cola import check_cola
+
+backends = ["torch", "tensorflow", "base"]
+
+
+def make_segmenter(backend: str = "base", *args, **kwargs):
+    if backend not in backends:
+        raise ValueError(f"Unsupported backend {backend}, availible: {backends}")
+
+    if backend == "torch":
+        from .SegmenterTorch import SegmenterTorch
+
+        return SegmenterTorch(*args, **kwargs)
+
+    if backend == "tensorflow":
+        from .SegmenterTensorFlow import SegmenterTensorFlow
+
+        return SegmenterTensorFlow(*args, **kwargs)
