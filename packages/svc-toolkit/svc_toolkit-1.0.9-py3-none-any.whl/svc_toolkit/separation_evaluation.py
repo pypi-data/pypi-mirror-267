a23ca9984f5bd14f5f6f75b3314ace95be440d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 15:03:59 +0000

    Add package svc-toolkit/1.0.9/svc_toolkit-1.0.9-py3-none-any.whl

---
+++ packages/svc-toolkit/svc_toolkit-1.0.9-py3-none-any.whl/svc_toolkit/separation_evaluation.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+from argparse import ArgumentParser
+
+import torch
+import matplotlib.pyplot as plt
+
+from svc_toolkit.separation.evaluator import Evaluator
+
+def main():
+    parser = ArgumentParser(description='Evaluate a separation model.')
+    parser.add_argument('-m', '--model_dir', type=str, required=True, help='Path to the model directory')
+    parser.add_argument('-t', '--test_csv', type=str, required=True, help='Path to the test csv file')
+    parser.add_argument('-p', '--precision', type=str, default='bf16', choices=['bf16', '32'], help='Precision')
+    parser.add_argument('-l', '--last', default=False, action=argparse.BooleanOptionalAction, help='Use the last model')
+    args = parser.parse_args()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    evaluator = Evaluator(args.model_dir, device, args.precision, args.last)
+    df_result = evaluator.evaluate(args.test_csv)
+
+    boxplot_file_name = f'boxplot{"_last" if args.last else ""}.png'
+    result_file_name = f'result{"_last" if args.last else ""}.csv'
+    summary_file_name = f'summary{"_last" if args.last else ""}.csv'
+
+    df_result.to_csv(os.path.join(args.model_dir, result_file_name), index=False)
+
+    summary_df = evaluator.summary(df_result)
+    summary_df.to_csv(os.path.join(args.model_dir, summary_file_name))
+
+    df_result.boxplot(grid=False)
+    plt.savefig(os.path.join(args.model_dir, boxplot_file_name))
+
+if __name__ == '__main__':
+    main()
