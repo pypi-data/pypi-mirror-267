cabe68fe36f768c7c71178ab6c120c917129df52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 07:04:10 +0000

    Add package htagui/0.0.1/htagui-0.0.1-py3-none-any.whl

---
+++ packages/htagui/htagui-0.0.1-py3-none-any.whl/htagui/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# #############################################################################
+# Copyright (C) 2024 manatlan manatlan[at]gmail(dot)com
+#
+# MIT licence
+#
+# https://github.com/manatlan/htag
+# #############################################################################
+
+__version__ = "0.0.1" # auto updated
+
+########################################################################################
+from .basics import Button,Input,Menu,Spinner,Select
+########################################################################################
+from .form import Form
+from .tabs import Tabs
+from .uiservice import UI
+from .splitters import HSplit #VSplit
+from .flex import hflex,vflex  # utilities (Htag contructor methods)
+
+ALL=[Button,Input,Select,Menu,Spinner,Form,Tabs,UI,HSplit]
