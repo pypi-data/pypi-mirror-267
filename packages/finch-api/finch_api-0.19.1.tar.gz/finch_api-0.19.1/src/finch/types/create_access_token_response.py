6e14830b1d26bc8acb9c8b8d9c75c515df567433
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 05:04:03 +0000

    Add package finch-api/0.19.1/finch_api-0.19.1.tar.gz

---
+++ packages/finch-api/finch_api-0.19.1.tar.gz/finch_api-0.19.1/src/finch/types/create_access_token_response.py
@@ -0,0 +1,9 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .._models import BaseModel
+
+__all__ = ["CreateAccessTokenResponse"]
+
+
+class CreateAccessTokenResponse(BaseModel):
+    access_token: str
