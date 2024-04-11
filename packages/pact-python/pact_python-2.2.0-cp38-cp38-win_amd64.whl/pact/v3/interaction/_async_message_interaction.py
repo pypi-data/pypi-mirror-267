b58bfc0de071aa76840d9098a506ee395e380bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:04:47 +0000

    Add package pact-python/2.2.0/pact_python-2.2.0-cp38-cp38-win_amd64.whl

---
+++ packages/pact-python/pact_python-2.2.0-cp38-cp38-win_amd64.whl/pact/v3/interaction/_async_message_interaction.py
@@ -0,0 +1,57 @@
+"""
+Asynchronous message interaction.
+"""
+
+from __future__ import annotations
+
+import pact.v3.ffi
+from pact.v3.interaction._base import Interaction
+
+
+class AsyncMessageInteraction(Interaction):
+    """
+    An asynchronous message interaction.
+
+    This class defines an asynchronous message interaction between a consumer
+    and a provider. It defines the kind of messages a consumer can accept, and
+    the is agnostic of the underlying protocol, be it a message queue, Apache
+    Kafka, or some other asynchronous protocol.
+
+    !!! warning
+
+        This class is not yet fully implemented and is not yet usable.
+    """
+
+    def __init__(self, pact_handle: pact.v3.ffi.PactHandle, description: str) -> None:
+        """
+        Initialise a new Asynchronous Message Interaction.
+
+        This function should not be called directly. Instead, an
+        AsyncMessageInteraction should be created using the
+        [`upon_receiving(...)`][pact.v3.Pact.upon_receiving] method of a
+        [`Pact`][pact.v3.Pact] instance using the `"Async"` interaction type.
+
+        Args:
+            pact_handle:
+                Handle for the Pact.
+
+            description:
+                Description of the interaction. This must be unique within the
+                Pact.
+        """
+        super().__init__(description)
+        self.__handle = pact.v3.ffi.new_message_interaction(pact_handle, description)
+
+    @property
+    def _handle(self) -> pact.v3.ffi.InteractionHandle:
+        """
+        Handle for the Interaction.
+
+        This is used internally by the library to pass the Interaction to the
+        underlying Pact library.
+        """
+        return self.__handle
+
+    @property
+    def _interaction_part(self) -> pact.v3.ffi.InteractionPart:
+        return pact.v3.ffi.InteractionPart.REQUEST
