0b06dd683d80a8f6b471e687b99fb0f86c66568a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 04:02:59 +0000

    Add package vcf2seq/0.4.0a0/vcf2seq-0.4.0a0-py3-none-any.whl

---
+++ packages/vcf2seq/vcf2seq-0.4.0a0-py3-none-any.whl/vcf2seq/info.py
@@ -0,0 +1,8 @@
+
+
+APPNAME   = "vcf2seq"
+SHORTDESC = "like seqTailor, give a VCF file, it return genomic sequence."
+LICENCE   = "GPL3"
+VERSION   = "0.4.0-alpha"
+AUTHOR    = "Benoit Guibert"
+AUTHOR_EMAIL = "benoit.guibert@inserm.fr"
