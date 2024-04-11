7f5aa0d00f1020f152161923920251b660ffb068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-11 16:04:42 +0000

    Add package outpostkit/0.0.50/outpostkit-0.0.50.tar.gz

---
+++ packages/outpostkit/outpostkit-0.0.50.tar.gz/outpostkit-0.0.50/outpostkit/logger.py
@@ -0,0 +1,10 @@
+import logging
+from typing import Union
+
+
+def init_outpost_logger(level: Union[str, int]) -> logging.Logger:
+    # Use the same settings as above for root logger
+    logging.basicConfig(format="%(asctime)s %(message)s")
+    outpost_logger = logging.getLogger("outpost_logger")
+    outpost_logger.setLevel(logging.getLevelName(level))
+    return outpost_logger
