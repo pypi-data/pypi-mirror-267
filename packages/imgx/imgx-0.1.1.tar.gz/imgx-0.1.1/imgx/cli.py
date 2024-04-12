9f2b675d5069ead073e7ab7faf73a5e6ccf8a3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:04:10 +0000

    Add package imgx/0.1.1/imgx-0.1.1.tar.gz

---
+++ packages/imgx/imgx-0.1.1.tar.gz/imgx-0.1.1/imgx/cli.py
@@ -0,0 +1,23 @@
+"""Console script for imgx."""
+import sys
+import click
+from .process_module import ProcessModule
+
+
+@click.command()
+@click.argument('task',type=str)
+def main(task:str):
+    print(f"Task: {task}")
+    imgx_process = ProcessModule()
+    if task  ==  'read':
+        print('reading')
+        imgx_process.read_metadata_and_write_to_file()
+    if task == 'flat':
+        print('flatting')
+        imgx_process.flatten_directory()
+    
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
