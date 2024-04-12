f39b0871a21719c128f45039c80eef926aee149a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:30 +0000

    Add package neuanwi02/0.0.3/neuanwi02-0.0.3-py3-none-any.whl

---
+++ packages/neuanwi02/neuanwi02-0.0.3-py3-none-any.whl/algo/easy.py
@@ -0,0 +1,13 @@
+import heapq
+from bisect import bisect_left, bisect_right
+
+def heapsort(iterable):
+		h = []
+   result = []
+   # 모든 원소를 차례대로 힙에 삽입
+   for value in iterable:
+   	heapq.heappush(h, value)
+   # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
+   for i in range(len(h)):
+       result.append(heapq.heappop(h))
+   return result
