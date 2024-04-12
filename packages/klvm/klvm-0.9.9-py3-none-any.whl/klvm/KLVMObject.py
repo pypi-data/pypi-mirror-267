54d52e7adfec5a945b9cc84d8bdb680be1e9398b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 04:04:51 +0000

    Add package klvm/0.9.9/klvm-0.9.9-py3-none-any.whl

---
+++ packages/klvm/klvm-0.9.9-py3-none-any.whl/klvm/KLVMObject.py
@@ -0,0 +1,29 @@
+import typing
+
+
+class KLVMObject:
+    """
+    This class implements the KLVM Object protocol in the simplest possible way,
+    by just having an "atom" and a "pair" field
+    """
+
+    atom: typing.Optional[bytes]
+
+    # this is always a 2-tuple of an object implementing the KLVM object
+    # protocol.
+    pair: typing.Optional[typing.Tuple[typing.Any, typing.Any]]
+    __slots__ = ["atom", "pair"]
+
+    def __new__(class_, v):
+        if isinstance(v, KLVMObject):
+            return v
+        self = super(KLVMObject, class_).__new__(class_)
+        if isinstance(v, tuple):
+            if len(v) != 2:
+                raise ValueError("tuples must be of size 2, cannot create KLVMObject from: %s" % str(v))
+            self.pair = v
+            self.atom = None
+        else:
+            self.atom = v
+            self.pair = None
+        return self
