0fae0d82aae0b69207277bc9548749169742da8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:40 +0000

    Add package knewkarma/4.1.0/knewkarma-4.1.0.tar.gz

---
+++ packages/knewkarma/knewkarma-4.1.0.tar.gz/knewkarma-4.1.0/knewkarma/__init__.py
@@ -0,0 +1,17 @@
+from ._core import (
+    Post,
+    Posts,
+    Subreddit,
+    Subreddits,
+    User,
+    Search,
+)
+
+__all__ = [
+    "Post",
+    "Posts",
+    "Subreddit",
+    "Subreddits",
+    "User",
+    "Search",
+]
