9438f7ab14512d802d95024194a750e9d1a69642
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:04:01 +0000

    Add package fgtools/2.1.26/fgtools-2.1.26.tar.gz

---
+++ packages/fgtools/fgtools-2.1.26.tar.gz/fgtools-2.1.26/fgtools/gui/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+import argparse
+
+def main():
+	argp = argparse.ArgumentParser(description="FGTools GUI - use all scripts from the FGTools suite through a GUI")
+	args = argp.parse_args()
+
+if __name__ == "__main__":
+	sys.exit(main())
+
