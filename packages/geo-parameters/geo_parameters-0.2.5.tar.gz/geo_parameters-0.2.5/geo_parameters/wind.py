ecdd77d38564390ac8113ea77eda2e17f3b93d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:04:03 +0000

    Add package geo-parameters/0.2.5/geo_parameters-0.2.5.tar.gz

---
+++ packages/geo-parameters/geo_parameters-0.2.5.tar.gz/geo_parameters-0.2.5/geo_parameters/wind.py
@@ -0,0 +1,79 @@
+from .metaparameter import MetaParameter
+import pint
+
+ureg = pint.UnitRegistry()
+ureg.default_format = "~C"
+
+
+class XWind(MetaParameter):
+    name = "x_wind"
+    _long_name = "x_wind_component"
+    _standard_name = [
+        "x_wind",
+        "grid_eastward_wind",
+    ]
+    _unit = ureg.m / ureg.s
+
+
+class YWind(MetaParameter):
+    name = "y_wind"
+    _long_name = "y_wind_component"
+    _standard_name = [
+        "y_wind",
+        "grid_northward_wind",
+    ]
+    _unit = ureg.m / ureg.s
+
+
+class Wind(MetaParameter):
+    name = "wind"
+    _long_name = "wind_speed"
+    _standard_name = "wind_speed"
+    _unit = ureg.m / ureg.s
+
+
+class WindDir(MetaParameter):
+    name = "wind_dir"
+    _long_name = "wind_direction"
+    _standard_name = "wind_from_direction"
+    _unit = ureg.deg
+
+class WindDirTo(MetaParameter):
+    name = "wind_dir"
+    _long_name = "wind_direction"
+    _standard_name = "wind_to_direction"
+    _unit = ureg.deg
+
+class XGust(MetaParameter):
+    name = "x_gust"
+    _long_name = "x_gust_component"
+    _standard_name = "x_wind_gust"
+    _unit = ureg.m / ureg.s
+
+
+class YGust(MetaParameter):
+    name = "y_gust"
+    _long_name = "y_gust_component"
+    _standard_name = "y_wind_gust"
+    _unit = ureg.m / ureg.s
+
+
+class Gust(MetaParameter):
+    name = "gust"
+    _long_name = "wind_gust"
+    _standard_name = "wind_speed_of_gust"
+    _unit = ureg.m / ureg.s
+
+
+class GustDir(MetaParameter):
+    name = "gust_dir"
+    _long_name = "wind_gust_direction"
+    _standard_name = "wind_gust_from_direction"
+    _unit = ureg.deg
+    
+class GustDirTo(MetaParameter):
+    name = "gust_dir"
+    _long_name = "wind_gust_direction"
+    _standard_name = "wind_gust_to_direction"
+    _unit = ureg.deg
+    _cf = False
