5717463785de7b1ead723055014cbd50369ca7a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 14:04:23 +0000

    Add package arxiv-client/0.3.2/arxiv_client-0.3.2-py3-none-any.whl

---
+++ packages/arxiv-client/arxiv_client-0.3.2-py3-none-any.whl/arxiv_client/__init__.py
@@ -0,0 +1,5 @@
+from .category import Category, Subject
+from .link import Link
+from .query import Query, SortCriterion, SortBy, SortOrder, Field
+from .article import Article
+from .client import Client
