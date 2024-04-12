0d149ea4c1847119e6c009bb4473887b56218fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:50 +0000

    Add package cpop/33.0.1/cpop-33.0.1.tar.gz

---
+++ packages/cpop/cpop-33.0.1.tar.gz/cpop-33.0.1/tests/conftest.py
@@ -0,0 +1,36 @@
+import sys
+import unittest.mock as mock
+
+import aiopath
+import cpop.hub
+import pytest
+
+
+@pytest.fixture()
+async def hub():
+    yield await cpop.hub.AsyncHub(
+        # Let each test manually add their structure
+        load_all_dynes=False,
+        load_all_subdirs=False,
+        recurse_subdirs=False,
+        logs=False,
+        load_config=False,
+    )
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def tpath():
+    code_dir = await aiopath.Path(__file__).parent.parent.absolute()
+    assert await code_dir.exists()
+
+    tpath_dir = code_dir / "tests" / "tpath"
+    assert await tpath_dir.exists()
+
+    NEW_PATH = [str(code_dir), str(tpath_dir)]
+
+    for p in sys.path:
+        if p not in NEW_PATH:
+            NEW_PATH.append(p)
+
+    with mock.patch("sys.path", NEW_PATH):
+        yield
