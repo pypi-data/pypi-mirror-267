469450794b982f024b579f3df73bb42778714634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:03:23 +0000

    Add package surfkit/0.1.33/surfkit-0.1.33.tar.gz

---
+++ packages/surfkit/surfkit-0.1.33.tar.gz/surfkit-0.1.33/surfkit/runtime/container/base.py
@@ -0,0 +1,57 @@
+from typing import List, Optional, Tuple, TypeVar, Type, Generic
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel
+
+
+R = TypeVar("R", bound="ContainerRuntime")
+C = TypeVar("C", bound="BaseModel")
+
+
+class ContainerRuntime(Generic[C, R], ABC):
+
+    @classmethod
+    def name(cls) -> str:
+        return cls.__name__
+
+    @classmethod
+    @abstractmethod
+    def connect_config_type(cls) -> Type[C]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def connect(cls, cfg: C) -> R:
+        pass
+
+    @abstractmethod
+    def create(self, image: str, name: Optional[str] = None) -> None:
+        pass
+
+    @abstractmethod
+    def call(
+        self,
+        name: str,
+        path: str,
+        method: str,
+        port: int = 8080,
+        data: Optional[dict] = None,
+        headers: Optional[dict] = None,
+    ) -> Tuple[int, str]:
+        pass
+
+    @abstractmethod
+    def delete(self, name: str) -> None:
+        pass
+
+    @abstractmethod
+    def list(self) -> List[str]:
+        pass
+
+    @abstractmethod
+    def clean(self) -> None:
+        pass
+
+    @abstractmethod
+    def logs(self, name: str) -> str:
+        pass
