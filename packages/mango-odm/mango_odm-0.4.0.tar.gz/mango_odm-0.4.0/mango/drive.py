3cfe2cadad1597fc55ad0a9b8aecf7e5e94bd408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:03:46 +0000

    Add package mango-odm/0.4.0/mango_odm-0.4.0.tar.gz

---
+++ packages/mango-odm/mango_odm-0.4.0.tar.gz/mango_odm-0.4.0/mango/drive.py
@@ -0,0 +1,144 @@
+import os
+from collections.abc import Iterator
+from typing import Any, ClassVar
+
+from motor.motor_asyncio import (
+    AsyncIOMotorClient,
+    AsyncIOMotorCollection,
+    AsyncIOMotorDatabase,
+)
+from typing_extensions import Self
+
+DEFAULT_CONNECT_URI = os.getenv("MANGO_URI") or "mongodb://localhost:27017"
+DEFAULT_DATABASE_NAME = "test"
+
+
+class Collection:
+    def __init__(self, collection: AsyncIOMotorCollection) -> None:
+        self.collection = collection
+
+    def __getattr__(self, name: str) -> Any:
+        return getattr(self.collection, name)
+
+    def __repr__(self) -> str:
+        return f"Collection(name={self.name}, db={self.full_name.split('.')[0]})"
+
+    @property
+    def name(self) -> str:
+        return self.collection.name
+
+    @property
+    def full_name(self) -> str:
+        return self.collection.full_name
+
+
+class Database:
+    def __init__(self, db: AsyncIOMotorDatabase) -> None:
+        self.db = db
+        self.collections: dict[str, Collection] = {}
+
+    def __getattr__(self, name: str) -> Collection:
+        try:
+            return self.collections[name]
+        except KeyError:
+            collection = self.db.get_collection(name)
+            self.collections[name] = Collection(collection)
+            return self.collections[name]
+
+    def __getitem__(self, name: str) -> Collection:
+        return self.__getattr__(name)
+
+    def __iter__(self) -> Iterator[Collection]:
+        return iter(self.collections.values())
+
+    def __repr__(self) -> str:
+        return (
+            f"Database(name={self.name}, "
+            f"host={self.client.HOST}, "
+            f"port={self.client.PORT})"
+        )
+
+    async def drop_collection(self, collection: str | Collection) -> None:
+        """删除集合"""
+        name = collection if isinstance(collection, str) else collection.name
+        await self.db.drop_collection(name)
+        self.collections.pop(name, None)
+
+    @property
+    def name(self) -> str:
+        return self.db.name
+
+    @property
+    def client(self) -> AsyncIOMotorClient:
+        return self.db.client
+
+
+class Client:
+    _clients: ClassVar[set[Self]] = set()
+
+    def __init__(self, uri: str = DEFAULT_CONNECT_URI, **kwargs: Any) -> None:
+        kwargs.setdefault("host", uri)
+        self.client = AsyncIOMotorClient(**kwargs)
+        self.databases: dict[str, Database] = {}
+        self.__class__._clients.add(self)
+
+    def __getattr__(self, name: str) -> Database:
+        try:
+            return self.databases[name]
+        except KeyError:
+            db = self.client.get_database(name)
+            self.databases[name] = Database(db)
+            return self.databases[name]
+
+    def __getitem__(self, name: str) -> Database:
+        return self.__getattr__(name)
+
+    def __iter__(self) -> Iterator[Database]:
+        return iter(self.databases.values())
+
+    def __repr__(self) -> str:
+        return f"Client(host={self.host}, port={self.port})"
+
+    def close(self) -> None:
+        """关闭连接"""
+        self.client.close()
+        self.__class__._clients.remove(self)
+
+    async def drop_database(self, database: str | Database) -> None:
+        """删除数据库"""
+        name = database if isinstance(database, str) else database.name
+        await self.client.drop_database(name)
+        self.databases.pop(name, None)
+
+    @classmethod
+    def get_database(cls, db: Database | str | None = None) -> Database:
+        """获取数据库"""
+        try:
+            client = next(iter(cls._clients))
+        except StopIteration:
+            client = cls()
+
+        if isinstance(db, Database):
+            return db
+        return client[db] if db else client.default_database
+
+    @property
+    def default_database(self) -> Database:
+        """默认为首次调用的数据库, 如果不存在, 则创建 test 数据库"""
+        try:
+            return next(iter(self.databases.values()))
+        except StopIteration:
+            ddb = self.client.get_default_database(DEFAULT_DATABASE_NAME)
+            return self[ddb.name]
+
+    @property
+    def host(self) -> str:
+        return self.client.HOST
+
+    @property
+    def port(self) -> int:
+        return self.client.PORT
+
+    @property
+    def address(self) -> tuple[str, int] | None:
+        return self.client.address
---
+++ packages/mango-odm/mango_odm-0.4.0.tar.gz/mango_odm-0.4.0/mango/result.py
@@ -0,0 +1,192 @@
+from collections.abc import AsyncGenerator, Generator, Mapping
+from typing import TYPE_CHECKING, Any, Generic, TypeAlias, TypeVar
+
+from motor.motor_asyncio import AsyncIOMotorCursor, AsyncIOMotorLatentCommandCursor
+from pydantic import BaseModel
+
+from mango.expression import Expression, ExpressionField
+from mango.index import Order
+from mango.utils import any_check, is_sequence, validate_fields
+
+if TYPE_CHECKING:  # pragma: no cover
+    from pymongo.results import DeleteResult
+
+    from mango.models import Document
+
+T_Model = TypeVar("T_Model", bound="Document")
+
+KeyField: TypeAlias = str | ExpressionField
+
+FindMapping: TypeAlias = Mapping[KeyField, Any]
+
+DirectionType: TypeAlias = Order
+
+SortType: TypeAlias = tuple[str, DirectionType]
+
+
+class FindOptions(BaseModel):
+    limit: int = 0
+    skip: int = 0
+    sort: list[SortType] = []
+
+    def kwdict(self, *exclude: str) -> dict[str, Any]:
+        return self.model_dump(exclude=set(exclude), exclude_defaults=True)
+
+
+class FindResult(Generic[T_Model]):
+    def __init__(
+        self,
+        model: type[T_Model],
+        *filter: FindMapping | Expression,
+    ) -> None:
+        self.model = model
+        self.collection = model.__collection__
+        self._filter = filter
+        self.options = FindOptions()
+
+    def __await__(self) -> Generator[None, None, list[T_Model]]:
+        """`await` : 等待时，将返回获取的模型列表"""
+        documents = yield from self.cursor.to_list(length=None).__await__()
+        instances: list[T_Model] = []
+        for document in documents:
+            instances.append(self.model.from_doc(document))
+            yield
+        return instances
+
+    async def __aiter__(self) -> AsyncGenerator[T_Model, None]:
+        """`async for`: 异步迭代查询结果"""
+        async for document in self.cursor:  # type: ignore
+            yield self.model.from_doc(document)
+
+    @property
+    def cursor(self) -> AsyncIOMotorCursor:
+        return self.collection.find(self.filter, **self.options.kwdict())
+
+    @property
+    def filter(self) -> dict[str, Any]:
+        """查询过滤条件"""
+        compiled: dict[str, Any] = {}
+        for condition in self._filter:
+            if isinstance(condition, Mapping):
+                condition = dict(condition)  # noqa: PLW2901
+            elif isinstance(condition, Expression):
+                condition = condition.struct()  # noqa: PLW2901
+            else:
+                raise TypeError("查询过滤条件不正确, 应为映射或表达式")
+            compiled |= self._compile(condition)
+        return compiled
+
+    def _compile(
+        self,
+        source: Mapping[KeyField, Any] | Mapping[str, Any],
+    ) -> dict[str, Any]:
+        compiled: dict[str, Any] = {}
+
+        for key, value in source.items():
+            key = str(key)  # noqa: PLW2901
+            if isinstance(value, Expression):
+                compiled[key] = value.struct()
+            elif isinstance(value, Mapping):
+                compiled[key] = self._compile(value)
+            elif is_sequence(value):
+                compiled[key] = []
+                for i in value:
+                    if isinstance(i, Expression):
+                        i = i.struct()  # noqa: PLW2901
+                    if isinstance(i, Mapping):
+                        i = self._compile(i)  # noqa: PLW2901
+                    compiled[key].append(i)
+            else:
+                compiled[key] = value
+
+        return compiled
+
+    def limit(self, limit: int = 0) -> "FindResult[T_Model]":
+        """限制查询条件返回结果的数量"""
+        self.options.limit += limit
+        return self
+
+    def skip(self, skip: int = 0) -> "FindResult[T_Model]":
+        """跳过指定数目的文档"""
+        self.options.skip += skip
+        return self
+
+    def sort(self, *orders: Any) -> "FindResult[T_Model]":
+        """对查询文档流进行排序"""
+        if not (len(orders) != 2 or any_check(orders, is_sequence)):  # noqa: PLR2004
+            orders = (orders,)
+        for order in orders:
+            direction = Order.ASC
+            if is_sequence(order):
+                key, direction = order
+            else:
+                key = order
+
+            try:
+                key, direction = str(key), Order(direction)
+            except ValueError as e:
+                raise TypeError(
+                    "键应为字符串或字段, 排序方向应为 Order 枚举成员"
+                ) from e
+            else:
+                self.options.sort.append((key, direction))
+
+        return self
+
+    def asc(self, *keys: Any) -> "FindResult[T_Model]":
+        """升序排列"""
+        self.options.sort.extend(self._sort_order(*keys))
+        return self
+
+    def desc(self, *keys: Any) -> "FindResult[T_Model]":
+        """降序排列"""
+        self.options.sort.extend(self._sort_order(*keys, reverse=True))
+        return self
+
+    def _sort_order(
+        self, *keys: Any, reverse: bool = False
+    ) -> Generator[SortType, None, None]:
+        direction: Order = Order.DESC if reverse else Order.ASC
+        for key in keys:
+            yield str(key), direction
+
+    async def count(self) -> int:
+        """获得符合条件的文档总数"""
+        return await self.collection.count_documents(
+            self.filter, **self.options.kwdict("sort")
+        )
+
+    async def get(self) -> T_Model | None:
+        """
+        从数据库中获取单个文档。
+        返回单个文档，如果没有找到匹配的文档，返回“None”。
+        """
+        if document := await self.collection.find_one(self.filter):
+            return self.model.from_doc(document)
+        return None
+
+    async def delete(self) -> int:
+        """删除符合条件的文档"""
+        result: DeleteResult = await self.collection.delete_many(self.filter)
+        return result.deleted_count
+
+    async def update(self, **kwargs: Any) -> None:
+        """使用提供的信息更新查找到的文档"""
+        values = validate_fields(self.model, kwargs)
+        await self.collection.update_many(self.filter, {"$set": values})
+
+
+class AggregateResult:
+    def __init__(self, cursor: AsyncIOMotorLatentCommandCursor) -> None:
+        self.cursor = cursor
+
+    def __await__(self) -> Generator[None, None, list[Mapping[str, Any]]]:
+        """
+        `await` : 等待时，将返回聚合管道的结果文档列表
+        """
+        return (yield from self.cursor.to_list(length=None).__await__())
+
+    async def __aiter__(self) -> AsyncGenerator[Mapping[str, Any], None]:
+        """`async for`: 异步迭代聚合管道的结果文档"""
+        async for document in self.cursor:  # type: ignore
+            yield document
