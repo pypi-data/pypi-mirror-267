07effc6f96beb8e6869e4a645aae5cda7f7f1573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:09 +0000

    Add package nomic/3.0.22/nomic-3.0.22.tar.gz

---
+++ packages/nomic/nomic-3.0.22.tar.gz/nomic-3.0.22/nomic/embed.py
@@ -0,0 +1,352 @@
+from __future__ import annotations
+
+import concurrent
+import concurrent.futures
+import logging
+import os
+import time
+from io import BytesIO
+from typing import TYPE_CHECKING, Any, Dict, Iterable, List, Literal, Optional, Tuple, Union, overload
+
+import PIL
+import PIL.Image
+import requests
+
+from .dataset import AtlasClass
+from .settings import *
+
+try:
+    from gpt4all import Embed4All
+except ImportError:
+    if not TYPE_CHECKING:
+        Embed4All = None
+
+atlas_class = None
+
+MAX_TEXT_REQUEST_SIZE = 50
+MAX_IMAGE_REQUEST_SIZE = 512
+MIN_EMBEDDING_DIMENSIONALITY = 64
+
+# mapping of Atlas model name -> Embed4All model filename
+_EMBED4ALL_MODELS = {
+    "nomic-embed-text-v1": "nomic-embed-text-v1.f16.gguf",
+    "nomic-embed-text-v1.5": "nomic-embed-text-v1.5.f16.gguf",
+}
+
+
+def is_backoff_status_code(code: int):
+    if code == 429 or code >= 500:
+        # server error, do backoff
+        return True
+    return False
+
+
+def request_backoff(
+    callable,
+    init_backoff=1.0,
+    ratio=2.0,
+    max_retries=5,
+    backoff_if=is_backoff_status_code,
+):
+    for attempt in range(max_retries + 1):
+        response = callable()
+        if attempt == max_retries:
+            return response
+        if backoff_if(response.status_code):
+            delay = init_backoff * (ratio**attempt)
+            logging.info(f"server error, backing off for {int(delay)}s")
+            time.sleep(delay)
+        else:
+            return response
+
+
+def text_api_request(texts: List[str], model: str, task_type: str, dimensionality: int = None, long_text_mode: str = "truncate"):
+    global atlas_class
+    response = request_backoff(
+        lambda: requests.post(
+            atlas_class.atlas_api_path + "/v1/embedding/text",
+            headers=atlas_class.header,
+            json={"texts": texts, "model": model, "task_type": task_type, "dimensionality": dimensionality, "long_text_mode": long_text_mode},
+        )
+    )
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception((response.status_code, response.text))
+
+
+@overload
+def text(
+    texts: list[str],
+    *,
+    model: str = ...,
+    task_type: str = ...,
+    dimensionality: int | None = ...,
+    long_text_mode: str = ...,
+    inference_mode: Literal["remote"] = ...,
+) -> dict[str, Any]: ...
+@overload
+def text(
+    texts: list[str],
+    *,
+    model: str = ...,
+    task_type: str = ...,
+    dimensionality: int | None = ...,
+    long_text_mode: str = ...,
+    inference_mode: Literal["local"],
+    device: str | None = ...,
+    **kwargs: Any,
+) -> dict[str, Any]: ...
+@overload
+def text(
+    texts: list[str],
+    *,
+    model: str = ...,
+    task_type: str = ...,
+    dimensionality: int | None = ...,
+    long_text_mode: str = ...,
+    inference_mode: str,
+    device: str | None = ...,
+    **kwargs: Any,
+) -> dict[str, Any]: ...
+
+
+def text(
+    texts: list[str],
+    *,
+    model: str = "nomic-embed-text-v1",
+    task_type: str = "search_document",
+    dimensionality: int | None = None,
+    long_text_mode: str = "truncate",
+    inference_mode: str = "remote",
+    device: str | None = None,
+    **kwargs: Any,
+) -> dict[str, Any]:
+    """
+    Generates embeddings for the given text.
+
+    Args:
+        texts: The text to embed.
+        model: The model to use when embedding.
+        task_type: The task type to use when embedding. One of `search_query`, `search_document`, `classification`, `clustering`.
+        dimensionality: The embedding dimension, for use with Matryoshka-capable models. Defaults to full-size.
+        long_text_mode: How to handle texts longer than the model can accept. One of `mean` or `truncate`.
+        inference_mode: How to generate embeddings. One of `remote` or `local` (Embed4All). Defaults to `remote`.
+        device: The device to use for local embeddings. Defaults to CPU, or Metal on Apple Silicon. It can be set to:
+            - "gpu": Use the best available GPU.
+            - "amd", "nvidia": Use the best available GPU from the specified vendor.
+			- A specific device name from the output of `GPT4All.list_gpus()`
+        kwargs: Remaining arguments are passed to the Embed4All contructor.
+
+    Returns:
+        A dict containing your embeddings and request metadata
+    """
+    if isinstance(texts, str):
+        raise TypeError("'texts' parameter must be list[str], not str")
+
+    modes = {
+        "remote": False,
+        "local": True,
+    }
+
+    try:
+        use_embed4all = modes[inference_mode]
+    except KeyError:
+        raise ValueError(f"Unknown inference mode: {inference_mode!r}") from None
+
+    if inference_mode == "remote":
+        if device is not None:
+            raise TypeError(f"device argument cannot be used with inference_mode='remote'")
+        if kwargs:
+            raise TypeError(f"Unexpected keyword arguments: {list(kwargs.keys())}")
+    elif Embed4All is None:
+        raise RuntimeError(
+            f"The 'gpt4all' package is required for local inference. Suggestion: `pip install \"nomic[local]\"`",
+        )
+
+    if use_embed4all:
+        return _text_embed4all(texts, model, task_type, dimensionality, long_text_mode, device=device, **kwargs)
+
+    return _text_atlas(texts, model, task_type, dimensionality, long_text_mode)
+
+
+def _text_atlas(
+    texts: list[str],
+    model: str,
+    task_type: str,
+    dimensionality: int | None,
+    long_text_mode: str,
+) -> dict[str, Any]:
+    global atlas_class
+    assert task_type in ["search_query", "search_document", "classification", "clustering"], f"Invalid task type: {task_type}"
+
+    if dimensionality and dimensionality < MIN_EMBEDDING_DIMENSIONALITY:
+        logging.warning(f"Dimensionality {dimensionality} is less than the suggested of {MIN_EMBEDDING_DIMENSIONALITY}. Performance may be degraded.")
+
+    if atlas_class is None:
+        atlas_class = AtlasClass()
+    max_workers = 10
+    chunksize = MAX_TEXT_REQUEST_SIZE
+    smallchunk = max(1, int(len(texts) / max_workers))
+    # if there are fewer texts per worker than the max chunksize just split them evenly
+    chunksize = min(smallchunk, chunksize)
+
+    combined = {'embeddings': [], 'usage': {}, 'model': model}
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for chunkstart in range(0, len(texts), chunksize):
+            chunkend = min(len(texts), chunkstart + chunksize)
+            chunk = texts[chunkstart:chunkend]
+            futures.append(executor.submit(text_api_request, chunk, model, task_type, dimensionality, long_text_mode))
+
+        for future in futures:
+            response = future.result()
+            assert response['model'] == model
+            combined['embeddings'] += response['embeddings']
+            for counter, value in response['usage'].items():
+                combined['usage'][counter] = combined['usage'].get(counter, 0) + value
+    return combined
+
+
+_embed4all: Embed4All | None = None
+_embed4all_kwargs: dict[str, Any] | None = None
+_embed4all_has_init_gpu = False
+
+
+def _text_embed4all(
+    texts: list[str],
+    model: str,
+    task_type: str,
+    dimensionality: int | None,
+    long_text_mode: str,
+    **kwargs: Any,
+) -> dict[str, Any]:
+    global _embed4all, _embed4all_kwargs, _embed4all_has_init_gpu
+
+    try:
+        g4a_model = _EMBED4ALL_MODELS[model]
+    except KeyError:
+        raise ValueError(f"Unsupported model for local embeddings: {model!r}") from None
+
+    if any(text == "" for text in texts):
+        raise NotImplementedError("Embedding an empty text is not implemented")
+
+    if not texts:
+        # special-case this since Embed4All doesn't allow it
+        return {"embeddings": [], "usage": {}, "model": model}
+
+    if _embed4all is None or _embed4all.gpt4all.config["filename"] != g4a_model or _embed4all_kwargs != kwargs:
+        using_gpu = kwargs.get("device") not in (None, "cpu")
+        if using_gpu and _embed4all_has_init_gpu:
+            raise NotImplementedError("GPU context can only be initialized once")
+
+        _embed4all = Embed4All(g4a_model, **kwargs)
+        _embed4all_kwargs = kwargs
+
+        if using_gpu:
+            _embed4all_has_init_gpu = True
+
+    output = _embed4all.embed(
+        texts,
+        prefix=task_type,
+        dimensionality=dimensionality,
+        long_text_mode=long_text_mode,
+        return_dict=True,
+        atlas=True,
+    )
+    ntok = output["n_prompt_tokens"]
+    usage = {"prompt_tokens": ntok, "total_tokens": ntok}
+    return {"embeddings": output["embeddings"], "usage": usage, "model": model}
+
+
+def free_embedding_model() -> None:
+    """Free the current Embed4All instance and its associated system resources."""
+    global _embed4all, _embed4all_kwargs
+    if _embed4all is not None:
+        _embed4all.close()
+        _embed4all = _embed4all_kwargs = None
+
+
+def image_api_request(images: List[Tuple[str, bytes]], model: str = 'nomic-embed-vision-v1'):
+    global atlas_class
+    response = request_backoff(
+        lambda: requests.post(
+            atlas_class.atlas_api_path + "/v1/embedding/image",
+            headers=atlas_class.header,
+            data={"model": model},
+            files=images,
+        )
+    )
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception((response.status_code, response.text))
+    
+    
+def resize_pil(img):
+    width, height = img.size
+    #if image is too large, downsample before sending over the wire
+    max_width = 512
+    max_height = 512
+    if max_width > 512 or max_height > 512:
+        downsize_factor = max(width/max_width, height/max_height)
+        img.resize((width/downsize_factor, height/downsize_factor))
+    return img
+
+
+def images(images: Iterable[Union[str, PIL.Image.Image]], model: str = 'nomic-embed-vision-v1'):
+    """
+    Generates embeddings for the given images.
+
+    Args:
+        images: the images to embed. Can be file paths to images, image-file bytes or Pillow objects
+        model: the model to use when embedding
+
+    Returns:
+        An object containing your embeddings and request metadata
+    """
+    global atlas_class
+
+    if atlas_class is None:
+        atlas_class = AtlasClass()
+
+    max_workers = 10
+    chunksize = MAX_IMAGE_REQUEST_SIZE
+    smallchunk = max(1, int(len(images) / max_workers))
+    # if there are fewer images per worker than the max chunksize just split them evenly
+    chunksize = min(smallchunk, chunksize)
+
+    image_batch = []
+    for image in images:
+        if isinstance(image, str) and os.path.exists(image):
+                img = resize_pil(PIL.Image.open(image))
+                buffered = BytesIO()
+                img.save(buffered, format="JPEG")
+                image_batch.append(("images", buffered.getvalue()))
+
+        elif isinstance(image, PIL.Image.Image):
+            img = resize_pil(image)
+            buffered = BytesIO()
+            img.save(buffered, format="JPEG")
+            image_batch.append(("images", buffered.getvalue()))
+        else:
+            raise ValueError(f"Not a valid file: {image}")
+
+
+    combined = {'embeddings': [], 'usage': {}, 'model': model}
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for chunkstart in range(0, len(image_batch), chunksize):
+            chunkend = min(len(image_batch), chunkstart + chunksize)
+            chunk = image_batch[chunkstart:chunkend]
+            futures.append(executor.submit(image_api_request, chunk, model))
+
+        for future in futures:
+            response = future.result()
+            assert response['model'] == model
+            combined['embeddings'] += response['embeddings']
+            for counter, value in response['usage'].items():
+                combined['usage'][counter] = combined['usage'].get(counter, 0) + value
+    return combined
---
+++ packages/nomic/nomic-3.0.22.tar.gz/nomic-3.0.22/tests/test_atlas_client.py
@@ -0,0 +1,384 @@
+import os
+import random
+import tempfile
+import time
+import uuid
+from datetime import datetime, timedelta
+
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import pytest
+import requests
+
+from nomic import AtlasDataset, atlas
+
+
+def gen_random_datetime(min_year=1900, max_year=datetime.now().year):
+    # generate a datetime in format yyyy-mm-dd hh:mm:ss.000000
+    start = datetime(min_year, 1, 1, 00, 00, 00)
+    years = max_year - min_year + 1
+    end = start + timedelta(days=365 * years)
+    return start + (end - start) * random.random()
+
+
+def gen_temp_identifier() -> str:
+    now = datetime.now().isoformat(timespec='seconds')
+    rand = random.randint(0, 10000)
+    return f'{now}-{rand}'
+
+
+def test_map_idless_embeddings():
+    num_embeddings = 50
+    embeddings = np.random.rand(num_embeddings, 512)
+
+    dataset = atlas.map_data(identifier=f"unittest-dataset-{gen_temp_identifier()}", embeddings=embeddings)
+    AtlasDataset(dataset.identifier).delete()
+
+
+def test_map_embeddings_with_errors():
+    num_embeddings = 20
+    embeddings = np.random.rand(num_embeddings, 10)
+
+    name = f'unittest-dataset-{gen_temp_identifier()}'
+    # test nested dictionaries
+    with pytest.raises(Exception):
+        data = [{'key': {'nested_key': 'nested_value'}} for i in range(len(embeddings))]
+        dataset = atlas.map_data(embeddings=embeddings, data=data, identifier=name, is_public=True)
+
+    try:
+        AtlasDataset(name).delete()
+    except BaseException:
+        pass
+
+    name = f'unittest-dataset-{gen_temp_identifier()}'
+    # test underscore
+    with pytest.raises(Exception):
+        data = [{'__hello': {'hello'}} for i in range(len(embeddings))]
+        dataset = atlas.map_data(embeddings=embeddings, data=data, identifier=name, is_public=True)
+
+    try:
+        AtlasDataset(name).delete()
+    except BaseException:
+        pass
+
+    name = f'unittest-dataset-{gen_temp_identifier()}'
+    # test to long ids
+    with pytest.raises(Exception):
+        data = [{'id': str(uuid.uuid4()) + 'a'} for i in range(len(embeddings))]
+        dataset = atlas.map_data(
+            embeddings=embeddings,
+            data=data,
+            identifier=name,
+            id_field='id',
+            is_public=True,
+        )
+
+        assert isinstance(dataset.created_timestamp, datetime)
+
+    try:
+        AtlasDataset(name).delete()
+    except BaseException:
+        pass
+
+
+def test_map_text_errors():
+    # no indexed field
+    name = f'unittest-dataset-{gen_temp_identifier()}'
+    with pytest.raises(Exception):
+        dataset = atlas.map_data(
+            data=[{'key': 'a'}],
+            indexed_field='text',
+            is_public=True,
+            identifier=name,
+            description='test map description',
+        )
+
+    try:
+        AtlasDataset(name).delete()
+    except BaseException:
+        pass
+
+
+def test_date_metadata():
+    num_embeddings = 20
+    embeddings = np.random.rand(num_embeddings, 10)
+    data = [
+        {'my_date': datetime(2022, 1, i), 'my_random_date': gen_random_datetime()}
+        for i in range(1, len(embeddings) + 1)
+    ]
+
+    dataset = atlas.map_data(
+        embeddings=embeddings, identifier=f"unittest-dataset-{gen_temp_identifier()}", data=data, is_public=True
+    )
+
+    assert dataset.id
+
+    dataset.delete()
+
+    # put an invalid iso timestamp after the first valid isotimestamp , make sure the client fails
+    with pytest.raises(Exception):
+        data[1]['my_date'] = data[1]['my_date'] + 'asdf'
+        dataset = atlas.map_data(
+            embeddings=embeddings,
+            identifier=f"unittest-dataset-{gen_temp_identifier()}",
+            id_field='id',
+            data=data,
+            is_public=True,
+        )
+        dataset.delete()
+
+def test_topics():
+    num_embeddings = 100
+    embeddings = np.random.rand(num_embeddings, 10)
+    texts = ['foo', 'bar', 'baz', 'bat']
+    dates = [datetime(2021, 1, 1), datetime(2022, 1, 1), datetime(2023, 1, 1)]
+    data = [
+        {'field': str(uuid.uuid4()), 'id': str(uuid.uuid4()), 'upload': 0.0, 'text': texts[i % 4], 'date': dates[i % 3]}
+        for i in range(len(embeddings))
+    ]
+
+    dataset = atlas.map_data(
+        embeddings=embeddings,
+        identifier=f"unittest-dataset-{gen_temp_identifier()}",
+        id_field='id',
+        data=data,
+        is_public=True,
+        topic_model=dict(topic_label_field='text'),
+    )
+
+    with dataset.wait_for_dataset_lock():
+        time.sleep(5)
+        assert len(dataset.maps[0].topics.metadata) > 0
+
+        q = np.random.random((3, 10))
+        assert len(dataset.maps[0].topics.vector_search_topics(q, depth=1, k=3)['topics']) == 3
+        assert isinstance(dataset.maps[0].topics.group_by_topic(topic_depth=1), list)
+
+        dataset.delete()
+
+
+def test_tagging_private():
+    dataset = AtlasDataset("wine-test-private")
+    map = dataset.maps[0]
+    datum_ids = map.tags.get_datums_in_tag("chardonnay")
+    assert len(datum_ids) > 0
+    tag_df = map.tags.df
+    assert len(tag_df.columns) == 2
+
+
+def test_data():
+    num_embeddings = 100
+    embeddings = np.random.rand(num_embeddings, 10)
+    texts = ['foo', 'bar', 'baz', 'bat']
+    data = [
+        {'field': str(uuid.uuid4()), 'id': str(uuid.uuid4()), 'upload': 0.0, 'text': str(i)}
+        for i in range(len(embeddings))
+    ]
+    all_columns = data[0].keys()
+
+    dataset = atlas.map_data(
+        embeddings=embeddings,
+        identifier=f"unittest-dataset-{gen_temp_identifier()}",
+        id_field='id',
+        data=data,
+        is_public=True,
+        topic_model=dict(build_topic_model=True, community_description_target_field='text'),
+    )
+
+    with dataset.wait_for_dataset_lock():
+        df = dataset.maps[0].data.df
+        assert len(df) > 0
+        # all uploaded columns, including the id column, should be in the downloaded data
+        for column in all_columns:
+            assert column in df.columns
+        dataset.delete()
+
+
+words = [
+    'foo',
+    'bar',
+    'baz',
+    'bat',
+    'glorp',
+    'gloop',
+    'glib',
+    'glub',
+    'florp',
+    'floop',
+    'flib',
+    'flub',
+    'blorp',
+    'bloop',
+    'blib',
+    'blub',
+    'slorp',
+    'sloop',
+    'slib',
+    'slub',
+    'clorp',
+    'cloop',
+    'clib',
+    'club',
+    'plorp',
+    'ploop',
+    'plib',
+    'plub',
+    'zlorp',
+    'zloop',
+    'zlib',
+    'zlub',
+    'xlorp',
+    'xloop',
+    'xlib',
+    'xlub',
+    'vlorp',
+    'vloop',
+    'vlib',
+    'vlub',
+    'nlorp',
+    'nloop',
+    'nlib',
+    'nlub',
+    'mlorp',
+    'mloop',
+    'mlib',
+    'mlub',
+]
+
+
+def test_flawed_ids():
+    """
+    Check that null and empty strings do not block an index build.
+    """
+    p = AtlasDataset(f"unittest-dataset-{gen_temp_identifier()}", unique_id_field='id')
+
+    elements = []
+    for i in range(10):
+        if i % 3 == 0 and i % 5 == 0:
+            elements.append({'text': 'fizzbuzz', 'id': str(i)})
+        elif i % 3 == 0:
+            elements.append({'text': 'fizz', 'id': str(i)})
+        elif i % 5 == 0:
+            elements.append({'text': 'buzz', 'id': str(i)})
+    p.add_data(data=elements)
+    with pytest.raises(ValueError):
+        p.add_data(data=[{'text': 'fizzbuzz', 'id': None}])
+    with pytest.raises(ValueError):
+        p.add_data(data=[{'text': 'fizzbuzz', 'id': 'A' * 100}])
+    p.delete()
+
+
+def test_weird_inputs():
+    """
+    Check that null and empty strings do not block an index build.
+    """
+    dataset = AtlasDataset(f"unittest-dataset-{gen_temp_identifier()}", unique_id_field='id')
+
+    elements = []
+    for i in range(20):
+        if i % 3 == 0 and i % 5 == 0:
+            elements.append({'text': 'fizzbuzz', 'id': str(i)})
+        elif i % 3 == 0:
+            elements.append({'text': 'fizz', 'id': str(i)})
+        elif i % 5 == 0:
+            elements.append({'text': 'buzz', 'id': str(i)})
+        elif i % 7 == 0:
+            elements.append({'text': None, 'id': str(i)})
+        elif i % 2 == 0:
+            elements.append({'text': '', 'id': str(i)})
+        else:
+            elements.append({'text': 'foo', 'id': str(i)})
+    dataset.add_data(data=elements)
+    dataset.create_index(indexed_field='text', topic_model=True)
+    with dataset.wait_for_dataset_lock():
+        assert True
+    dataset.delete()
+
+
+def test_map_embeddings():
+    num_embeddings = 20
+    embeddings = np.random.rand(num_embeddings, 10)
+    data = [{'field': str(uuid.uuid4()), 'id': str(uuid.uuid4())} for i in range(len(embeddings))]
+
+    dataset = atlas.map_data(
+        embeddings=embeddings,
+        identifier=f"unittest-dataset-{gen_temp_identifier()}",
+        id_field='id',
+        data=data,
+        is_public=True,
+    )
+
+    map = dataset.maps[0]
+
+    num_tries = 0
+    while map.dataset.is_locked:
+        time.sleep(10)
+        num_tries += 1
+        if num_tries > 5:
+            raise TimeoutError('Timed out while waiting for project to unlock')
+
+    retrieved_embeddings = map.embeddings.latent
+
+    assert dataset.total_datums == num_embeddings
+    assert retrieved_embeddings.shape[0] == num_embeddings
+    map = dataset.maps[0]
+
+    assert len(map.topics.df) == 20
+
+    assert isinstance(map.topics.hierarchy, dict)
+
+    dataset.create_index()
+    with dataset.wait_for_dataset_lock():
+        neighbors, _ = map.embeddings.vector_search(queries=np.random.rand(1, 10), k=2)
+        assert len(neighbors[0]) == 2
+
+    for map in dataset.projections:
+        assert map.map_link
+
+    dataset.delete()
+
+
+def test_map_text_pandas():
+    size = 50
+    data = pd.DataFrame(
+        {
+            'field': [str(uuid.uuid4()) for i in range(size)],
+            'color': [random.choice(['red', 'blue', 'green']) for i in range(size)],
+        }
+    )
+
+    dataset = atlas.map_data(identifier=f'UNITTEST_pandas_text-{gen_temp_identifier()}', indexed_field="color", data=data, is_public=True)
+
+    assert dataset.total_datums == 50
+
+    dataset.delete()
+
+
+def test_map_text_arrow():
+    size = 50
+    data = pa.Table.from_pydict(
+        {
+            'field': [str(uuid.uuid4()) for i in range(size)],
+            'id': [str(uuid.uuid4()) for i in range(size)],
+            'color': [random.choice(['red', 'blue', 'green']) for i in range(size)],
+        }
+    )
+
+    dataset = atlas.map_data(
+        identifier=f'UNITTEST_arrow_text-{gen_temp_identifier()}',
+        id_field='id',
+        indexed_field="color",
+        data=data,
+        is_public=True,
+    )
+
+    assert dataset.total_datums == 50
+
+    dataset.delete()
+
+
+def test_contrastors_access():
+    keys = atlas._get_datastream_credentials()
+    assert keys["access_key"] is not None
+    assert keys["secret_key"] is not None
