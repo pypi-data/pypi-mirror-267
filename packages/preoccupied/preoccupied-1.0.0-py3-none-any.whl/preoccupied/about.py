c89cbf731845038308c0ef091e51f138e924ebe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:04:08 +0000

    Add package preoccupied/1.0.0/preoccupied-1.0.0-py3-none-any.whl

---
+++ packages/preoccupied/preoccupied-1.0.0-py3-none-any.whl/preoccupied/about.py
@@ -0,0 +1,29 @@
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this library; if not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+Placeholder for the preoccupied namespace package
+
+:author: Christopher O'Brien  <obriencj@preoccupied.net>
+:license: GPL version 3
+"""
+
+
+AUTHOR = "Christopher O'Brien"
+AUTHOR_EMAIL = "obriencj@preoccupied.net"
+
+HOME_PAGE = "https://github.com/obriencj/python-preoccupied"
+
+
+# The end.
