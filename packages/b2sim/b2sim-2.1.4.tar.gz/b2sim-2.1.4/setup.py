0addc2111651a7081b3bb775351f811fdb6d2845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:53 +0000

    Add package b2sim/2.1.4/b2sim-2.1.4.tar.gz

---
+++ packages/b2sim/b2sim-2.1.4.tar.gz/b2sim-2.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='b2sim',
+   version='2.1.4',
+   description='A module to simulate the generation of eco and farms in Bloons TD Battles 2',
+   long_description='TODO',
+   author='redlaserbm',
+   author_email='redlaserbm@gmail.com',
+   packages=find_packages(),  #same as name
+   extras_require= {
+      'analysis': [
+           'pandas', 
+           'matplotlib',
+           'neat'
+      ],
+   },
+   include_package_data=True
+)
