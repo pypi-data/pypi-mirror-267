d69a4df465f9190a7c31497692b66cc406dfd809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:03:28 +0000

    Add package PyNucleus/0.0.2.3/PyNucleus-0.0.2.3.tar.gz

---
+++ packages/PyNucleus/PyNucleus-0.0.2.3.tar.gz/PyNucleus-0.0.2.3/PyNucleus/__init__.py
@@ -0,0 +1,3 @@
+from PyNucleus.commands import CommandRegister, CommandBase
+from PyNucleus.functions import generate_id, generate_custom_id, PrintError
+from PyNucleus.calculator import Calculator
---
+++ packages/PyNucleus/PyNucleus-0.0.2.3.tar.gz/PyNucleus-0.0.2.3/README.md
@@ -0,0 +1,4 @@
+# PyEssentials 
+## by Bartek Kansy
+
+### This package contains classes and functions that can be used to create programs, consoles etc.
