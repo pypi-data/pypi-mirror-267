4a711f58d7448d1f6f72b126e9f9dbf4ed124213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:39 +0000

    Add package ynab-api-import/1.1.0/ynab_api_import-1.1.0-py3-none-any.whl

---
+++ packages/ynab-api-import/ynab_api_import-1.1.0-py3-none-any.whl/ynabapiimport/models/exceptions.py
@@ -0,0 +1,20 @@
+
+
+class NoRequisitionError(Exception):
+	pass
+
+
+class NoAccountError(Exception):
+	pass
+
+
+class MultipleAccountsError(Exception):
+	pass
+
+
+class ReferenceNotUnique(Exception):
+	pass
+
+
+class ReferenceNotValid(Exception):
+	pass
