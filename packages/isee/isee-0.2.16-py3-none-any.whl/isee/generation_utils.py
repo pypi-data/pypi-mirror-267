216a0807226a0640ae44e7e4fadde1b51a97b2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:04:01 +0000

    Add package isee/0.2.16/isee-0.2.16-py3-none-any.whl

---
+++ packages/isee/isee-0.2.16-py3-none-any.whl/isee/generation_utils.py
@@ -0,0 +1,59 @@
+import os
+import re
+from packaging.version import parse
+import semver
+from epythet.autogen import make_autodocs
+from epythet.setup_docsrc import make_docsrc
+
+from isee.common import get_env_var, git
+
+DFLT_NEW_VERSION = '0.1.0'
+
+
+def gen_semver(
+    *, dir_path: str = None, version_patch_prefix: str = '',
+):
+    def get_version():
+        def bump(latest):
+            commit_message = git('show-branch', '--no-name', 'HEAD')
+            if re.search(r'\[bump major\]', commit_message):
+                return semver.bump_major(latest)
+            if re.search(r'\[bump minor\]', commit_message):
+                return semver.bump_minor(latest)
+            return semver.bump_patch(latest)
+
+        def format_version(version):
+            version_parts = version.split('.')
+            version_parts[2] = f'{version_patch_prefix}{version_parts[2]}'
+            return '.'.join(version_parts)
+
+        tags = git('tag').split('\n')
+        pattern = rf'^(\d+.){{2}}{version_patch_prefix}\d+$'
+        sorted_versions = sorted(
+            [
+                x.replace(f'{version_patch_prefix}', '')
+                for x in tags
+                if re.match(pattern, x)
+            ],
+            key=parse,
+            reverse=True,
+        )
+        if len(sorted_versions) > 0:
+            new_version = bump(sorted_versions[0])
+        else:
+            # No tags in the repository
+            new_version = DFLT_NEW_VERSION
+        return format_version(new_version)
+
+    if dir_path:
+        os.chdir(dir_path)
+    version = get_version()
+    print(version)
+
+
+def generate_documentation(*, project_dir=None):
+    if not project_dir:
+        project_dir = get_env_var('GITHUB_WORKSPACE')
+    if not os.path.exists(project_dir + '/docsrc'):
+        make_docsrc(project_dir)
+    make_autodocs(project_dir)
