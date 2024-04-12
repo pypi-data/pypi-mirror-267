07565bb202f228c58f9d2ab4dd5108b640b6487d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:04:00 +0000

    Add package django-customvueadmin/0.1.5/django_customvueadmin-0.1.5-py3-none-any.whl

---
+++ packages/django-customvueadmin/django_customvueadmin-0.1.5-py3-none-any.whl/custom_admin/views/admin_page.py
@@ -0,0 +1,37 @@
+import json
+
+from django.http import HttpResponse
+
+from custom_admin.views.template import CUSTOM_ADMIN_TEMPLATE_STRING
+from django.conf import settings
+from django.template import Context, Template
+from django.views import View
+
+
+class CustomAdminView(View):
+    '''
+    # Settings in case of h2ttps:
+    USE_X_FORWARDED_HOST = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    '''
+
+    template = 'custom_admin/admin_index.html'
+
+    def get(self, request):
+        admin_settings = {
+            'title': settings.CUSTOM_ADMIN.get('title'),
+            'base_admin_url': settings.CUSTOM_ADMIN.get('base_admin_url') or 'admin/',
+            'static_prefix': settings.CUSTOM_ADMIN.get('static_prefix') or '/static',
+        }
+
+        if not admin_settings.get('backend_prefix'):
+            admin_settings['backend_prefix'] = request.build_absolute_uri('/custom_admin/')
+
+        context = {
+            'SETTINGS': admin_settings,
+            'SETTINGS_JSON': json.dumps(admin_settings),
+        }
+
+        template = Template(CUSTOM_ADMIN_TEMPLATE_STRING)
+        code = template.render(Context(context))
+        return HttpResponse(code)
