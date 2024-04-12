047baebf5b6dd2027d431dc1740caa86312d1d8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:53 +0000

    Add package lilavati/1.0.0/lilavati-1.0.0-py3-none-any.whl

---
+++ packages/lilavati/lilavati-1.0.0-py3-none-any.whl/lilavati/Algebra/__init__.py
@@ -0,0 +1,16 @@
+from .squares import *
+from .cube import *
+from .addition import *
+from .cuberoots import *
+from .quadraticEqns import *
+from .sqroot import *
+from .subtraction import *
+from .unitConverter import *
+from .combination import *
+from .permutation import *
+from .sum_of_permutation import *
+
+"""
+    Initialisation script for Arithmatic sub-package
+    Please write your code in seperate file and import it here
+"""
---
+++ packages/lilavati/lilavati-1.0.0-py3-none-any.whl/lilavati/__init__.py
@@ -0,0 +1,6 @@
+from .Algebra import *
+from .Geometry import *
+"""
+    Initialisation script for leelavati library
+    please do not write any code here write your code in seperate file and import it here
+"""
