10f4bfee4e7bd1fcbfd0d2019f7a784f2f452979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:03:37 +0000

    Add package arcor2-execution-data/1.2.0/arcor2_execution_data-1.2.0-py311-none-any.whl

---
+++ packages/arcor2-execution-data/arcor2_execution_data-1.2.0-py311-none-any.whl/arcor2_execution_data/common.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from datetime import datetime, timezone
+
+from dataclasses_jsonschema import JsonSchemaMixin
+
+from arcor2.data.common import Project
+from arcor2.data.execution import PackageMeta
+
+
+@dataclass
+class ProjectMeta(JsonSchemaMixin):
+    """Provides data about the project embedded in the execution package."""
+
+    id: str
+    name: str
+    description: str
+    modified: datetime = field(metadata=dict(description="Last modification."))
+
+    @classmethod
+    def from_project(cls, project: Project) -> ProjectMeta:
+        return ProjectMeta(
+            project.id,
+            project.name,
+            project.description,
+            project.modified if project.modified is not None else datetime.fromtimestamp(0, tz=timezone.utc),
+        )
+
+
+@dataclass
+class PackageSummary(JsonSchemaMixin):
+    id: str
+    package_meta: PackageMeta = field(metadata=dict(description="Content of 'package.json'."))
+    project_meta: None | ProjectMeta = None
