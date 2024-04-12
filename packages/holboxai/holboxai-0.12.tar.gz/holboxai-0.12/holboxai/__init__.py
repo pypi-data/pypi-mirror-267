a84e3d1b4b0b3270420853539b894a1ba8c28b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 04:04:16 +0000

    Add package holboxai/0.12/holboxai-0.12.tar.gz

---
+++ packages/holboxai/holboxai-0.12.tar.gz/holboxai-0.12/holboxai/__init__.py
@@ -0,0 +1,12 @@
+# from holboxai.file1 import firstclass
+# from holboxai.file2 import secondclass
+# from holboxai.file3 import sort
+from holboxai.s3_reader import S3DocReader
+from holboxai.docs_query import DocsQuery
+from holboxai.relevant_docs import RelevantDocs
+from holboxai.csv_query import CsvQuery
+from holboxai.text2image import text2image
+from holboxai.sentiment_analysis import SentimentAnalysis
+from holboxai.name_entity_recognition import NameEntityRecognition
+from holboxai.summarizer import Summarizer
+from holboxai.meeting_summarizer import MeetingSummarizer
