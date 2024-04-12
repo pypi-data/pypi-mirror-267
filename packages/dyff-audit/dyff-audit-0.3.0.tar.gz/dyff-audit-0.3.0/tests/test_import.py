238fef5b101ba60bc4f4ec47103667d42e01f47b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:33 +0000

    Add package dyff-audit/0.3.0/dyff-audit-0.3.0.tar.gz

---
+++ packages/dyff-audit/dyff-audit-0.3.0.tar.gz/dyff-audit-0.3.0/tests/test_import.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2024 UL Research Institutes
+# SPDX-License-Identifier: Apache-2.0
+
+import importlib
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "module_name",
+    [
+        "dyff.audit",
+        "dyff.audit.data",
+        "dyff.audit.data.text",
+        "dyff.audit.metrics.base",
+        "dyff.audit.metrics.text",
+        "dyff.audit.metrics",
+        "dyff.audit.scoring.base",
+        "dyff.audit.scoring.example",
+        "dyff.audit.scoring.text",
+        "dyff.audit.scoring",
+        "dyff.audit.scoring.classification",
+        "dyff.audit.workflows",
+    ],
+)
+def test_import_module(module_name):
+    importlib.import_module(module_name)
