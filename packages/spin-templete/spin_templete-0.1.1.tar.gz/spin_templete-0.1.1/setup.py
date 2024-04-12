917c6608a7453536686452297e94bd083a9c516e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:07:07 +0000

    Add package spin-templete/0.1.1/spin_templete-0.1.1.tar.gz

---
+++ packages/spin-templete/spin_templete-0.1.1.tar.gz/spin_templete-0.1.1/setup.py
@@ -0,0 +1,101 @@
+from setuptools import setup, find_packages
+
+
+
+VERSION = '0.1.1'
+DESCRIPTION = "spin templete is the layer of Spin-python-sdk for render html"
+
+
+# Setting up
+setup(
+    name="spin_templete",
+    version=VERSION,
+    author="mominiqbal1234",
+    author_email="<mominiqbal1214@gmail.com>",
+    description=DESCRIPTION,
+    long_description="""
+    
+
+# What is Spin
+Spin is a framework for building and running event-driven microservice applications with WebAssembly (Wasm) components.
+
+# Spin Templete
+
+spin templete is the layer of Spin-python-sdk for render html
+
+# How to install spin_templete
+
+```python
+pip install spin_templete
+```
+
+### setup
+```
+mkdir templete
+cd templete
+touch index.html
+```
+### index.html
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body>
+<h1>Hello World Again Momin</h1>
+
+<ul>
+{% for item in items %}
+    <li>{{ item }}</li>
+{% endfor %}
+</ul>   
+<ul>
+{% for item in mylist %}
+    <li>{{ item }}</li>
+{% endfor %}
+</ul>   
+
+
+
+<h1>{{name}}</h1>
+
+</body>
+</html>
+```
+
+### app.py
+```python
+from spin_sdk.http import IncomingHandler, Request, Response
+from spintemplete.templete import Render
+
+
+html = Render("templete")
+
+class IncomingHandler(IncomingHandler):
+def handle_request(self, request: Request) -> Response:
+return html.render("index.html",items=[1,2,3,4,5],mylist=["hello","world"],name="momin")
+```
+
+
+Check Our Site : https://mefiz.com/about </br>
+Developed by : Momin Iqbal
+
+
+
+    """,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["Jinja2==3.1.3","componentize-py==0.13.2","spin-sdk==3.0.0","mypy==1.8.0"],
+    keywords=['spin-python-sdk','spin''python', 'python webassembly', 'webassembly', 'webassembly in python','webassembly api','build python webassembly api','python to webassembly'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
