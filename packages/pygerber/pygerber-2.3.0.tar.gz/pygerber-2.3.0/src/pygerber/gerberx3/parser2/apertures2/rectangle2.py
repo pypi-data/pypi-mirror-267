9397261898817835ae6b3e6e47218b3202dd5ef9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 14:04:07 +0000

    Add package pygerber/2.3.0/pygerber-2.3.0.tar.gz

---
+++ packages/pygerber/pygerber-2.3.0.tar.gz/pygerber-2.3.0/src/pygerber/gerberx3/parser2/apertures2/rectangle2.py
@@ -0,0 +1,51 @@
+"""Parser level abstraction of rectangle aperture info for Gerber AST parser,
+version 2.
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from pygerber.gerberx3.math.bounding_box import BoundingBox
+from pygerber.gerberx3.math.offset import Offset
+from pygerber.gerberx3.parser2.apertures2.aperture2 import Aperture2
+
+if TYPE_CHECKING:
+    from decimal import Decimal
+
+    from typing_extensions import Self
+
+    from pygerber.gerberx3.parser2.commands2.flash2 import Flash2
+    from pygerber.gerberx3.renderer2.abstract import Renderer2
+
+
+class Rectangle2(Aperture2):
+    """Parser level abstraction of aperture info for rectangle aperture."""
+
+    x_size: Offset
+    y_size: Offset
+    hole_diameter: Optional[Offset]
+
+    def render_flash(self, renderer: Renderer2, command: Flash2) -> None:
+        """Render draw operation."""
+        renderer.hooks.render_flash_rectangle(command, self)
+
+    def get_stroke_width(self) -> Offset:
+        """Return stroke width of aperture."""
+        return (self.x_size + self.y_size) / 2
+
+    def get_bounding_box(self) -> BoundingBox:
+        """Return bounding box of aperture."""
+        return BoundingBox.from_rectangle(self.x_size, self.y_size)
+
+    def get_scaled(self, scale: Decimal) -> Self:
+        """Get copy of this aperture scaled by factor."""
+        return self.model_copy(
+            update={
+                "x_size": self.x_size * scale,
+                "y_size": self.y_size * scale,
+                "hole_diameter": (
+                    None if self.hole_diameter is None else self.hole_diameter * scale
+                ),
+            },
+        )
