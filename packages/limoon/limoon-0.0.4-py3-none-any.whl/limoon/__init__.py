d825480c7a8a3278e19f724388fb8a0872369e99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:03:41 +0000

    Add package limoon/0.0.4/limoon-0.0.4-py3-none-any.whl

---
+++ packages/limoon/limoon-0.0.4-py3-none-any.whl/limoon/__init__.py
@@ -0,0 +1,19 @@
+from limoon.core import *
+from limoon.model import *
+
+
+__all__ = (
+    get_topic,
+    get_entry,
+    get_author,
+    get_author_topic,
+    get_author_rank,
+    get_agenda,
+    get_debe,
+    search_topic,
+    Author,
+    Entry,
+    Rank,
+    Topic,
+)
+__version__ = "0.0.4"
---
+++ packages/limoon/limoon-0.0.4-py3-none-any.whl/limoon/exception.py
@@ -0,0 +1,15 @@
+from typing import Final
+
+SHIT_MESSAGE: Final = "büyük başarısızlıklar sözkonusu"
+
+
+class TopicNotFound(Exception):
+    "The topic record is not available."
+
+
+class EntryNotFound(Exception):
+    "The entry record is not available."
+
+
+class AuthorNotFound(Exception):
+    "The author record is not available."
