807c4c7e542b36a4ad19410b0fec40b1eb4648aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:04:13 +0000

    Add package lddya/4.0.2/lddya-4.0.2.tar.gz

---
+++ packages/lddya/lddya-4.0.2.tar.gz/lddya-4.0.2/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: lidongdong
+# Mail: 927052521@qq.com
+# Created Time: 2022.10.21  19.50
+############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "lddya",
+    version = "4.0.2",
+    keywords = {"pip", "license","licensetool", "tool", "gm"},
+    description = "新增了优化器类别",
+    long_description = "具体功能，请自行挖掘。",
+    license = "MIT Licence",
+
+    url = "https://github.com/not_define/please_wait",
+    author = "lidongdong",
+    author_email = "927052521@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['numpy','matplotlib','pygame','pandas']
+)
