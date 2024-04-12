28dc7155f754836dbd440bf824259216f99f3d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:03:42 +0000

    Add package msfabricpysdkcore/0.0.5/msfabricpysdkcore-0.0.5.tar.gz

---
+++ packages/msfabricpysdkcore/msfabricpysdkcore-0.0.5.tar.gz/msfabricpysdkcore-0.0.5/msfabricpysdkcore/tests/test_items_incl_lakehouse.py
@@ -0,0 +1,103 @@
+import unittest
+from datetime import datetime
+from dotenv import load_dotenv
+from time import sleep
+from msfabricpysdkcore.coreapi import FabricClientCore
+
+class TestFabricClientCore(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestFabricClientCore, self).__init__(*args, **kwargs)
+        #load_dotenv()
+        self.fc = FabricClientCore()
+        self.workspace_id = "c3352d34-0b54-40f0-b204-cc964b1beb8d"
+
+        datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+        self.item_name = "testitem" + datetime_str
+        self.item_type = "Notebook"
+    
+    def test_item_end_to_end(self):
+
+        item = self.fc.create_item(display_name=self.item_name, type=self.item_type, workspace_id=self.workspace_id) 
+        self.assertEqual(item.display_name, self.item_name)
+        self.assertEqual(item.type, self.item_type)
+        self.assertEqual(item.workspace_id, self.workspace_id)
+        self.assertEqual(item.description, "")
+
+        item = self.fc.get_item(workspace_id=self.workspace_id, item_id=item.id)
+        item_ = self.fc.get_item(workspace_id=self.workspace_id,
+                                  item_name=self.item_name, item_type=self.item_type)
+        self.assertEqual(item.id, item_.id)
+        self.assertEqual(item.display_name, self.item_name)
+        self.assertEqual(item.type, self.item_type)
+        self.assertEqual(item.workspace_id, self.workspace_id)
+        self.assertEqual(item.description, "")
+
+        item_list = self.fc.list_items(workspace_id=self.workspace_id)
+        self.assertTrue(len(item_list) > 0)
+
+        item_ids = [item_.id for item_ in item_list]
+        self.assertIn(item.id, item_ids)
+
+        self.fc.update_item(workspace_id=self.workspace_id, item_id=item.id, display_name=f"u{self.item_name}")
+        item = self.fc.get_item(workspace_id=self.workspace_id, item_id=item.id)
+        self.assertEqual(item.display_name, f"u{self.item_name}")
+
+        status_code = self.fc.delete_item(workspace_id=self.workspace_id, item_id=item.id)
+
+        self.assertAlmostEqual(status_code, 200)
+
+    def test_item_definition(self):
+
+        sjd = self.fc.get_item(workspace_id=self.workspace_id, item_name="blubb", item_type="SparkJobDefinition")
+        self.assertIsNotNone(sjd.definition)
+        datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+        blubb2 = "blubb2" + datetime_str
+        blubb3 = "blubb3" + datetime_str
+        blubb2 = self.fc.create_item(display_name=blubb2, type="SparkJobDefinition", workspace_id=self.workspace_id,
+                                   definition=sjd.definition)
+
+        blubb3 =  self.fc.create_item(display_name=blubb3, type="SparkJobDefinition", workspace_id=self.workspace_id)
+
+        blubb3 = self.fc.update_item_definition(workspace_id=self.workspace_id,
+                                                item_id=blubb3.id, definition=sjd.definition)
+        
+        self.assertEqual(blubb3.definition, sjd.definition)
+        
+        self.assertNotEqual(blubb2.id, sjd.id)
+        self.assertEqual(blubb2.definition, sjd.definition)
+        self.assertNotEqual(blubb2.id, blubb3.id)
+        
+        blubb2.delete()
+        blubb3.delete()
+
+    def test_warehouse(self):
+        warehouse = self.fc.get_item(workspace_id=self.workspace_id, item_name="testitem20240404134030", item_type="Warehouse")
+        self.assertIsNotNone(warehouse.properties)
+
+    def test_spark_job_definition(self):
+        spark_job_definition = self.fc.get_item(workspace_id=self.workspace_id,
+                                                 item_name="blubb", item_type="SparkJobDefinition")
+        self.assertIsNotNone(spark_job_definition.properties)
+
+    def test_lakehouse(self):
+
+        lakehouse = self.fc.get_item(workspace_id=self.workspace_id, item_name="lakehouse1", item_type="Lakehouse")
+        self.assertIsNotNone(lakehouse.properties)
+        item_id = lakehouse.id
+        date_str = datetime.now().strftime("%Y%m%d%H%M%S")
+        table_name = f"table{date_str}"
+
+
+        status_code = self.fc.load_table(workspace_id=self.workspace_id, item_id=item_id, table_name=table_name, 
+                                         path_type="File", relative_path="Files/folder1/titanic.csv")
+
+        self.assertEqual(status_code, 202)
+
+        table_list = self.fc.list_tables(workspace_id=self.workspace_id, item_id=item_id)
+        table_names = [table["name"] for table in table_list]
+
+        self.assertIn(table_name, table_names)
+
+if __name__ == "__main__":
+    unittest.main()
