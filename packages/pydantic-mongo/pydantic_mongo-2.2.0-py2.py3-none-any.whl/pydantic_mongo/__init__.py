f0174b7a14a627ed28a695e0317c166dc3b1dc79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 14:04:16 +0000

    Add package pydantic-mongo/2.2.0/pydantic_mongo-2.2.0-py2.py3-none-any.whl

---
+++ packages/pydantic-mongo/pydantic_mongo-2.2.0-py2.py3-none-any.whl/pydantic_mongo/__init__.py
@@ -0,0 +1,5 @@
+from .abstract_repository import AbstractRepository
+from .fields import ObjectIdField
+from .version import __version__  # noqa: F401
+
+__all__ = ["ObjectIdField", "AbstractRepository"]
