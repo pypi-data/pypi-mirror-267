70c627b65e89aca5927d182cf7b76c1bbc380632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 04:04:41 +0000

    Add package fremake-canopy/0.1.4/fremake_canopy-0.1.4.tar.gz

---
+++ packages/fremake-canopy/fremake_canopy-0.1.4.tar.gz/fremake_canopy-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fremake_canopy',
+    version='0.1.4',
+    description='Implementation of fremake',
+    author='Thomas Robinson, Dana Singh',
+    author_email='gfdl.climate.mode.info@noaa.gov',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pyyaml',
+        'argparse',
+        'jsonschema',
+    ],
+)
