fdcfc464fb3fc20ad405a9611211e4bf0ec0d766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 14:04:34 +0000

    Add package types-aiobotocore-bedrock/2.12.3/types_aiobotocore_bedrock-2.12.3-py3-none-any.whl

---
+++ packages/types-aiobotocore-bedrock/types_aiobotocore_bedrock-2.12.3-py3-none-any.whl/types_aiobotocore_bedrock/__init__.pyi
@@ -0,0 +1,43 @@
+"""
+Main interface for bedrock service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_bedrock import (
+        BedrockClient,
+        Client,
+        ListCustomModelsPaginator,
+        ListModelCustomizationJobsPaginator,
+        ListProvisionedModelThroughputsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("bedrock") as client:
+        client: BedrockClient
+        ...
+
+
+    list_custom_models_paginator: ListCustomModelsPaginator = client.get_paginator("list_custom_models")
+    list_model_customization_jobs_paginator: ListModelCustomizationJobsPaginator = client.get_paginator("list_model_customization_jobs")
+    list_provisioned_model_throughputs_paginator: ListProvisionedModelThroughputsPaginator = client.get_paginator("list_provisioned_model_throughputs")
+    ```
+"""
+
+from .client import BedrockClient
+from .paginator import (
+    ListCustomModelsPaginator,
+    ListModelCustomizationJobsPaginator,
+    ListProvisionedModelThroughputsPaginator,
+)
+
+Client = BedrockClient
+
+__all__ = (
+    "BedrockClient",
+    "Client",
+    "ListCustomModelsPaginator",
+    "ListModelCustomizationJobsPaginator",
+    "ListProvisionedModelThroughputsPaginator",
+)
