1b81f1aa7ec0f4ed42b81a91d4db00b317a4281a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 14:04:20 +0000

    Add package open-space-toolkit-mathematics/3.0.2/open_space_toolkit_mathematics-3.0.2-py38-none-manylinux2014_aarch64.whl

---
+++ packages/open-space-toolkit-mathematics/open_space_toolkit_mathematics-3.0.2-py38-none-manylinux2014_aarch64.whl/ostk/mathematics/test/geometry/d3/transformation/rotation/test_rotation_matrix.py
@@ -0,0 +1,24 @@
+# Apache License 2.0
+
+from ostk.mathematics import geometry
+
+
+Angle = geometry.Angle
+Quaternion = geometry.d3.transformation.rotation.Quaternion
+RotationMatrix = geometry.d3.transformation.rotation.RotationMatrix
+
+
+# isDefined
+# getRowAt
+# getColumnAt
+# toTransposed
+# transpose
+# Undefined
+# Unit
+# RX
+# RY
+# RZ
+# Rows
+# Columns
+# Quaternion
+# RotationVector
