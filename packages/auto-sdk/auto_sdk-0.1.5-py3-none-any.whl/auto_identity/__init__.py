19894f71935fc467fe3cfea287a646af39f63948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:03:40 +0000

    Add package auto-sdk/0.1.5/auto_sdk-0.1.5-py3-none-any.whl

---
+++ packages/auto-sdk/auto_sdk-0.1.5-py3-none-any.whl/auto_identity/__init__.py
@@ -0,0 +1,40 @@
+"""
+Auto Identity Module
+
+This module provides functionalities for managing digital identities within the Auto SDK, 
+including key generation, Auto ID management, and Auto ID registration and verification.
+"""
+
+from substrateinterface import Keypair
+from .key_management import (
+    generate_rsa_key_pair,
+    generate_ed25519_key_pair,
+    key_to_hex,
+    key_to_pem,
+    pem_to_private_key,
+    load_private_key,
+    pem_to_public_key,
+    load_public_key,
+    save_key)
+from .certificate_manager import CertificateManager
+from .registry import Registry
+from .utils import der_encode_signature_algorithm_oid, blake2b_256
+
+__version__ = '0.1.5'
+
+__all__ = [
+    "generate_rsa_key_pair",
+    "generate_ed25519_key_pair",
+    "key_to_hex",
+    "load_private_key",
+    "load_public_key",
+    "save_key",
+    "pem_to_private_key",
+    "pem_to_public_key",
+    "key_to_pem",
+    "CertificateManager",
+    "der_encode_signature_algorithm_oid",
+    "blake2b_256",
+    "Registry",
+    "Keypair",
+]
