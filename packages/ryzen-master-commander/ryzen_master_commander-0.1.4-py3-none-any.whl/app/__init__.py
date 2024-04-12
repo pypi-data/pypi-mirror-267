c10bb8c276a3cadd97d664ce901472313bb8686e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:03:27 +0000

    Add package ryzen-master-commander/0.1.4/ryzen_master_commander-0.1.4-py3-none-any.whl

---
+++ packages/ryzen-master-commander/ryzen_master_commander-0.1.4-py3-none-any.whl/app/__init__.py
@@ -0,0 +1,8 @@
+import ttkbootstrap as ttk
+from app.main_window import MainWindow
+
+def main():
+    root = ttk.Window(themename="darkly")
+    root.title("Ryzen Master and Commander")
+    app = MainWindow(root)
+    root.mainloop()
