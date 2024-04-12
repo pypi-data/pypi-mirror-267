fe4242107be970f42f20264512b886a9924c4d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:14 +0000

    Add package pyloggermanager/0.1.3/pyloggermanager-0.1.3-py3-none-any.whl

---
+++ packages/pyloggermanager/pyloggermanager-0.1.3-py3-none-any.whl/pyloggermanager/textstyles/__init__.py
@@ -0,0 +1,23 @@
+__all__ = [
+    "TextBackgroundColor",
+    "TextColor",
+    "TextEffect"
+]
+__name__ = "pyloggermanager.textstyles"
+__description__ = """
+The pyloggermanager.textstyles package provides utilities for defining and applying text styles
+to log messages within the logger manager framework. It includes classes for specifying text
+colors, background colors, and text effects, allowing users to customize the appearance of log
+messages according to their preferences.
+
+This package is designed to enhance the visual representation of log messages by providing a
+flexible and intuitive way to apply various text styles. By incorporating these text styles
+into log messages, users can improve readability, emphasize important information, and differentiate
+between different types of log entries.
+
+Overall, the pyloggermanager.textstyles package complements the logger manager framework by
+offering tools for creating visually appealing and informative log messages, contributing to a
+more effective logging experience.
+"""
+
+from pyloggermanager.textstyles.__main__ import TextBackgroundColor, TextColor, TextEffect
