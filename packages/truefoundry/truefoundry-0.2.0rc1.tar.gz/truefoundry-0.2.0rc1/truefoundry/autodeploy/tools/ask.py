903db8acf72a79e45f1bdb12379a41386e2806ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:07:20 +0000

    Add package truefoundry/0.2.0rc1/truefoundry-0.2.0rc1.tar.gz

---
+++ packages/truefoundry/truefoundry-0.2.0rc1.tar.gz/truefoundry-0.2.0rc1/truefoundry/autodeploy/tools/ask.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import Iterable
+
+from rich.console import Console
+
+from truefoundry.autodeploy.tools.base import Event, RequestEvent, ResponseEvent, Tool
+
+
+class AskQuestion(Event):
+    question: str
+
+    def render(self, console: Console) -> str:
+        console.print(f"[bold magenta]TFY-Agent:[/] {self.question}")
+        response = console.input("[bold green]You:[/] ")
+        return response
+
+
+class Ask(Tool):
+    description = """
+Ask a question to the user.
+"""
+
+    class Request(RequestEvent):
+        question: str
+
+    class Response(ResponseEvent):
+        response: str
+
+    def run(self, request: Ask.Request) -> Iterable[Event]:
+        response = yield AskQuestion(**request.model_dump())
+        return Ask.Response(response=response)
