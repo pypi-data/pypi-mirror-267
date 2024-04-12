1e703a19a18d3424eba4f521c56464e103e2619e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:16 +0000

    Add package clip-util/0.1.25/clip_util-0.1.25-py3-none-any.whl

---
+++ packages/clip-util/clip_util-0.1.25-py3-none-any.whl/clipboard/__init__.py
@@ -0,0 +1,35 @@
+from clipboard.clipboard import Clipboard
+from clipboard.clipboard import ClipboardFormat
+from clipboard.clipboard import get_available_formats
+from clipboard.clipboard import get_clipboard
+from clipboard.clipboard import set_clipboard
+from clipboard.errors import ClipboardError
+from clipboard.errors import EmptyClipboardError
+from clipboard.errors import FormatNotSupportedError
+from clipboard.errors import GetClipboardError
+from clipboard.errors import LockError
+from clipboard.errors import OpenClipboardError
+from clipboard.errors import SetClipboardError
+from clipboard.html_clipboard import HTML_ENCODING
+from clipboard.formats import get_format_name
+
+
+__all__ = [
+    "Clipboard",
+    "HTML_ENCODING",
+    # Formats
+    "ClipboardFormat",
+    "get_format_name",
+    # Convenience Functions
+    "get_available_formats",
+    "get_clipboard",
+    "set_clipboard",
+    # Errors
+    "ClipboardError",
+    "EmptyClipboardError",
+    "FormatNotSupportedError",
+    "GetClipboardError",
+    "LockError",
+    "OpenClipboardError",
+    "SetClipboardError",
+]
