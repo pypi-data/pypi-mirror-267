4728c882dcab7391d7890d09a465387439268dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 14:04:09 +0000

    Add package l2m2/0.0.3/l2m2-0.0.3.tar.gz

---
+++ packages/l2m2/l2m2-0.0.3.tar.gz/l2m2-0.0.3/README.md
@@ -0,0 +1,7 @@
+# L2M2: Simple LLM Manager for Python
+
+[L2M2](https://pypi.org/project/l2m2/) ("LLM Manager" &rarr; "LLMM" &rarr; "L2M2") is a simple LLM manager for Python.
+
+I'll complete the rest of this readme soon.
+
+The link is [asdf](google.com)
---
+++ packages/l2m2/l2m2-0.0.3.tar.gz/l2m2-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name="l2m2",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=[
+        "cohere>=5.2.5",
+        "openai>=1.17.0",
+        "anthropic>=0.25.1",
+        "groq>=0.5.0",
+    ],
+    long_description=readme,
+    long_description_content_type="text/markdown",
+)
