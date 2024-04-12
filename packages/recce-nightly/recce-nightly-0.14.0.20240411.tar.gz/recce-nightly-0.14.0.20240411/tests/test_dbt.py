e182b32b0f743ad6df43558f5c1392e4ea568037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:03:47 +0000

    Add package recce-nightly/0.14.0.20240411/recce-nightly-0.14.0.20240411.tar.gz

---
+++ packages/recce-nightly/recce-nightly-0.14.0.20240411.tar.gz/recce-nightly-0.14.0.20240411/tests/test_dbt.py
@@ -0,0 +1,28 @@
+import os
+
+import pytest
+from packaging import version
+
+import recce.dbt as dbt
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+@pytest.mark.skipif(dbt.dbt_version < version.parse('v1.6'), reason='skip manifest test before dbt-core 1.6')
+def test_load_lineage():
+    manifest = dbt.load_manifest(path=os.path.join(current_dir, 'manifest.json'))
+    assert manifest is not None
+
+    catalog = dbt.load_catalog(path=os.path.join(current_dir, 'catalog.json'))
+    assert catalog is not None
+
+    dbt_context = dbt.DBTContext(curr_manifest=manifest)
+    lineage = dbt_context.get_lineage()
+    assert lineage is not None
+    assert lineage['nodes']['model.jaffle_shop.orders'] is not None
+    assert 'columns' not in lineage['nodes']['model.jaffle_shop.orders']
+
+    dbt_context = dbt.DBTContext(curr_manifest=manifest, curr_catalog=catalog)
+    lineage = dbt_context.get_lineage()
+    assert lineage is not None
+    assert len(lineage['nodes']['model.jaffle_shop.orders']['columns']) == 16
