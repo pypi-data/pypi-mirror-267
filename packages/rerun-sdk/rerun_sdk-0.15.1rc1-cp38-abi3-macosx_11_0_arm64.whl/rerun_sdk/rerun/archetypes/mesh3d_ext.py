bf36ad178dcf553937896acdb99a6bf20fa22cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:03:56 +0000

    Add package rerun-sdk/0.15.1rc1/rerun_sdk-0.15.1rc1-cp38-abi3-macosx_11_0_arm64.whl

---
+++ packages/rerun-sdk/rerun_sdk-0.15.1rc1-cp38-abi3-macosx_11_0_arm64.whl/rerun_sdk/rerun/archetypes/mesh3d_ext.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from typing import Any
+
+import numpy.typing as npt
+
+from .. import datatypes
+from ..error_utils import catch_and_log_exceptions
+
+
+class Mesh3DExt:
+    """Extension for [Mesh3D][rerun.archetypes.Mesh3D]."""
+
+    def __init__(
+        self: Any,
+        *,
+        vertex_positions: datatypes.Vec3DArrayLike,
+        indices: npt.ArrayLike | None = None,
+        mesh_properties: datatypes.MeshPropertiesLike | None = None,
+        vertex_normals: datatypes.Vec3DArrayLike | None = None,
+        vertex_colors: datatypes.Rgba32ArrayLike | None = None,
+        vertex_texcoords: datatypes.Vec2DArrayLike | None = None,
+        albedo_texture: datatypes.TensorDataLike | None = None,
+        mesh_material: datatypes.MaterialLike | None = None,
+        class_ids: datatypes.ClassIdArrayLike | None = None,
+    ):
+        """
+        Create a new instance of the Mesh3D archetype.
+
+        Parameters
+        ----------
+        vertex_positions:
+            The positions of each vertex.
+            If no `indices` are specified, then each triplet of positions is interpreted as a triangle.
+        indices:
+            If specified, a flattened array of indices that describe the mesh's triangles,
+            i.e. its length must be divisible by 3.
+            Mutually exclusive with `mesh_properties`.
+        mesh_properties:
+            Optional properties for the mesh as a whole (including indexed drawing).
+            Mutually exclusive with `indices`.
+        vertex_normals:
+            An optional normal for each vertex.
+            If specified, this must have as many elements as `vertex_positions`.
+        vertex_texcoords:
+            An optional texture coordinate for each vertex.
+            If specified, this must have as many elements as `vertex_positions`.
+        vertex_colors:
+            An optional color for each vertex.
+        mesh_material:
+            Optional material properties for the mesh as a whole.
+        albedo_texture:
+            Optional albedo texture. Used with `vertex_texcoords` on `Mesh3D`.
+            Currently supports only sRGB(A) textures, ignoring alpha.
+            (meaning that the tensor must have 3 or 4 channels and use the `u8` format)
+        class_ids:
+            Optional class Ids for the vertices.
+            The class ID provides colors and labels if not specified explicitly.
+
+        """
+        with catch_and_log_exceptions(context=self.__class__.__name__):
+            if indices is not None:
+                if mesh_properties is not None:
+                    raise ValueError("indices and mesh_properties are mutually exclusive")
+                mesh_properties = datatypes.MeshProperties(indices=indices)
+
+            # You can define your own __init__ function as a member of Mesh3DExt in mesh3d_ext.py
+            self.__attrs_init__(
+                vertex_positions=vertex_positions,
+                mesh_properties=mesh_properties,
+                vertex_normals=vertex_normals,
+                vertex_colors=vertex_colors,
+                vertex_texcoords=vertex_texcoords,
+                albedo_texture=albedo_texture,
+                mesh_material=mesh_material,
+                class_ids=class_ids,
+            )
+            return
+
+        self.__attrs_clear__()
