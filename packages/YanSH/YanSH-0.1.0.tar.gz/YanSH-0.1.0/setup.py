0cb8dfc211c044162bdde03542bd33a26a3fa2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:54 +0000

    Add package YanSH/0.1.0/YanSH-0.1.0.tar.gz

---
+++ packages/YanSH/YanSH-0.1.0.tar.gz/YanSH-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="YanSH",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'PyQt5>=5.15.0'
+    ],
+
+    author="PyllerAya",
+    author_email="yansh9773@163.com",
+    description="一个用于GUI界面设计的库，适用于PyQt5e",
+    url="https://github.com/PyllerAya/YanSH"
+)
