eed165f397556a5d55971e31f9b24d24afc36690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 04:04:45 +0000

    Add package st-input-slider/0.0.4/st-input-slider-0.0.4.tar.gz

---
+++ packages/st-input-slider/st-input-slider-0.0.4.tar.gz/st-input-slider-0.0.4/st_input_slider/example.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+from st_input_slider import st_input_slider
+
+st.title("Slider + Input Example!")
+options = {
+    "color": "#9E9A35",
+    "disableUnderline": True,
+    "marks": False,
+}
+slider_value = st_input_slider(
+    label="A custom slider", min_value=-100, max_value=200, value=50, step=0.1, options=options)
+st.markdown("Value: %s" % (slider_value))
+
+with st.sidebar:
+    st.markdown("This is another slider with all default options:")
+    value = st_input_slider()
+    value
