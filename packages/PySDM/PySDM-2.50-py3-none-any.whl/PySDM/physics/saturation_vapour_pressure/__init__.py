530e5bc96d0bca7a1bb4bb3b679a51e5ed9eef4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:31 +0000

    Add package PySDM/2.50/PySDM-2.50-py3-none-any.whl

---
+++ packages/PySDM/PySDM-2.50-py3-none-any.whl/PySDM/physics/saturation_vapour_pressure/__init__.py
@@ -0,0 +1,8 @@
+"""
+Alternative formulations of saturation vapour pressure temperature dependence approximations
+"""
+
+from .august_roche_magnus import AugustRocheMagnus
+from .flatau_walko_cotton import FlatauWalkoCotton
+from .lowe1977 import Lowe1977
+from .murphy_koop_2005 import MurphyKoop2005
