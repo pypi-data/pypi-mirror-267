e8f77a6ba334296269012a5aa589df2c498f83aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:51 +0000

    Add package thanosql/0.2.1/thanosql-0.2.1-py3-none-any.whl

---
+++ packages/thanosql/thanosql-0.2.1-py3-none-any.whl/thanosql/_client.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+import os
+from typing import Optional
+
+from thanosql._base_client import ThanoSQLBaseClient
+from thanosql._error import ThanoSQLValueError
+from thanosql.resources import (
+    FileService,
+    QueryService,
+    SchemaService,
+    TableService,
+    ViewService,
+)
+
+
+class ThanoSQL(ThanoSQLBaseClient):
+    def __init__(
+        self,
+        api_token: Optional[str] = None,
+        engine_url: Optional[str] = None,
+        api_version: str = "v1",
+    ) -> None:
+        if api_token is None:
+            api_token = os.environ.get("THANOSQL_API_TOKEN", "")
+        if not api_token:
+            raise ThanoSQLValueError(
+                "Please input a valid API token. You can do this either by passing it as a parameter or setting the THANOSQL_API_TOKEN environment variable."
+            )
+
+        if engine_url is None:
+            engine_url = os.environ.get("THANOSQL_ENGINE_URL", "")
+        if not engine_url:
+            raise ThanoSQLValueError(
+                "Please input a valid engine URL. You can do this either by passing it as a parameter or setting the THANOSQL_ENGINE_URL environment variable."
+            )
+
+        super().__init__(token=api_token, base_url=engine_url, version=api_version)
+
+    @property
+    def query(self) -> QueryService:
+        """Access the QueryService."""
+        return QueryService(self)
+
+    @property
+    def file(self) -> FileService:
+        """Access the FileService."""
+        return FileService(self)
+
+    @property
+    def schema(self) -> SchemaService:
+        """Access the SchemaService."""
+        return SchemaService(self)
+
+    @property
+    def table(self) -> TableService:
+        """Access the TableService."""
+        return TableService(self)
+
+    @property
+    def view(self) -> ViewService:
+        """Access the ViewService."""
+        return ViewService(self)
---
+++ packages/thanosql/thanosql-0.2.1-py3-none-any.whl/thanosql/resources/_view.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Optional
+
+from pydantic import Field, TypeAdapter
+
+from thanosql._service import ThanoSQLService
+from thanosql.resources import Column
+from thanosql.resources._model import BaseModel
+
+if TYPE_CHECKING:
+    from thanosql._client import ThanoSQL
+
+
+class View(BaseModel):
+    name: str
+    table_schema: Optional[str] = Field(alias="schema", default=None)
+    columns: List[Column] = []
+    definition: str = ""
+
+
+class ViewService(ThanoSQLService):
+    def __init__(self, client: ThanoSQL) -> None:
+        super().__init__(client=client, tag="view")
+
+    def list(
+        self,
+        schema: Optional[str] = None,
+        verbose: Optional[bool] = None,
+        offset: Optional[int] = None,
+        limit: Optional[int] = None,
+    ) -> List[View]:
+        path = f"/{self.tag}/"
+        query_params = self._create_input_dict(
+            schema=schema,
+            verbose=verbose,
+            offset=offset,
+            limit=limit,
+        )
+
+        raw_response = self.client._request(
+            method="get", path=path, query_params=query_params
+        )
+
+        views_adapter = TypeAdapter(List[View])
+        parsed_response = views_adapter.validate_python(raw_response["views"])
+        return parsed_response
+
+    def get(self, name: str, schema: Optional[str] = None) -> View:
+        path = f"/{self.tag}/{name}"
+        query_params = self._create_input_dict(schema=schema)
+
+        raw_response = self.client._request(
+            method="get", path=path, query_params=query_params
+        )
+
+        view_adapter = TypeAdapter(View)
+        parsed_response = view_adapter.validate_python(raw_response["view"])
+        return parsed_response
+
+    def delete(self, name: str, schema: Optional[str] = None) -> dict:
+        path = f"/{self.tag}/{name}"
+        query_params = self._create_input_dict(schema=schema)
+
+        return self.client._request(
+            method="delete", path=path, query_params=query_params
+        )
