65e35b6e3481059e0039aa6697aa8b28b406a173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:03:36 +0000

    Add package prefect/2.17.0/prefect-2.17.0.tar.gz

---
+++ packages/prefect/prefect-2.17.0.tar.gz/prefect-2.17.0/src/prefect/server/ui/assets/404-Bc9DX7l3.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,u as a,o as t,cm as r}from"./index-BTiRcrar.js";import{u as s}from"./usePageTitle-YKBWMPdU.js";const _=e({__name:"404",setup(n){return s("Page Not Found"),(c,u)=>(t(),o(a(r)))}});export{_ as default};
+//# sourceMappingURL=404-Bc9DX7l3.js.map
---
+++ packages/prefect/prefect-2.17.0.tar.gz/prefect-2.17.0/src/prefect/server/ui/assets/usePageTitle-YKBWMPdU.js
@@ -0,0 +1,2 @@
+import{e as o,ar as u,u as c}from"./index-BTiRcrar.js";function i(...t){const r=[...t,"Prefect Server"],a=o(()=>r.map(e=>c(e)).filter(e=>e!==null).join(" • "));u(()=>document.title=a.value)}export{i as u};
+//# sourceMappingURL=usePageTitle-YKBWMPdU.js.map
