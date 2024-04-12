503f8789fea355c19f8e5598397126c96993ac5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:36 +0000

    Add package list-basculas/0.1/list_basculas-0.1.tar.gz

---
+++ packages/list-basculas/list_basculas-0.1.tar.gz/list_basculas-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='list_basculas',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'Django',
+        # Add any other dependencies here
+    ],
+)
