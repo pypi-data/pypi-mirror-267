870e6c8a1145a63bdcb8803d3bb02c0349866849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:03:50 +0000

    Add package egypt/0.0.1/egypt-0.0.1.tar.gz

---
+++ packages/egypt/egypt-0.0.1.tar.gz/egypt-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='egypt',
+    version='0.0.1',
+    description='A small example packag for pyramid',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
