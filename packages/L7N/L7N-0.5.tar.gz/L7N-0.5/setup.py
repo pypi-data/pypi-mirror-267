80315d9ae7c7d178619a551d0aec90343b121886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:04:02 +0000

    Add package L7N/0.5/L7N-0.5.tar.gz

---
+++ packages/L7N/L7N-0.5.tar.gz/L7N-0.5/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+    name='L7N',
+    version='0.5',
+    author='Programmer L7N - Fahal ',
+    description='Module By @g_4_q In telegram ',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
