8fcc1ab7e5f8cc81bdf139131a326500e038848c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:07:43 +0000

    Add package docker-inspector/0.1.10/docker_inspector-0.1.10.tar.gz

---
+++ packages/docker-inspector/docker_inspector-0.1.10.tar.gz/docker_inspector-0.1.10/docker_inspector/app.py
@@ -0,0 +1,51 @@
+from textual.app import App, ComposeResult
+from textual.screen import Screen
+from textual.containers import ScrollableContainer
+from textual.widgets import Header, Footer, Button, Static, DataTable, Log, Label, TabbedContent, TabPane, Placeholder
+from textual.reactive import reactive
+
+from .widgets.container_list import ContainerList
+
+
+# class TestScreen(Screen):
+#     def compose(self) -> ComposeResult:
+#         yield Header()
+#         yield Footer()
+#         yield ScrollableContainer(Static("Hello, world!"))
+
+
+class DockerInspectorApp(App):
+    TITLE = "Docker Inspector"
+    CSS_PATH = 'styles/main.css'
+    BINDINGS = [
+        ("r", "refresh", "Refresh"),
+        ("d", "toggle_dark", "Toggle dark mode"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+        with TabbedContent():
+            with TabPane("Containers"):
+                yield ContainerList()
+            with TabPane("Volumes"):
+                yield Placeholder("TODO: Volumes", id="volumes")
+
+    def action_refresh(self) -> None:
+        """An action to refresh the container list."""
+        container_list = self.query_one(ContainerList)
+        container_list.refresh_data()
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
+
+
+def run():
+    app = DockerInspectorApp()
+    app.run()
+
+
+if __name__ == "__main__":
+    run()
