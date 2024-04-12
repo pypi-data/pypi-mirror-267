eef38fca222cebf0472df8bd42c0b1c10903b8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:20 +0000

    Add package pandas-datatypes/1.1.2/pandas_datatypes-1.1.2.tar.gz

---
+++ packages/pandas-datatypes/pandas_datatypes-1.1.2.tar.gz/pandas_datatypes-1.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pandas_datatypes",
+    version="1.1.2",
+    author="Brajesh",
+    description="A library for getting correct datatype from pandas DataFrame",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=["pandas_datatypes", "pandas_datatypes.*"]),
+    install_requires=[
+        "pandas",
+    ],
+)
