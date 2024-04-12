303fac8ecfdbd0aedff14029c446d33f09952d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:04:20 +0000

    Add package modelpack/2.2.12/modelpack-2.2.12-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/modelpack/modelpack-2.2.12-cp38-cp38-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/deepview/modelpack/trainer/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 by Au-Zone Technologies.  All Rights Reserved.
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential.
+#
+# This source code is provided solely for runtime interpretation by Python.
+# Modifying or copying any source code is explicitly forbidden.
+
+# Silence over zealous tensorflow autograd logging...
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+
+from deepview.modelpack.trainer.detection import DetectionTrainer
+from deepview.modelpack.trainer.segmentation import SegmentationTrainer
