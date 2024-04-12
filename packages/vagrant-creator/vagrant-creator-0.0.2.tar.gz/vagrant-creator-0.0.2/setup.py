72d64d8843291c441f27af0c4b872c690cd2b2b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:07:10 +0000

    Add package vagrant-creator/0.0.2/vagrant-creator-0.0.2.tar.gz

---
+++ packages/vagrant-creator/vagrant-creator-0.0.2.tar.gz/vagrant-creator-0.0.2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name='vagrant-creator',
+    version='0.0.2',
+    author='Chandula Jayathilake',
+    author_email='chandulaj3000@gmail.com',
+    description='A tool to generate Vagrant init files with custom configurations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ChandulaJ/vagrant-creator',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Utilities',
+    ],
+    package_data={
+        '': ['*.sh'],
+    },
+entry_points={
+    'console_scripts': [
+        'vagrant-creator=vagrant_creator:generate_vagrantfile',
+
+    ],
+},
+    include_package_data=True,
+    install_requires=[],
+)
+
