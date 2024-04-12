50863cdd188ec06a005c90f998c102c05b55539a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:07:12 +0000

    Add package pure-cv/0.1.7/pure-cv-0.1.7.tar.gz

---
+++ packages/pure-cv/pure-cv-0.1.7.tar.gz/pure-cv-0.1.7/src/pure_cv/__init__.py
@@ -0,0 +1,11 @@
+from .opencv import imread, grayscale, threshold, dilate, erode, cvtColor, imdecode
+try:
+  from .plot import show
+except ImportError:
+  ...
+from .encoding import b64encode, b64decode, encode, decode
+from .colors import mod_color
+from .misc import black
+from .rescaling import descale_h, rescale_h
+from .rotation import rotate, Rotation
+from . import draw
