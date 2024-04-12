2ca635b54b14ae55f310a77da70c57af7698393d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 15:03:55 +0000

    Add package chiefgram/4.0/chiefgram-4.0.tar.gz

---
+++ packages/chiefgram/chiefgram-4.0.tar.gz/chiefgram-4.0/chiefgram/methods/decorators/on_disconnect.py
@@ -0,0 +1,43 @@
+#  chiefgram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2024-present Dan <https://github.com/delivrance>
+#
+#  This file is part of chiefgram.
+#
+#  chiefgram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  chiefgram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with chiefgram.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Callable
+
+import chiefgram
+
+
+class OnDisconnect:
+    def on_disconnect(self=None) -> Callable:
+        """Decorator for handling disconnections.
+
+        This does the same thing as :meth:`~chiefgram.Client.add_handler` using the
+        :obj:`~chiefgram.handlers.DisconnectHandler`.
+        """
+
+        def decorator(func: Callable) -> Callable:
+            if isinstance(self, chiefgram.Client):
+                self.add_handler(chiefgram.handlers.DisconnectHandler(func))
+            else:
+                if not hasattr(func, "handlers"):
+                    func.handlers = []
+
+                func.handlers.append((chiefgram.handlers.DisconnectHandler(func), 0))
+
+            return func
+
+        return decorator
