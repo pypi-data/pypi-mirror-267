d3008a3385d1af2d93858c91a1f803af7e0b1a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 04:05:36 +0000

    Add package cognite-sdk/7.34.0/cognite_sdk-7.34.0.tar.gz

---
+++ packages/cognite-sdk/cognite_sdk-7.34.0.tar.gz/cognite_sdk-7.34.0/cognite/client/_constants.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+try:
+    from pyodide.ffi import IN_BROWSER  # type: ignore [import-not-found]
+except ModuleNotFoundError:
+    IN_BROWSER = False
+
+_RUNNING_IN_BROWSER = IN_BROWSER
+DEFAULT_LIMIT_READ = 25
+# Max JavaScript-safe integer 2^53 - 1
+MAX_VALID_INTERNAL_ID = 9007199254740991
+DATA_MODELING_DEFAULT_LIMIT_READ = 10
