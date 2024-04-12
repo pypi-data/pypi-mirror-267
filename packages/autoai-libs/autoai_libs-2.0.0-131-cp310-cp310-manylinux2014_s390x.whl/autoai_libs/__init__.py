bf713a3157682954f609fb66e80d48d5d07410cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:07:53 +0000

    Add package autoai-libs/2.0.0/autoai_libs-2.0.0-131-cp310-cp310-manylinux2014_s390x.whl

---
+++ packages/autoai-libs/autoai_libs-2.0.0-131-cp310-cp310-manylinux2014_s390x.whl/autoai_libs/__init__.py
@@ -0,0 +1,25 @@
+################################################################################
+# IBM Confidential
+# OCO Source Materials
+# 5737-H76, 5725-W78, 5900-A1R, 5737-L65
+# (c) Copyright IBM Corp. 2019-2024. All Rights Reserved.
+# The source code for this program is not published or otherwise divested of its trade secrets,
+# irrespective of what has been deposited with the U.S. Copyright Office.
+################################################################################
+
+
+### Uncomment, once we are ready to expose this functionality
+
+# pkg_name = "autoai-libs"
+#
+# try:
+#     from importlib.metadata import version
+#     __version__ = version(pkg_name)
+#
+# except (ModuleNotFoundError, AttributeError):
+#     try:
+#         from importlib_metadata import version as imp_lib_ver
+#         __version__ = imp_lib_ver(pkg_name)
+#
+#     except ModuleNotFoundError:
+#         __version__ = None
