0c7747904a79b29e4c4cc05e56adc8a5ec01ff56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:03:49 +0000

    Add package preprocess-docs/0.0.2/preprocess-docs-0.0.2.tar.gz

---
+++ packages/preprocess-docs/preprocess-docs-0.0.2.tar.gz/preprocess-docs-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="preprocess-docs",
+    version="0.0.2",
+    author="Eddy Jin",
+    description="An open source document preprocessor for AI.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eddyjin1/preprocess",
+    packages=setuptools.find_packages(),
+    install_requires = [],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
