73e8d398f4f7547a5203940a1ca4db99ebf48445
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:03:52 +0000

    Add package BoSeokDonga/0.0.0/BoSeokDonga-0.0.0-py3-none-any.whl

---
+++ packages/BoSeokDonga/BoSeokDonga-0.0.0-py3-none-any.whl/BoSeokDonga/BoSeokDonga.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+
+def stusis(num):
+    return np.ceil(1 + np.log2(num))
+    
+def csvSample():
+    return print("file_df = pd.read_csv(\"C:/'''/sample3.txt\", sep='\t', encoding=\"UTF-8\")")
+
+def excelSample():
+    return print("file_df = pd.read_excel(\"C:/'''/sample4.xlsx\")")
+
