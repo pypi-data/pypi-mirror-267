1c86d9e01ee5d4739ecc667c295f7d8b50556587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:03:57 +0000

    Add package modelscope-studio/0.1.7/modelscope_studio-0.1.7.tar.gz

---
+++ packages/modelscope-studio/modelscope_studio-0.1.7.tar.gz/modelscope_studio-0.1.7/backend/modelscope_studio/__init__.py
@@ -0,0 +1,8 @@
+from .components.Chatbot import ModelScopeChatbot as Chatbot
+from .components.Markdown import ModelScopeMarkdown as Markdown
+from .components.MultimodalInput import \
+  ModelScopeMultimodalInput as MultimodalInput
+from .components.WaterfallGallery import \
+  ModelScopeWaterfallGallery as WaterfallGallery
+
+__all__ = ['Chatbot', 'Markdown', 'MultimodalInput', 'WaterfallGallery']
