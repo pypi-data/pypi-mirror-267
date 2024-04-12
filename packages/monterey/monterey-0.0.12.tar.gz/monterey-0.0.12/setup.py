a992df3ad9956f0d43650ead32b39ba8f4bd4dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:03:53 +0000

    Add package monterey/0.0.12/monterey-0.0.12.tar.gz

---
+++ packages/monterey/monterey-0.0.12.tar.gz/monterey-0.0.12/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="monterey",
+    version="0.0.12",
+    author="Hammad Saeed",
+    author_email="hammad@supportvectors.com",
+    description="monterey tools",
+    long_description="""
+Monterey Tools
+    """,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.8',
+    install_requires=[
+"art",
+"customtkinter",
+"fire",
+"prompt_toolkit",
+"pathlib",
+"pillow",
+"rich-cli",
+    ],
+)
