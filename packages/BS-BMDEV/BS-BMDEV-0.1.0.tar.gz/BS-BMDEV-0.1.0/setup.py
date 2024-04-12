ae1e59c5e0aa492574dd2c4373fb7b0c93af90b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 05:04:00 +0000

    Add package BS-BMDEV/0.1.0/BS-BMDEV-0.1.0.tar.gz

---
+++ packages/BS-BMDEV/BS-BMDEV-0.1.0.tar.gz/BS-BMDEV-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+name='BS-BMDEV',
+version='0.1.0',
+author='Laetitia Guérout, Théo Berthet, Lucas Bodelle & Synne Trettenes',
+author_email='laetitia.guerout@insa-lyon.fr, theo.berthet@insa-lyon.fr, lucas.bodelle@insa-lyon.fr, synne-moe.trettenes@insa-lyon.fr',
+description='Software to help victims portray a culprit using an autoencoder model trained on the CelebA dataset and a genetic algorithm',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
