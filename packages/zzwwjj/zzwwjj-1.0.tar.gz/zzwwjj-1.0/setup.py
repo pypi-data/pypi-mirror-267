b3e51adaed96fdc302d62ba6a22d707f1fabb956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:06:27 +0000

    Add package zzwwjj/1.0/zzwwjj-1.0.tar.gz

---
+++ packages/zzwwjj/zzwwjj-1.0.tar.gz/zzwwjj-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='zzwwjj',
+    version='1.0',
+    packages=['zzwwjj'],
+    install_requires=[
+        'numpy'
+    ],
+)
+
---
+++ packages/zzwwjj/zzwwjj-1.0.tar.gz/zzwwjj-1.0/zzwwjj/example.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def hello(name):
+    a = np.array([1])
+    print("hello", name)
