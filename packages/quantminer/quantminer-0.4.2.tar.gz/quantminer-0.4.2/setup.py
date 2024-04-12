b531be623338021ccc545fed4b1190dbf01442bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:29 +0000

    Add package quantminer/0.4.2/quantminer-0.4.2.tar.gz

---
+++ packages/quantminer/quantminer-0.4.2.tar.gz/quantminer-0.4.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='quantminer',
+    version='0.4.2', 
+    description='Data/Pattern Mining Algorithms for Financial Data',
+    author='Jerry Inyang',
+    author_email='jerprog0@gmail.com',
+    packages=find_packages(),  # Automatically finds your package
+    install_requires=[  # List any dependencies here
+        "kneed",
+        "quantstats",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "sktime",
+        "dtaidistance",
+        "matplotlib",
+        "tslearn ",
+        "plotly"
+    ]
+)
+
+# /Users/jerryinyang/anaconda3/bin/python
