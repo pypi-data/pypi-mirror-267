acf4eb914888e9e46ee1fe2674cf65ef31f24225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 14:04:17 +0000

    Add package BatsaevAb/0.1/BatsaevAb-0.1.tar.gz

---
+++ packages/BatsaevAb/BatsaevAb-0.1.tar.gz/BatsaevAb-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BatsaevAb',
+    version='0.1',
+    author='Abdula Batsaev',
+    author_email='abdyla05122004@gmail.com',
+    description='Списывать - легко!',
+    long_description='Установи, спиши и обезательно удали библиотеку из проекта(чтоб не спалили).',
+    long_description_content_type='text/markdown',
+    url='https://github.com/saiyan05122004',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
