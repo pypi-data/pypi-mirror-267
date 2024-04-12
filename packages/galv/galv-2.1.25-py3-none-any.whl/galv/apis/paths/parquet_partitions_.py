4d1c33d533efc783e1ba3b6053ae88f7692c81db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:03:51 +0000

    Add package galv/2.1.25/galv-2.1.25-py3-none-any.whl

---
+++ packages/galv/galv-2.1.25-py3-none-any.whl/galv/apis/paths/parquet_partitions_.py
@@ -0,0 +1,9 @@
+from galv.paths.parquet_partitions_.get import ApiForget
+from galv.paths.parquet_partitions_.post import ApiForpost
+
+
+class ParquetPartitions(
+    ApiForget,
+    ApiForpost,
+):
+    pass
