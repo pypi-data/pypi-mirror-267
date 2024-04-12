2d6141f56ddd40336420c8b544e493c33f1a945b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:03:24 +0000

    Add package qbraid-core/0.1.2.dev0/qbraid_core-0.1.2.dev0-py3-none-any.whl

---
+++ packages/qbraid-core/qbraid_core-0.1.2.dev0-py3-none-any.whl/qbraid_core/system/__init__.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2024, qBraid Development Team
+# All rights reserved.
+
+"""
+Module serving qBraid system information.
+
+.. currentmodule:: qbraid_core.system
+
+Classes
+--------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   FileManager
+
+Functions
+----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   is_exe
+   is_valid_python
+   get_python_version_from_cfg
+   get_python_version_from_exe
+   get_venv_site_packages_path
+   get_active_site_packages_path
+   get_local_package_path
+   get_local_package_version
+   get_latest_package_version
+   replace_str
+   echo_log
+
+Exceptions
+------------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   QbraidSystemError
+   UnknownFileSystemObjectError
+
+"""
+from .exceptions import QbraidSystemError, UnknownFileSystemObjectError
+from .executables import (
+    get_python_version_from_cfg,
+    get_python_version_from_exe,
+    is_exe,
+    is_valid_python,
+)
+from .generic import echo_log, replace_str
+from .packages import (
+    get_active_site_packages_path,
+    get_latest_package_version,
+    get_local_package_path,
+    get_local_package_version,
+    get_venv_site_packages_path,
+)
+from .threader import FileManager
