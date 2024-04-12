d55b155572a1364dc674c513ece7b6c8574d9798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 16:07:22 +0000

    Add package vedro-replay/1.1.0/vedro-replay-1.1.0.tar.gz

---
+++ packages/vedro-replay/vedro-replay-1.1.0.tar.gz/vedro-replay-1.1.0/vedro_replay/request.py
@@ -0,0 +1,39 @@
+import json
+from typing import Any, Dict, Optional
+from urllib.parse import urlparse
+
+
+class Request:
+    def __init__(
+            self, method: str, url: str,
+            comment: str = "", headers: Optional[Dict[Any, Any]] = None, json_body: Optional[Dict[Any, Any]] = None
+    ) -> None:
+        self.comment = comment
+        self.method = method
+        self.url = url
+        self.path = urlparse(url).path
+        self.headers = headers or {}
+        self.json_body = json_body
+
+    def add_header(self, key: str, value: str) -> "Request":
+        self.headers[key] = value
+        return self
+
+    def add_json_body(self, json_body: Dict[Any, Any]) -> "Request":
+        self.json_body = json_body
+        return self
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> str:
+        result = f"{self.method} {self.url}\n"
+
+        if self.headers:
+            for key, value in self.headers.items():
+                result += f"{key}: {value}\n"
+
+        if self.json_body:
+            result += f"{json.dumps(self.json_body)}\n"
+
+        return result
