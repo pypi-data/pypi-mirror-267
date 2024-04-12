e70d9bae0393ddcdc088940710d5890185b3b4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:03:49 +0000

    Add package cmeel-mim-solvers/0.0.4/cmeel_mim_solvers-0.0.4.tar.gz

---
+++ packages/cmeel-mim-solvers/cmeel_mim_solvers-0.0.4.tar.gz/cmeel_mim_solvers-0.0.4/include/mim_solvers/python.hpp
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////
+// BSD 3-Clause License
+// Copyright (C) 2023, New York University
+//
+// Copyright note valid unless otherwise stated in individual files.
+// All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __mim_solvers_python__
+#define __mim_solvers_python__
+
+#include <boost/python.hpp>
+
+#include "mim_solvers/ddp.hpp"
+#include "mim_solvers/fddp.hpp"
+#include "mim_solvers/sqp.hpp"
+#include "mim_solvers/csqp.hpp"
+
+#ifdef MIM_SOLVERS_WITH_PROXQP
+    #include "mim_solvers/csqp_proxqp.hpp"
+#endif
+
+namespace mim_solvers{
+    void exposeSolverDDP();
+    void exposeSolverFDDP();
+    void exposeSolverSQP();
+    void exposeSolverCSQP();
+#ifdef MIM_SOLVERS_WITH_PROXQP
+    void exposeSolverPROXQP();
+#endif
+} // namespace mim_solvers
+
+#endif
