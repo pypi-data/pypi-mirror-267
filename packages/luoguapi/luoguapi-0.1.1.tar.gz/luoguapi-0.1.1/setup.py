0d77434f66134a74594c6fe9a7c05ba0b5967df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 04:04:14 +0000

    Add package luoguapi/0.1.1/luoguapi-0.1.1.tar.gz

---
+++ packages/luoguapi/luoguapi-0.1.1.tar.gz/luoguapi-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='luoguapi',
+      version='0.1.1',
+      description='An API Client for Luogu',
+      author='CodingOIer',
+      author_email='codingoier-project@outlook.com',
+      )
