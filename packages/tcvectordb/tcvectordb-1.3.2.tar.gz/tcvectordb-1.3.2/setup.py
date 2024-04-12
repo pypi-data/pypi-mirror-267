fb39661ac46b690ebba0c804dc2f7a42ff519aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 15:03:54 +0000

    Add package tcvectordb/1.3.2/tcvectordb-1.3.2.tar.gz

---
+++ packages/tcvectordb/tcvectordb-1.3.2.tar.gz/tcvectordb-1.3.2/setup.py
@@ -0,0 +1,17 @@
+# setup for package tools
+# python3 setup.py sdist bdist_wheel
+from setuptools import setup, find_packages
+
+setup(
+    name='tcvectordb',
+    version='1.3.2',
+    description='Tencent VectorDB Python SDK',
+    author='tencent vdb team',
+    url='',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'cos-python-sdk-v5>=1.9.26',
+    ],
+    python_requires='>=3'
+)
