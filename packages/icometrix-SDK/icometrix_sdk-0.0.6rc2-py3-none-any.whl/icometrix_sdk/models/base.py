537a6ad03a4eacb7bcb5fedf8457b917615be1ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 17:03:52 +0000

    Add package icometrix-SDK/0.0.6rc2/icometrix_sdk-0.0.6rc2-py3-none-any.whl

---
+++ packages/icometrix-SDK/icometrix_sdk-0.0.6rc2-py3-none-any.whl/icometrix_sdk/models/base.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel
+from typing import Generic, TypeVar, List, Optional, Iterator, Literal
+
+T = TypeVar("T")
+
+DicomModality = Literal[
+    "CR", "CT", "MR", "US", "OT", "BI", "CD", "DD", "DG", "ES", "LS", "PT", "RG", "ST", "TG", "XA",
+    "RF", "HC", "DX", "NM", "MG", "IO", "PX", "GM", "SM", "XC", "PR", "AU", "EPS", "HD", "SR", "OP",
+    "IVUS", "SMR", "RTIMAGE", "RTDOSE", "RTSTRUCT", "RTPLAN", "RTRECORD"]
+
+
+class BackendEntity(BaseModel):
+    id: str
+    uri: Optional[str] = None
+    update_timestamp: str
+    creation_timestamp: str
+
+
+class PaginatedResultSet(BaseModel):
+    count: int
+    offset: int
+    limit: int
+
+
+class PaginatedMetaData(BaseModel):
+    result_set: PaginatedResultSet
+
+
+class PaginatedResponse(BaseModel, Generic[T]):
+    meta_data: PaginatedMetaData
+    results: List[T]
+    _itr_index: int
+
+    def has_next(self) -> bool:
+        """
+        Returns True if there is a next page in the backend
+
+        :return: bool
+        """
+        result_set = self.meta_data.result_set
+        return int(result_set.count) > (int(result_set.offset) + int(result_set.limit))
+
+    def __iter__(self) -> Iterator[T]:
+        return iter(self.results)
+
+    def __getitem__(self, index) -> T:
+        return self.results[index]
+
+    def __next__(self) -> T:
+        if len(self.results) < self._itr_index:
+            raise StopIteration
+
+        item: T = self.results[self._itr_index]
+        self._itr_index += 1
+        return item
+
+    def __len__(self) -> int:
+        return len(self.results)
