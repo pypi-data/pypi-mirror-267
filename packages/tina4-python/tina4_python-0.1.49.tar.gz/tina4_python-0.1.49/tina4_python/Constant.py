02d17ae24acfa0291fb13ef5c6eec99ddf7f2303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 06:04:31 +0000

    Add package tina4-python/0.1.49/tina4_python-0.1.49.tar.gz

---
+++ packages/tina4-python/tina4_python-0.1.49.tar.gz/tina4_python-0.1.49/tina4_python/Constant.py
@@ -0,0 +1,39 @@
+#
+# Tina4 - This is not a 4ramework.
+# Copy-right 2007 - current Tina4
+# License: MIT https://opensource.org/licenses/MIT
+#
+DEBUG_INFO = 0
+DEBUG_WARNING = 1
+DEBUG_DEBUG = 2
+DEBUG_ERROR = 3
+DEBUG_ALL = 6
+
+TINA4_GET = "GET"
+TINA4_POST = "POST"
+TINA4_ANY = "ANY"
+TINA4_PATCH = "PATCH"
+TINA4_PUT = "PUT"
+TINA4_DELETE = "DELETE"
+TINA4_OPTIONS = "OPTIONS"
+
+HTTP_OK = 200
+HTTP_CREATED = 201
+HTTP_NO_CONTENT = 204
+HTTP_PARTIAL_CONTENT = 206
+HTTP_BAD_REQUEST = 400
+HTTP_UNAUTHORIZED = 401
+HTTP_FORBIDDEN = 403
+HTTP_NOT_FOUND = 404
+
+LOOKUP_HTTP_CODE = {HTTP_OK: "OK", HTTP_CREATED: "Created", HTTP_BAD_REQUEST: "Bad Request",
+                    HTTP_PARTIAL_CONTENT: "Partial Content", HTTP_UNAUTHORIZED: "Unauthorized",
+                    HTTP_FORBIDDEN: "Forbidden", HTTP_NOT_FOUND: "Not Found", HTTP_NO_CONTENT: "No Content"}
+
+TEXT_HTML = "text/html"
+TEXT_CSS = "text/css"
+TEXT_PLAIN = "text/plain"
+APPLICATION_JSON = "application/json"
+APPLICATION_XML = "application/xml"
+
+
