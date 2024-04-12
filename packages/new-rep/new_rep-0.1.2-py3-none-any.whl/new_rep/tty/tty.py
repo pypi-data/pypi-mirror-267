956782a316898d2c7186c40a24394d48f0bac8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-12 03:03:33 +0000

    Add package new-rep/0.1.2/new_rep-0.1.2-py3-none-any.whl

---
+++ packages/new-rep/new_rep-0.1.2-py3-none-any.whl/new_rep/tty/tty.py
@@ -0,0 +1,10 @@
+from new_rep.tty.tty_object import TTYObject
+
+class TTY(TTYObject):
+    def __init__(self, name_s: str, label_s: str, input_s: str, output_s: str):
+        self.name = name_s
+        self.label = label_s
+        self.input = input_s
+        self.output = output_s
+        
+        super().__init__()
