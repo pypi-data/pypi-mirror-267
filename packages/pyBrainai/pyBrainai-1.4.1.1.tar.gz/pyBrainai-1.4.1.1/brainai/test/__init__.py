f34959bc54ebeae10509a93d76614868685428ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:04:19 +0000

    Add package pyBrainai/1.4.1.1/pyBrainai-1.4.1.1.tar.gz

---
+++ packages/pyBrainai/pyBrainai-1.4.1.1.tar.gz/pyBrainai-1.4.1.1/brainai/test/__init__.py
@@ -0,0 +1,35 @@
+from brainos_test import (
+    create_knowledge,
+    knowledge_list,
+    delete_knowledge,
+    upload_files,
+    files_learn,
+    file_list,
+    delete_file_list,
+    add_chat,
+    history_chat,
+    llm_chat,
+    knowledge_chat,
+    delete_chat,
+    chat_list,
+)
+
+from models_test import model_list,model_chat
+
+__all__ = [
+    "model_list",
+    "model_chat",
+    "create_knowledge",
+    "knowledge_list",
+    "delete_knowledge",
+    "upload_files",
+    "files_learn",
+    "file_list",
+    "delete_file_list",
+    "add_chat",
+    "history_chat",
+    "llm_chat",
+    "knowledge_chat",
+    "delete_chat",
+    "chat_list",
+]
