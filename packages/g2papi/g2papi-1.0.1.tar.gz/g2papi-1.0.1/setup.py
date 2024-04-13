37bb8a5843d33a4ec28c47aaa42c8a343d7a861a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:13 +0000

    Add package g2papi/1.0.1/g2papi-1.0.1.tar.gz

---
+++ packages/g2papi/g2papi-1.0.1.tar.gz/g2papi-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='g2papi',
+    version='1.0.1',
+    author='Jordan Safer',
+    author_email='genomics2proteins@gmail.com',
+    description='A python client and CLI for easy access to G2P portal APIs',
+    #long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'g2papi=g2papi.cli:main',
+        ],
+    },
+)
