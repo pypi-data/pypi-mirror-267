a48ffc5b0d3eb9f5e1f9a6995b65b6f0e6f9d893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 04:04:21 +0000

    Add package pispy-client/0.5.0/pispy-client-0.5.0.tar.gz

---
+++ packages/pispy-client/pispy-client-0.5.0.tar.gz/pispy-client-0.5.0/pispy/widgets/__init__.py
@@ -0,0 +1,11 @@
+"""Widgets used in the application."""
+
+##############################################################################
+# Local imports.
+from .package_info import PackageInfo
+
+##############################################################################
+# Export widgets.
+__all__ = ["PackageInfo"]
+
+### __init__.py ends here
