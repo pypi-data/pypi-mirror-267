fc9e539caacb0436b1a81071ea346d5d794bb428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:29 +0000

    Add package orix/0.12.0/orix-0.12.tar.gz

---
+++ packages/orix/orix-0.12.tar.gz/orix-0.12/orix/__init__.py
@@ -0,0 +1,20 @@
+__name__ = "orix"
+__version__ = "0.12.0"
+__author__ = "orix developers"
+__author_email__ = "pyxem.team@gmail.com"
+__description__ = "orix is an open-source Python library for handling crystal orientation mapping data."
+# Sorted by line contributions (ideally excluding lines in notebook files)
+__credits__ = [
+    "Håkon Wiik Ånes",
+    "Ben Martineau",
+    "Paddy Harrison",
+    "Phillip Crout",
+    "Austin Gerlt",
+    "Duncan Johnstone",
+    "Niels Cautaerts",
+    "Anders Christian Mathisen",
+    "Carter Francis",
+    "Simon Høgås",
+    "Viljar Johan Femoen",
+    "Alexander Clausen",
+]
