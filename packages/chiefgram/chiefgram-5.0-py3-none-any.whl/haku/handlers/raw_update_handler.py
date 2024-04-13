55821438c2a8e3b868545444cd7b624d9feccf61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:37 +0000

    Add package chiefgram/5.0/chiefgram-5.0-py3-none-any.whl

---
+++ packages/chiefgram/chiefgram-5.0-py3-none-any.whl/haku/handlers/raw_update_handler.py
@@ -0,0 +1,68 @@
+#  Pyrofork - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
+#
+#  This file is part of Pyrofork.
+#
+#  Pyrofork is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrofork is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Callable
+
+from .handler import Handler
+
+
+class RawUpdateHandler(Handler):
+    """The Raw Update handler class. Used to handle raw updates. It is intended to be used with
+    :meth:`~haku.Client.add_handler`
+
+    For a nicer way to register this handler, have a look at the
+    :meth:`~haku.Client.on_raw_update` decorator.
+
+    Parameters:
+        callback (``Callable``):
+            A function that will be called when a new update is received from the server. It takes
+            *(client, update, users, chats)* as positional arguments (look at the section below for
+            a detailed description).
+
+    Other Parameters:
+        client (:obj:`~haku.Client`):
+            The Client itself, useful when you want to call other API methods inside the update handler.
+
+        update (``Update``):
+            The received update, which can be one of the many single Updates listed in the
+            :obj:`~haku.raw.base.Update` base type.
+
+        users (``dict``):
+            Dictionary of all :obj:`~haku.types.User` mentioned in the update.
+            You can access extra info about the user (such as *first_name*, *last_name*, etc...) by using
+            the IDs you find in the *update* argument (e.g.: *users[1768841572]*).
+
+        chats (``dict``):
+            Dictionary of all :obj:`~haku.types.Chat` and
+            :obj:`~haku.raw.types.Channel` mentioned in the update.
+            You can access extra info about the chat (such as *title*, *participants_count*, etc...)
+            by using the IDs you find in the *update* argument (e.g.: *chats[1701277281]*).
+
+    Note:
+        The following Empty or Forbidden types may exist inside the *users* and *chats* dictionaries.
+        They mean you have been blocked by the user or banned from the group/channel.
+
+        - :obj:`~haku.raw.types.UserEmpty`
+        - :obj:`~haku.raw.types.ChatEmpty`
+        - :obj:`~haku.raw.types.ChatForbidden`
+        - :obj:`~haku.raw.types.ChannelForbidden`
+    """
+
+    def __init__(self, callback: Callable):
+        super().__init__(callback)
---
+++ packages/chiefgram/chiefgram-5.0-py3-none-any.whl/haku/methods/decorators/on_deleted_bot_business_messages.py
@@ -0,0 +1,62 @@
+#  Pyrofork - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
+#  Copyright (C) 2022-present Mayuri-Chan <https://github.com/Mayuri-Chan>
+#
+#  This file is part of Pyrofork.
+#
+#  Pyrofork is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pyrofork is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Callable
+
+import haku
+from haku.filters import Filter
+
+
+class OnDeletedBotBusinessMessages:
+    def on_deleted_bot_business_messages(
+        self=None,
+        filters=None,
+        group: int = 0
+    ) -> Callable:
+        """Decorator for handling deleted bot business messages.
+
+        This does the same thing as :meth:`~haku.Client.add_handler` using the
+        :obj:`~haku.handlers.DeletedBotBusinessMessagesHandler`.
+
+        Parameters:
+            filters (:obj:`~haku.filters`, *optional*):
+                Pass one or more filters to allow only a subset of messages to be passed
+                in your function.
+
+            group (``int``, *optional*):
+                The group identifier, defaults to 0.
+        """
+
+        def decorator(func: Callable) -> Callable:
+            if isinstance(self, haku.Client):
+                self.add_handler(haku.handlers.DeletedBotBusinessMessagesHandler(func, filters), group)
+            elif isinstance(self, Filter) or self is None:
+                if not hasattr(func, "handlers"):
+                    func.handlers = []
+
+                func.handlers.append(
+                    (
+                        haku.handlers.DeletedBotBusinessMessagesHandler(func, self),
+                        group if filters is None else filters
+                    )
+                )
+
+            return func
+
+        return decorator
