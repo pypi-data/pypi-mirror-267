8b9d1e6890531240b742715ace73786542f7fa33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:04:27 +0000

    Add package pygpt-net/2.1.47/pygpt_net-2.1.47-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.1.47-py3-none-any.whl/pygpt_net/provider/loaders/file_pdf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.02.28 22:00:00                  #
+# ================================================== #
+
+from llama_index.core.readers.base import BaseReader
+from llama_index.readers.file.docs import PDFReader
+
+from .base import BaseLoader
+
+
+class Loader(BaseLoader):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id = "pdf"
+        self.name = "PDF documents"
+        self.extensions = ["pdf"]
+        self.type = ["file"]
+        self.init_args = {
+            "return_full_document": False,
+        }
+
+    def get(self) -> BaseReader:
+        """
+        Get reader instance
+
+        :return: Data reader instance
+        """
+        args = self.get_args()
+        return PDFReader(**args)
