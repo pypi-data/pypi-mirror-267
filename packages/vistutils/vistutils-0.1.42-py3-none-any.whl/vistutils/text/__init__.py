fd77be90c04956342fa6b19ab340079ba5ea1867
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 04:04:48 +0000

    Add package vistutils/0.1.42/vistutils-0.1.42-py3-none-any.whl

---
+++ packages/vistutils/vistutils-0.1.42-py3-none-any.whl/vistutils/text/__init__.py
@@ -0,0 +1,9 @@
+"""The 'text' module provides consistent and convenient text related
+utility functions"""
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._mono_space import monoSpace
+from ._string_list import stringList
+from ._join_words import joinWords
