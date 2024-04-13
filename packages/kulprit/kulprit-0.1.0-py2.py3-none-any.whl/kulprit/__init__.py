25dc89b3e56070a6e455e51f71adf19688da326b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 04:04:21 +0000

    Add package kulprit/0.1.0/kulprit-0.1.0-py2.py3-none-any.whl

---
+++ packages/kulprit/kulprit-0.1.0-py2.py3-none-any.whl/kulprit/__init__.py
@@ -0,0 +1,14 @@
+"""
+Kulprit.
+
+Kullback-Leibler projections for Bayesian model selection.
+"""
+
+from kulprit.reference import ProjectionPredictive
+from kulprit.datasets import *
+
+
+__version__ = "0.1.0"
+
+
+__all__ = ["ProjectionPredictive"]
