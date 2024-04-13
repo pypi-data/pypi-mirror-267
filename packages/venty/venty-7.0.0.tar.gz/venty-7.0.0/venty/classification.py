a1d3d2425fe198b741215d071c418b7b131948bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:05:16 +0000

    Add package venty/7.0.0/venty-7.0.0.tar.gz

---
+++ packages/venty/venty-7.0.0.tar.gz/venty-7.0.0/venty/classification.py
@@ -0,0 +1,26 @@
+from typing import Optional, TypeVar, Any, Type, List
+
+from cloudevents.pydantic import CloudEvent
+
+
+T = TypeVar("T")
+
+
+def may_be(type_: Type[T], value: Any) -> Optional[T]:
+    if isinstance(value, type_):
+        return value
+    if issubclass(type_, CloudEvent):
+        if value.type == type_.__fields__["type"].default:
+            return type_.parse_obj(value.dict())
+    return None
+
+
+def must_be(type_: Type[T], value: Any) -> T:
+    result = may_be(type_, value)
+    if result is None:
+        raise ValueError(f"Expected {type_.__name__}, got {value.__class__.__name__}")
+    return result
+
+
+def must_be_list_of(type_: Type[T], value: Any) -> List[T]:
+    return [must_be(type_, item) for item in must_be(list, value)]
