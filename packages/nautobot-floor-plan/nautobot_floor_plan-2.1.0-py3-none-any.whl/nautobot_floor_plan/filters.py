a3f9eafa9ce590464b0a3ea6c596449a72fb1009
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:42 +0000

    Add package nautobot-floor-plan/2.1.0/nautobot_floor_plan-2.1.0-py3-none-any.whl

---
+++ packages/nautobot-floor-plan/nautobot_floor_plan-2.1.0-py3-none-any.whl/nautobot_floor_plan/filters.py
@@ -0,0 +1,57 @@
+"""Filtering for nautobot_floor_plan."""
+
+import django_filters
+
+from nautobot.dcim.models import Location, Rack
+from nautobot.apps.filters import NautobotFilterSet
+from nautobot.apps.filters import NaturalKeyOrPKMultipleChoiceFilter, SearchFilter
+
+from nautobot_floor_plan import models
+
+
+class FloorPlanFilterSet(NautobotFilterSet):
+    """Filter for FloorPlan."""
+
+    q = SearchFilter(
+        filter_predicates={
+            "location__name": "icontains",
+        },
+    )
+    location = NaturalKeyOrPKMultipleChoiceFilter(
+        queryset=Location.objects.all(),
+        label="Location (name or ID)",
+    )
+
+    class Meta:
+        """Meta attributes for filter."""
+
+        model = models.FloorPlan
+        fields = ["x_size", "y_size", "tile_width", "tile_depth", "tags"]
+
+
+class FloorPlanTileFilterSet(NautobotFilterSet):
+    """Filter for FloorPlanTile."""
+
+    q = SearchFilter(
+        filter_predicates={
+            "floor_plan__location__name": "icontains",
+            "rack__name": "icontains",
+        },
+    )
+    floor_plan = django_filters.ModelMultipleChoiceFilter(queryset=models.FloorPlan.objects.all())
+    location = NaturalKeyOrPKMultipleChoiceFilter(
+        field_name="floor_plan__location",
+        queryset=Location.objects.all(),
+        label="Location (name or ID)",
+    )
+    rack = NaturalKeyOrPKMultipleChoiceFilter(
+        queryset=Rack.objects.all(),
+        to_field_name="name",
+        label="Rack (name or ID)",
+    )
+
+    class Meta:
+        """Meta attributes."""
+
+        model = models.FloorPlanTile
+        fields = ["x_origin", "y_origin", "tags"]
