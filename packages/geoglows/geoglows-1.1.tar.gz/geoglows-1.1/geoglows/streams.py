598b60b52ea3f2b171d963896d6019d4990d5232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:41 +0000

    Add package geoglows/1.1.0/geoglows-1.1.tar.gz

---
+++ packages/geoglows/geoglows-1.1.tar.gz/geoglows-1.1/geoglows/streams.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from .data import metadata_tables
+
+__all__ = ['river_to_vpu', 'latlon_to_river', 'river_to_latlon', ]
+
+
+def river_to_vpu(reach_id: int) -> str or int:
+    return (
+        metadata_tables(columns=['LINKNO', 'VPUCode'])
+        .loc[lambda x: x['LINKNO'] == reach_id, 'VPUCode']
+        .values[0]
+    )
+
+
+def latlon_to_river(lat: float, lon: float) -> int:
+    df = metadata_tables(columns=['LINKNO', 'lat', 'lon'])
+    df['dist'] = ((df['lat'] - lat) ** 2 + (df['lon'] - lon) ** 2) ** 0.5
+    return df.loc[lambda x: x['dist'] == df['dist'].min(), 'LINKNO'].values[0]
+
+
+def river_to_latlon(reach_id: int) -> np.ndarray:
+    return (
+        metadata_tables(columns=['LINKNO', 'lat', 'lon'])
+        .loc[lambda x: x['LINKNO'] == reach_id, ['lat', 'lon']]
+        .values[0]
+    )
