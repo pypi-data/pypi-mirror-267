b33b705de98f6b6b1189014cdfd531a48fc0fdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:33 +0000

    Add package sysnet-taxonomy/1.0.1.1/sysnet-taxonomy-1.0.1.1.tar.gz

---
+++ packages/sysnet-taxonomy/sysnet-taxonomy-1.0.1.1.tar.gz/sysnet-taxonomy-1.0.1.1/taxonomy/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from taxonomy.api.admins_api import AdminsApi
+from taxonomy.api.developers_api import DevelopersApi
+from taxonomy.api.public_api import PublicApi
