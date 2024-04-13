81a26a2d449ae04eb24db1109f3ae8a42f7e6d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 14:04:38 +0000

    Add package django-sceneid/0.1.2/django-sceneid-0.1.2.tar.gz

---
+++ packages/django-sceneid/django-sceneid-0.1.2.tar.gz/django-sceneid-0.1.2/CHANGELOG.md
@@ -0,0 +1,19 @@
+Changelog
+=========
+
+0.1.2 (2024-04-12)
+
+ * Confirm Django 5.x support and remove upper bound on Django dependency
+
+
+0.1.1 (2022-07-13)
+------------------
+
+ * Confirm Django 4.x support
+ * Fix Django 3.0 and 3.1 compatibility by providing a default_app_config setting
+
+
+0.1 (2021-05-08)
+----------------
+
+* Initial release
---
+++ packages/django-sceneid/django-sceneid-0.1.2.tar.gz/django-sceneid-0.1.2/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="django-sceneid",
+    version="0.1.2",
+    packages=find_packages(exclude=('tests',)),
+    include_package_data=True,
+    test_suite="tests",
+    url="https://github.com/demozoo/django-sceneid/",
+
+    author="Matt Westcott",
+    author_email="matt@west.co.tt",
+    description="SceneID authentication for Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django :: 3.0",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Topic :: Internet :: WWW/HTTP",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'Django>=3.0',
+        'requests>=2,<3',
+    ],
+    extras_require={
+        "testing": [
+            'responses>=0.14,<0.21',
+        ]
+    },
+    license="BSD",
+)
