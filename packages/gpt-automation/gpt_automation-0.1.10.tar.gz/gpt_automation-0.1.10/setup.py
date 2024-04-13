4d68642721473ae589899ea63f6b989ca16f6f8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:28 +0000

    Add package gpt-automation/0.1.10/gpt_automation-0.1.10.tar.gz

---
+++ packages/gpt-automation/gpt_automation-0.1.10.tar.gz/gpt_automation-0.1.10/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gpt_automation",
+    version="0.1.10",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "autogpt = gpt_automation.main:main",
+        ],
+    },
+)
