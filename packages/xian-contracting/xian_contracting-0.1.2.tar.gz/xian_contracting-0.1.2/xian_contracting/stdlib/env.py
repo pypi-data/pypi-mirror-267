c9418c9f9a2a8293dab4dcfb9f0800c691c0106c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 14:04:47 +0000

    Add package xian-contracting/0.1.2/xian_contracting-0.1.2.tar.gz

---
+++ packages/xian-contracting/xian_contracting-0.1.2.tar.gz/xian_contracting-0.1.2/xian_contracting/stdlib/env.py
@@ -0,0 +1,28 @@
+from xian_contracting.stdlib.bridge.orm import exports as orm_exports
+from xian_contracting.stdlib.bridge.hashing import exports as hash_exports
+from xian_contracting.stdlib.bridge.time import exports as time_exports
+from xian_contracting.stdlib.bridge.random import exports as random_exports
+from xian_contracting.stdlib.bridge.imports import exports as imports_exports
+from xian_contracting.stdlib.bridge.access import exports as access_exports
+from xian_contracting.stdlib.bridge.decimal import exports as decimal_exports
+
+# TODO create a module instead and return it inside of a dictionary like:
+# {
+#    'stdlib': module
+# }
+#
+# Then stdlib.datetime becomes available, etc
+
+
+def gather():
+    env = {}
+
+    env.update(orm_exports)
+    env.update(hash_exports)
+    env.update(time_exports)
+    env.update(random_exports)
+    env.update(imports_exports)
+    env.update(access_exports)
+    env.update(decimal_exports)
+
+    return env
