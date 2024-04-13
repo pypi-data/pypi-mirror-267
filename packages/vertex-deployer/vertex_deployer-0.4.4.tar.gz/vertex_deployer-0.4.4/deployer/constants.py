0beaf1851b4a8f4d15ad75086824b3f91a8ca66b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:05:38 +0000

    Add package vertex-deployer/0.4.4/vertex_deployer-0.4.4.tar.gz

---
+++ packages/vertex-deployer/vertex_deployer-0.4.4.tar.gz/vertex_deployer-0.4.4/deployer/constants.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+DEFAULT_PIPELINE_ROOT_PATH = Path("vertex/pipelines")
+DEFAULT_CONFIG_ROOT_PATH = Path("vertex/configs")
+DEFAULT_LOG_LEVEL = "INFO"
+
+DEFAULT_SCHEDULER_TIMEZONE = "Europe/Paris"
+DEFAULT_LOCAL_PACKAGE_PATH = Path("vertex/pipelines/compiled_pipelines")
+DEFAULT_TAGS = None
+
+TEMP_LOCAL_PACKAGE_PATH = ".vertex-deployer-temp"
+
+
+PIPELINE_MINIMAL_TEMPLATE = """import kfp.dsl
+
+
+@kfp.dsl.pipeline(name="{pipeline_name}")
+def {pipeline_name}():
+    pass
+
+"""
+
+PYTHON_CONFIG_TEMPLATE = """from kfp.dsl import Artifact, Dataset, Input, Output, Metrics
+
+parameter_values = {}
+input_artifacts = {}
+
+"""
+
+PIPELINE_CHECKS_TABLE_COLUMNS = [
+    "Status",
+    "Pipeline",
+    "Pipeline Error Message",
+    "Config File",
+    "Attribute",
+    "Config Error Type",
+    "Config Error Message",
+]
---
+++ packages/vertex-deployer/vertex_deployer-0.4.4.tar.gz/vertex_deployer-0.4.4/deployer/utils/exceptions.py
@@ -0,0 +1,18 @@
+class TagNotFoundError(Exception):
+    """Raised when a tag is not found."""
+
+
+class MissingGoogleArtifactRegistryHostError(Exception):
+    """Raised when the Google Artifact Registry host is missing."""
+
+
+class UnsupportedConfigFileError(Exception):
+    """Raised when the config file is not supported."""
+
+
+class BadConfigError(ValueError):
+    """Raised when a config is invalid."""
+
+
+class InvalidPyProjectTOMLError(Exception):
+    """Raised when the configuration is invalid."""
