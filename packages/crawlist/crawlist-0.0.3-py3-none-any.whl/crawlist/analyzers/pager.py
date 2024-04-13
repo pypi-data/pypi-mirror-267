fad088a445257e87cfd09d675dfc13369f6d8cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 15:04:39 +0000

    Add package crawlist/0.0.3/crawlist-0.0.3-py3-none-any.whl

---
+++ packages/crawlist/crawlist-0.0.3-py3-none-any.whl/crawlist/analyzers/pager.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+
+class BasePager(object):
+    pass
+
+
+class Pager(BasePager):
+
+    def __init__(self, interval: float = 0.1):
+        """
+        :param interval: Grab the list frequency and adjust it according to the actual situation of the webpage
+        """
+        self.interval: float = interval
+        self.half_interval: float = interval / 2
+
+    def next(self) -> None:
+        """
+        Data Incremental Method
+        """
+        raise NotImplementedError
+
+    @property
+    def html(self) -> str:
+        """
+        HTML text in the current state
+        :return: The html text
+        """
+        raise NotImplementedError
+
+    def __call__(self):
+        self.next()
+
+    def sleep(self):
+        time.sleep(random.uniform(self.half_interval / 2, self.interval))
+
+
