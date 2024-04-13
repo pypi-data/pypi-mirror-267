a2896340ccb4d468a5c0d59b032ce69cfb9d1f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:04:44 +0000

    Add package fleece-network/0.2.0rc3/fleece_network-0.2.0rc3.tar.gz

---
+++ packages/fleece-network/fleece_network-0.2.0rc3.tar.gz/fleece_network-0.2.0rc3/fleece_network/messages.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import Generic, Optional, TypeVar
+from .aiortc import RTCSessionDescription
+from fastapi import Response
+from pydantic import BaseModel  # type: ignore
+
+
+@dataclass
+class RegisterRequest:
+    worker_id: str
+
+
+@dataclass
+class RegisterReply:
+    status: str
+
+
+@dataclass
+class ConnectRequest:
+    from_worker_id: str
+    to_worker_id: str
+    sdp: RTCSessionDescription
+
+
+@dataclass
+class ConnectReply:
+    from_worker_id: str
+    to_worker_id: str
+    sdp: Optional[RTCSessionDescription]
+
+
+T = TypeVar("T", bound=BaseModel)
+
+
+@dataclass
+class SimpleRequest(Generic[T]):
+    id: int
+    op: str
+    data: T
+
+
+@dataclass
+class SimpleReply:
+    id: int
+    data: Response
