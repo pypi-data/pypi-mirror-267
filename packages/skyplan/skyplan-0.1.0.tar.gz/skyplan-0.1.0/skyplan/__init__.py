da00c7d5dae89293b02c596bee14e6f5402a9f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:47 +0000

    Add package skyplan/0.1.0/skyplan-0.1.0.tar.gz

---
+++ packages/skyplan/skyplan-0.1.0.tar.gz/skyplan-0.1.0/skyplan/__init__.py
@@ -0,0 +1,30 @@
+from datazets import get as import_example
+from skyplan.skyplan import skyplan
+
+# from skyplan.skyplan import (
+    # import_example,
+    # )
+
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+skyplan
+=====================================================================
+
+skyplan is for...
+
+Example
+-------
+>>> import skyplan as skyplan
+>>> model = skyplan.fit_transform(X)
+>>> fig,ax = skyplan.plot(model)
+
+References
+----------
+https://github.com/erdogant/skyplan
+
+"""
