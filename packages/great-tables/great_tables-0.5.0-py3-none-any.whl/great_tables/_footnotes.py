a80850130f7e75c5a74df548f4968ca67352bded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 04:04:36 +0000

    Add package great-tables/0.5.0/great_tables-0.5.0-py3-none-any.whl

---
+++ packages/great-tables/great_tables-0.5.0-py3-none-any.whl/great_tables/_footnotes.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from typing import Optional, List
+from enum import Enum, auto
+
+
+# TODO: create the `tab_footnote()` function
---
+++ packages/great-tables/great_tables-0.5.0-py3-none-any.whl/great_tables/_text.py
@@ -0,0 +1,71 @@
+from typing import Union, List, Literal
+from dataclasses import dataclass
+import html
+
+import commonmark
+import re
+
+
+@dataclass
+class Text:
+    text: str
+    type: Literal["from_markdown", "html"]
+
+
+class StringBuilder:
+    pieces: List[Union[str, "StringBuilder"]]
+
+    def __init__(self, *args: Union[str, "StringBuilder"]):
+        self.pieces = list(args)
+
+    def _collect(self, lst: List[str]):
+        for piece in self.pieces:
+            if isinstance(piece, str):
+                lst.append(piece)
+            else:
+                piece._collect(lst)
+
+    def make_string(self) -> str:
+        lst = []
+        self._collect(lst)
+        return "".join(lst)
+
+    def append(self, *args: str):
+        self.pieces.extend(args)
+
+    def prepend(self, *args: str):
+        self.pieces[0:0] = args
+
+
+def _md_html(x: str) -> str:
+    str = commonmark.commonmark(x)
+    return re.sub(r"^<p>|</p>\n$", "", str)
+
+
+def _process_text(x: Union[Text, str, None]) -> str:
+    if x is None:
+        return ""
+
+    if isinstance(x, str):
+        text = x
+        type = "plaintext"
+    else:
+        text = x.text
+        type = x.type
+
+    if type == "from_markdown":
+        x_out = _md_html(text)
+    elif type == "html":
+        x_out = text
+    else:
+        x_out = _html_escape(text)
+
+    return x_out
+
+
+def _process_text_id(x: Union[Text, str, None]) -> str:
+    return _process_text(x)
+
+
+def _html_escape(x: str) -> str:
+    return html.escape(x)
