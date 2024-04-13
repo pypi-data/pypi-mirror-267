69ab84dcbb5528b67aa9db552b87108397e99606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:41 +0000

    Add package dannytrigo-onkyo-eiscp/1.2.14/dannytrigo_onkyo_eiscp-1.2.14.tar.gz

---
+++ packages/dannytrigo-onkyo-eiscp/dannytrigo_onkyo_eiscp-1.2.14.tar.gz/dannytrigo_onkyo_eiscp-1.2.14/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf8
+
+from setuptools import setup, find_packages
+
+# Get long_description from README
+import os
+here = os.path.dirname(os.path.abspath(__file__))
+f = open(os.path.join(here, 'README.rst'))
+long_description = f.read().strip()
+f.close()
+
+setup(
+    name='dannytrigo-onkyo-eiscp',
+    version='1.2.14',
+    url='https://github.com/dannytrigo/onkyo-eiscp',
+    license='MIT',
+    author='Michael Elsdörfer',
+    author_email='michael@elsdoerfer.com',
+    description='Control Onkyo receivers over ethernet. (dannytrigo fork for bugfix)',
+    long_description=long_description,
+    packages = find_packages(exclude=('tests*',)),
+    entry_points="""[console_scripts]\nonkyo = eiscp.script:run\n""",
+    install_requires=['docopt>=0.4.1', 'netifaces', 'xmltodict>=0.12.0'],
+    platforms='any',
+    classifiers=[
+        'Topic :: System :: Networking',
+        'Topic :: Games/Entertainment',
+        'Topic :: Multimedia',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
