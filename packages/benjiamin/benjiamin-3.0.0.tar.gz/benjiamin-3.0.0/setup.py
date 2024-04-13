21a16529a671edd4e58a25d56cf05ffc968c82f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:42 +0000

    Add package benjiamin/3.0.0/benjiamin-3.0.0.tar.gz

---
+++ packages/benjiamin/benjiamin-3.0.0.tar.gz/benjiamin-3.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+  name='benjiamin',
+  version='3.0.0',
+  author='Your Name',
+  author_email='your.email@example.com',
+  description='A short description of your package',
+  packages=find_packages(),
+  classifiers=[
+  'Programming Language :: Python :: 3',
+  'License :: OSI Approved :: MIT License',
+  'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
