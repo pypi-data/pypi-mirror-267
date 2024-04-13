c640983842472b75df886bc10925f0de6db383f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:05:23 +0000

    Add package RoffIO/1.0.3b1/RoffIO-1.0.3b1.tar.gz

---
+++ packages/RoffIO/RoffIO-1.0.3b1.tar.gz/RoffIO-1.0.3b1/src/roffio/__init__.py
@@ -0,0 +1,8 @@
+import roffio.version
+from _roffio.reading import lazy_read, read
+from _roffio.writing import Format, write
+
+__author__ = """Equinor"""
+__email__ = "fg_sib-scout@equinor.com"
+
+__version__ = roffio.version.version
