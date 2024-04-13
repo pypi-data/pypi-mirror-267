dd53b7550483bec70b6324480751141032e1996f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 15:04:24 +0000

    Add package querent/3.0.2/querent-3.0.2.tar.gz

---
+++ packages/querent/querent-3.0.2.tar.gz/querent-3.0.2/querent/common/types/querent_event.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+
+class EventType:
+    Graph = "Graph"
+    Vector = "Vector"
+    Terminate="Terminate"
+    QueryResult="QueryResult"
+
+
+class EventState:
+    def __init__(self, event_type: EventType, timestamp: float, payload: Any, file: str):
+        self.event_type = event_type
+        self.timestamp = timestamp
+        self.payload = payload
+        self.file = file
+        self.file = file
