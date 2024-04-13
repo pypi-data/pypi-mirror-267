8235a982363f4abbcdc752aa2d89cef4f34aaedc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:04:57 +0000

    Add package tina4-python/0.1.51/tina4_python-0.1.51.tar.gz

---
+++ packages/tina4-python/tina4_python-0.1.51.tar.gz/tina4_python-0.1.51/tina4_python/Response.py
@@ -0,0 +1,37 @@
+#
+# Tina4 - This is not a 4ramework.
+# Copy-right 2007 - current Tina4
+# License: MIT https://opensource.org/licenses/MIT
+#
+import json
+import inspect
+from tina4_python import Constant
+
+
+class Response:
+    """
+    response object for router
+    :param content
+    :param http_code
+    :param content_type
+    """
+
+    def __init__(self, content, http_code=Constant.HTTP_OK, content_type=Constant.TEXT_HTML):
+        # convert a class into a dictionary
+        if not isinstance(content, bool) and not isinstance(content, bytes) and not isinstance(content, str) and not isinstance(content, list) and inspect.isclass(type(content)):
+            content = dict(content)
+
+        # convert the dictionary or list into JSON
+        if not isinstance(content, bool) and type(content) is dict or type(content) is list:
+            content = json.dumps(content)
+            content_type = Constant.APPLICATION_JSON
+
+        if isinstance(content, bool):
+            if content:
+                content = "True"
+            else:
+                content = "False"
+
+        self.content = content
+        self.http_code = http_code
+        self.content_type = content_type
