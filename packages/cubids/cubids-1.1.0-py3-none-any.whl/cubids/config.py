687f716d4187f7b01ff6f2a1c69afd20d238597d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 04:04:14 +0000

    Add package cubids/1.1.0/cubids-1.1.0-py3-none-any.whl

---
+++ packages/cubids/cubids-1.1.0-py3-none-any.whl/cubids/config.py
@@ -0,0 +1,17 @@
+"""Functions for configuring CuBIDS."""
+
+from pathlib import Path
+
+import yaml
+from pkg_resources import resource_filename as pkgrf
+
+
+def load_config(config_file):
+    """Load a YAML file containing a configuration for param groups."""
+    if config_file is None:
+        config_file = Path(pkgrf("cubids", "data/config.yml"))
+
+    with config_file.open() as f:
+        config = yaml.safe_load(f)
+
+    return config
