8258228192aaf2c22d76db542e89c26b58c9d4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:04:57 +0000

    Add package quickdl/0.0.2/quickdl-0.0.2.tar.gz

---
+++ packages/quickdl/quickdl-0.0.2.tar.gz/quickdl-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+install_requires = []
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='quickdl',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    author='Manbehindthemadness',
+    author_email='manbehindthemadness@gmail.com',
+    description='A simple file downloader',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/manbehindthemadness/quickdl',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
