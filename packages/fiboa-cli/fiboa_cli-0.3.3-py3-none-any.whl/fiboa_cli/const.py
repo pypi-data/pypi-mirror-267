3948e711343a0501c61bb1dff742cbcbb027d8c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 04:04:20 +0000

    Add package fiboa-cli/0.3.3/fiboa_cli-0.3.3-py3-none-any.whl

---
+++ packages/fiboa-cli/fiboa_cli-0.3.3-py3-none-any.whl/fiboa_cli/const.py
@@ -0,0 +1,11 @@
+LOG_STATUS_COLOR = {
+    "info": "white",
+    "warning": "yellow",
+    "error": "red",
+    "success": "green"
+}
+
+SUPPORTED_PROTOCOLS = ["http", "https", "s3", "gs"]
+
+STAC_COLLECTION_SCHEMA = "http://schemas.stacspec.org/v1.0.0/collection-spec/json-schema/collection.json"
+STAC_TABLE_EXTENSION = "https://stac-extensions.github.io/table/v1.2.0/schema.json"
