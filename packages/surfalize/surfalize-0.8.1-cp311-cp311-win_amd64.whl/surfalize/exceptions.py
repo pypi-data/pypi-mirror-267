225f1fe5ade11048b87a5a59f41661629217f6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:05:21 +0000

    Add package surfalize/0.8.1/surfalize-0.8.1-cp311-cp311-win_amd64.whl

---
+++ packages/surfalize/surfalize-0.8.1-cp311-cp311-win_amd64.whl/surfalize/exceptions.py
@@ -0,0 +1,25 @@
+class FileFormatError(Exception):
+    """
+    Base class for file related errors.
+    """
+    pass
+
+
+class UnsupportedFileFormatError(FileFormatError):
+    """
+    Raised when a file format is not supported.
+    """
+    pass
+
+
+class CorruptedFileError(FileFormatError):
+    """
+    Raised when a file is corrupted.
+    """
+    pass
+
+
+class FittingError(Exception):
+    """
+    Raised when a fitting operation has failed.
+    """
