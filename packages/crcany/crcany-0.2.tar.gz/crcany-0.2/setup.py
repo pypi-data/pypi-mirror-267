f9ab079a13816176584b4bbdc003709d9c69bdc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:04:55 +0000

    Add package crcany/0.2.0/crcany-0.2.tar.gz

---
+++ packages/crcany/crcany-0.2.tar.gz/crcany-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, Extension
+
+setup(
+    name = 'crcany',
+    version = '0.2.0',
+    ext_modules = [Extension(name='crcany.crcany', sources=['src/crcany/crcany.pyx', 'lib/crcany/model.c', 'lib/crcany/crc.c'])]
+)
