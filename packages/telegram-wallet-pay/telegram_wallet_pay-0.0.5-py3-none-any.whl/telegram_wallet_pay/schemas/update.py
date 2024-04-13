d87ff8c895dd10b03df10c16ec6fb1edbd1dbb13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:05:38 +0000

    Add package telegram-wallet-pay/0.0.5/telegram_wallet_pay-0.0.5-py3-none-any.whl

---
+++ packages/telegram-wallet-pay/telegram_wallet_pay-0.0.5-py3-none-any.whl/telegram_wallet_pay/schemas/update.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from typing import List, Literal, Iterator
+
+from pydantic import Field
+
+from ._default import DefaultModel, DefaultRootModel
+from .webhook_payload import WebhookPayload
+
+
+class Update(DefaultModel):
+    event_datetime: datetime = Field(alias="eventDateTime")
+    event_id: int
+    type: Literal["ORDER_FAILED", "ORDER_PAID"]
+    payload: WebhookPayload
+
+
+class Updates(DefaultRootModel):
+
+    root: List[Update]
+
+    def __iter__(self) -> Iterator[Update]:
+        return iter(self.root)
