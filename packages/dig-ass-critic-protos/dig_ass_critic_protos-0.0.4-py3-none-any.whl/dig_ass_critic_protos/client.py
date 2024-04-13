3c60b1a6f0b201e507beedde8d302839a2252f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:37 +0000

    Add package dig-ass-critic-protos/0.0.4/dig_ass_critic_protos-0.0.4-py3-none-any.whl

---
+++ packages/dig-ass-critic-protos/dig_ass_critic_protos-0.0.4-py3-none-any.whl/dig_ass_critic_protos/client.py
@@ -0,0 +1,18 @@
+from google.protobuf.json_format import ParseDict
+from dig_ass_critic_protos.DigitalAssistantCritic_pb2_grpc import DigitalAssistantCriticStub
+from dig_ass_critic_protos.DigitalAssistantCritic_pb2 import DigitalAssistantCriticRequest, DigitalAssistantCriticResponse, ChatItem
+
+from .abstract_client import AbstractClient
+
+class CriticClient(AbstractClient):
+    def __init__(self, address) -> None:
+        super().__init__(address)
+        self._stub = DigitalAssistantCriticStub(self._channel)
+
+    def __call__(self, text: str, chat: dict):
+        request = DigitalAssistantCriticRequest(
+            Text=text,
+            Chat=ParseDict(chat, ChatItem()),
+        )
+        response: DigitalAssistantCriticResponse = self._stub.GetTextResponse(request)
+        return response.Score
