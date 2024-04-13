c502b1da3aff0c7b3bcf25239463f6cba2711df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 04:04:35 +0000

    Add package aero-svo-api/0.1.2/aero_svo_api-0.1.2-py3-none-any.whl

---
+++ packages/aero-svo-api/aero_svo_api-0.1.2-py3-none-any.whl/aero_svo_api/__init__.py
@@ -0,0 +1,4 @@
+from .api import AsyncSvoApi
+
+__all__ = (AsyncSvoApi,)
+__version__ = '0.1.2'
---
+++ packages/aero-svo-api/aero_svo_api-0.1.2-py3-none-any.whl/aero_svo_api/urls.py
@@ -0,0 +1,7 @@
+
+
+timetable = 'https://www.svo.aero/bitrix/timetable/'
+terminals = 'https://www.svo.aero/bitrix/map/terminals/'
+season_airports = 'https://www.svo.aero/bitrix/season/?locale=en'
+season = 'https://www.svo.aero/bitrix/season/list_v2/?city=432&terminal=all&direction=departure&locale=en'
+airlines = 'https://www.svo.aero/bitrix/airlines/?perPage=100&page=1&locale=ru'
