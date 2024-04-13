f93562fcb03089244d5fda6e75bdff2c08dba1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:17 +0000

    Add package hnt-nf-jira-library/0.3.1/hnt_nf_jira_library-0.3.1.tar.gz

---
+++ packages/hnt-nf-jira-library/hnt_nf_jira_library-0.3.1.tar.gz/hnt_nf_jira_library-0.3.1/nf_jira/entities/fatura.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+from .dados_basicos_fatura import DadosBasicos
+from .pagamento import Pagamento
+from .jira_info import JiraInfo
+
+class Fatura(BaseModel):
+    dados_basicos: DadosBasicos
+    pagamento: Pagamento
+    jira_info: JiraInfo
