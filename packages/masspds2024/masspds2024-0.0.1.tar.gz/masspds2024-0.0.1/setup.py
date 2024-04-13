dbf7c6cb0b5008796b19c63072981996655904b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 15:04:29 +0000

    Add package masspds2024/0.0.1/masspds2024-0.0.1.tar.gz

---
+++ packages/masspds2024/masspds2024-0.0.1.tar.gz/masspds2024-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='masspds2024',
+    version='0.0.1',
+    author='Math And Science Summer Programming Data Science 2024',
+    author_email='nghi08122003@gmail.com',
+    description='A short description of your package',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
