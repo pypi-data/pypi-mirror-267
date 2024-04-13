f4dc477cc561b7e651b89e03d6177b6f5ba902bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:34 +0000

    Add package mors-client/0.0.1/mors-client-0.0.1.tar.gz

---
+++ packages/mors-client/mors-client-0.0.1.tar.gz/mors-client-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+name='mors-client',
+version='0.0.1',
+author='MorS',
+author_email='derictor228@example.com',
+description='Cool Web3 Client on Python',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
+
