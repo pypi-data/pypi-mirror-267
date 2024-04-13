6df341262256bebab2ff4153752779e0d0b86e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 15:04:10 +0000

    Add package grid-data-models/0.0.1/grid_data_models-0.0.1-py3-none-any.whl

---
+++ packages/grid-data-models/grid_data_models-0.0.1-py3-none-any.whl/gdm/distribution/equipment/matrix_impedance_fuse_equipment.py
@@ -0,0 +1,28 @@
+""" This module contains matrix impedacne phase."""
+
+from typing import Annotated
+
+from pydantic import Field
+from infrasys.quantities import Time
+
+
+from gdm.distribution.equipment.matrix_impedance_branch_equipment import (
+    MatrixImpedanceBranchEquipment,
+)
+from gdm.distribution.curve import TimeCurrentCurve
+
+
+class MatrixImpedanceFuseEquipment(MatrixImpedanceBranchEquipment):
+    """Interface for impedance based fuse equipment."""
+
+    delay: Annotated[Time, Field(description="Delay time before blowing the fuse.")]
+    tcc_curve: Annotated[TimeCurrentCurve, Field(description="Time current curve")]
+
+    @classmethod
+    def example(cls) -> "MatrixImpedanceFuseEquipment":
+        """Example for matrix impedance model."""
+        return MatrixImpedanceFuseEquipment(
+            **MatrixImpedanceBranchEquipment.example().model_dump(exclude_none=True),
+            delay=Time(0, "minutes"),
+            tcc_curve=TimeCurrentCurve.example(),
+        )
