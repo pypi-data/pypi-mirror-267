12ac7c3e404cf66ba96df970656720f84a0fb597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:19 +0000

    Add package pyBlindRL/0.0.2rc49.post1/pyblindrl-0.0.2rc49.post1.tar.gz

---
+++ packages/pyBlindRL/pyblindrl-0.0.2rc49.post1.tar.gz/pyblindrl-0.0.2rc49.post1/src/pyBlindRL/__init__.py
@@ -0,0 +1,10 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Logan Walker. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""A Python implementation of blind Richardson-Lucy deconvolution"""
+from __future__ import annotations
+
+__version__ = "0.0.2""-rc49-post1"
+
+import torch
