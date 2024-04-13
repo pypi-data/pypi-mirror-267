4f858eac87f295c0f48f6d78eeefc571e2787398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 15:04:03 +0000

    Add package seventilation-client/0.1.0/seventilation_client-0.1.0-py3-none-any.whl

---
+++ packages/seventilation-client/seventilation_client-0.1.0-py3-none-any.whl/seventilation_client/api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from seventilation_client.api.areas_api import AreasApi
+from seventilation_client.api.controller_api import ControllerApi
+from seventilation_client.api.devices_api import DevicesApi
+from seventilation_client.api.gateway_api import GatewayApi
+from seventilation_client.api.notifications_api import NotificationsApi
+from seventilation_client.api.settings_api import SettingsApi
+from seventilation_client.api.setup_api import SetupApi
+from seventilation_client.api.telemetry_api import TelemetryApi
+from seventilation_client.api.users_api import UsersApi
