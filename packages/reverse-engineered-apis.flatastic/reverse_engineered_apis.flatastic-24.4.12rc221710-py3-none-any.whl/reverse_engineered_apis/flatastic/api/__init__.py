a68bd6314da7c6814eef2ac335de5d3c681852ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:14 +0000

    Add package reverse-engineered-apis.flatastic/24.4.12rc221710/reverse_engineered_apis.flatastic-24.4.12rc221710-py3-none-any.whl

---
+++ packages/reverse-engineered-apis.flatastic/reverse_engineered_apis.flatastic-24.4.12rc221710-py3-none-any.whl/reverse_engineered_apis/flatastic/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from reverse_engineered_apis.flatastic.api.auth_api import AuthApi
+from reverse_engineered_apis.flatastic.api.cashflow_api import CashflowApi
+from reverse_engineered_apis.flatastic.api.chores_api import ChoresApi
+from reverse_engineered_apis.flatastic.api.shoppinglist_api import ShoppinglistApi
+from reverse_engineered_apis.flatastic.api.shouts_api import ShoutsApi
+from reverse_engineered_apis.flatastic.api.subscriptions_api import SubscriptionsApi
+from reverse_engineered_apis.flatastic.api.user_api import UserApi
+from reverse_engineered_apis.flatastic.api.wg_api import WgApi
+
