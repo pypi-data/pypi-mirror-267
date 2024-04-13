744d8024a0047f8636923c5c929335f28dee39bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 15:04:10 +0000

    Add package grid-data-models/0.0.1/grid_data_models-0.0.1.tar.gz

---
+++ packages/grid-data-models/grid_data_models-0.0.1.tar.gz/grid_data_models-0.0.1/docs/api/distribution_branch.md
@@ -0,0 +1,9 @@
+# Distribution Branch
+
+
+```{eval-rst}
+.. automodule:: gdm
+    :members: SequenceImpedanceBranchEquipment, MatrixImpedanceBranchEquipment, GeometryBranchEquipment, DistributionBranch, MatrixImpedanceBranch, GeometryBranch
+    :exclude-members: model_fields, model_config, validate_fields
+
+```
---
+++ packages/grid-data-models/grid_data_models-0.0.1.tar.gz/grid_data_models-0.0.1/src/gdm/distribution/limitset.py
@@ -0,0 +1,32 @@
+""" This module contains interface for operation limit sets. """
+
+from typing import Annotated
+from infrasys import Component
+
+from gdm.quantities import PositiveCurrent, PositiveVoltage
+from gdm.distribution.distribution_enum import LimitType
+from pydantic import Field
+
+
+class VoltageLimitSet(Component):
+    """Interface for voltage limit set."""
+    name: Annotated[str, Field('', description="Name of the voltage limit set.")]
+    limit_type: Annotated[LimitType, Field(..., description="Limit type used.")]
+    value: Annotated[PositiveVoltage, Field(..., description="Voltage threshold.")]
+
+    @classmethod
+    def example(cls) -> "VoltageLimitSet":
+        """Example for voltage limit set."""
+        return VoltageLimitSet(limit_type=LimitType.MIN, value=PositiveVoltage(400 * 0.9, "volt"))
+
+
+class ThermalLimitSet(Component):
+    """Interface for voltage limit set."""
+    name: Annotated[str, Field('', description="Name of the thermal limit set.")]
+    limit_type: Annotated[LimitType, Field(..., description="Limit type used.")]
+    value: Annotated[PositiveCurrent, Field(..., description="Current threshold.")]
+
+    @classmethod
+    def example(cls) -> "ThermalLimitSet":
+        """Example for thermal limit set."""
+        return ThermalLimitSet(limit_type=LimitType.MAX, value=PositiveCurrent(110, "ampere"))
