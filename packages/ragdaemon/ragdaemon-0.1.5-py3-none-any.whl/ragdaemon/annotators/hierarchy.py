848d17e4a13ba4b31f42889c8922acb9d2ef9202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:05:06 +0000

    Add package ragdaemon/0.1.5/ragdaemon-0.1.5-py3-none-any.whl

---
+++ packages/ragdaemon/ragdaemon-0.1.5-py3-none-any.whl/ragdaemon/annotators/hierarchy.py
@@ -0,0 +1,157 @@
+import fnmatch
+from pathlib import Path
+
+import networkx as nx
+from spice import Spice
+
+from ragdaemon.annotators.base_annotator import Annotator
+from ragdaemon.database import MAX_TOKENS_PER_EMBEDDING, Database
+from ragdaemon.errors import RagdaemonError
+from ragdaemon.llm import DEFAULT_COMPLETION_MODEL
+from ragdaemon.utils import get_document, get_non_gitignored_files, hash_str
+
+
+def match_path_with_patterns(path: Path, cwd: Path, patterns: list[str] = []) -> bool:
+    """Check if the given absolute path matches any of the patterns.
+
+    Args:
+        `path` - An absolute path
+        `patterns` - A set of absolute paths/glob patterns
+
+    Return:
+        A boolean flag indicating if the path matches any of the patterns
+    """
+    if not path.is_absolute():
+        path = cwd / path
+    for pattern in patterns:
+        # Check if the pattern is a glob pattern match
+        if fnmatch.fnmatch(str(path), str(pattern)):
+            return True
+        pattern = Path(pattern)
+        if not pattern.is_absolute():
+            pattern = cwd / pattern
+        # Check if the path is relative to the pattern
+        if path.is_relative_to(pattern):
+            return True
+    return False
+
+
+def get_active_checksums(
+    cwd: Path,
+    db: Database,
+    refresh: bool = False,
+    verbose: bool = False,
+    ignore_patterns: list[str] = [],
+) -> dict[Path:str]:
+    checksums: dict[Path:str] = {}
+    git_paths = get_non_gitignored_files(cwd)
+    add_to_db = {
+        "ids": [],
+        "documents": [],
+        "metadatas": [],
+    }
+    for path in git_paths:
+        if match_path_with_patterns(path, cwd, ignore_patterns):
+            continue
+        try:
+            path_str = path.as_posix()
+            ref = path_str
+            document = get_document(ref, cwd)
+            tokens = Spice().count_tokens(document, DEFAULT_COMPLETION_MODEL)
+            if tokens > MAX_TOKENS_PER_EMBEDDING:  # e.g. package-lock.json
+                continue
+            checksum = hash_str(document)
+            existing_record = len(db.get(checksum)["ids"]) > 0
+            if refresh or not existing_record:
+                # add new items to db (will generate embeddings)
+                metadatas = {
+                    "id": path_str,
+                    "type": "file",
+                    "ref": ref,
+                    "checksum": checksum,
+                    "active": False,
+                }
+                add_to_db["ids"].append(checksum)
+                add_to_db["documents"].append(document)
+                add_to_db["metadatas"].append(metadatas)
+            checksums[path] = checksum
+        except UnicodeDecodeError:  # Ignore non-text files
+            pass
+        except RagdaemonError as e:
+            if verbose:
+                print(f"Error processing path {path}: {e}")
+    if len(add_to_db["ids"]) > 0:
+        db.upsert(**add_to_db)
+    return checksums
+
+
+def files_checksum(cwd: Path, ignore_patterns: list[str] = []) -> str:
+    timestamps = ""
+    for path in get_non_gitignored_files(cwd):
+        if match_path_with_patterns(path, cwd, ignore_patterns):
+            continue
+        try:
+            timestamps += str(path.stat().st_mtime)
+        except FileNotFoundError:
+            pass
+    return hash_str(timestamps)
+
+
+class Hierarchy(Annotator):
+    name = "hierarchy"
+
+    def __init__(self, *args, ignore_patterns: list[str] = [], **kwargs):
+        self.ignore_patterns = ignore_patterns
+        super().__init__(*args, **kwargs)
+
+    def is_complete(self, graph: nx.MultiDiGraph, db: Database = None) -> bool:
+        cwd = Path(graph.graph["cwd"])
+        return graph.graph.get("files_checksum") == files_checksum(
+            cwd, self.ignore_patterns
+        )
+
+    async def annotate(
+        self, old_graph: nx.MultiDiGraph, db: Database = None, refresh: bool = False
+    ) -> nx.MultiDiGraph:
+        """Build a graph of active files and directories with hierarchy edges."""
+        cwd = Path(old_graph.graph["cwd"])
+        checksums = get_active_checksums(
+            cwd,
+            db,
+            refresh=refresh,
+            verbose=self.verbose,
+            ignore_patterns=self.ignore_patterns,
+        )
+        _files_checksum = files_checksum(cwd, self.ignore_patterns)
+
+        # Initialize an empty graph. We'll build it from scratch.
+        graph = nx.MultiDiGraph()
+        graph.graph["cwd"] = str(cwd)
+        edges_to_add = set()
+        for path, checksum in checksums.items():
+            # add db reecord
+            id = path.as_posix()
+            results = db.get(checksum)
+            data = results["metadatas"][0]
+            graph.add_node(id, **data)
+
+            # add hierarchy edges
+            def _link_to_cwd(_path: Path):
+                _parent = _path.parent.as_posix() if len(_path.parts) > 1 else "ROOT"
+                edges_to_add.add((_parent, _path.as_posix()))
+                if _parent != "ROOT":
+                    _link_to_cwd(_path.parent)
+
+            _link_to_cwd(path)
+
+        # Add directory nodes with checksums
+        for source, target in edges_to_add:
+            for id in (source, target):
+                if id not in graph:
+                    # add directories to graph (to link hierarchy) but not db
+                    record = {"id": id, "type": "directory", "ref": id}
+                    graph.add_node(id, **record)
+            graph.add_edge(source, target, type="hierarchy")
+
+        graph.graph["files_checksum"] = _files_checksum
+        return graph
---
+++ packages/ragdaemon/ragdaemon-0.1.5-py3-none-any.whl/ragdaemon/context.py
@@ -0,0 +1,155 @@
+from pathlib import Path
+from typing import Any
+
+import networkx as nx
+
+from ragdaemon.annotators.diff import parse_diff_id
+from ragdaemon.database import Database
+from ragdaemon.utils import parse_path_ref
+
+
+class ContextBuilder:
+    """Renders items from a graph into an llm-readable string."""
+
+    def __init__(self, graph: nx.MultiDiGraph, db: Database, verbose: bool = False):
+        self.graph = graph
+        self.db = db
+        self.verbose = verbose
+        self.context = dict[
+            str, dict[str, Any]
+        ]()  # {path: {lines, tags, document, diff}}
+
+    def _add_path(self, path_str: str):
+        """Create a new record in the context for the given path."""
+        cwd = self.graph.graph["cwd"]
+        if path_str not in self.graph:  # e.g. deleted file
+            document = ""
+        else:
+            checksum = self.graph.nodes[path_str]["checksum"]
+            document = self.db.get(checksum)["documents"][0]
+        message = {
+            "lines": set(),
+            "tags": set(),
+            "document": document,
+            "diffs": set(),
+        }
+        self.context[path_str] = message
+
+    def add_ref(self, path_ref: str, tags: list[str] = []):
+        """Manually include path_refs"""
+        path, lines = parse_path_ref(path_ref)
+        path_str = path.as_posix()
+        if path_str not in self.context:
+            self._add_path(path_str)
+        self.context[path_str]["tags"].update(tags)
+        if not lines:
+            document = self.context[path_str]["document"]
+            lines = list(range(1, len(document.splitlines())))
+        self.context[path_str]["lines"].update(lines)
+
+    def add_diff(self, id: str):
+        """Take a diff id and add to context"""
+        _, path, _ = parse_diff_id(id)
+        if not path:  # e.g. diff 'parent' nodes
+            return
+        path_str = path.as_posix()
+        if path_str not in self.context:
+            self._add_path(path_str)
+        self.context[path_str]["diffs"].add(id)
+        self.context[path_str]["tags"].add("diff")
+
+    def remove_ref(self, ref: str, tags: list[str] = []):
+        """Remove the given id from the context."""
+        path, lines = parse_path_ref(ref)
+        path_str = path.as_posix()
+        if path_str not in self.context:
+            if self.verbose:
+                print(f"Warning: no matching message found for {path_str}.")
+            return
+        if lines:
+            self.context[path_str]["lines"] -= lines
+        else:
+            self.context[path_str]["lines"] = set()
+        if tags:
+            self.context[path_str]["tags"] -= set(tags)
+        if not self.context[path_str]["lines"] and not self.context[path_str]["diffs"]:
+            del self.context[path_str]
+        return ref
+
+    def remove_diff(self, id: str):
+        """Remove the given id from the context."""
+        _, path, _ = parse_diff_id(id)
+        path_str = path.as_posix()
+        if path_str not in self.context:
+            if self.verbose:
+                print(f"Warning: no matching message found for {path_str}.")
+            return
+        self.context[path_str]["diffs"].remove(id)
+        self.context[path_str]["tags"].remove("diff")
+        if not self.context[path_str]["lines"] and not self.context[path_str]["diffs"]:
+            del self.context[path_str]
+        return id
+
+    def render(self) -> str:
+        """Return a formatted context message for the given nodes."""
+        output = ""
+        for path_str, data in self.context.items():
+            if output:
+                output += "\n"
+            tags = "" if not data["tags"] else f" ({', '.join(sorted(data['tags']))})"
+            output += f"{path_str}{tags}\n"
+            if data["lines"]:
+                file_lines = data["document"].splitlines()
+                last_rendered = 0
+                for line in sorted(data["lines"]):
+                    if line - last_rendered > 1:
+                        output += "...\n"
+                    output += f"{line}:{file_lines[line]}\n"
+                    last_rendered = line
+                if last_rendered < len(file_lines) - 1:
+                    output += "...\n"
+            if data["diffs"]:
+                output += self.render_diffs(data["diffs"])
+        return output
+
+    def render_diffs(self, ids: set[str]) -> str:
+        output = ""
+        diff_str, _, _ = parse_diff_id(next(iter(ids)))
+        git_command = "--git diff"
+        if diff_str != "DEFAULT":
+            git_command += f" {diff_str}"
+        output += f"{git_command}\n"
+        cwd = self.graph.graph["cwd"]
+        for id in sorted(ids):
+            checksum = self.graph.nodes[id]["checksum"]
+            document = self.db.get(checksum)["documents"][0]
+            # TODO: Add line numbers
+            without_git_command = "\n".join(document.splitlines()[1:])
+            output += without_git_command + "\n"
+        return output
+
+    def to_refs(self) -> list[str]:
+        """Return a list of path:interval,interval for everything in current context."""
+        refs = dict[Path, str]()
+        for path, data in self.context.items():
+            if len(data["lines"]) == 0:
+                continue
+            elif len(data["lines"]) == data["document"].splitlines():
+                refs[path] = ""
+                continue
+            segments = []
+            current_segment = ""
+            last_line = 0
+            for line in sorted(data["lines"]):
+                if line - last_line > 1:
+                    current_segment += f"-{last_line}"
+                    segments.append(current_segment)
+                    current_segment = ""
+                if not current_segment:
+                    current_segment = str(line)
+                last_line = line
+            if current_segment:
+                current_segment += f"-{last_line}"
+                segments.append(current_segment)
+            refs[path] = ",".join(segments)
+        return [f"{path}:{ref}" for path, ref in refs.items()]
