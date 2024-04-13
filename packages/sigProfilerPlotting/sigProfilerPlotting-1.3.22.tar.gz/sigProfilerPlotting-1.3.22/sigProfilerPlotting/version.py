ad55a8882af30989b5d9e6741190b08f862bb73f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:41 +0000

    Add package sigProfilerPlotting/1.3.22/sigProfilerPlotting-1.3.22.tar.gz

---
+++ packages/sigProfilerPlotting/sigProfilerPlotting-1.3.22.tar.gz/sigProfilerPlotting-1.3.22/sigProfilerPlotting/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILERPLOTTING SETUP.PY
+short_version = '1.3.22'
+version = '1.3.22'
+update = 'Upgrade v1.3.22: Update input_processing index.name handling to be more robust.'
+    
+    
