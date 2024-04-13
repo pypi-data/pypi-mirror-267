5e775e0760c6acd6bfaa943b969f1c82f8e9d721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:05:38 +0000

    Add package telegram-wallet-pay/0.0.5/telegram_wallet_pay-0.0.5.tar.gz

---
+++ packages/telegram-wallet-pay/telegram_wallet_pay-0.0.5.tar.gz/telegram_wallet_pay-0.0.5/telegram_wallet_pay/schemas/__init__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    "MoneyAmount",
+    "OrderNew",
+    "OrderPreview",
+    "OrderResult",
+    "PaymentOption",
+    "Update",
+    "Updates",
+    "WebhookPayload",
+]
+
+from .money_amount import MoneyAmount
+from .order_new import OrderNew
+from .order_preview import OrderPreview
+from .order_result import OrderResult
+from .payment_option import PaymentOption
+from .update import Update, Updates
+from .webhook_payload import WebhookPayload
