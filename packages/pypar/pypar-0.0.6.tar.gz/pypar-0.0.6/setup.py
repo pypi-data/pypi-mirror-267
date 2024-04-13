4a1bc746f22197cb086195a2756738a28e803f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 14:04:41 +0000

    Add package pypar/0.0.6/pypar-0.0.6.tar.gz

---
+++ packages/pypar/pypar-0.0.6.tar.gz/pypar-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+# Description
+with open('README.md') as file:
+    long_description = file.read()
+
+
+setup(
+    name='pypar',
+    version='0.0.6',
+    description='Python phoneme alignment representation',
+    author='Max Morrison',
+    author_email='maxrmorrison@gmail.com',
+    url='https://github.com/maxrmorrison/pypar',
+    install_requires=['numpy'],
+    packages=['pypar'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['align', 'duration', 'phoneme', 'speech'],
+    classifiers=['License :: OSI Approved :: MIT License'],
+    license='MIT')
