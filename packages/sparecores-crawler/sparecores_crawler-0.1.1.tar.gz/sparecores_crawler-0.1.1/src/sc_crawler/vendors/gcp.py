bb21853db84cd382189f6b15b5a55f934c8ead9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 05:04:29 +0000

    Add package sparecores-crawler/0.1.1/sparecores_crawler-0.1.1.tar.gz

---
+++ packages/sparecores-crawler/sparecores_crawler-0.1.1.tar.gz/sparecores_crawler-0.1.1/src/sc_crawler/vendors/gcp.py
@@ -0,0 +1,38 @@
+def inventory_compliance_frameworks(vendor):
+    return []
+
+
+def inventory_datacenters(vendor):
+    return []
+
+
+def inventory_zones(vendor):
+    return []
+
+
+def inventory_servers(vendor):
+    return []
+
+
+def inventory_server_prices(vendor):
+    return []
+
+
+def inventory_server_prices_spot(vendor):
+    return []
+
+
+def inventory_storages(vendor):
+    return []
+
+
+def inventory_storage_prices(vendor):
+    return []
+
+
+def inventory_traffic_prices(vendor):
+    return []
+
+
+def inventory_ipv4_prices(vendor):
+    return []
