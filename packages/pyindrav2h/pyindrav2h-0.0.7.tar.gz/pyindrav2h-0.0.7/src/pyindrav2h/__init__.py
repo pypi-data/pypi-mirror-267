90ed3bf0543a6249f2a4c3991d50c0015dc43164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:05:36 +0000

    Add package pyindrav2h/0.0.7/pyindrav2h-0.0.7.tar.gz

---
+++ packages/pyindrav2h/pyindrav2h-0.0.7.tar.gz/pyindrav2h-0.0.7/src/pyindrav2h/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.7"
+
+V2H_MODES = {
+    'LOAD_MATCH': 'loadmatch',
+    'SCHEDULE': 'clear',
+    'IDLE': 'idle',
+    'CHARGE': 'charge',
+    'DISCHARGE': 'discharge',
+    'EXPORT_MATCH': 'exportmatch'
+}
