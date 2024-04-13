49bed7dba8b094516e1be79aa935e73b56639eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:52 +0000

    Add package vulcan-utils/1.11.3/vulcan_utils-1.11.3.tar.gz

---
+++ packages/vulcan-utils/vulcan_utils-1.11.3.tar.gz/vulcan_utils-1.11.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open("docs/README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="vulcan-utils",
+    version="1.11.3",
+    description="A utility package for Python",
+    packages=find_packages(),
+    install_requires=[
+        "coloredlogs",
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
