d58982b68bd2f1f18518b63e5c473ab98bebf3c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 03:04:36 +0000

    Add package rtrafactor/0.3/rtrafactor-0.3.tar.gz

---
+++ packages/rtrafactor/rtrafactor-0.3.tar.gz/rtrafactor-0.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rtrafactor',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+        'google',
+    ],
+    author='Udit Raj',
+    author_email='udit_2312res708@iitp.ac.in',
+    description='RTRA official library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/uditakhourii/rtrafactor',
+)
