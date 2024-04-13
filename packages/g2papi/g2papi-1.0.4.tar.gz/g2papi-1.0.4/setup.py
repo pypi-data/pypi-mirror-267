c31d0d89bd6b66a925950538febd2439412d6819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-13 06:05:10 +0000

    Add package g2papi/1.0.4/g2papi-1.0.4.tar.gz

---
+++ packages/g2papi/g2papi-1.0.4.tar.gz/g2papi-1.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='g2papi',
+    version='1.0.4',
+    author='Jordan Safer',
+    author_email='genomics2proteins@gmail.com',
+    description='A python client and CLI for easy access to G2P portal APIs',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "GitHub": "https://www.github.com/broadinstitute/g2papi",
+        "G2P portal": "https://g2p.broadinstitute.org"
+    },
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pandas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'g2papi=g2papi.cli:main',
+        ],
+    },
+)
