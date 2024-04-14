9808e876cc5fbe69ddcf7be7de55f7e7b18b3407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 15:15:27 +0000

    Add package thuonguyenpackage/0.1/thuonguyenpackage-0.1.tar.gz

---
+++ packages/thuonguyenpackage/thuonguyenpackage-0.1.tar.gz/thuonguyenpackage-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='thuonguyenpackage',
+    version='0.1',
+    author='Thuong Nguyen',
+    author_email='thuongntk2008@gmail.com',
+    description='String utils',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
