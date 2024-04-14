199ba9efba74c69d99dbea2e4ea80902d79dab13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 15:16:42 +0000

    Add package python-flux/1.0.0/python_flux-1.0.0-py3-none-any.whl

---
+++ packages/python-flux/python_flux-1.0.0-py3-none-any.whl/python_flux/flux_utilis.py
@@ -0,0 +1,32 @@
+import traceback
+
+
+class FluxUtils:
+
+    @staticmethod
+    def default_error_resume(value, ex, context):
+        raise value
+    @staticmethod
+    def default_action(value, context):
+        pass
+
+    @staticmethod
+    def default_predicate(value, context):
+        return True
+
+    @staticmethod
+    def default_success(value, context):
+        pass
+
+    @staticmethod
+    def default_error(e, context):
+        if not isinstance(e, StopIteration):
+            traceback.print_exception(type(e), e, e.__traceback__)
+
+    @staticmethod
+    def try_or(statement, value, ctx) -> (object, Exception):
+        try:
+            v = statement(value, ctx)
+            return v, None
+        except Exception as ex:
+            return None, ex
