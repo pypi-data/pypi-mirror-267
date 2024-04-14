3156f8a7c9bab7b7718bbfae6862e80d9bab52a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:36 +0000

    Add package pipez/0.0.82/pipez-0.0.82.tar.gz

---
+++ packages/pipez/pipez-0.0.82.tar.gz/pipez-0.0.82/setup.py
@@ -0,0 +1,55 @@
+from itertools import chain
+from setuptools import setup, find_packages
+from os.path import join, dirname
+from pipez import __version__
+
+requires = [
+
+]
+
+extra_requires = {
+    'cv': [
+        'numpy',
+        'opencv-python'
+    ],
+    'ort': [
+        'numpy'
+        'onnxruntime'
+    ],
+    'fastapi': [
+        'fastapi',
+        'uvicorn'
+    ],
+    'shared': [
+        'shared-memory-dict'
+    ],
+    'metrics': [
+        'fastapi',
+        'uvicorn'
+        'Jinja2'
+    ]
+}
+
+extra_requires["all"] = list(
+    set(chain.from_iterable(extra_requires.values()))
+)
+
+setup(
+    name='pipez',
+    version=__version__,
+    author="Alexander Timofeev",
+    author_email="tam2511@mail.ru",
+    python_requires=">=3.8",
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tam2511/pipez",
+    install_requires=requires,
+    extras_require=extra_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True
+)
