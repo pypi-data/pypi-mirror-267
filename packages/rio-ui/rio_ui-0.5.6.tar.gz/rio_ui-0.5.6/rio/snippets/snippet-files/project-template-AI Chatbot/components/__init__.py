a39b7059f0d8d3e1349678afbc7110d06f6b1e3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:38 +0000

    Add package rio-ui/0.5.6/rio_ui-0.5.6.tar.gz

---
+++ packages/rio-ui/rio_ui-0.5.6.tar.gz/rio_ui-0.5.6/rio/snippets/snippet-files/project-template-AI Chatbot/components/__init__.py
@@ -0,0 +1,4 @@
+from .chat_message import ChatMessage
+from .chat_suggestion_card import ChatSuggestionCard
+from .empty_chat_placeholder import EmptyChatPlaceholder
+from .generating_response_placeholder import GeneratingResponsePlaceholder
