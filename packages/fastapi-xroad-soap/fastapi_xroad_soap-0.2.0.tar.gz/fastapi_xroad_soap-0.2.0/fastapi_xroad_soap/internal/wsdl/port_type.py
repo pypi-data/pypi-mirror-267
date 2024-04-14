8c738c1902f26aad4f5f39802dfd4087b2169f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:53 +0000

    Add package fastapi-xroad-soap/0.2.0/fastapi_xroad_soap-0.2.0.tar.gz

---
+++ packages/fastapi-xroad-soap/fastapi_xroad_soap-0.2.0.tar.gz/fastapi_xroad_soap-0.2.0/fastapi_xroad_soap/internal/wsdl/port_type.py
@@ -0,0 +1,56 @@
+#
+#   European Union Public License 1.2
+#
+#   Copyright (c) 2024, Centre of Registers and Information Systems
+#
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#
+#   SPDX-License-Identifier: EUPL-1.2
+#
+import typing as t
+from pydantic_xml import BaseXmlModel, element, attr
+from ..constants import WSDL_NSMAP
+
+
+__all__ = [
+    "WSDLFaultPort",
+    "WSDLOutputPort",
+    "WSDLInputPort",
+    "WSDLDocumentation",
+    "WSDLOperationPort",
+    "WSDLPortType"
+]
+
+
+class WSDLFaultPort(BaseXmlModel, tag="fault", nsmap=WSDL_NSMAP):
+    message: str = attr()
+    name: str = attr()
+
+
+class WSDLOutputPort(BaseXmlModel, tag="output", nsmap=WSDL_NSMAP):
+    message: str = attr()
+    name: str = attr()
+
+
+class WSDLInputPort(BaseXmlModel, tag="input", nsmap=WSDL_NSMAP):
+    message: str = attr()
+    name: str = attr()
+
+
+class WSDLDocumentation(BaseXmlModel, tag="documentation", nsmap=WSDL_NSMAP):
+    title: str = element(ns='xroad', nsmap=WSDL_NSMAP)
+    notes: str = element(ns='xroad', nsmap=WSDL_NSMAP)
+
+
+class WSDLOperationPort(BaseXmlModel, tag="operation", ns="wsdl", nsmap=WSDL_NSMAP):
+    documentation: WSDLDocumentation
+    input: WSDLInputPort
+    output: WSDLOutputPort
+    fault: WSDLFaultPort
+    name: str = attr()
+
+
+class WSDLPortType(BaseXmlModel, tag="portType", ns="wsdl", nsmap=WSDL_NSMAP):
+    name: str = attr(default="servicePortType")
+    operations: t.List[WSDLOperationPort]
