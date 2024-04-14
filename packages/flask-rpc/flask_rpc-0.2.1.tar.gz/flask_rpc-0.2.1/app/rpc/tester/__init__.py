6d3fac013c9c9cbd74f50e14637d4bcd840539bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 03:24:30 +0000

    Add package flask-rpc/0.2.1/flask_rpc-0.2.1.tar.gz

---
+++ packages/flask-rpc/flask_rpc-0.2.1.tar.gz/flask_rpc-0.2.1/app/rpc/tester/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+from flask_rpc.latest import RPC
+from .funcs import add_numbers, add_string
+
+bp = Blueprint("tester", __name__, url_prefix="/tester")
+
+rpc = RPC(bp)
+
+rpc.functions(add_numbers=add_numbers, add_string=add_string)
