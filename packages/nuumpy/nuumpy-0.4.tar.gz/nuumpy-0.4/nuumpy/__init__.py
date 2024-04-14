ab00edbeaebaaa2a78a6c723378e0bd515e2da66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:57 +0000

    Add package nuumpy/0.4/nuumpy-0.4.tar.gz

---
+++ packages/nuumpy/nuumpy-0.4.tar.gz/nuumpy-0.4/nuumpy/__init__.py
@@ -0,0 +1,1 @@
+from .nuumpy import nuumpy
---
+++ packages/nuumpy/nuumpy-0.4.tar.gz/nuumpy-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nuumpy',
+    version='0.4',
+    description='NimmPy',
+    author='Your Name',
+    author_email='your@email.com',
+    packages=find_packages(),
+    install_requires=[
+    ]
+)
