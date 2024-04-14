caa9aacb8c070e2871dd7da5c93efb93052fd78c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:42 +0000

    Add package image-preprocessorpy/1.0.1/image-preprocessorpy-1.0.1.tar.gz

---
+++ packages/image-preprocessorpy/image-preprocessorpy-1.0.1.tar.gz/image-preprocessorpy-1.0.1/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='image-preprocessorpy',
+    version='1.0.1',
+    description='A package for preprocessing images by resizing and cropping them to a target size.',
+    author='Avinrique',
+    author_email='avigupta2001ad@gmail.com',
+    url='https://github.com/avinrique/image-preprocessor',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow>=8.4.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
