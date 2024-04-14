25e06395b97484e413a9e30136d124a272a61752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 03:24:31 +0000

    Add package furtheredge/0.0.1/furtheredge-0.0.1-py3-none-any.whl

---
+++ packages/furtheredge/furtheredge-0.0.1-py3-none-any.whl/furtheredge/main.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from modules.universal_life.run import universal_life_module
+
+
+# Loading input data
+def universal_life():
+    model_points = pd.read_csv("furtheredge/inputs/model_points2.csv")
+    ri_rates = pd.read_csv(
+        "furtheredge/inputs/ri_rates.csv", sep=";", decimal=","
+    )
+    wop_rates = pd.read_csv(
+        "furtheredge/inputs/wop_rates.csv", sep=";", decimal=","
+    )
+    tech_assumptions = pd.read_csv(
+        "furtheredge/inputs/tech_assumptions.csv", sep=";", decimal=","
+    )
+    product = pd.read_csv(
+        "furtheredge/inputs/product.csv", sep=";", decimal=","
+    )
+
+    # print(f"Model points length: {len(model_points)}")
+
+    result_universal_life = universal_life_module(
+        model_points,
+        {"time_horizon": 50, "projection_date": "2023-10-31"},
+        tech_assumptions,
+        ri_rates,
+        wop_rates,
+        product,
+    )
+
+    # print(result_universal_life[result_universal_life["MP_ID"] == 169])
+    return result_universal_life
+
+
+# result_universal_life = universal_life()
+# result_universal_life.to_csv(
+#     "furtheredge/outputs/universal_life_output.csv", index=False
+# )
