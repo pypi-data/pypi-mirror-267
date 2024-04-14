e0220e5d0998a1c99a160a3ab4a9180ecfe81480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:29 +0000

    Add package jxtr/2.1.5/jxtr-2.1.5.tar.gz

---
+++ packages/jxtr/jxtr-2.1.5.tar.gz/jxtr-2.1.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='jxtr',
+      version='2.1.5',
+      description='Utilities, syllabic timestamp',
+      packages=['jxtr'],
+      author="jxtrbtk",
+      author_email="jt.bontinck@gmail.com",
+      url="https://github.com/jxtrbtk/jxtr",
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',      
+      zip_safe=False)
