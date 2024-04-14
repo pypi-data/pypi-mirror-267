1f3ce31f6b2c25616e6fffd0785fce6bb05dcf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 03:24:40 +0000

    Add package furtheredge/0.0.2/furtheredge-0.0.2.tar.gz

---
+++ packages/furtheredge/furtheredge-0.0.2.tar.gz/furtheredge-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="furtheredge",
+    version="0.0.2",
+    description="""furtheredge-modules""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Novacture",
+    author_email="amine.zemni@novacture.com",
+    packages=find_packages(include=["furtheredge"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=required,
+    entry_points={"console_scripts": ["furtheredge = furtheredge.main:main"]},
+    # url="https://github.com/Novacture/furtheredge-modules",
+)
