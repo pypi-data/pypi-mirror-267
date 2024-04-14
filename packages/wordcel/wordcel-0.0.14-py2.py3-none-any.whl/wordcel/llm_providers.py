58a0c6e6af1337dacb5884a007a233b0ed9d7126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 15:16:47 +0000

    Add package wordcel/0.0.14/wordcel-0.0.14-py2.py3-none-any.whl

---
+++ packages/wordcel/wordcel-0.0.14-py2.py3-none-any.whl/wordcel/llm_providers.py
@@ -0,0 +1,44 @@
+import time
+import openai
+
+
+def openai_call(
+    prompt, model="gpt-3.5-turbo", temperature=0, max_tokens=1024, stop=["```"], sleep=60
+):
+    """Wrapper over OpenAI's completion API."""
+    client = openai.OpenAI()
+    try:
+        response = client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user", "content": prompt}],
+            max_tokens=max_tokens,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            temperature=temperature,
+            stop=stop,
+        )
+        text = response.choices[0].message.content
+
+    # With the following code, you'd get
+    # "TypeError: catching classes that do not inherit from BaseException is not allowed"
+    # except (
+    #     openai.RateLimitError,
+    #     openai.APIError,
+    #     openai.Timeout,
+    #     openai.APIConnectionError,
+    # ) as exc:
+    # So we use this instead. But this does not seem quite right either?
+    except Exception as exc:
+        print(exc)
+        print("Error from OpenAI's API. Sleeping for a few seconds.")
+        time.sleep(sleep)
+        text = openai_call(
+            prompt,
+            model=model,
+            temperature=temperature,
+            max_tokens=max_tokens,
+            stop=stop,
+        )
+
+    return text
