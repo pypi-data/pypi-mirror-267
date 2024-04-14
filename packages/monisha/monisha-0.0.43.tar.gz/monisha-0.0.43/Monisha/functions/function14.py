cd2116d2a187485c2137add4134b3e73413a198c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 15:15:53 +0000

    Add package monisha/0.0.43/monisha-0.0.43.tar.gz

---
+++ packages/monisha/monisha-0.0.43.tar.gz/monisha-0.0.43/Monisha/functions/function14.py
@@ -0,0 +1,37 @@
+import os, time
+from ..scripts.es import Folder
+from ..scripts.en import Scripted
+#===========================================================================
+
+async def CDirectory(dname=Folder.DATA07, fname=Scripted.DATA01):
+    name01 = str(dname)
+    name02 = str(fname)
+    osemse = os.getcwd()
+    moonse = os.path.join(osemse, name01, name02)
+    moonse if os.path.isdir(moonse) else os.makedirs(moonse)
+    return moonse
+
+#===========================================================================
+
+async def UDirectory(dname=Folder.DATA07, fname=Scripted.DATA01):
+    name01 = str(dname)
+    name02 = str(fname)
+    osemse = os.getcwd()
+    timeso = str(round(time.time()))
+    moonse = os.path.join(osemse, name01, timeso, name02)
+    moonse if os.path.isdir(moonse) else os.makedirs(moonse)
+    return moonse
+
+#===========================================================================
+
+async def BDirectory(uid, dname=Folder.DATA07, fname=Scripted.DATA01):
+    usered = str(uid)
+    name01 = str(dname)
+    name02 = str(fname)
+    osemse = os.getcwd()
+    timeso = str(round(time.time()))
+    moonse = os.path.join(osemse, name01, usered, timeso, name02)
+    moonse if os.path.isdir(moonse) else os.makedirs(moonse)
+    return moonse
+
+#===========================================================================
