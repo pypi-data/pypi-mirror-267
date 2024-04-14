2ebc357924817725c17c4be2c07bfc299425808f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 15:16:05 +0000

    Add package ok-script/0.0.9/ok-script-0.0.9.tar.gz

---
+++ packages/ok-script/ok-script-0.0.9.tar.gz/ok-script-0.0.9/ok/gui/Communicate.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PySide6.QtCore import Signal, QObject
+
+
+class Communicate(QObject):
+    frame = Signal(np.ndarray)
+    log = Signal(int, str)
+    fps = Signal(int)
+    frame_time = Signal(int)
+    scene = Signal(str)
+    draw_box = Signal(str, object, str)
+    tasks = Signal()
+    task_info = Signal()
+    window = Signal(bool, int, int, int, int, int, int, float)
+    loading_progress = Signal(str)
+    init = Signal(bool, str)
+    notification = Signal(str, str)
+    executor_paused: Signal = Signal(bool)
+    adb_devices: Signal = Signal()
+
+
+communicate = Communicate()
