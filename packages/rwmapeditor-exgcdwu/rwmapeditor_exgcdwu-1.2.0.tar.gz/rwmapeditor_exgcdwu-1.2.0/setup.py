ca25c3ed993596ca148ecabfcf73ca0256fb68e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 03:24:31 +0000

    Add package rwmapeditor-exgcdwu/1.2.0/rwmapeditor_exgcdwu-1.2.0.tar.gz

---
+++ packages/rwmapeditor-exgcdwu/rwmapeditor_exgcdwu-1.2.0.tar.gz/rwmapeditor_exgcdwu-1.2.0/setup.py
@@ -0,0 +1,27 @@
+# setup.py
+
+#import os
+from setuptools import setup, find_packages
+
+__version__ = '1.2.0'
+#current_dir = os.path.dirname(__file__)
+#requirements = open(current_dir + '/requirements.txt').readlines()
+
+def readme():
+    with open('README.md') as file:
+        return file.read()
+
+strreadme = readme()
+
+setup(
+    name = 'rwmapeditor_exgcdwu',
+    version = __version__,
+    author = 'exgcdwu',
+    author_email = '1006605318@qq.com',
+    url = "https://github.com/exgcdwu/Rusted-Warfare-map-editor-for-city-occupation-play-",
+    long_description = strreadme,
+    long_description_content_type = "text/markdown",
+    packages = find_packages(exclude=["tests"]),
+    python_requires = '>=3.0.0'
+    #,install_requires = requirements
+)
