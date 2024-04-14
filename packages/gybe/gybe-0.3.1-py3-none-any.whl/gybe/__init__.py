20ce5f8e414b87abf18585b151f44549714438d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 03:25:30 +0000

    Add package gybe/0.3.1/gybe-0.3.1-py3-none-any.whl

---
+++ packages/gybe/gybe-0.3.1-py3-none-any.whl/gybe/__init__.py
@@ -0,0 +1,13 @@
+"""A simple YAML transpilation tool for rendering kubernetes manifests"""
+
+__version__ = '0.3.1'
+
+
+from typing import List
+
+from gybe import k8s
+from gybe.decorators import transpiler
+
+Manifest = List[k8s.K8sSpec]
+
+__all__ = ['k8s', 'Manifest', 'transpiler']
---
+++ packages/gybe/gybe-0.3.1-py3-none-any.whl/gybe/decorators.py
@@ -0,0 +1,42 @@
+import sys
+
+import click
+from cattrs import Converter, transform_error
+
+from gybe.exceptions import InvalidOutputError
+from gybe.modeling import create_input_model
+from gybe.yaml import yaml_dumps, yaml_loads
+
+c = Converter(omit_if_default=True)
+
+
+def _bind_function(f):
+    @click.argument('file', required=True, type=click.File('r'))
+    def func(file):
+        input_data = yaml_loads(file.read()) or dict()
+        try:
+            input_obj = c.structure(input_data, create_input_model(f))
+        except Exception as exc:
+            print('Validation Error:')
+            for m in transform_error(exc):
+                print('-', m)
+            sys.exit(-1)
+
+        _kwargs = c.unstructure(input_obj)
+        manifest = f(**_kwargs)
+
+        # Validate outputs
+        if not isinstance(manifest, list):
+            raise InvalidOutputError()
+
+        # Print manifest
+        print('---\n'.join([yaml_dumps(c.unstructure(r)) for r in manifest]))
+
+    func.__name__ = f.__name__
+    return func
+
+
+def transpiler(f):
+    """Command that takes in a YAML file and outputs a Kubernetes manifest YAML file."""
+    func = _bind_function(f)
+    return click.command()(func)
