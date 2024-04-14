ca1b10e8a6e1b7c27e0d0f52ca0685f1c43d39fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 03:25:04 +0000

    Add package area-of-a-shape/0.0.3/area_of_a_shape-0.0.3.tar.gz

---
+++ packages/area-of-a-shape/area_of_a_shape-0.0.3.tar.gz/area_of_a_shape-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+    
+
+setup(
+    name='area_of_a_shape',
+    version='0.0.3',
+    author='Akim',
+    author_email='akimserverov75@gmail.com',
+    description='A library that can calculate the area of a circle by radius and a triangle on three sides.',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://vk.com/mrrobot1234',
+    packages=find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    keywords='triangle rectangle',
+    project_urls={
+        'Profile author': 'https://vk.com/mrrobot1234'
+    },
+    python_requires='>=3.6'
+)
