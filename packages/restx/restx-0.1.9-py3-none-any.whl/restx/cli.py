317f47bc25b8740f38d5de7fa73f9598fc1ab24f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:30 +0000

    Add package restx/0.1.9/restx-0.1.9-py3-none-any.whl

---
+++ packages/restx/restx-0.1.9-py3-none-any.whl/restx/cli.py
@@ -0,0 +1,80 @@
+import typer
+from rich.console import Console
+
+from restx.utils import crud_manager, print_additional_info, print_response_body
+
+
+app = typer.Typer()
+console = Console()
+
+
+@app.command()
+def get(
+    url: str,
+    headers: str = typer.Option(None, "--header", "-H", help="Custom headers"),
+) -> None:
+    """Send a GET request."""
+    response, response_time = crud_manager(
+        url=url, method="GET", headers=headers
+    ).values()
+    print_additional_info(response, response_time)
+    print_response_body(response)
+
+
+@app.command()
+def post(
+    url: str,
+    payload: str = typer.Option(..., "--payload", "-p", help="Payload data"),
+    headers: str = typer.Option(None, "--header", "-H", help="Custom headers"),
+) -> None:
+    """Send a POST request."""
+    response, response_time = crud_manager(
+        url=url, method="POST", payload=payload, headers=headers
+    ).values()
+    print_additional_info(response, response_time)
+    print_response_body(response)
+
+
+@app.command()
+def put(
+    url: str,
+    payload: str = typer.Option(..., "--payload", "-p", help="Payload data"),
+    headers: str = typer.Option(None, "--header", "-H", help="Custom headers"),
+) -> None:
+    """Send a PUT request."""
+    response, response_time = crud_manager(
+        url=url, method="PUT", payload=payload, headers=headers
+    ).values()
+    print_additional_info(response, response_time)
+    print_response_body(response)
+
+
+@app.command()
+def patch(
+    url: str,
+    payload: str = typer.Option(..., "--payload", "-p", help="Payload data"),
+    headers: str = typer.Option(None, "--header", "-H", help="Custom headers"),
+) -> None:
+    """Send a PATCH request."""
+    response, response_time = crud_manager(
+        url=url, method="PATCH", payload=payload, headers=headers
+    ).values()
+    print_additional_info(response, response_time)
+    print_response_body(response)
+
+
+@app.command()
+def delete(
+    url: str,
+    headers: str = typer.Option(None, "--header", "-H", help="Custom headers"),
+) -> None:
+    """Send a DELETE request."""
+    response, response_time = crud_manager(
+        url=url, method="DELETE", headers=headers
+    ).values()
+    print_additional_info(response, response_time)
+    print_response_body(response)
+
+
+if __name__ == "__main__":
+    app()
