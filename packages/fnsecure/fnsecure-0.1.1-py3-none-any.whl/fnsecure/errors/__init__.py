7eeab7fe8d72c3894b7ea71d93a8b685fde85d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 03:24:27 +0000

    Add package fnsecure/0.1.1/fnsecure-0.1.1-py3-none-any.whl

---
+++ packages/fnsecure/fnsecure-0.1.1-py3-none-any.whl/fnsecure/errors/__init__.py
@@ -0,0 +1,8 @@
+from .errors import (
+    EmptyHandlerList,
+    FailedToContinueWithValue,
+    HandledExceptionError,
+    HandlerSetupError,
+    RetryFunctionCallFailed,
+    UnHandledExceptionError,
+)
---
+++ packages/fnsecure/fnsecure-0.1.1-py3-none-any.whl/fnsecure/shared/common.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass
+from typing import Callable, Dict, List, Type
+
+from .exception_types import ExceptionHandler
+
+
+@dataclass
+class FunctionDescriptor:
+    """
+    Contains all relevant information about the function.
+
+    Parameters
+    ----------
+    name : str
+        The name of the function.
+    exceptions : List[Type[Exception]]
+        All the exceptions that the given function can raise.
+    exception_handlers : List[ExceptionHandler]
+        Contains all exception handlers for the given function at a given time.
+    """
+
+    name: str
+    exceptions: List[Type[Exception]]
+    exception_handlers: List[ExceptionHandler]
+
+
+class Context:
+    """
+    Manages a given context and it contains all managed functions in this
+    context.
+    """
+
+    def __init__(self) -> None:
+        self.a = 10
+        self._functions: Dict[str, FunctionDescriptor] = {}
+        self._handler_group_attr_name = "_handler_list"
+        self._last_caller_attr_name = "_last_caller_name"
+
+    def register(self, func_desc: FunctionDescriptor) -> None:
+        """Register a function if it is not registered already."""
+        _func = self._functions.get(func_desc.name, None)
+        if _func:
+            return
+
+        self._functions[func_desc.name] = func_desc
+
+    def get_function_exceptions(self, func_name: str) -> List[Type[Exception]]:
+        """Returns the registered list of exception for the given function."""
+        return self._functions[func_name].exceptions
+
+    def update_handler_list(
+        self, function_name: str, handlers: List[ExceptionHandler]
+    ) -> None:
+        """
+        Updates the handlers list given the managed function's name.
+
+        Parameters
+        ----------
+        function_name : str
+            The name of the managed/handled function.
+        handlers : List[ExceptionHandler]
+            The new list of ExceptionHandlers to use.
+        """
+        _func = self._functions.get(function_name, None)
+        assert (
+            _func
+        ), f"Handlers cannot be updated because function {function_name} is not registered."
+
+        self._functions[function_name].exception_handlers = handlers
+
+    def get_handler_list(self, function_name: str) -> List[ExceptionHandler]:
+        """
+        Returns the list of handlers given the managed function's name.
+        Returns an empty list in case the attribute was not set up.
+        """
+        _handlers = self._functions.get(function_name, None).exception_handlers
+        return _handlers if _handlers else []
+
+
+_MAIN_CONTEXT = Context()
