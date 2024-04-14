fcdeb139cf38f27c77c09f9825e8c6ebc40276ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-14 04:08:34 +0000

    Add package par-run/0.3.0/par_run-0.3.0.tar.gz

---
+++ packages/par-run/par_run-0.3.0.tar.gz/par_run-0.3.0/py_tests/conftest.py
@@ -0,0 +1,38 @@
+"""pytest fixtures."""
+
+from collections import OrderedDict
+
+import pytest
+
+from par_run.executor import Command, CommandGroup
+
+
+@pytest.fixture()
+def command_data() -> dict:
+    """Return basic command data for testing.
+
+    Returns:
+        dict: _description_
+
+    """
+    return {
+        "group.test": [
+            ("command1", "do something"),
+            ("command2", "do something else"),
+        ],
+    }
+
+
+@pytest.fixture()
+def expected_command_groups():
+    return [
+        CommandGroup(
+            name="test",
+            cmds=OrderedDict(
+                [
+                    ("command1", Command(name="command1", cmd="do something")),
+                    ("command2", Command(name="command2", cmd="do something else")),
+                ],
+            ),
+        ),
+    ]
