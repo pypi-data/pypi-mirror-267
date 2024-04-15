684f9d0b2e51e56dbb9b4ed10a2f55f9d5bdf81f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:07:07 +0000

    Add package pdfToAudio/1.0.2/pdfToAudio-1.0.2.tar.gz

---
+++ packages/pdfToAudio/pdfToAudio-1.0.2.tar.gz/pdfToAudio-1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='pdfToAudio',
+    version='1.0.2',
+    packages=find_packages(),
+    description='This package creates audiobooks from PDF files',
+    author='Thiago S Adriano',
+    author_email='prof.thiagoadriano@gmail.com',
+    install_requires=[
+        'gTTS==2.2.3',
+        'pypdf==2.4.0'
+    ],
+    url='https://github.com/programadriano/pdfToAudio',  
+    license='MIT',  
+    long_description=long_description,
+    long_description_content_type='text/markdown' 
+)
