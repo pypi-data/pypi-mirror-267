2732547d698728282c881369f7cca276340f3c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 17:04:15 +0000

    Add package atomrdf/0.4.0/atomrdf-0.4.0.tar.gz

---
+++ packages/atomrdf/atomrdf-0.4.0.tar.gz/atomrdf-0.4.0/tests/test_graph.py
@@ -0,0 +1,24 @@
+import pytest
+from atomrdf import KnowledgeGraph, System
+
+def test_structuregraph():
+	s = KnowledgeGraph()
+	sys = System.create.element.Fe(graph=s)
+
+	vis = s.visualise()
+	assert(vis != None)
+
+	vis = s.visualise()
+	assert(vis != None)
+
+	s.write("temp.ttl", format="turtle")
+	s = KnowledgeGraph(graph_file="temp.ttl")
+
+	sys = System.create.element.Fe(graph=s)
+	sys.add_vacancy(0.5, number=1)
+
+	s = KnowledgeGraph()
+	sys = System.create.element.Fe(graph=s)
+	assert s.n_samples == 1
+	#res = s.query_sample("NumberOfAtoms", 2)
+	#assert(len(res) == 1)
