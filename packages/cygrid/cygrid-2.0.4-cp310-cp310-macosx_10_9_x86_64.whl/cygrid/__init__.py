c4beb07bd6111293c3d622eb6ba2eebce761109a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:03 +0000

    Add package cygrid/2.0.4/cygrid-2.0.4-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/cygrid/cygrid-2.0.4-cp310-cp310-macosx_10_9_x86_64.whl/cygrid/__init__.py
@@ -0,0 +1,10 @@
+from .cygrid import *
+from .healpix import *
+from .hphashtab import *
+from .helpers import *
+from .mock import *
+from .init_testrunner import *
+from .version import version
+
+
+__version__ = version
