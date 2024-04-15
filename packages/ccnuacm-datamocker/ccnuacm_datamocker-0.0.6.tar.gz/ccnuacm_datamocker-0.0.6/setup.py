0757534dc4a2a719474a42c054f5bd10490f6d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 04:20:12 +0000

    Add package ccnuacm-datamocker/0.0.6/ccnuacm_datamocker-0.0.6.tar.gz

---
+++ packages/ccnuacm-datamocker/ccnuacm_datamocker-0.0.6.tar.gz/ccnuacm_datamocker-0.0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="ccnuacm_datamocker",
+    version="0.0.6",
+    description="A data mocking library for CCNU ACM",
+    author="JixiangXiong",
+    author_email="xiongjx751@qq.com",
+    url="https://github.com/CCNU-ACM-Official/CCNUACM_DataMocker.git",
+    packages=["ccnuacm_datamocker"],
+    install_requires=[
+        "numpy",
+        "jupyter",
+        "tqdm",
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
