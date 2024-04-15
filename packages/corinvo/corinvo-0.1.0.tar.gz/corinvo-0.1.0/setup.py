4fde9a4a9463403e0dfa12baaa594a1994f6a527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 16:07:06 +0000

    Add package corinvo/0.1.0/corinvo-0.1.0.tar.gz

---
+++ packages/corinvo/corinvo-0.1.0.tar.gz/corinvo-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='corinvo',
+    version='0.1.0',
+    author='Corinvo Open Source Team',
+    author_email='your.email@example.com',
+    description='This package name is reserved by Corinvo LLC',
+    packages=find_packages(),
+    classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
