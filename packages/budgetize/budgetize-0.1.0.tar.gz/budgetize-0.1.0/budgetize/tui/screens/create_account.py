7b95959aa2462f617b6cc125954af0e8a90f8d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:18 +0000

    Add package budgetize/0.1.0/budgetize-0.1.0.tar.gz

---
+++ packages/budgetize/budgetize-0.1.0.tar.gz/budgetize-0.1.0/budgetize/tui/screens/create_account.py
@@ -0,0 +1,106 @@
+"""Module that defines the CreateAccount screen."""
+
+import logging
+
+from textual.app import ComposeResult
+from textual.binding import Binding
+from textual.containers import Grid, Horizontal, VerticalScroll
+from textual.screen import Screen
+from textual.validation import Number
+from textual.widgets import Button, Footer, Header, Input, Label, Select
+
+from budgetize.db.database import Database
+from budgetize.tui.modals.error_modal import ErrorModal
+from budgetize.utils import _, get_select_currencies
+
+
+class CreateAccount(Screen):
+    """Screen that allows the user to create a new account."""
+
+    CSS_PATH = "css/create_account.tcss"
+
+    DB: Database = None  # type: ignore
+    BINDINGS = [
+        Binding(
+            key="c,C", key_display="C", action="pop_screen", description=_("Cancel")
+        ),
+    ]
+
+    def __init__(self) -> None:
+        """Creates a new CreateAccount Screen"""
+        CreateAccount.DB = Database(self.app)
+        super().__init__()
+
+    def compose(self) -> ComposeResult:
+        """Called when the screen is composed."""
+
+        logging.info("Composing CreateAccount Screen...")
+        self.app.sub_title = _("Create Account")
+        yield Header()
+        yield Footer()
+
+        yield VerticalScroll(
+            Grid(
+                # Place them in this way because grids are filled in row-order
+                Label(_("Account Currency"), id="currency-label"),
+                Label(_("Account Name"), id="name-label"),
+                Select(
+                    get_select_currencies(), id="currency-select", allow_blank=False
+                ),
+                Input(placeholder=_("Bank Account"), id="account-name-input"),
+            ),
+            Label(_("Starting Balance"), id="balance-label"),
+            Input(
+                type="number",
+                placeholder="250",
+                id="balance-input",
+                validators=[
+                    Number(
+                        minimum=0,
+                        failure_description=_("Initial Balance must be atleast zero."),
+                    )
+                ],
+            ),
+            Horizontal(
+                Button.success(_("Create Account"), id="create-account-button"),
+                Button.error(_("Cancel"), id="cancel-button"),
+            ),
+        )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Button press handlers"""
+
+        if event.button.id == "create-account-button":
+            name: str = self.get_widget_by_id("account-name-input").value  # type: ignore
+            currency: str = self.get_widget_by_id("currency-select").value  # type: ignore
+            starting_balance: float = self.get_widget_by_id("balance-input").value  # type: ignore
+
+            logging.debug(f"Creating Account: {name} {currency} {starting_balance}")
+
+            if self.DB.account_name_exists(name):
+                logging.warning("Account Name already exists! Showing error")
+                self.app.push_screen(
+                    ErrorModal(
+                        _("Account Name already Exists"),
+                        traceback_msg=_(
+                            "You cannot have 2 accounts with the same name!"
+                        ),
+                    )
+                )
+                return
+
+            self.DB.add_account(
+                name=name,
+                currency=currency,
+                starting_balance=starting_balance,
+            )
+
+            self.notify(_("Account created successfully."), title="Account Created")
+            self.get_widget_by_id("account-name-input").value = ""  # type: ignore
+            self.get_widget_by_id("balance-input").value = ""  # type: ignore
+            self.app.pop_screen()
+        else:
+            # Clear the input fields
+            self.get_widget_by_id("account-name-input").value = ""  # type: ignore
+            self.get_widget_by_id("balance-input").value = ""  # type: ignore
+            self.app.pop_screen()
---
+++ packages/budgetize/budgetize-0.1.0.tar.gz/budgetize-0.1.0/budgetize/tui/tui_app.py
@@ -0,0 +1,39 @@
+"""Main Module that starts the Terminal User Interface (TUI)"""
+
+import logging
+
+from textual.app import App
+
+from budgetize.consts import VERSION
+from budgetize.settings_manager import SettingsManager
+from budgetize.tui.screens.create_account import CreateAccount
+from budgetize.tui.screens.initial_config import InitialConfig
+from budgetize.tui.screens.main_menu import MainMenu
+from budgetize.utils import create_logger
+
+create_logger()
+
+
+class TuiApp(App):
+    """App that handles the TUI"""
+
+    def on_mount(self) -> None:
+        """Called when the app is mounted"""
+        self.title = f"Budgetize (v{VERSION})"
+        logging.info("Checking if user has default settings...")
+        settings = SettingsManager()
+
+        if settings.is_default_settings():
+            logging.info("User has default settings. Redirecting to InitialConfig...")
+            self.push_screen(InitialConfig())
+        else:
+            logging.info("User has custom settings. Redirecting to MainMenu...")
+            logging.info("Installing Main Menu screen...")
+            self.install_screen(MainMenu(), "main_menu")
+            logging.info("Success installing Main Menu Screen")
+            logging.info("Installing CreateAccount screen...")
+            self.install_screen(CreateAccount(), "create_account")
+            logging.info("Succes installing CreateAccount screen")
+
+            logging.info("Showing Main Menu screen...")
+            self.push_screen("main_menu")
