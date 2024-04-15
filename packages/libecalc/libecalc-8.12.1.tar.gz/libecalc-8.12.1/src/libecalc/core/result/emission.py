33a4b11b3e7691ae340e9967c1870e99a255a602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 17:04:14 +0000

    Add package libecalc/8.12.1/libecalc-8.12.1.tar.gz

---
+++ packages/libecalc/libecalc-8.12.1.tar.gz/libecalc-8.12.1/src/libecalc/core/result/emission.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+
+from datetime import datetime
+from typing import List, Optional
+
+from libecalc.common.units import Unit
+from libecalc.common.utils.rates import TimeSeriesStreamDayRate
+from libecalc.core.result.base import EcalcResultBaseModel
+
+
+class EmissionResult(EcalcResultBaseModel):
+    """The emissions for a result component."""
+
+    name: str
+    timesteps: List[datetime]
+    rate: TimeSeriesStreamDayRate  # ton/day
+    emission_rate_to_volume_factor: Optional[float] = None
+
+    @classmethod
+    def create_empty(cls, name: str, timesteps: List[datetime]):
+        return cls(
+            name=name,
+            timesteps=timesteps,
+            rate=TimeSeriesStreamDayRate(
+                timesteps=timesteps,
+                values=[0] * len(timesteps),
+                unit=Unit.TONS_PER_DAY,
+            ),
+        )
