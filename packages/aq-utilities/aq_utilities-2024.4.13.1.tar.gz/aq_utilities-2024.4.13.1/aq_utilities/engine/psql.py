2f87201fda716b96d51f321539fe8733d57ac2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:04:41 +0000

    Add package aq-utilities/2024.4.13.1/aq_utilities-2024.4.13.1.tar.gz

---
+++ packages/aq-utilities/aq_utilities-2024.4.13.1.tar.gz/aq_utilities-2024.4.13.1/aq_utilities/engine/psql.py
@@ -0,0 +1,34 @@
+import os
+
+from sqlalchemy import create_engine
+
+PGUSER = os.environ.get("PGUSER", "pgadmin")
+PGPASSWORD = os.environ.get("PGPASSWORD", "")
+PGHOST = os.environ.get("PGHOST", "localhost")
+PGDATABASE = os.environ.get("PGDATABASE", "aq")
+ENGINE = None
+
+
+def get_engine(
+    pg_user: str = PGUSER,
+    pg_password: str = PGPASSWORD,
+    pg_host: str = PGHOST,
+    pg_database: str = PGDATABASE,
+):
+    global ENGINE
+    if ENGINE is None:
+        ENGINE = create_engine(
+            f"postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}/{pg_database}",
+            echo=False,
+            execution_options={"isolation_level": "AUTOCOMMIT"},
+            connect_args={"sslmode": "require"},
+            pool_size=10,
+            max_overflow=10,
+            pool_timeout=30,
+        )
+    return ENGINE
+
+def reset_engine():
+    global ENGINE
+    ENGINE = None
+    return ENGINE
