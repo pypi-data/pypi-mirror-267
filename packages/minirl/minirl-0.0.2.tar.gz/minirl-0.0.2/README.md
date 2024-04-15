ef4a6d4159684ae58476a1b06ca56ce2e0c67715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:04:58 +0000

    Add package minirl/0.0.2/minirl-0.0.2.tar.gz

---
+++ packages/minirl/minirl-0.0.2.tar.gz/minirl-0.0.2/README.md
@@ -0,0 +1,16 @@
+## Requirements
+
+- Generate synthetic data
+- View attention maps
+- Use torch 2x
+- Train inside docker compose with cuda
+- Build as package
+- Deploy on hugging face
+- Automated network testing
+- Use lighting, accelerator, tqdm
+- Use wandb
+- Build the model with pytorch models
+- Parameter search (ray tune)
+- View easily
+- Use mkdocs in black
+- Use rerun
---
+++ packages/minirl/minirl-0.0.2.tar.gz/minirl-0.0.2/minirl/vit/network/transformer.py
@@ -0,0 +1,37 @@
+from torch import nn
+
+from .attention import MultiheadSelfAttentionBlock
+from .mlp import MLPBlock
+
+# 1. Create a class that inherits from nn.Module
+class TransformerEncoderBlock(nn.Module):
+    """Creates a Transformer Encoder block."""
+    # 2. Initialize the class with hyperparameters from Table 1 and Table 3
+    def __init__(self,
+                 embedding_dim:int=768, # Hidden size D from Table 1 for ViT-Base
+                 num_heads:int=12, # Heads from Table 1 for ViT-Base
+                 mlp_size:int=3072, # MLP size from Table 1 for ViT-Base
+                 mlp_dropout:float=0.1, # Amount of dropout for dense layers from Table 3 for ViT-Base
+                 attn_dropout:float=0): # Amount of dropout for attention layers
+        super().__init__()
+
+        # 3. Create MSA block (equation 2)
+        self.msa_block = MultiheadSelfAttentionBlock(embedding_dim=embedding_dim,
+                                                     num_heads=num_heads,
+                                                     attn_dropout=attn_dropout)
+
+        # 4. Create MLP block (equation 3)
+        self.mlp_block =  MLPBlock(embedding_dim=embedding_dim,
+                                   mlp_size=mlp_size,
+                                   dropout=mlp_dropout)
+
+    # 5. Create a forward() method
+    def forward(self, x):
+
+        # 6. Create residual connection for MSA block (add the input to the output)
+        x =  self.msa_block(x) + x
+
+        # 7. Create residual connection for MLP block (add the input to the output)
+        x = self.mlp_block(x) + x
+
+        return x
