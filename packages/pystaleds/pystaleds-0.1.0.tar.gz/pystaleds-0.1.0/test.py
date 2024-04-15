cb1ac9230914cc470bab556bd5fc3fc81ac8cd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:06:46 +0000

    Add package pystaleds/0.1.0/pystaleds-0.1.0.tar.gz

---
+++ packages/pystaleds/pystaleds-0.1.0.tar.gz/pystaleds-0.1.0/test.py
@@ -0,0 +1,17 @@
+def f(x: int):
+    """
+    Hey.
+
+    Args:
+        x (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    "Oi"
+    return x
+
+
+def g(x):
+    """Hey."""
+    return x
