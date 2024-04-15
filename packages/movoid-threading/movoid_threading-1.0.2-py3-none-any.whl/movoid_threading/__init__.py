61d121d17e3281b79d28f0e84fa14a2c7f72f4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:58:11 +0000

    Add package movoid-threading/1.0.2/movoid_threading-1.0.2-py3-none-any.whl

---
+++ packages/movoid-threading/movoid_threading-1.0.2-py3-none-any.whl/movoid_threading/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/4/3 1:49
+# Description   : 
+"""
+from .thread import Thread, ThreadLib, ThreadTerminateSignal, ThreadStopSignal, ThreadLoopStopSignal
