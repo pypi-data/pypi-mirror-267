e1b2da344f918ade51fffe0ad7d887a13ae63f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 16:07:39 +0000

    Add package radical.utils/1.48.1/radical_utils-1.48.1.tar.gz

---
+++ packages/radical.utils/radical_utils-1.48.1.tar.gz/radical_utils-1.48.1/src/radical/utils/__init__.py
@@ -0,0 +1,106 @@
+# pylint: disable=reimported
+
+# allow star import, unused symbols
+# flake8: noqa: F401
+
+__author__    = 'RADICAL-Cybertools Team'
+__copyright__ = 'Copyright 2013-2022, The RADICAL-Cybertools Team'
+__license__   = 'MIT'
+
+# we want atfork imported first, specifically before os and logging
+from .atfork         import *
+
+# import constants
+from .constants      import *
+
+# import utility classes
+from .object_cache   import ObjectCache
+from .plugin_manager import PluginManager, PluginBase
+from .singleton      import Singleton
+from .heartbeat      import Heartbeat
+from .threads        import is_main_thread, is_this_thread, cancel_main_thread
+from .threads        import main_thread, this_thread, get_thread_name, gettid
+from .threads        import set_cancellation_handler, unset_cancellation_handler
+from .threads        import raise_in_thread, ThreadExit, SignalRaised
+from .futures        import Future
+from .futures        import NEW, RUNNING, DONE, FAILED, CANCELED
+from .url            import Url
+from .host           import get_hostip, get_hostname
+from .host           import get_hostlist, get_hostlist_by_range
+from .host           import create_hostfile, compress_hostlist, is_localhost
+from .lockable       import Lockable
+from .lockfile       import Lockfile
+from .registry       import Registry, READONLY, READWRITE
+from .ru_regex       import ReString, ReSult
+from .lease_manager  import LeaseManager
+from .daemon         import Daemon, daemonize
+from .poll           import Poller, POLLIN, POLLOUT, POLLERR, POLLALL
+from .poll           import POLLNVAL, POLLPRI, POLLHUP
+from .shell          import sh_quote
+from .shell          import sh_callout, sh_callout_bg, sh_callout_async
+from .testing        import sys_exit
+from .testing        import TestConfig
+from .testing        import set_test_config, add_test_config, get_test_config
+from .env            import env_read, env_write, env_read_lines, env_eval
+from .env            import env_prep, env_diff, EnvProcess
+from .stack          import stack
+from .modules        import import_module, find_module, import_file
+from .modules        import get_type, load_class
+
+from .dict_mixin     import DictMixin, dict_merge, dict_stringexpand, dict_diff
+from .dict_mixin     import PRESERVE, OVERWRITE, iter_diff
+from .typeddict      import TypedDict, TypedDictMeta, as_dict
+from .config         import Config, DefaultConfig
+
+from .zmq            import Message
+from .zmq            import Bridge
+from .zmq            import Queue,  Putter,    Getter
+from .zmq            import PubSub, Publisher, Subscriber
+from .zmq            import Server, Client
+
+from .flux           import FluxHelper
+
+from .logger         import DEBUG, INFO, WARNING, WARN, ERROR, CRITICAL, OFF
+from .logger         import Logger
+from .reporter       import Reporter
+from .profile        import Profiler, timestamp, event_to_label
+from .profile        import read_profiles, combine_profiles, clean_profile
+from .profile        import TIME, EVENT, COMP, TID, UID, STATE, MSG, ENTITY
+from .profile        import PROF_KEY_MAX
+
+from .json_io        import read_json, read_json_str, write_json
+from .json_io        import parse_json, parse_json_str, metric_expand
+from .which          import which
+from .tracer         import trace, untrace
+from .get_version    import get_version
+
+
+# import various utility methods
+from .ids            import *
+from .debug          import *
+from .misc           import *
+from .algorithms     import *
+
+# import decorators
+from .timing         import timed_method, epoch, dt_epoch, Time
+
+# import sub-modules
+from . import scheduler
+from . import config
+from . import zmq
+
+
+# ------------------------------------------------------------------------------
+#
+import os
+
+_mod_root = os.path.dirname (__file__)
+
+version_short, version_detail, version_base, \
+               version_branch, sdist_name,   \
+               sdist_path = get_version(_mod_root)
+version = version_short
+
+
+# ------------------------------------------------------------------------------
+
