882f8e7593716e50eb3decdec1637dc4fd85dcc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 17:04:09 +0000

    Add package soildx-fastapi-jwt-auth/0.0.1/soildx_fastapi_jwt_auth-0.0.1.tar.gz

---
+++ packages/soildx-fastapi-jwt-auth/soildx_fastapi_jwt_auth-0.0.1.tar.gz/soildx_fastapi_jwt_auth-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='soildx_fastapi_jwt_auth',
+    version="0.0.1",
+    author="Akash",
+    description="fastapi-jwt-auth simple jwt token authentication",
+    packages=find_packages(),
+    install_requires=[
+        'fastapi>=0.110.1',
+        'python-jose[cryptography]',
+        'passlib[bcrypt]'
+    ]
+)
