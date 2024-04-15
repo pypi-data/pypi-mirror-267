068a5b3f266528d2009afe3126cdefe3fa06f1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:12 +0000

    Add package securechat/0.0.0/securechat-0.0.0.tar.gz

---
+++ packages/securechat/securechat-0.0.0.tar.gz/securechat-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='securechat',
+    version='0.0.0',
+    description='SCH',
+    author='Your Name',
+    author_email='your@email.com',
+    url='https://github.com/nawafalqari/sch',
+    packages=['securechat'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
