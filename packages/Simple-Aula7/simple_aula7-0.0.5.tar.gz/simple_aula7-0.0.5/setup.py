5038a973abb6e5b93434c7614a76658f296a5711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:57:41 +0000

    Add package Simple-Aula7/0.0.5/simple_aula7-0.0.5.tar.gz

---
+++ packages/Simple-Aula7/simple_aula7-0.0.5.tar.gz/simple_aula7-0.0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup  # type: ignore
+
+__project__ = "Simple-Aula7"
+__version__ = "0.0.5"
+__description__ = (
+    "Aula7 blueprint for SimpleAxel (https://git.tech.eus/EuskadiTech/Simple-Aula7)"
+)
+__packages__ = ["aula7"]
+__url__ = "https://git.tech.eus/EuskadiTech/Simple-Aula7"
+__author__ = "EuskadiTech"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+]
+__requires__ = ["requests", "Flask", "stripe", "SimpleSDK"]
+
+setup(
+    name=__project__,
+    version=__version__,
+    description=__description__,
+    packages=__packages__,
+    url=__url__,
+    author=__author__,
+    classifiers=__classifiers__,
+    requires=__requires__,
+    include_package_data=True,
+    package_data={__packages__[0]: ["templates/*"]},
+)
