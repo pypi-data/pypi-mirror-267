430bc69e5c5991e63b5cf2b8af0c841731402052
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 04:20:15 +0000

    Add package py-modelrunner/0.17.1/py_modelrunner-0.17.1-py3-none-any.whl

---
+++ packages/py-modelrunner/py_modelrunner-0.17.1-py3-none-any.whl/modelrunner/__init__.py
@@ -0,0 +1,32 @@
+"""
+.. codeauthor:: David Zwicker <david.zwicker@ds.mpg.de>
+"""
+
+# determine the package version
+try:
+    # try reading version of the automatically generated module
+    from ._version import __version__
+except ImportError:
+    # determine version automatically from CVS information
+    from importlib.metadata import PackageNotFoundError, version
+
+    try:
+        __version__ = version("modelrunner")
+    except PackageNotFoundError:
+        # package is not installed, so we cannot determine any version
+        __version__ = "unknown"
+    del PackageNotFoundError, version  # clean name space
+
+
+from .model import (
+    ModelBase,
+    make_model,
+    make_model_class,
+    run_function_with_cmd_args,
+    run_script,
+    set_default,
+)
+from .parameters import HideParameter, Parameter, Parameterized
+from .results import Result, ResultCollection
+from .run import submit_job, submit_jobs
+from .storage import open_storage
