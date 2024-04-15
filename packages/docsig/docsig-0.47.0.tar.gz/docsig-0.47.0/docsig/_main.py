a545f6e110fde814ac79cc60be3c7bf7cbe43f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:57:36 +0000

    Add package docsig/0.47.0/docsig-0.47.0.tar.gz

---
+++ packages/docsig/docsig-0.47.0.tar.gz/docsig-0.47.0/docsig/_main.py
@@ -0,0 +1,47 @@
+"""
+docsig._main
+============
+
+Contains package entry point.
+"""
+
+from __future__ import annotations as _
+
+from ._config import Parser as _Parser
+from ._core import docsig as _docsig
+from ._hooks import pretty_print_error as _pretty_print_error
+
+
+def main() -> str | int:
+    """Main function for package.
+
+    Collect config and arguments for the commandline.
+
+    :return: Exit status for whether test failed or not.
+    """
+    parser = _Parser()
+    _pretty_print_error()
+    return _docsig(
+        *parser.args.path,
+        string=parser.args.string,
+        list_checks=parser.args.list_checks,
+        check_class=parser.args.check_class,
+        check_class_constructor=parser.args.check_class_constructor,
+        check_dunders=parser.args.check_dunders,
+        check_protected_class_methods=(
+            parser.args.check_protected_class_methods
+        ),
+        check_nested=parser.args.check_nested,
+        check_overridden=parser.args.check_overridden,
+        check_protected=parser.args.check_protected,
+        check_property_returns=parser.args.check_property_returns,
+        ignore_no_params=parser.args.ignore_no_params,
+        ignore_args=parser.args.ignore_args,
+        ignore_kwargs=parser.args.ignore_kwargs,
+        no_ansi=parser.args.no_ansi,
+        summary=parser.args.summary,
+        verbose=parser.args.verbose,
+        targets=parser.args.target,
+        disable=parser.args.disable,
+        exclude=parser.args.exclude,
+    )
