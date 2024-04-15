586d711f992748574b6c67da39caa0a586258ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 16:07:02 +0000

    Add package cadwyn/3.14.0/cadwyn-3.14.0.tar.gz

---
+++ packages/cadwyn/cadwyn-3.14.0.tar.gz/cadwyn-3.14.0/cadwyn/__init__.py
@@ -0,0 +1,16 @@
+import importlib.metadata
+
+from .applications import Cadwyn
+from .codegen import generate_code_for_versioned_packages
+from .route_generation import InternalRepresentationOf, VersionedAPIRouter, generate_versioned_routers
+from .structure import VersionBundle
+
+__version__ = importlib.metadata.version("cadwyn")
+__all__ = [
+    "Cadwyn",
+    "VersionedAPIRouter",
+    "generate_code_for_versioned_packages",
+    "VersionBundle",
+    "generate_versioned_routers",
+    "InternalRepresentationOf",
+]
