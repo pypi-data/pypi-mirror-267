5e5d5ce8ef1164bc78c40a73906e5b27f42a323d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:06:02 +0000

    Add package datapools/1.0.41/datapools-1.0.41.tar.gz

---
+++ packages/datapools/datapools-1.0.41.tar.gz/datapools-1.0.41/datapools/worker/plugins/theguardian/theguardian.py
@@ -0,0 +1,132 @@
+import re
+import json
+import requests
+from bs4 import BeautifulSoup, Comment
+from html2text import HTML2Text
+from urllib.parse import urljoin, urlparse, urlunparse
+
+from ..base_plugin import BasePlugin, BaseTag, WorkerTask
+from ....common.logger import logger
+from ....common.storage import BaseStorage
+from ....common.types import (
+    CrawlerBackTask,
+    CrawlerContent,
+    CrawlerNop,
+    DatapoolContentType,
+)
+
+DOMAIN = "www.theguardian.com"
+
+class TheGuardian(BasePlugin):
+
+    base_url = f"https://{DOMAIN}/"
+
+    def __init__(self, ctx):
+        super().__init__(ctx)
+        
+    @staticmethod
+    def is_supported(url):
+        u = BasePlugin.parse_url(url)
+        # logger.info( f'dataphoenix.info {u=}')
+        return u.netloc[-16:] == '.theguardian.com'
+         
+
+    def is_article(self, url):
+        path = urlparse(url).path
+        pattern = "^/[\w/-]+/\d+/\w+/\d+/[\w/-]+$"
+        return bool(re.match(pattern, path))
+
+    def normalize(self, url):
+        parts = list(urlparse(url))
+        parts[5] = ""   # remove fragment
+        clean_url = urlunparse(parts)
+        return clean_url
+          
+    def extract(self, soup):
+        content = soup.find("article")
+        if not content:
+            logger.debug(f"No <article>. Skipped.")
+            return None
+
+        filter_list = [
+            dict(string = lambda s: isinstance(s, Comment)),
+            dict(name = "img"), 
+            dict(name = "svg"),
+            dict(name = "button"),
+            dict(name = "video"),
+            dict(name = "picture"),
+            dict(name = "source"),
+            dict(name = "small"),
+            dict(name = "footer"),
+            dict(name = "gu-island")
+        ]
+        for tag_params in filter_list:
+            for element in content.find_all(**tag_params):
+                element.extract()
+
+        unwrap_tags = ["figure", "figcaption", "form", "span", "a"]
+        for tag in unwrap_tags:
+            for element in content.find_all(tag):
+                element.unwrap()
+            
+        for element in content.descendants:
+            if element.name:
+                element.attrs = {}
+
+        text_maker = HTML2Text(bodywidth = 80)
+        text_maker.ignore_links = True
+        markdown = text_maker.handle(str(content))
+        markdown = re.sub("\n[ \t]+", "\n", markdown)
+        markdown = re.sub("\n{2,}", "\n\n", markdown)
+
+        i = markdown.find("Explore more on these topics")
+        if i > 0:
+            markdown = markdown[:i].strip()
+        
+        snippet = re.sub("\n+", " ", markdown)[:160].strip()
+        logger.info(f"Extracted content: {snippet}...")
+        return markdown
+    
+    async def process(self, task:WorkerTask):
+        url = task.url
+        logger.info(f"{url} - Processing...")
+        
+        response = requests.get(url)
+        if response.url != url:
+            logger.info(f"{url} - Redirect to {response.url}")
+            url = response.url
+            
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        
+        platform_tag = await self.get_platform_tag(DOMAIN, soup, 3600)
+        # FIXME: remove this when theguardian adds real tag!!!
+        platform_tag = BaseTag("theguard")
+        if platform_tag and platform_tag.is_crawling_allowed() is False:
+            logger.info("Crawling disabled by tag")
+            return        
+
+        logger.debug(f"Adding new links...")
+        for link in soup.find_all('a', href = True):
+            href = link['href']
+            next_url = urljoin(url, href)
+            next_url = self.normalize(next_url)
+            yield CrawlerBackTask(url=next_url)
+
+        if self.is_article(url):
+            content = self.extract(soup)
+            
+            storage_id = BaseStorage.gen_id(url)
+            logger.info(f"putting article into {storage_id=}")
+
+            await self.ctx.storage.put(
+                storage_id,
+                BasePlugin.make_text_storage_value(content),
+            )            
+            yield CrawlerContent(
+                tag_id=str(platform_tag) if platform_tag is not None else None,
+                type=DatapoolContentType.Text,
+                storage_id=storage_id,
+                url=url,
+            )
+
