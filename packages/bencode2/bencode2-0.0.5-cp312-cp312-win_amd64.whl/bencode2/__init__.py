5a46ec1a14491c612eb3fd1bbb8cba20e8958b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:58:10 +0000

    Add package bencode2/0.0.5/bencode2-0.0.5-cp312-cp312-win_amd64.whl

---
+++ packages/bencode2/bencode2-0.0.5-cp312-cp312-win_amd64.whl/bencode2/__init__.py
@@ -0,0 +1,42 @@
+# cython: language_level=3
+
+from typing import Any
+
+from ._decoder import BencodeDecoder
+from ._encoder import encode
+from ._exceptions import BencodeDecodeError, BencodeEncodeError
+
+__all__ = (
+    "BencodeDecodeError",
+    "BencodeEncodeError",
+    "bencode",
+    "bdecode",
+)
+
+
+def bencode(value: Any) -> bytes:
+    """
+    Encode ``value`` into the bencode format.
+
+    :param value: Value
+    :type value: object
+
+    :return: Bencode formatted string
+    """
+    return encode(value)
+
+
+_decoder = BencodeDecoder()
+
+
+def bdecode(value: bytes) -> Any:
+    """
+    Decode bencode formatted byte string ``value``.
+
+    :param value: Bencode formatted string
+    :type value: bytes
+
+    :return: Decoded value
+    :rtype: object
+    """
+    return _decoder.decode(value)
