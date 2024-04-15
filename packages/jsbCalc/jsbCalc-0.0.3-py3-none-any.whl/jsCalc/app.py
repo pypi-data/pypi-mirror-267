e0aa7c098fb6c8da9bbab4469a7f72279054af92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 04:20:12 +0000

    Add package jsbCalc/0.0.3/jsbCalc-0.0.3-py3-none-any.whl

---
+++ packages/jsbCalc/jsbCalc-0.0.3-py3-none-any.whl/jsCalc/app.py
@@ -0,0 +1,17 @@
+
+def pow(a, b):
+  return a**b
+
+def add(a, b):
+  return a + b
+
+def substract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  return a / b
+
+
