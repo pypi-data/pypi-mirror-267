9487209adecd96c857f059fe075c2d00894908d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:07:03 +0000

    Add package swpclib/4.3.2/swpclib-4.3.2.tar.gz

---
+++ packages/swpclib/swpclib-4.3.2.tar.gz/swpclib-4.3.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+test_requirements = [
+    "pytest>=3",
+]
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+if __name__ == "__main__":
+    setup(
+        setup_requires=["setuptools-git-versioning"],
+        test_suite="test",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        tests_require=test_requirements,
+        install_requires=["aiohttp>=3.8.1", "pillow>=10.3.0"],
+        name="swpclib",
+    )
