137ff27436b3db3e4c7fd438a8e7761a6d9051ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:06:01 +0000

    Add package aiosolaredge/0.1.0/aiosolaredge-0.1.0-py3-none-any.whl

---
+++ packages/aiosolaredge/aiosolaredge-0.1.0-py3-none-any.whl/aiosolaredge/solaredge.py
@@ -0,0 +1,117 @@
+from datetime import datetime
+from typing import Any, Iterable, Literal, Optional
+
+import aiohttp
+import yarl
+
+_BASE_URL = yarl.URL("https://monitoringapi.solaredge.com")
+_DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+class SolarEdge:
+    """SolarEdge API client."""
+
+    def __init__(
+        self,
+        api_key: str,
+        session: Optional[aiohttp.ClientSession] = None,
+        timeout: int = 10,
+    ) -> None:
+        """Initialize the SolarEdge API client."""
+        self.api_key = api_key
+        self.session = session or aiohttp.ClientSession()
+        self._created_session = not session
+        self.timeout = timeout
+
+    async def close(self) -> None:
+        """Close the SolarEdge API client."""
+        if self._created_session:
+            await self.session.close()
+
+    def _get_site_url(self, site_id: int) -> yarl.URL:
+        """Get the site URL."""
+        return _BASE_URL.joinpath("site", str(site_id))
+
+    async def get_details(self, site_id: int) -> dict[str, Any]:
+        """
+        Get details of the SolarEdge system.
+
+        :param site_id: The site ID.
+        :return: The details of the SolarEdge system.
+        """
+        return await self._get_json(self._get_site_url(site_id).joinpath("details"))
+
+    async def get_overview(self, site_id: int) -> dict[str, Any]:
+        """
+        Get overview of the SolarEdge system.
+
+        :param site_id: The site ID.
+        :return: The overview of the SolarEdge system.
+        """
+        return await self._get_json(self._get_site_url(site_id).joinpath("overview"))
+
+    async def get_inventory(self, site_id: int) -> dict[str, Any]:
+        """
+        Get inventory of the SolarEdge system.
+
+        :param site_id: The site ID.
+        :return: The inventory of the SolarEdge system.
+        """
+        return await self._get_json(self._get_site_url(site_id).joinpath("inventory"))
+
+    async def get_energy_details(
+        self,
+        site_id: int,
+        start_time: datetime,
+        end_time: datetime,
+        meters: Iterable[
+            Literal[
+                "PRODUCTION", "CONSUMPTION", "SELFCONSUMPTION", "FEEDIN", "PURCHASED"
+            ]
+        ] = [],
+        time_unit: str = "DAY",
+    ) -> dict[str, Any]:
+        """
+        Get energy details of the SolarEdge system.
+
+        :param site_id: The site ID.
+        :param start_time: The start time.
+        :param end_time: The end time.
+        :param meters: The meters.
+               an iterable of PRODUCTION,CONSUMPTION,SELFCONSUMPTION,FEEDIN,PURCHASED
+        :param time_unit: The time unit.
+               one of "QUARTER_OF_AN_HOUR", "HOUR", "DAY", "WEEK", "MONTH", "YEAR"
+        :return: The energy details of the SolarEdge system.
+        """
+        url = self._get_site_url(site_id).joinpath("energyDetails")
+        params = {
+            "startTime": start_time.strftime(_DATETIME_FORMAT),
+            "endTime": end_time.strftime(_DATETIME_FORMAT),
+            "timeUnit": time_unit,
+        }
+        if meters:
+            params["meters"] = ",".join(meters)
+        return await self._get_json(url, params=params)
+
+    async def get_current_power_flow(self, site_id: int) -> dict[str, Any]:
+        """
+        Get current power flow of the SolarEdge system.
+
+        :param site_id: The site ID.
+        :return: The current power flow of the SolarEdge system.
+        """
+        return await self._get_json(
+            self._get_site_url(site_id).joinpath("currentPowerFlow")
+        )
+
+    async def _get_json(
+        self, url: yarl.URL, params: dict[str, Any] | None = None
+    ) -> dict[str, Any]:
+        """Get JSON from the SolarEdge API."""
+        response = await self.session.get(
+            url,
+            params={"api_key": self.api_key, **(params or {})},
+            timeout=self.timeout,
+        )
+        response.raise_for_status()
+        return await response.json()
