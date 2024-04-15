04b8deeaec3f6e666cff5ab3953b5b6662ee8db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 04:20:16 +0000

    Add package transfa/0.2.6/transfa-0.2.6-py2.py3-none-any.whl

---
+++ packages/transfa/transfa-0.2.6-py2.py3-none-any.whl/transfa/__init__.py
@@ -0,0 +1,16 @@
+# Transfa Python bindings
+# API docs at http://docs.transfapp.com
+# Authors:
+# Kolawole Mangabo <kolawole.mangabo@transfapp.com>
+# Atinho Ruben <atinhoruben@transfapp.com>
+
+# Configuration variables
+
+api_key = None
+private_secret = None
+api_base = "https://api.transfapp.com"
+verify_ssl = True
+
+# API Resources
+
+from transfa.api_resources import *
