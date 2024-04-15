77eda537de10b4e1a95ffbe60db7637559cd070f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:12 +0000

    Add package foamlib/0.2.10/foamlib-0.2.10.tar.gz

---
+++ packages/foamlib/foamlib-0.2.10.tar.gz/foamlib-0.2.10/foamlib/__init__.py
@@ -0,0 +1,15 @@
+"""A Python interface for interacting with OpenFOAM."""
+
+__version__ = "0.2.10"
+
+from ._cases import AsyncFoamCase, FoamCase, FoamCaseBase
+from ._files import FoamDictionaryBase, FoamFieldFile, FoamFile
+
+__all__ = [
+    "FoamCase",
+    "AsyncFoamCase",
+    "FoamCaseBase",
+    "FoamFile",
+    "FoamFieldFile",
+    "FoamDictionaryBase",
+]
