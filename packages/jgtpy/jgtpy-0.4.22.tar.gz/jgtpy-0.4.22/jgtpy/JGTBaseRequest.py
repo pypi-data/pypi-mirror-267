2dd1709bc899619234a904609e969f20207012e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:58:09 +0000

    Add package jgtpy/0.4.22/jgtpy-0.4.22.tar.gz

---
+++ packages/jgtpy/jgtpy-0.4.22.tar.gz/jgtpy-0.4.22/jgtpy/JGTBaseRequest.py
@@ -0,0 +1,8 @@
+class JGTBaseRequest:
+    def __init__(self, quiet=True, verbose_level=0):
+        self.quiet = quiet
+        self.verbose_level = verbose_level
+
+    def to_json(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=2)
+    
