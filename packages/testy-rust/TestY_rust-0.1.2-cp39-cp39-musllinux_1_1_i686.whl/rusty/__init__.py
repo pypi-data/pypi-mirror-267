2f9dde4b00b01f47e10b2a11b581e7e2c3e1cbf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 17:04:08 +0000

    Add package testy-rust/0.1.2/TestY_rust-0.1.2-cp39-cp39-musllinux_1_1_i686.whl

---
+++ packages/testy-rust/TestY_rust-0.1.2-cp39-cp39-musllinux_1_1_i686.whl/rusty/__init__.py
@@ -0,0 +1,11 @@
+from .constants import *
+from .types import *
+from .rusty import *
+
+__all__ = (
+    "CASES_SEARCH_SUITE_FIELDS",
+    "CASES_SEARCH_CASE_FIELDS",
+    "CaseSearchQueryParams",
+    "Prefetch",
+    "DataSetObject",
+)
---
+++ packages/testy-rust/TestY_rust-0.1.2-cp39-cp39-musllinux_1_1_i686.whl/rusty/constants.py
@@ -0,0 +1,19 @@
+CASES_SEARCH_SUITE_FIELDS = [
+    'id',
+    'name',
+    'project',
+    'description',
+    'parent',
+    'descendant_count',
+    'cases_count',
+    'total_cases_count',
+    'estimates',
+    'total_estimates',
+    'title',
+]
+
+CASES_SEARCH_CASE_FIELDS = [
+    'id',
+    'name',
+    'suite_id',
+]
