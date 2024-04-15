ff2d9ecebe537e936862a36f3e813296546c35df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:12 +0000

    Add package PokemonLibrary/1.0.2/PokemonLibrary-1.0.2.tar.gz

---
+++ packages/PokemonLibrary/PokemonLibrary-1.0.2.tar.gz/PokemonLibrary-1.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PokemonLibrary',
+    version='1.0.2',
+    author='Martha Morales',
+    author_email='martha_morales@live.com.mx',
+    description='Una biblioteca que contiene la clase RandomPokemon',
+    packages=find_packages(),
+    package_data={'PokemonLibrary': ['pokemon.csv']},
+    install_requires=[
+        'pandas',  # Asegúrate de agregar cualquier dependencia aquí
+    ],
+)
+
