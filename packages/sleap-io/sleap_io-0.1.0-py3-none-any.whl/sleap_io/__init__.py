ed69d8957c2631e3a8a4bc5d34456bcc35681cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:57:58 +0000

    Add package sleap-io/0.1.0/sleap_io-0.1.0-py3-none-any.whl

---
+++ packages/sleap-io/sleap_io-0.1.0-py3-none-any.whl/sleap_io/__init__.py
@@ -0,0 +1,30 @@
+"""This module exposes all high level APIs for sleap-io."""
+
+# Define package version.
+# This is read dynamically by setuptools in pyproject.toml to determine the release version.
+__version__ = "0.1.0"
+
+from sleap_io.model.skeleton import Node, Edge, Skeleton, Symmetry
+from sleap_io.model.video import Video
+from sleap_io.model.instance import (
+    Point,
+    PredictedPoint,
+    Track,
+    Instance,
+    PredictedInstance,
+)
+from sleap_io.model.labeled_frame import LabeledFrame
+from sleap_io.model.labels import Labels
+from sleap_io.io.main import (
+    load_slp,
+    save_slp,
+    load_nwb,
+    save_nwb,
+    load_labelstudio,
+    save_labelstudio,
+    load_jabs,
+    save_jabs,
+    load_video,
+    load_file,
+    save_file,
+)
