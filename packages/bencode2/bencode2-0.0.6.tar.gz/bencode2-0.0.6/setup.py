d85e3e4fe5b969d14725746ee9ee971d3d5d3ade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:57:27 +0000

    Add package bencode2/0.0.6/bencode2-0.0.6.tar.gz

---
+++ packages/bencode2/bencode2-0.0.6.tar.gz/bencode2-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+from Cython.Build import cythonize
+
+
+def get_readme():
+    """Load README.rst for display on PyPI."""
+    with open("readme.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="bencode2",
+    version="0.0.6",
+    description="bencode serialize/deserialize library",
+    long_description=get_readme(),
+    long_description_content_type="text/markdown",
+    author="trim21",
+    author_email="trim21.me@gmail.com",
+    url="https://github.com/trim21/py-bencode",
+    package_dir={"bencode2": "bencode2"},
+    packages=["bencode2"],
+    package_data={"bencode2": ["py.typed"]},
+    include_package_data=True,
+    ext_modules=cythonize(
+        "bencode2/**/*.pyx",
+        compiler_directives={"language_level": "3"},
+    ),
+)
