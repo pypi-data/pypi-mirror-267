01eb004a7d24420466fde0e4f705a839b7953a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:57:35 +0000

    Add package nbnlp/0.1/nbnlp-0.1.tar.gz

---
+++ packages/nbnlp/nbnlp-0.1.tar.gz/nbnlp-0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nbnlp',
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    author="xl",
+    author_email="123456@qq.com",
+    description="A short description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    package_data={
+        'nbnlp': ['data/*', 'model/*'],
+    },
+    install_requires=[
+        'mysql-connector-python',
+        'hanlp',
+        'Flask'
+    ]
+)
