914cc831800953d41539b4cecd84ee65c47cc9db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 04:20:31 +0000

    Add package plotly3d/0.4.0/plotly3d-0.4.0.tar.gz

---
+++ packages/plotly3d/plotly3d-0.4.0.tar.gz/plotly3d-0.4.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="plotly3d",
+    version="0.4.0",
+    packages=find_packages(),
+    install_requires=[
+        "plotly",
+        "scikit-learn",
+        "pandas",
+        "numpy",
+        "matplotlib"
+    ],
+    author="Xingzhi Sun",
+    author_email="xingzhi.sun@yale.edu",
+    description="A helper for ploting 3D scatter plots with plotly",
+    keywords="plotly 3d plotting",
+)
