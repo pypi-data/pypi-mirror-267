f35f9a02b5a99061419ccbec01023d1a911927fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:06:12 +0000

    Add package pyslth/0.0.3/pyslth-0.0.3.tar.gz

---
+++ packages/pyslth/pyslth-0.0.3.tar.gz/pyslth-0.0.3/slth/__init__.py
@@ -0,0 +1,113 @@
+import re
+import os
+import yaml
+import warnings
+from django.apps import apps
+from pathlib import Path
+from django.db import models
+from django.core.exceptions import FieldDoesNotExist
+from django.db.models import manager, Q, CharField, ForeignKey, DecimalField, OneToOneField, ManyToManyField, TextField, CASCADE
+from django.db.models.aggregates import Sum, Avg
+from django.db.models.base import ModelBase
+from .queryset import QuerySet
+from functools import reduce
+from django.utils.translation import gettext_lazy as _
+from django.utils.autoreload import autoreload_started
+from .serializer import Serializer
+
+warnings.filterwarnings('ignore', module='urllib3')
+
+FILENAME = 'application.yml'
+ENDPOINTS = {}
+PROXIED_MODELS = []
+APPLICATON = None
+
+if APPLICATON is None and os.path.exists(FILENAME):
+    with open(FILENAME) as file:
+        content = file.read()
+        for variable in re.findall(r'\$[a-zA-z0-9_]+', content):
+            content = content.replace(variable, os.environ.get(variable[1:], ''))
+    APPLICATON = yaml.safe_load(content).get('application')
+
+class ModelMixin(object):
+
+    @classmethod
+    def get_field(cls, lookup):
+        model = cls
+        attrs = lookup.split('__')
+        while attrs:
+            attr_name = attrs.pop(0)
+            if attrs:  # go deeper
+                field = model._meta.get_field(attr_name)
+                model = field.related_model
+            else:
+                try:
+                    return model._meta.get_field(attr_name)
+                except FieldDoesNotExist:
+                    pass
+        return None
+
+    def getroles(self, username_lookup='username'):
+        roles = getattr(self, '_roles', None)
+        if roles is None:
+            obj = self
+            for attr_name in username_lookup.split('__'):
+                obj = getattr(obj, attr_name)
+            roles = apps.get_model('api.role').objects.filter(username=obj)
+            setattr(self, '_roles', roles)
+        return roles
+
+    def getuser(self, username_lookup):
+        obj = self
+        for attr_name in username_lookup.split('__'):
+            obj = getattr(obj, attr_name)
+        return apps.get_model('auth.user').objects.get(username=obj)
+    
+    def serializer(self) -> Serializer:
+        return Serializer(self)
+
+
+class BaseManager(manager.BaseManager):
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def all(self):
+        return self.get_queryset().all()
+
+    def __call__(self, model):
+        return apps.get_model(model)
+
+
+class Manager(BaseManager.from_queryset(QuerySet)):
+    pass
+
+
+___new___ = ModelBase.__new__
+
+
+def __new__(mcs, name, bases, attrs, **kwargs):
+    if attrs['__module__'] != '__fake__':
+        # See .db.models.Manager
+        if 'objects' in attrs and isinstance(attrs['objects'], QuerySet):
+            queryset_class = attrs['objects']
+            attrs.update(objects=BaseManager.from_queryset(type(queryset_class))())
+        # Defining the objects Manager using .db.models.QuerySet
+        if 'objects' not in attrs and not all(['objects' in dir(cls) for cls in bases]):
+            attrs.update(objects=BaseManager.from_queryset(QuerySet)())
+
+    if ModelMixin not in bases:
+        bases = bases + (ModelMixin, )
+    cls = ___new___(mcs, name, bases, attrs, **kwargs)
+    if cls._meta.proxy_for_model:
+        PROXIED_MODELS.append(cls._meta.proxy_for_model)
+    return cls
+
+
+ModelBase.__new__ = __new__
+models.QuerySet = QuerySet
+models.Manager = Manager
+
+def api_watchdog(sender, **kwargs):
+    sender.extra_files.add(Path('application.yml'))
+
+autoreload_started.connect(api_watchdog)
