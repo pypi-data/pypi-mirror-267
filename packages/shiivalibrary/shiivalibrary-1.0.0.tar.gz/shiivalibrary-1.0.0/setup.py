1fbd0f7620f60e57288b3b442696207922440d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 04:20:18 +0000

    Add package shiivalibrary/1.0.0/shiivalibrary-1.0.0.tar.gz

---
+++ packages/shiivalibrary/shiivalibrary-1.0.0.tar.gz/shiivalibrary-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='shiivalibrary',
+  version='1.0.0',
+  author='shiivastuff',
+  author_email='shiivastuff@gmail.com',
+  description='Python Module for fast AMV Leaks repacks',
+  long_description_content_type='text/markdown',
+  url='https://t.me/shiivastuff',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='Python Leaks Files',
+  project_urls={
+    'Documentation': 'https://shiivastuff.github.io/'
+  },
+  python_requires='>=3.7'
+)
