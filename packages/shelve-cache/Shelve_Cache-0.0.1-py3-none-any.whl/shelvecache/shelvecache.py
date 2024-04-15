dd3272b8a086acbb6016deea574f2cc038ef96e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:57:46 +0000

    Add package shelve-cache/0.0.1/Shelve_Cache-0.0.1-py3-none-any.whl

---
+++ packages/shelve-cache/Shelve_Cache-0.0.1-py3-none-any.whl/shelvecache/shelvecache.py
@@ -0,0 +1,29 @@
+import shelve
+import functools
+
+def persistent_cache(filename):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            with shelve.open(filename) as cache:
+                key = (args, frozenset(kwargs.items()))
+                if key in cache:
+                    print("Cache hit!")
+                    return cache[key]
+                else:
+                    print("Cache miss!")
+                    result = func(*args, **kwargs)
+                    cache[key] = result
+                    return result
+        return wrapper
+    return decorator
+
+# Example usage:
+@persistent_cache("cache.db")
+def expensive_operation(x, y):
+    print("Performing expensive operation...")
+    return x * y
+
+# Test the decorated function
+print(expensive_operation(2, 3))  # Output: Performing expensive operation... Cache miss! 6
+print(expensive_operation(2, 3))  # Output: Cache hit! 6
