8711cd2b0a832349d0b51c72977b6ff9d7ae652e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:58:11 +0000

    Add package pytest-himark/0.1.3/pytest_himark-0.1.3.tar.gz

---
+++ packages/pytest-himark/pytest_himark-0.1.3.tar.gz/pytest_himark-0.1.3/README.rst
@@ -0,0 +1,105 @@
+=============
+pytest-himark
+=============
+
+.. image:: https://img.shields.io/pypi/v/pytest-himark.svg
+    :target: https://pypi.org/project/pytest-himark
+    :alt: PyPI version
+
+.. image:: https://img.shields.io/pypi/pyversions/pytest-himark.svg
+    :target: https://pypi.org/project/pytest-himark
+    :alt: Python versions
+
+.. image:: https://github.com/supermete/pytest-himark/actions/workflows/main.yml/badge.svg
+    :target: https://github.com/supermete/pytest-himark/actions/workflows/main.yml
+    :alt: See Build Status on GitHub Actions
+
+A plugin that will filter pytest's test collection using a json file.
+It will read a json file provided with a --json argument in pytest command line
+(or in pytest.ini), search the 'markers' key and automatically add -m option to
+the command line for filtering out the tests marked with disabled markers.
+
+----
+
+This `pytest`_ plugin was generated with `Cookiecutter`_ along with `@hackebrot`_'s `cookiecutter-pytest-plugin`_ template.
+
+
+Requirements
+------------
+
+* json >= 2.0.9
+
+
+Installation
+------------
+
+You can install "pytest-himark" via `pip`_ from `PyPI`_::
+
+    $ pip install pytest-himark
+
+
+Usage
+-----
+
+After installing this plugin, pytest will automatically load it when launching tests.
+You will simply need to add the --json option to the command line with the path to the
+json containing the markers you want to enable. Alternatively, you can add the --json
+option and the path in the pytest.ini directly, in the addopts variable.
+
+Example:
+    - pytest.ini:
+
+    .. code-block:: INI
+
+        addopts = --json=path/to/my/config.json
+
+    - config.json:
+
+.. code-block:: JSON
+
+        {
+            "markers": {
+                "my_marker1": true,
+                "my_marker2": true,
+                "my_marker3": false,
+                "my_marker4": false
+            }
+        }
+
+..
+
+    Launching pytest now will automatically add he following to the command line:
+
+.. code-block:: python
+
+    >> pytest -m "(my_marker1 or my_marker2) and not (my_marker3 or my_marker4)"
+
+
+Contributing
+------------
+Contributions are very welcome. Tests can be run with `tox`_, please ensure
+the coverage at least stays the same before you submit a pull request.
+
+License
+-------
+
+Distributed under the terms of the `MIT`_ license, "pytest-himark" is free and open source software
+
+
+Issues
+------
+
+If you encounter any problems, please `file an issue`_ along with a detailed description.
+
+.. _`Cookiecutter`: https://github.com/audreyr/cookiecutter
+.. _`@hackebrot`: https://github.com/hackebrot
+.. _`MIT`: https://opensource.org/licenses/MIT
+.. _`BSD-3`: https://opensource.org/licenses/BSD-3-Clause
+.. _`GNU GPL v3.0`: https://www.gnu.org/licenses/gpl-3.0.txt
+.. _`Apache Software License 2.0`: https://www.apache.org/licenses/LICENSE-2.0
+.. _`cookiecutter-pytest-plugin`: https://github.com/pytest-dev/cookiecutter-pytest-plugin
+.. _`file an issue`: https://github.com/supermete/pytest-himark/issues
+.. _`pytest`: https://github.com/pytest-dev/pytest
+.. _`tox`: https://tox.readthedocs.io/en/latest/
+.. _`pip`: https://pypi.org/project/pip/
+.. _`PyPI`: https://pypi.org/project
---
+++ packages/pytest-himark/pytest_himark-0.1.3.tar.gz/pytest_himark-0.1.3/tests/test_himark.py
@@ -0,0 +1,197 @@
+from pathlib import Path
+
+current_directory = Path(__file__).parent
+config_json = current_directory.joinpath('config.json')
+config_disabled_json = current_directory.joinpath('config_disabled.json')
+
+
+def test_json_filter1(pytester):
+    """Check that only tests matching the marker from config.json is executed."""
+
+    # create a temporary pytest test module
+    pytester.makepyfile("""
+        import pytest
+        
+        @pytest.mark.marker1
+        def test_marker1():
+            assert True
+            
+        @pytest.mark.marker2
+        def test_marker2():
+            assert True
+        
+        def test_marker3():
+            assert True
+    """)
+
+    # run pytest with the following cmd args
+    result = pytester.runpytest(
+        f'--json={config_json}',
+        '-vvv'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_marker2 PASSED*',
+    ])
+    result.stdout.no_fnmatch_line(
+        '*::test_marker1 PASSED*',
+    )
+    result.stdout.no_fnmatch_line(
+        '*::test_marker3 PASSED*'
+    )
+
+    # make sure that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_json_filter2(pytester):
+    """Check that only tests matching the marker from config.json is executed."""
+
+    # create a temporary pytest test module
+    pytester.makepyfile("""
+        import pytest
+        
+        @pytest.mark.marker2
+        def test_marker1():
+            assert True
+            
+        @pytest.mark.marker1
+        def test_marker2():
+            assert True
+            
+        def test_marker3():
+            assert True
+    """)
+
+    # run pytest with the following cmd args
+    result = pytester.runpytest(
+        f'--json={config_json}',
+        '-vvv'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_marker1 PASSED*',
+    ])
+    result.stdout.no_fnmatch_line(
+        '*::test_marker2 PASSED*'
+    )
+    result.stdout.no_fnmatch_line(
+        '*::test_marker3 PASSED*'
+    )
+
+    # make sure that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_json_filte3(pytester):
+    """Check that tests that are marked with at least one disabled marker is not executed."""
+
+    # create a temporary pytest test module
+    pytester.makepyfile("""
+        import pytest
+        
+        @pytest.mark.marker1
+        @pytest.mark.marker2
+        def test_marker1():
+            assert True
+            
+        @pytest.mark.marker2
+        def test_marker2():
+            assert True
+            
+        def test_marker3():
+            assert True
+    """)
+
+    # run pytest with the following cmd args
+    result = pytester.runpytest(
+        f'--json={config_json}',
+        '-vvv'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_marker2 PASSED*',
+    ])
+    result.stdout.no_fnmatch_line(
+        '*::test_marker1 PASSED*'
+    )
+    result.stdout.no_fnmatch_line(
+        '*::test_marker3 PASSED*'
+    )
+
+    # make sure that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_json_filter_none(pytester):
+    """Check that all tests are executed when no json is used."""
+
+    # create a temporary pytest test module
+    pytester.makepyfile("""
+        import pytest
+        
+        @pytest.mark.marker2
+        def test_marker1():
+            assert True
+            
+        @pytest.mark.marker1
+        def test_marker2():
+            assert True
+        
+        def test_marker3():
+            assert True
+    """)
+
+    # run pytest with the following cmd args
+    result = pytester.runpytest(
+        '-vvv'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_marker1 PASSED*',
+        '*::test_marker2 PASSED*',
+        '*::test_marker3 PASSED*',
+    ])
+
+    # make sure that we get a '0' exit code for the testsuite
+    assert result.ret == 0
+
+
+def test_json_filter_disabled(pytester):
+    """Check that all tests are executed when all markers are disabled."""
+
+    # create a temporary pytest test module
+    pytester.makepyfile("""
+        import pytest
+        
+        @pytest.mark.marker2
+        def test_marker1():
+            assert True
+            
+        @pytest.mark.marker1
+        def test_marker2():
+            assert True
+        
+        def test_marker3():
+            assert True
+    """)
+
+    # run pytest with the following cmd args
+    result = pytester.runpytest(
+        f'--json={config_disabled_json}',
+        '-vvv'
+    )
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*::test_marker1 PASSED*',
+        '*::test_marker2 PASSED*',
+        '*::test_marker3 PASSED*',
+    ])
+
+    # make sure that we get a '0' exit code for the testsuite
+    assert result.ret == 0
