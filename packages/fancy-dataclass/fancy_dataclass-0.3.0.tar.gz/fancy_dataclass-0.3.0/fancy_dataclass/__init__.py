9d131237da26dc6093e49420afa54f97e4d10221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:23 +0000

    Add package fancy-dataclass/0.3.0/fancy_dataclass-0.3.0.tar.gz

---
+++ packages/fancy-dataclass/fancy_dataclass-0.3.0.tar.gz/fancy_dataclass-0.3.0/fancy_dataclass/__init__.py
@@ -0,0 +1,12 @@
+# top-level class exports
+from .cli import ArgparseDataclass, CLIDataclass
+from .config import ConfigDataclass
+from .dict import DictDataclass
+from .json import JSONBaseDataclass, JSONDataclass, JSONSerializable
+from .mixin import DataclassMixin
+from .sql import SQLDataclass
+from .subprocess import SubprocessDataclass
+from .toml import TOMLDataclass
+
+
+__version__ = '0.3.0'
