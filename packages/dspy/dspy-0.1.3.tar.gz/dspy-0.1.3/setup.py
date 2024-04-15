e4c5d4e6c97738fcdcb31f4bb0961d4fa3f30c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:07:10 +0000

    Add package dspy/0.1.3/dspy-0.1.3.tar.gz

---
+++ packages/dspy/dspy-0.1.3.tar.gz/dspy-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['dspy']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['ka = kiera.main:main', 'kiera = kiera.main:main']}
+
+setup_kwargs = {
+    'name': 'dspy',
+    'version': '0.1.3',
+    'description': 'Placeholder package for DSPy',
+    'long_description': '',
+    'author': 'Tom Dörr',
+    'author_email': 'tomdoerr96@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.3,<4.0',
+}
+
+
+setup(**setup_kwargs)
