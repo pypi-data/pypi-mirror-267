d69bef0bc9e70f1d902e878fbd23a7fa858c67ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:06:15 +0000

    Add package libreppc/0.1.4/libreppc-0.1.4-py3-none-any.whl

---
+++ packages/libreppc/libreppc-0.1.4-py3-none-any.whl/libreppc/__main__.py
@@ -0,0 +1,27 @@
+from flask import Flask, send_file
+from .libreppc import LibrePPC 
+
+app = Flask(__name__, template_folder='templates')
+libreppc = LibrePPC(app)
+
+@app.route('/')
+def main_page() -> str:
+    return libreppc.render_main()
+
+@app.route('/post/<string:postId>')
+def post_page(postId: str) -> str:
+    return libreppc.render_post(postId)
+
+@app.route('/feed.atom')
+def feed():
+    libreppc.generate_rss().atom_file('feed.atom')
+    return send_file('feed.atom', download_name='feed.atom', mimetype='application/atom+xml')
+
+if __name__ == '__main__':
+    args = libreppc.parse_args()
+    if args.build:
+        libreppc.build()
+    elif args.serve:
+        app.run(debug=True, host="0.0.0.0", use_reloader=True)
+
+
