5615babe3b62eeae0d6bf61d2b789183ae99d7f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 03:58:05 +0000

    Add package YahooProject/1.1/yahooproject-1.1.tar.gz

---
+++ packages/YahooProject/yahooproject-1.1.tar.gz/yahooproject-1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YahooProject',
+    version='1.1',
+    packages=['YahooProject'],
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    author='Nick37',
+    description='A package for scraping search results from Yahoo Search.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
