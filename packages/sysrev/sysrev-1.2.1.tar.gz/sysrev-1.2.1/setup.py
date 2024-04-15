49d306b2da521aa30227840ab089aaeaa1192d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 14:06:46 +0000

    Add package sysrev/1.2.1/sysrev-1.2.1.tar.gz

---
+++ packages/sysrev/sysrev-1.2.1.tar.gz/sysrev-1.2.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='sysrev',
+      version='1.2.1',
+      description='Gets annotations from Sysrev API',
+      url='https://github.com/sysrev/PySysrev',
+      author='Thomas Luechtefeld',
+      author_email='tom@insilica.co',
+      packages=['sysrev'],
+      install_requires=[
+          'pandas',
+          'requests',
+          'pathlib'
+      ],
+      python_requires='>=3.6',
+      zip_safe=False)
