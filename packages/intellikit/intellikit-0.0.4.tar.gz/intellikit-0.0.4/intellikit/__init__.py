c5e408b8df262ff359c19be871c1c10eb537fba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:20 +0000

    Add package intellikit/0.0.4/intellikit-0.0.4.tar.gz

---
+++ packages/intellikit/intellikit-0.0.4.tar.gz/intellikit-0.0.4/intellikit/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for intellikit."""
+
+__author__ = """Arthur Kakande"""
+__email__ = "kaksarthur@gmail.com"
+__version__ = "0.0.4"
+
+from .intellikit import *
+from .sim import *
+from .ir import *
