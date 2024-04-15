96315f82c995a47115b3cdffc8c64d93d8c62a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 15:05:15 +0000

    Add package pyvk/0.2.3/pyvk-0.2.3-py3-none-any.whl

---
+++ packages/pyvk/pyvk-0.2.3-py3-none-any.whl/pyvk/helpers/reqn/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+    pyvk.helpers.reqn
+    ~~~~
+
+    Package file.
+
+    :copyright: (c) 2013-2020 by Max Kuznetsov.
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+from .reqn import reqn
