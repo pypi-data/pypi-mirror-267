89498e6acca56d6dc514d37b296ba17e08ff9d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-15 04:20:11 +0000

    Add package ccnuacm-datamocker/0.0.4/ccnuacm_datamocker-0.0.4.tar.gz

---
+++ packages/ccnuacm-datamocker/ccnuacm_datamocker-0.0.4.tar.gz/ccnuacm_datamocker-0.0.4/ccnuacm_datamocker/__init__.py
@@ -0,0 +1,19 @@
+__all__ = ["set_seed", "set_work_dir"]
+__author__ = "JixiangXiong"
+
+
+def set_seed(seed_):
+    from .common import context
+    context().seed = seed_
+    context().logger.info(f"Setting random seed to `{seed_}`")
+
+
+def set_work_dir(work_dir):
+    from .common import context
+    context().work_dir = work_dir
+
+
+def set_compiler(compiler_path):
+    from .common import context
+    context().CXX = compiler_path
+    context().logger.info(f"Setting compiler path to `{compiler_path}`")
