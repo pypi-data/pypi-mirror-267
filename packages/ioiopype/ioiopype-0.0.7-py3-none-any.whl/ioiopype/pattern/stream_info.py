ac5e68e977c759f806de744e638c297cc83e752a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:05:16 +0000

    Add package ioiopype/0.0.7/ioiopype-0.0.7-py3-none-any.whl

---
+++ packages/ioiopype/ioiopype-0.0.7-py3-none-any.whl/ioiopype/pattern/stream_info.py
@@ -0,0 +1,28 @@
+from enum import Enum
+import json
+
+class StreamInfo:
+    class Datatype(Enum):
+        Sample = 1,
+        Frame = 1,
+        Variable = 1,
+
+    def __init__(self, id : int, name :str, type : Datatype):
+        self.Id : int = id
+        self.Name : str = name
+        self.Type = type
+    
+    def __dict__(self):
+        return {
+            "id": self.Id,
+            "name": self.Name,
+            "type": self.Type.name,
+        }
+    
+    def __str__(self):
+        return json.dumps(self.__dict__(), indent=4)
+    
+    @classmethod
+    def initialize(cls, data):
+        ds = json.loads(data)
+        return cls(**ds)
