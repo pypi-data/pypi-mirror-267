15fcfda8f1dd8538661b854cca06fe84047c8161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:05:47 +0000

    Add package reactionxd/0.0.1/reactionxd-0.0.1.tar.gz

---
+++ packages/reactionxd/reactionxd-0.0.1.tar.gz/reactionxd-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    author='User',
+    author_email='example@mail.com',
+    name='reactionxd',
+    version='0.0.1',
+    description='A simple package for https://app.tea.xyz/. Example tea-xyz1 - https://github.com/madest92/tea-xyz1 and tea-xyz2 - https://github.com/madest92/tea-xyz2',
+    url='https://github.com/ReaLys158/reactionxd',
+    project_urls={
+        'Homepage': 'https://github.com/ReaLys158/reactionxd',
+        'Source': 'https://github.com/ReaLys158/reactionxd',
+    },
+    py_modules=['hi_tea'],
+    entry_points={
+        'console_scripts': [
+            'hi-tea=hi_tea:hello_tea_xyz'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'tea-xyz1',
+        'tea-xyz2',
+        # add your projects
+    ],
+)
