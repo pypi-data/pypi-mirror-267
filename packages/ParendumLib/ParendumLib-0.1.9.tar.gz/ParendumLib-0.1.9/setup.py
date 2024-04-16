779818761ee72076ab479263c6e4f06d27a76baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:06:11 +0000

    Add package ParendumLib/0.1.9/ParendumLib-0.1.9.tar.gz

---
+++ packages/ParendumLib/ParendumLib-0.1.9.tar.gz/ParendumLib-0.1.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='ParendumLib',
+    version='0.1.9',
+    packages=find_packages(),
+    install_requires=requirements,
+    author='Parendum',
+    author_email='info@parendum.com',
+    description='Parendum Official Library',
+    keywords='logger',
+)
