86f4d0c7166b7c454d44979305d909008ab537f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-04-16 03:06:02 +0000

    Add package libsegmenter/0.7/libsegmenter-0.7-cp310-cp310-win_amd64.whl

---
+++ packages/libsegmenter/libsegmenter-0.7-cp310-cp310-win_amd64.whl/libsegmenter/Segmenter.py
@@ -0,0 +1,18 @@
+import numpy as np
+from .default_window_selector import default_window_selector
+
+backends = ["torch", "tensorflow", "base"]
+
+def make_segmenter(backend: str = "base", *args, **kwargs):
+    if backend not in backends:
+        raise ValueError(f"Unsupported backend {backend}, availible: {backends}")
+
+    if backend == "torch":
+        from .SegmenterTorch import SegmenterTorch
+
+        return SegmenterTorch(*args, **kwargs)
+
+    if backend == "tensorflow":
+        from .SegmenterTensorFlow import SegmenterTensorFlow
+
+        return SegmenterTensorFlow(*args, **kwargs)
